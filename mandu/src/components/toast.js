824327d563e1d61c2675100660ecd70947345ade
charmeee <mandugohigh@gmail.com>
2023-06-28 00:03:52 +0900

    feat: toast에 progressbar 추가

--- mandu/src/components/toast.js
+++ mandu/src/components/toast.js
@@ -2,7 +2,7 @@ import {useEffect} from "react";
 
 //delay , autohide, animation, position, icon, title, message, close button,
 
-function Toast({isOpen, setOpen, message, delay}) {
+function Toast({isOpen, setOpen, message, delay, hasProgressBar = true}) {
     useEffect(
         () => {
             if (isOpen) {
@@ -17,21 +17,35 @@ function Toast({isOpen, setOpen, message, delay}) {
 
     return (
         <div style={{
-            display: isOpen ? 'flex' : 'none',
+            display: isOpen ? 'block' : 'none',
             zIndex: 9999,
             position: 'fixed',
             bottom: '20px',
             right: '20px',
             border: '1px solid black',
             borderRadius: '4px',
-            padding: '0px 12px',
             minWidth: '200px',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
         }}>
-            <h4>{message}</h4>
-            <button onClick={() => setOpen(false)}>X</button>
+            <div style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                flexDirection: 'row',
+                margin: '0px 12px',
+            }}>
+                <h4>{message}</h4>
+                <button onClick={() => setOpen(false)}>✕</button>
+            </div>
+            {
+                hasProgressBar && (<div className="progress-bar" style={{
+                    backgroundColor: 'lightgreen',
+                    width: '100%',
+                    height: '8px',
+                    animation: `progress ${delay ?? 3000}ms linear`,
+                }}/>)
+            }
+
+
         </div>
     );
 
