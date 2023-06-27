8637d38760892c58b07053f70bed2ab396e00c67
lcqff <yeonnu82g@gmail.com>
2023-06-27 15:28:16 +0900

    feat: Toggle ui

---
+++ week1/src/components/Toggle/Toggle.js
@@ -0,0 +1,13 @@
+import "../../styles/Toggle.scss";
+
+const Toggle = () => {
+  return (
+    <>
+      <div class="toggleBox">
+        <div class="toggleButton"></div>
+      </div>
+    </>
+  );
+};
+
+export default Toggle;
--- week1/src/pages/ComponentTest.js
+++ week1/src/pages/ComponentTest.js
@@ -1,8 +1,10 @@
 import Radio from "../components/RadioButton/Radio";
+import Toggle from "../components/Toggle/Toggle";
 
 const ComponentTest = () => {
   return (
     <>
+      <Toggle />
       <Radio />
       <div>컴포넌트 테스트 페이지</div>
     </>
