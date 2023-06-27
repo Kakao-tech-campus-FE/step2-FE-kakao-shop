e1e692c561757e9da8c45b153b570f51e51a8672
lcqff <yeonnu82g@gmail.com>
2023-06-27 16:29:35 +0900

    feat: Apply checkbox css

--- week1/src/components/CheckList/CheckList.js
+++ week1/src/components/CheckList/CheckList.js
@@ -1,16 +1,17 @@
+import "../../styles/Checkbox.scss";
+
 const CheckList = () => {
   return (
-    <div>
+    <div className="CheckboxPaper">
+      <div className="title">To Do List</div>
       <div>
         <input type="checkbox" id="create" />
         <label htmlFor="create">체크박스 만들기</label>
       </div>
-
       <div>
         <input type="checkbox" id="css" />
         <label htmlFor="css">css 적용하기</label>
       </div>
-
       <div>
         <input type="checkbox" id="manage" />
         <label htmlFor="manage">상태관리하기</label>
