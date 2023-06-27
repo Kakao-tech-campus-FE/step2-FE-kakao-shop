71e5518919689f443f19521ce0029e414959cb84
lcqff <yeonnu82g@gmail.com>
2023-06-27 16:22:41 +0900

    feat: Create checkboxes

--- week1/src/components/CheckList/CheckList.js
+++ week1/src/components/CheckList/CheckList.js
@@ -1,8 +1,21 @@
 const CheckList = () => {
   return (
-    <>
-      <input type="checkbox"></input>
-    </>
+    <div>
+      <div>
+        <input type="checkbox" id="create" />
+        <label htmlFor="create">체크박스 만들기</label>
+      </div>
+
+      <div>
+        <input type="checkbox" id="css" />
+        <label htmlFor="css">css 적용하기</label>
+      </div>
+
+      <div>
+        <input type="checkbox" id="manage" />
+        <label htmlFor="manage">상태관리하기</label>
+      </div>
+    </div>
   );
 };
 
