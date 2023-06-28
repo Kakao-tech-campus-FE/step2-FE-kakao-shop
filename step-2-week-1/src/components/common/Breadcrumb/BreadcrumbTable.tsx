7e3dee82b9f29a02b555aa49ac1d1daefdfaf7fd
loopy-lim <loopy@jnu.ac.kr>
2023-06-28 17:29:05 +0900

    feat: add breadcrumbtable component
    - Breadcrumbtable must have crumbs
    - last crumb don't have /

--- step-2-week-1/src/components/common/Breadcrumb/BreadcrumbTable.tsx
+++ step-2-week-1/src/components/common/Breadcrumb/BreadcrumbTable.tsx
@@ -1,5 +1,21 @@
-const BreadCrumbTable = () => {
-  return <div>BreadCrumbTable</div>;
+import { FC } from "react";
+import Breadcrumb, { BreadcrumbProps } from "./Breadcrumb";
+
+interface BreadcrumbTableProps {
+  crumbs: BreadcrumbProps[];
+}
+
+const BreadcrumbTable: FC<BreadcrumbTableProps> = ({ crumbs }) => {
+  return (
+    <div>
+      {crumbs.map((crumb, index) => (
+        <>
+          <Breadcrumb key={index} title={crumb.title} link={crumb.link} />
+          {index < crumbs.length - 1 && <span> / </span>}
+        </>
+      ))}
+    </div>
+  );
 };
 
-export default BreadCrumbTable;
+export default BreadcrumbTable;
