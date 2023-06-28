212dbbb0023a7e348d094ad0238228f1fe8e510c
H-sooyeon <wndqhr__@naver.com>
2023-06-28 16:29:01 +0900

    feat: ui 컴포넌트 한 페이지로 합치기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Toast from './components/Toast';
+import Slider from './components/Slider';
+import Radio from './components/Radio';
 
 function App() {
+  const [toast, setToast] = useState(false);
+  const [carousel, setCarousel] = useState(false);
+  const [radio, setRadio] = useState(false);
+
+  const handleToastClick = () => {
+    setToast(true);
+  };
+
+  const handleCarouselClick = () => {
+    setCarousel(!carousel);
+    setRadio(false);
+  }
+
+  const handleRadioClick = () => {
+    setRadio(!radio);
+    setCarousel(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        <li onClick={handleToastClick}>
+          toast
+          {toast && (
+            <Toast
+              setToast={setToast}
+              text="카카오 테크 캠퍼스 과제1 UI 컴포넌트 Toast"
+            />
+          )}
+        </li>
+        <li>breadCrumbs</li>
+        <li onClick={handleCarouselClick}>carousel</li>
+        <li onClick={handleRadioClick}>radioButton</li>
+        <li>toggleButton</li>
+        <li>checkList</li>
+      </ul>
+      {carousel && (<Slider />)}
+      {radio && (<Radio />)}
     </div>
   );
 }
