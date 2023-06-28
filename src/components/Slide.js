581feb19fcea6684fd5931b998687d20d8ecb02b
H-sooyeon <wndqhr__@naver.com>
2023-06-28 16:30:31 +0900

    feat: radio, carousel, toast 컴포넌트 추가

---
+++ src/components/Slide.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Slide({ img }) {
+  return <IMG src={img} />;
+}
+
+const IMG = styled.img`
+  width: 300px;
+  height: 300px;
+`;
