c4e46f2e99ccd977a293563774562bd43d64d431
kjh302903 <kjh302903@gmail.com>
2023-06-28 20:54:11 +0900

    feat: Icon을 컴포넌트로 내보냄
    Icon을 컴포넌트로 내보내도록 함

---
+++ src/components/Icons/index.ts
@@ -0,0 +1,5 @@
+import { ReactComponent as SuccessIcon } from "@assets/icons/ic-success.svg";
+import { ReactComponent as ErrorIcon } from "@assets/icons/ic-error.svg";
+import { ReactComponent as WarningIcon } from "@assets/icons/ic-warning.svg";
+
+export { SuccessIcon, ErrorIcon, WarningIcon };
