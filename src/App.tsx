b033aa55e61b4974daa68be805179314e980d915
kjh302903 <kjh302903@gmail.com>
2023-06-28 20:57:44 +0900

    feat: Toast 추가
    Toast 추가. 버튼 눌렀을때, 생성되고, 2초뒤에 사라짐

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Toast from "./components/Toast";
 
+type Type = "success" | "error" | "warning";
+
+type ToastData = {
+  id: number;
+  type: Type;
+  message: string;
+};
+
 function App() {
-  const [toast, setToast] = useState(false);
+  const [toastList, setToastList] = useState<ToastData[]>([]);
+
+  const onClick = (type: Type, message: string) => {
+    setToastList((prev) =>
+      prev.concat({
+        id: Date.now(),
+        type,
+        message,
+      })
+    );
+  };
 
   return (
     <div className="App">
-      {toast && <Toast setToast={setToast} />}
-      <button
-        onClick={() => {
-          setToast(true);
-        }}
-      >
-        토스트
+      <Toast toastList={toastList} setToastList={setToastList} />
+      <button onClick={() => onClick("success", "This is a success toast.")}>
+        Success 토스트
+      </button>
+      <button onClick={() => onClick("error", "This is an error toast.")}>
+        Error 토스트
+      </button>
+      <button onClick={() => onClick("warning", "This is a warning toast.")}>
+        Warning 토스트
       </button>
     </div>
   );
--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,5 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
+import ToastItem from "./ToastItem";
+import { ErrorIcon, SuccessIcon, WarningIcon } from "@components/Icons";
 
 const Wrapper = styled.div`
   position: fixed;
@@ -7,12 +9,56 @@ const Wrapper = styled.div`
   right: 20px;
   display: flex;
   flex-direction: column;
-  z-index: 10001;
+  z-index: 10;
   gap: 12px;
 `;
 
-const Toast = ({ setToast }: any) => {
-  return <Wrapper>Toast</Wrapper>;
+type ToastData = {
+  id: number;
+  type: "success" | "error" | "warning";
+  message: string;
+};
+
+type Props = {
+  toastList: ToastData[];
+  setToastList: React.Dispatch<React.SetStateAction<ToastData[]>>;
+};
+
+const Toast = ({ toastList, setToastList }: Props) => {
+  return (
+    <Wrapper>
+      {toastList.map((item: ToastData, index: number) => {
+        let backgroundColor: string;
+        let icon;
+
+        switch (item.type) {
+          case "success":
+            backgroundColor = "#28a745";
+            icon = <SuccessIcon />;
+            break;
+          case "error":
+            backgroundColor = "#dc3545";
+            icon = <ErrorIcon />;
+            break;
+          case "warning":
+            backgroundColor = "#f0ad4e";
+            icon = <WarningIcon />;
+            break;
+          default:
+            backgroundColor = "";
+        }
+        return (
+          <ToastItem
+            key={item.id}
+            setToastList={setToastList}
+            toastData={item}
+            backgroundColor={backgroundColor}
+            icon={icon}
+          />
+        );
+      })}
+    </Wrapper>
+  );
 };
 
 export default Toast;
