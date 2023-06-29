c12f8fa473dea20b09ad178ab91bfcc576ff6ea8
kjh302903 <kjh302903@gmail.com>
2023-06-29 19:06:25 +0900

    feat: Radio 컴포넌트 추가
    라디오 컴포넌트 추가 색 변경 가능

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Toast from "./components/Toast";
 import Carousel from "@components/Carousel";
 import CheckList from "@components/CheckList";
 import Toggle from "@components/Toggle";
+import Radio from "@components/Radio";
 
 type ToastData = {
   id: number;
@@ -40,6 +41,18 @@ function App() {
           offColor: "rgb(233, 233, 234)",
           onColor: "rgb(0, 200, 102)",
         }}
+      />
+      <Radio
+        data={{ id: "1", value: "딸기", name: "fruits", text: "딸기" }}
+        color={"#dc3545"}
+      />
+      <Radio
+        data={{ id: "2", value: "포도", name: "fruits", text: "포도" }}
+        color={"#dc3545"}
+      />
+      <Radio
+        data={{ id: "3", value: "바나나", name: "fruits", text: "바나나" }}
+        color={"#dc3545"}
       />
       <CheckList />
       <Carousel />
