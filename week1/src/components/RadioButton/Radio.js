92e33056e15388abdfc389d8c743af8f661a4395
lcqff <yeonnu82g@gmail.com>
2023-06-29 23:45:05 +0900

    feat: 라디오 버튼 상태관리

--- week1/src/components/RadioButton/Radio.js
+++ week1/src/components/RadioButton/Radio.js
@@ -1,10 +1,51 @@
+import React, { useEffect, useState, useRef } from "react";
+import oneGrayCat from "../../assets/images/a_gray_cat.png";
+import twoGrayCats from "../../assets/images/two_gray_cats.jpg";
+import manyGrayCats from "../../assets/images/cute_gray_cats.jpg";
+
 const Radio = () => {
+  const [whichGrayCat, setWhichGrayCat] = useState("oneGrayCat");
+  const grayCat = {
+    oneGrayCat: oneGrayCat,
+    twoGrayCats: twoGrayCats,
+    manyGrayCats: manyGrayCats,
+  };
+  const defaultSelection = useRef();
+  const onChange = (e) => {
+    setWhichGrayCat(e.target.value);
+  };
+  useEffect(() => {
+    defaultSelection.current.checked = "checked";
+  }, []);
+
   return (
     <div>
-      <input type="radio" id="R1" name="radios" value="Radio1" />
-      <label htmlFor="R1">Radio1</label>
-      <input type="radio" id="R2" name="radios" value="Radio2" />
-      <label htmlFor="R2">Radio2</label>
+      <input
+        type="radio"
+        id="R1"
+        name="radios"
+        value="oneGrayCat"
+        ref={defaultSelection}
+        onChange={(event) => onChange(event)}
+      />
+      <label htmlFor="R1">one Gray Cat</label>
+      <input
+        type="radio"
+        id="R2"
+        name="radios"
+        value="twoGrayCats"
+        onChange={(event) => onChange(event)}
+      />
+      <label htmlFor="R2">two Gray Cats</label>
+      <input
+        type="radio"
+        id="R3"
+        name="radios"
+        value="manyGrayCats"
+        onChange={(event) => onChange(event)}
+      />
+      <label htmlFor="R3">many Gray Cats</label>
+      <img src={grayCat[whichGrayCat]} alt="grayCatPic" />
     </div>
   );
 };
