c43355f45cf76234fa368708e8ed4bd9cf02d287
catnofat <catnofat@gmail.com>
2023-06-29 22:14:52 +0900

    [feat] react checklist code
    simple checklist

---
+++ Week1/react-checklist/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Checklist } from "./components/checklist";
+
+function App() {
+  return (
+    <div className="Checklists">
+      <Checklist />
+    </div>
+  );
+}
+
+export default App;
