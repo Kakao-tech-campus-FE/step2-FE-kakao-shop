b4c371935c7777bcc7a2e038b36d1e6e2a1bf105
lcqff <yeonnu82g@gmail.com>
2023-06-30 21:04:36 +0900

    feat: Radio button 스타일 수정

--- week1/src/components/RadioButton/Radio.js
+++ week1/src/components/RadioButton/Radio.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
+import "../../styles/Radio.scss";
 
 const Radio = ({ radios, imgURL }) => {
   const defaultSelection = useRef([]);
@@ -14,23 +15,25 @@ const Radio = ({ radios, imgURL }) => {
   }, []);
 
   return (
-    <div>
-      {radios.map((key, index) => (
-        <div key={key}>
-          <input
-            type="radio"
-            id={key}
-            name="radios"
-            value={index}
-            ref={(el) => {
-              defaultSelection.current.push(el);
-            }}
-            onChange={(event) => OnChange(event)}
-          />
-          <label htmlFor={key}>{key}</label>
-        </div>
-      ))}
-      <img src={imgURL[radioIdx]} alt="grayCatPic" />
+    <div className="radio-wrapper">
+      <div className="radio">
+        {radios.map((key, index) => (
+          <div key={key}>
+            <input
+              type="radio"
+              id={key}
+              name="radios"
+              value={index}
+              ref={(el) => {
+                defaultSelection.current.push(el);
+              }}
+              onChange={(event) => OnChange(event)}
+            />
+            <label htmlFor={key}>{key}</label>
+          </div>
+        ))}
+      </div>
+      <img className="radio-img" src={imgURL[radioIdx]} alt="grayCatPic" />
     </div>
   );
 };
