548e0f2fa57f7c4dab2d8dd3ccb124129f702669
SeonghyeonKim <kkgojn@naver.com>
2023-06-30 23:23:46 +0900

    Test

--- kakao-store/src/components/RadioButton.jsx
+++ kakao-store/src/components/RadioButton.jsx
@@ -1,8 +1,23 @@
-const RadioButton = () => {
-    return (
-        <>
-        </>
-    );
+import { useContext } from "react";
+import { RadioContent } from "../App";
+import "../styles/radiobutton.css"
+
+const RadioButton = ({ children, value, name, defaultChecked }) => {
+  const radioList = useContext(RadioContent);
+
+  return (
+    <label>
+      <input
+        type="radio"
+        value={value}
+        name={name}
+        defaultChecked={defaultChecked}
+        checked={radioList.value !== undefined ? value === radioList.value : undefined}
+        onChange={(e) => radioList.onChange && radioList.onChange(e.target.value)}
+      />
+      {children}
+    </label>
+  );
 }
 
 export default RadioButton;
--- kakao-store/src/components/ToggleButton.jsx
+++ kakao-store/src/components/ToggleButton.jsx
@@ -1,7 +1,21 @@
-const ToggleButton = () => {
+import { useState } from "react";
+import "../styles/togglebutton.css"
+import classNames from "classnames";
+
+const ToggleButton = ({getToggle}) => {
+    const [toggle, setToggle] = useState(false);
+    
+    const handleClickToggle = () => {
+        setToggle(!toggle);
+        getToggle(toggle);
+    }
+
     return (
-        <>
-        </>
+        <div className="toggle-container">
+            <div className={classNames("toggle-track", {checked: toggle})} onClick={handleClickToggle}>
+                <div className={classNames("toggle-ball", {checked: toggle})}></div>
+            </div>
+        </div>
     );
 }
 
