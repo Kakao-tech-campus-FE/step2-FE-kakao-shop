4207ded934d93992c9db824f1d7628f94bc3dd1a
lcqff <yeonnu82g@gmail.com>
2023-06-30 11:20:52 +0900

    feat: Componentize radio button

--- week1/src/components/RadioButton/Radio.js
+++ week1/src/components/RadioButton/Radio.js
@@ -1,51 +1,33 @@
-import React, { useEffect, useState, useRef } from "react";
-import oneGrayCat from "../../assets/images/a_gray_cat.png";
-import twoGrayCats from "../../assets/images/two_gray_cats.jpg";
-import manyGrayCats from "../../assets/images/cute_gray_cats.jpg";
+import React, { useEffect, useRef } from "react";
 
-const Radio = () => {
-  const [whichGrayCat, setWhichGrayCat] = useState("oneGrayCat");
-  const grayCat = {
-    oneGrayCat: oneGrayCat,
-    twoGrayCats: twoGrayCats,
-    manyGrayCats: manyGrayCats,
+const Radio = ({ radios, setRadioIdx }) => {
+  const defaultSelection = useRef([]);
+  const OnChange = (e) => {
+    setRadioIdx(e.target.value);
   };
-  const defaultSelection = useRef();
-  const onChange = (e) => {
-    setWhichGrayCat(e.target.value);
-  };
+
   useEffect(() => {
-    defaultSelection.current.checked = "checked";
+    setRadioIdx(0);
+    defaultSelection.current[0].checked = "checked";
   }, []);
 
   return (
     <div>
-      <input
-        type="radio"
-        id="R1"
-        name="radios"
-        value="oneGrayCat"
-        ref={defaultSelection}
-        onChange={(event) => onChange(event)}
-      />
-      <label htmlFor="R1">one Gray Cat</label>
-      <input
-        type="radio"
-        id="R2"
-        name="radios"
-        value="twoGrayCats"
-        onChange={(event) => onChange(event)}
-      />
-      <label htmlFor="R2">two Gray Cats</label>
-      <input
-        type="radio"
-        id="R3"
-        name="radios"
-        value="manyGrayCats"
-        onChange={(event) => onChange(event)}
-      />
-      <label htmlFor="R3">many Gray Cats</label>
-      <img src={grayCat[whichGrayCat]} alt="grayCatPic" />
+      {radios.map((key, index) => (
+        <div key={key}>
+          <input
+            type="radio"
+            id={key}
+            name="radios"
+            value={index}
+            ref={(el) => {
+              defaultSelection.current.push(el);
+            }}
+            onChange={(event) => OnChange(event)}
+          />
+          <label htmlFor={key}>{key}</label>
+        </div>
+      ))}
     </div>
   );
 };
--- week1/src/pages/ComponentTest.js
+++ week1/src/pages/ComponentTest.js
@@ -3,17 +3,28 @@ import Toggle from "../components/Toggle/Toggle";
 import CheckList from "../components/CheckList/CheckList";
 import React, { useState } from "react";
 import ProcessBar from "../components/ProcessBar/processBar";
+import Toast from "../components/Toast/Toast";
+import oneGrayCat from "../assets/images/a_gray_cat.png";
+import twoGrayCats from "../assets/images/two_gray_cats.jpg";
+import manyGrayCats from "../assets/images/cute_gray_cats.jpg";
 
 class CheckListProps {
   static title = "To Do List";
   static item = ["체크리스트 만들기", "css 적용하기", "상태관리 적용하기"];
 }
 
+class RadioProps {
+  static item = ["one gray cat", "two gray cat", "many gray cat"];
+  static URL = [oneGrayCat, twoGrayCats, manyGrayCats];
+}
+
 const ComponentTest = () => {
   const [checkedItemNum, setCheckedItemNum] = useState(0);
+  const [radioIdx, setRadioIdx] = useState(0);
 
   return (
     <>
+      <Toast />
       <CheckList
         title={CheckListProps.title}
         item={CheckListProps.item}
@@ -26,7 +37,8 @@ const ComponentTest = () => {
         )}
       />
       <Toggle />
-      <Radio />
+      <Radio radios={RadioProps.item} setRadioIdx={setRadioIdx} />
+      <img src={RadioProps.URL[radioIdx]} alt="grayCatPic" />
       <div>컴포넌트 테스트 페이지</div>
     </>
   );
