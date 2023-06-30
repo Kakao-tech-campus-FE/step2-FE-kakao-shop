02b5e57eed121a73aa941369a89567786a536601
unknown <jsh1147@naver.com>
2023-06-30 21:13:53 +0900

    feat: Breadcrumb 컴포넌트 구현
    Breadcrumb 컴포넌트를 구현하여 App 페이지에 구성함

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
-import Toast from "./components/Toast.js"
+import Toast from "./components/Toast.js";
+import Breadcrumb from "./components/Breadcrumb.js";
 
 function App() {
   return (
     <div className="App">
       <h1>JsH 쇼핑하기</h1>
-      <h3>TEST Page</h3>
+      <h2>TEST Page</h2>
+      <h3>Toast</h3>
       <Toast></Toast>
+      <br />
+      <h3>Breadcrumb</h3>
+      <Breadcrumb paths={["쇼핑하기", "TEST", "Breadcrumb"]}></Breadcrumb>
     </div>
   );
 }
