95492bc13706d6902bc320e67c212b899b66b8fb
dongyeopCha <ckehdduq95@gmail.com>
2023-06-30 15:56:48 +0900

    fix: position 객체 타입선언

--- src/constants/position.ts
+++ src/constants/position.ts
@@ -1,11 +1,13 @@
-const positionObj = {
-  center: 'top:50%; right:50%;',
-  'top-right': 'top:3%; right:3%;',
-  'top-center': 'top:3%; right:50%',
-  'top-left': 'top:3%; left:3%',
-  'bottom-right': 'bottom:3%;right:3%',
-  'bottom-center': 'bottom:3%; right:50%',
-  'bottom-left': 'bottom:3%; left:3%',
+interface IPositionObj {
+  [position: string]: string;
+}
+const positionObj: IPositionObj = {
+  'top-right': 'top:3%; right:0;',
+  'top-center': 'top:0; right:50%;',
+  'top-left': 'top:3%; left:0;',
+  'bottom-right': 'bottom:3%; right:0',
+  'bottom-center': 'bottom:0; right:50%',
+  'bottom-left': 'bottom:3%; left:0',
 };
 
 export default positionObj;
