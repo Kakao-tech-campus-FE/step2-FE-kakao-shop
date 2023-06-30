a0c6d979d906c369788e373a3f4d55633278746d
baegyeong <beth0929@naver.com>
2023-06-30 20:08:04 +0900

    feat: breadcrumb, checklist 컴포넌트 구현

---
+++ kakao-shop/src/components/breadcrumb.jsx
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+import { useState } from "react";
+
+const UnStyledLink = styled.a`
+  all: unset;
+  padding: 0.3rem;
+  cursor: pointer;
+
+  &:nth-last-child(1) {
+    font-weight: bold;
+  }
+`;
+
+const BreadCrumbDiv = styled.div`
+  > ${UnStyledLink}:not(:last-child) {
+    color: gray;
+  }
+`;
+
+const CategoryList = styled.div`
+  .categoryList& {
+    background-color: #d9d9d9;
+  }
+`;
+
+const TotalDiv = styled.body`
+  margin: 4rem;
+  width: 13rem;
+  height: fit-content;
+`;
+
+const breadCrumbArr = [
+  "여성의류",
+  "남성의류",
+  "언더웨어",
+  "신발/수제화",
+  "가방/지갑/잡화",
+];
+
+const Breadcrumb = ({ main }) => {
+  const [selectedItem, setSelectedItem] = useState("");
+
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+  };
+
+  return (
+    <TotalDiv>
+      <BreadCrumbDiv>
+        <UnStyledLink href={main}>{main} ></UnStyledLink>
+        <UnStyledLink href={selectedItem}>{selectedItem}</UnStyledLink>
+      </BreadCrumbDiv>
+      <CategoryList className="categoryList">
+        {breadCrumbArr.map((item, index) => (
+          <div key={index} onClick={() => handleItemClick(item)}>
+            {item}
+          </div>
+        ))}
+      </CategoryList>
+    </TotalDiv>
+  );
+};
+
+export default Breadcrumb;
