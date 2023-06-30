4fe84d627677c26f46da74b568169943100962e6
JiHye.Song <48706964+qwer15948@users.noreply.github.com>
2023-06-30 11:39:56 +0900

    체크리스트 테스트 페이지

--- week1/component-practice/src/pages/ChecklistTest.jsx
+++ week1/component-practice/src/pages/ChecklistTest.jsx
@@ -3,17 +3,42 @@ import React, { useState } from "react";
 
 export default function ChecklistTest() {
   const [checked, setChecked] = useState([
-    { id: 1, checked: false, text: "체크박스1" },
-    { id: 2, checked: false, text: "체크박스2" },
-    { id: 3, checked: false, text: "체크박스3" },
-    { id: 4, checked: false, text: "체크박스4" },
+    { id: 1, checked: false, text: "Apple" },
+    { id: 2, checked: false, text: "Orange" },
+    { id: 3, checked: false, text: "Grape" },
+    { id: 4, checked: false, text: "Banana" },
   ]);
+
+  const handleChecklistChange = (itemId) => {
+    const updatedChecked = checked.map((item) => {
+      if (item.id === itemId) {
+        return {
+          ...item,
+          checked: !item.checked,
+        };
+      }
+      return item;
+    });
+
+    setChecked(updatedChecked);
+  };
+
+  const checkedItem = checked.filter((item) => item.checked === true);
 
   return (
     <>
       {checked.map((item) => (
-        <Checklist key={item.id} item={item} />
+        <Checklist key={item.id} item={item} onChange={handleChecklistChange} />
       ))}
+
+      {checkedItem.length > 0 && (
+        <p>
+          Selected item:{" "}
+          {checkedItem.map((item) => (
+            <span key={item.id}>{item.text} </span>
+          ))}
+        </p>
+      )}
     </>
   );
 }
