d27996b89f5e20d800c714246ace0f5fb097f6ec
unknown <jsh1147@naver.com>
2023-06-30 22:51:57 +0900

    feat: Carousel 컴포넌트 구현
    Carousel 컴포넌트를 구현하여 App 페이지에 구성함

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
+// css
 import "./App.css";
+
+// component
 import Toast from "./components/Toast.js";
 import Breadcrumb from "./components/Breadcrumb.js";
+import Carousel from "./components/Carousel.js";
+
+// image
+import carouselImage1 from "./assets/images/carousel/carouselItem1.jpeg";
+import carouselImage2 from "./assets/images/carousel/carouselItem2.jpeg";
+import carouselImage3 from "./assets/images/carousel/carouselItem3.jpeg";
 
 function App() {
   return (
     <div className="App">
       <h1>JsH 쇼핑하기</h1>
       <h2>TEST Page</h2>
-      <h3>Toast</h3>
+      <h3>[Toast]</h3>
       <Toast></Toast>
       <br />
-      <h3>Breadcrumb</h3>
+      <br />
+      <h3>[Breadcrumb]</h3>
       <Breadcrumb paths={["쇼핑하기", "TEST", "Breadcrumb"]}></Breadcrumb>
+      <br />
+      <br />
+      <h3>[Carousel]</h3>
+      <Carousel
+        images={[carouselImage1, carouselImage2, carouselImage3]}
+      ></Carousel>
     </div>
   );
 }
---
+++ src/components/Carousel.js
@@ -0,0 +1,53 @@
+// indicator의 경우 실제 사이트에 상호작용이 없기에 간소화함
+
+// package
+import { useState, useEffect } from "react";
+
+// css
+import "../styles/Carousel.css";
+
+export default function Carousel({ images }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevImage = () => {
+    setCurrentIndex(
+      (currentIndex) => (currentIndex - 1 + images.length) % images.length
+    );
+  };
+
+  const nextImage = () => {
+    setCurrentIndex((currentIndex) => (currentIndex + 1) % images.length);
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      nextImage();
+      console.log("hi");
+    }, 3000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  useEffect(() => {}, [currentIndex]);
+
+  return (
+    <div className="carousel-div">
+      <img
+        className="carousel-img"
+        src={images[currentIndex]}
+        alt={"carouselItem" + (currentIndex + 1)}
+      />
+      <span className="carousel-span">
+        {images.map((_, index) => (index === currentIndex ? "■" : "○"))}
+      </span>
+      <button className="carousel-button carousel-button-left" onClick={prevImage}>
+        {"<"}
+      </button>
+      <button className="carousel-button carousel-button-right" onClick={nextImage}>
+        {">"}
+      </button>
+    </div>
+  );
+}
