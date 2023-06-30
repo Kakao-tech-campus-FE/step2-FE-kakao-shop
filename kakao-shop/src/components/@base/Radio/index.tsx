692ab4d9d3428629fffa0a42c9952019b5a2b58c
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-06-30 12:28:49 +0900

    feat: 라디오 버튼 구현

---
+++ kakao-shop/src/components/@base/Radio/index.tsx
@@ -0,0 +1,170 @@
+import { InputHTMLAttributes, PropsWithChildren } from "react";
+import styled from "@emotion/styled";
+
+const Radio = ({ id, name, children, checked, ...props }: Props) => {
+  return (
+    <FlexContainer>
+      <StyledContainerLabel htmlFor={id}>
+        <StyledInput
+          type="radio"
+          id={id}
+          name={name}
+          {...props}
+          checked={checked}
+        />
+        {children}
+      </StyledContainerLabel>
+    </FlexContainer>
+  );
+};
+
+Radio.Regular = function ({ id, name, children, checked, ...props }: Props) {
+  return (
+    <Radio id={id} name={name} checked={checked} {...props}>
+      {checked ? (
+        <StyledCheckRadio></StyledCheckRadio>
+      ) : (
+        <StyledRadio></StyledRadio>
+      )}
+      <StyledLabel>{children}</StyledLabel>
+    </Radio>
+  );
+};
+
+Radio.Big = function ({ id, name, children, checked, ...props }: Props) {
+  return (
+    <Radio id={id} name={name} checked={checked} {...props}>
+      {checked ? (
+        <StyledCheckBigBlackRadio></StyledCheckBigBlackRadio>
+      ) : (
+        <StyledBigBlackRadio></StyledBigBlackRadio>
+      )}
+      <StyledLabel>{children}</StyledLabel>
+    </Radio>
+  );
+};
+
+Radio.Info = function ({ children }: PropsWithChildren) {
+  return (
+    <InfoRoot>
+      <InfoPromotion>{children}</InfoPromotion>
+    </InfoRoot>
+  );
+};
+
+export default Radio;
+
+type Props = {
+  name: string;
+} & PropsWithChildren<InputHTMLAttributes<HTMLInputElement>>;
+
+const FlexContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const StyledContainerLabel = styled.label`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  cursor: pointer;
+`;
+
+const StyledInput = styled.input`
+  cursor: pointer;
+  display: none;
+`;
+
+const StyledCheckRadio = styled.span`
+  width: 26px;
+  height: 26px;
+  display: inline-block;
+  overflow: hidden;
+  font-size: 1px;
+  line-height: 0;
+  color: transparent;
+  background: url("https://st.kakaocdn.net/commerce_ui/front-sp/real/20230629/093545/ico_shoporder_230607.5abf9bcf37cc40a9.png")
+    no-repeat;
+  background-size: 150px 225px;
+  background-position: -50px -100px;
+
+  vertical-align: top;
+`;
+
+const StyledRadio = styled.span`
+  width: 26px;
+  height: 26px;
+  display: inline-block;
+  overflow: hidden;
+  font-size: 1px;
+  line-height: 0;
+  color: transparent;
+  background: url("https://st.kakaocdn.net/commerce_ui/front-sp/real/20230629/093545/ico_shoporder_230607.5abf9bcf37cc40a9.png")
+    no-repeat;
+  background-size: 150px 225px;
+  background-position: -20px -100px;
+
+  vertical-align: top;
+`;
+
+const StyledCheckBigBlackRadio = styled.span`
+  width: 26px;
+  height: 26px;
+  display: inline-block;
+  overflow: hidden;
+  font-size: 1px;
+  line-height: 0;
+  color: transparent;
+  background: url("https://st.kakaocdn.net/commerce_ui/front-sp/real/20230629/093545/pay_icon_200401.4e2af07d62fbb994.png")
+    no-repeat;
+  background-size: 210px 50px;
+  background-position: -90px 0;
+
+  vertical-align: top;
+`;
+
+const StyledBigBlackRadio = styled.span`
+  width: 26px;
+  height: 26px;
+  display: inline-block;
+  overflow: hidden;
+  font-size: 1px;
+  line-height: 0;
+  color: transparent;
+  background: url("https://st.kakaocdn.net/commerce_ui/front-sp/real/20230629/093545/pay_icon_200401.4e2af07d62fbb994.png")
+    no-repeat;
+  background-size: 210px 50px;
+  background-position: -60px 0;
+
+  vertical-align: top;
+`;
+
+const StyledLabel = styled.span`
+  cursor: pointer;
+  display: inline-block;
+  padding-left: 8px;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 24px;
+  color: #333;
+  letter-spacing: -0.036em;
+  vertical-align: top;
+`;
+
+const InfoRoot = styled.div`
+  padding: 3px 0 0 38px;
+`;
+
+const InfoPromotion = styled.div`
+  height: 26px;
+  width: fit-content;
+  padding: 0 14px;
+  font-size: 12px;
+  font-weight: 400;
+  color: #343218;
+  background-color: #fff68c;
+  vertical-align: middle;
+  line-height: 29px;
+  letter-spacing: -1px;
+  border-radius: 14px;
+`;
--- kakao-shop/src/components/domains/Pay/index.tsx
+++ kakao-shop/src/components/domains/Pay/index.tsx
@@ -1,6 +1,8 @@
 import styled from "@emotion/styled";
 import { Fragment, PropsWithChildren } from "react";
 import CheckBoxItem from "../../@molecules/CheckBoxItem";
+import Radio from "../../@base/Radio";
+import { ChangeEvent } from "react";
 
 const Pay = ({ children }: PropsWithChildren) => {
   return <Fragment>{children}</Fragment>;
@@ -8,34 +10,101 @@ const Pay = ({ children }: PropsWithChildren) => {
 
 Pay.Agree = function () {
   return (
-    <S.Root>
-      <S.Header>
+    <Agree.Root>
+      <Agree.Header>
         <CheckBoxItem.Big>전체 동의하기</CheckBoxItem.Big>
-      </S.Header>
+      </Agree.Header>
 
-      <S.Body>
+      <Agree.Body>
         <CheckBoxItem>
           카카오페이 결제조건 및 개인정보 제3자 제공 동의
         </CheckBoxItem>
         <CheckBoxItem>개인정보 제3자 제공 동의</CheckBoxItem>
-      </S.Body>
+      </Agree.Body>
 
-      <S.Footer>
-        <S.Terms>법적 고지</S.Terms>
-        <S.TermsText>
+      <Agree.Footer>
+        <Agree.Terms>법적 고지</Agree.Terms>
+        <Agree.TermsText>
           (주)카카오에서 판매하는 상품 중에는 개별 판매자가 판매하는 상품이
           포함되어 있습니다. 개별 판매자가 판매하는 상품에 대해 (주)카카오는
           통신중개 판매업자로서 통신판매의 당사자가 아니며 상품의 주문, 배송 및
           환불 등과 관련한 의무와 책임은 각 판매자에게 있습니다.
-        </S.TermsText>
-      </S.Footer>
-    </S.Root>
+        </Agree.TermsText>
+      </Agree.Footer>
+    </Agree.Root>
+  );
+};
+
+Pay.Method = function ({ payMethodData, state, onChange }: PayMethod) {
+  return (
+    <Method.Root>
+      <Method.Header>
+        <Method.Logo></Method.Logo>
+      </Method.Header>
+
+      <Method.PaymentTypeList>
+        {payMethodData.map((item) => (
+          <li key={item.name}>
+            <Radio.Big
+              name={item.name}
+              value={item.value}
+              onChange={onChange}
+              checked={item.value === state}
+            >
+              {item.name}
+            </Radio.Big>
+            {item.info !== "" && <Radio.Info>{item.info}</Radio.Info>}
+          </li>
+        ))}
+      </Method.PaymentTypeList>
+    </Method.Root>
+  );
+};
+
+Pay.CashReceipt = function ({
+  payCashReceiptData,
+  state,
+  onChange,
+}: PayCashReceipt) {
+  return (
+    <CashReceipt.Root>
+      {payCashReceiptData.map((item) => (
+        <li key={item.name}>
+          <Radio.Regular
+            name={item.name}
+            value={item.value}
+            onChange={onChange}
+            checked={item.value === state}
+          >
+            {item.name}
+          </Radio.Regular>
+        </li>
+      ))}
+    </CashReceipt.Root>
   );
 };
 
 export default Pay;
 
-const S = {
+type PayMethodData = {
+  name: string;
+  value: string;
+  info: string;
+};
+
+type PayMethod = {
+  payMethodData: PayMethodData[];
+  state: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+};
+
+type PayCashReceiptData = Omit<PayMethodData, "info">;
+
+type PayCashReceipt = {
+  payCashReceiptData: PayCashReceiptData[];
+} & Omit<PayMethod, "payMethodData">;
+
+const Agree = {
   Root: styled.div`
     background-color: #fff;
 
@@ -77,5 +146,53 @@ const S = {
     font-size: 13px;
     line-height: 20px;
     color: #666;
+  `,
+};
+
+const Method = {
+  Root: styled.div`
+    background-color: #ffeb00;
+  `,
+  Header: styled.div`
+    position: relative;
+    height: 60px;
+  `,
+  Logo: styled.span`
+    position: absolute;
+    top: 20px;
+    left: 15px;
+    width: 52px;
+    height: 16px;
+    background-position: 0 0;
+    overflow: hidden;
+    font-size: 1px;
+    line-height: 0;
+    color: transparent;
+    background: url("https://st.kakaocdn.net/commerce_ui/front-sp/real/20230629/093545/pay_icon_200401.4e2af07d62fbb994.png")
+      no-repeat;
+    vertical-align: top;
+    background-size: 210px 50px;
+  `,
+  PaymentTypeList: styled.ul`
+    background-color: #ffeb00;
+    padding: 0 15px 15px 15px;
+
+    & > li {
+      position: relative;
+      padding: 13px 0 15px;
+    }
+
+    & > li:first-of-type {
+      padding-top: 0px;
+      padding-bottom: 7px;
+    }
+  `,
+};
+
+const CashReceipt = {
+  Root: styled.div`
+    display: flex;
+    gap: 18px;
+    padding: 15px 16px;
   `,
 };
