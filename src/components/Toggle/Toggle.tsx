ab30c7994dd5b88b38800b006222b4ea07e094cf
publdaze <publdaze@naver.com>
2023-06-30 20:42:19 +0900

    feat: 체크 여부에 따른 토글 동작 구현

--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Toggle.css';
 
 const Toggle = () => {
+  const [isOn, setIsOn] = useState(false);
+
+  const handleToggleSwitchChange = () => {
+    setIsOn((prev) => !prev);
+  };
+
   return (
     <div>
       <label htmlFor='toggle-switch'>
-        <input id='toggle-switch' type='checkbox' className='toggle-switch' />
+        <input
+          id='toggle-switch'
+          type='checkbox'
+          className='toggle-switch'
+          checked={isOn}
+          onChange={handleToggleSwitchChange}
+        />
       </label>
     </div>
   );
