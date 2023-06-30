b5c0a0a0bb8e16e6823876e3cc0d6c3342b37f2f
Heo_Donghyuk <missko30@pusan.ac.kr>
2023-06-30 21:05:23 +0900

    feat: align card content center

--- week1/assignment2_3/src/components/mainpage/mainpage.jsx
+++ week1/assignment2_3/src/components/mainpage/mainpage.jsx
@@ -1,14 +1,22 @@
+import Badge from "../badge/Badge";
 import Card from "../card/Card";
+import Carousel from "../carousel/Carousel";
+import Radio from "../radio/Radio";
+import Toggle from "../togglebtn/ToggleBtn";
 import "./mainpage.css";
 const MainPage = () => {
   return (
     <div className="main">
-      <Card url={"/badge"} title="Badge" />
-      <Card url={"/radio"} title="Radio Button" />
-      <Card url={"/toggle"} title="Toggle Button" />
-      <Card url={"/carousel"} title="Carousel" />
-      <Card url={"/toast"} title="Toast" />
-      <Card url={"/checklist"} title="Checklist" />
+      <Card url={"/badge"} title="Badge" content={<Badge text={"New!"} />} />
+      <Card
+        url={"/radio"}
+        title="Radio Button"
+        content={<Radio checked={true} />}
+      />
+      <Card url={"/toggle"} title="Toggle Button" content={<Toggle />} />
+      <Card url={"/carousel"} title="Carousel" content={"Carousel"} />
+      <Card url={"/toast"} title="Toast" content="Toast" />
+      <Card url={"/checklist"} title="Checklist" content="Checklist" />
     </div>
   );
 };
