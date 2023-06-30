cba5ebc4e4a10c822be7418b888a76a9ede743fe
MINJOO-KIM <gkminju2000@gmail.com>
2023-06-30 23:37:37 +0900

    feat: 과제3 - 컴포넌트 테스트 페이지 구현

--- kakao-shopping/src/App.jsx
+++ kakao-shopping/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+import Carousel from "./components/Carousel";
+import RadioButton from "./components/RadioButton";
+import Breadcrumb from "./components/BreadCrumb";
+
+import Toggle from "./components/Toggle";
+import CheckList from "./components/CheckList";
+import Toast from "./components/Toast";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="toast-container">
+        <h1>Toast</h1>
+        {/* <Toast></Toast> */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="breadcrumb-container">
+        <h1>Breadcrumb</h1>
+        {/* <Breadcrumb></Breadcrumb> */}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="carousel">
+        <h1>Carousel</h1>
+        <Carousel></Carousel>
+      </div>
+      <div className="radio-button-container">
+        <h1>Radio Button</h1>
+        <RadioButton></RadioButton>
+      </div>
+      <div className="toggle-button-container">
+        <h1> Toggle Button</h1>
+        <Toggle></Toggle>
+      </div>
+      <div className="checklist-container">
+        <h1>CheckList</h1>
+        <CheckList></CheckList>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
