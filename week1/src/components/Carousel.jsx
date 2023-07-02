c3323319f5e17b52133989b3ba152e8d0a508eaf
GimGayeon <kky1373@naver.com>
2023-07-02 13:21:58 +0900

    minus modules

--- week1/src/components/Carousel.jsx
+++ week1/src/components/Carousel.jsx
@@ -4,20 +4,42 @@ import styles from "../css/Carousel.module.css";
 
 
 const Carousel = () => {
-    const [Carousel, setCarousel] = useState(false);
+    let list = 0;
+    const [Carousel, setCarousel] = useState(0);
+
+
+    const nextBtn = () => {
+        setCarousel(() => {
+            if (list === 3) {
+                return 0;
+            } else {
+                return list + 1;
+            }
+        });
+    };
+
+    const prevBtn = () => {
+        setCarousel(() => {
+            if (list === 0) {
+                return list - 1;
+            } else {
+                return list - 1;
+            }
+        });
+    };
+
 
     return (
         <div className={styles.carouselDiv}>
             <h2 className={styles.h2}>3. 캐러셀 구현</h2>
             <ul className={styles.carouselUl}>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
+                <li className={`${styles.carouselSlide} ${Carousel === 1 ? styles.active : ''}`}>1</li>
+                <li className={`${styles.carouselSlide} ${Carousel === 2 ? styles.active : ''}`}>2</li>
+                <li className={`${styles.carouselSlide} ${Carousel === 3 ? styles.active : ''}`}>3</li>
             </ul>
             <div className={styles.carouselBtn}>
-                <button>prev</button>
-                <button>next</button>
+                <button onClick={prevBtn}>prev</button>
+                <button onClick={nextBtn}>next</button>
             </div>
         </div >
     )
