05e9cae1aa757d977a5e12f0163b99c58118888d
ba0061@naver.com <sweet.waiting0l@icloud.com>
2023-07-04 11:28:27 +0900

    feat: 로그인 확인
    로컬스토리지로 로그인 확인
    로그인 돼있을 시, navigate로 메인페이지 이동

--- kakao-shopping/src/index.js
+++ kakao-shopping/src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import authReducer from './reducers/authReducer';
+import thunk from 'redux-thunk'
+
+const rootReducer = combineReducers({
+  authReducer
+});
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
--- kakao-shopping/src/pages/LoginPage.jsx
+++ kakao-shopping/src/pages/LoginPage.jsx
@@ -1,7 +1,46 @@
 import LoginForm from "../components/organisms/LoginForm";
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {loginSuccess } from '../actions/authActions';
+import LogoutButton from "../components/molecules/LogoutButton";
+import { useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
-  return <LoginForm />;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const storedIsLoggedIn = localStorage.getItem('isLoggedIn');
+    if(storedIsLoggedIn !== null && storedIsLoggedIn >= calculateTime()) { //로컬스토리지에 저장이 돼있고, 시간도 하루가 안 지났다면
+      dispatch(loginSuccess()); //로그인 성공
+    }
+  },[dispatch]);
+
+  const isLoggedIn = useSelector((state) => state.authReducer.isLoggedIn);
+  console.log(isLoggedIn);
+
+  const navigate = useNavigate();
+  if(isLoggedIn) {
+    navigate('/main');
+  }
+  return (
+  isLoggedIn ? (
+    <>
+      <LogoutButton />
+      {localStorage.getItem('userInfo') || 'No user information available'}
+    </>
+  ) : (
+    <LoginForm />
+  )
+  )
 }
 
+const calculateTime = () => {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear().toString().padStart(4, '0');
+  const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+  const day = currentDate.getDate().toString().padStart(2, '0');
+  const hours = currentDate.getHours().toString().padStart(2, '0');
+  const minutes = currentDate.getMinutes().toString().padStart(2, '0');
+  const formattedTime = year + month + day + hours + minutes;
+  return formattedTime;
+}
 export default LoginPage;
