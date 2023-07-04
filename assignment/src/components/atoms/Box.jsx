3fb012f52a1444ba66c773af31917977717779e3
YANGSEOKWOO <didtjrdn030@naver.com>
2023-07-04 23:32:55 +0900

    feat - finish atoms component
    회원가입 페이지를 위한 Box.jsx, Contatiner.jsx파일 완성

---
+++ assignment/src/components/atoms/Box.jsx
@@ -0,0 +1,9 @@
+import './../../styles/box.css'
+
+const Box = ({children}) => {
+    return(
+        <div className = "box"> {children} </div>
+    );
+};
+
+export default Box;
---
+++ assignment/src/components/atoms/Container.jsx
@@ -0,0 +1,9 @@
+import './../../styles/atoms/Container.css'
+
+const Container = ({children}) => {
+    return(
+        <div className = "container"> {children} </div>
+    );
+};
+
+export default Container;
