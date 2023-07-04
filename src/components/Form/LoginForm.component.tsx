1d9080aa6aa618b55a4e5b12863d9a9e46859c30
loopy-lim <loopy@jnu.ac.kr>
2023-07-04 10:57:33 +0900

    feat: add LoginForm
    - add LoginForm
    - add value, onChange at InputFormItem

---
+++ src/components/Form/LoginForm.component.tsx
@@ -0,0 +1,56 @@
+import { FC } from "react";
+import ButtonFormItem from "../FormItem/ButtonFormItem.component";
+import InputFormItem from "../FormItem/InputFormItem.component";
+
+interface LoginFormProps {
+  email: {
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  };
+  name: {
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  };
+  password: {
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  };
+  passwordConfirm: {
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  };
+}
+
+const LoginForm: FC<LoginFormProps> = ({
+  email,
+  name,
+  password,
+  passwordConfirm,
+}) => {
+  return (
+    <form>
+      <InputFormItem
+        label="이메일 (아이디)"
+        placeholder="이메일"
+        type="email"
+        {...email}
+      />
+      <InputFormItem label="이름" placeholder="이름" type="text" {...name} />
+      <InputFormItem
+        label="비밀번호"
+        placeholder="비밀번호"
+        type="password"
+        {...password}
+      />
+      <InputFormItem
+        label="비밀번호 확인"
+        placeholder="비밀번호 확인"
+        type="password"
+        {...passwordConfirm}
+      />
+      <ButtonFormItem label="회원가입" type="submit" />
+    </form>
+  );
+};
+
+export default LoginForm;
--- src/components/FormItem/InputFormItem.component.tsx
+++ src/components/FormItem/InputFormItem.component.tsx
@@ -4,18 +4,28 @@ interface InputFormItemProps {
   label: string;
   type: "text" | "number" | "email" | "password";
   placeholder?: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 const InputFormItem: FC<InputFormItemProps> = ({
   label,
   type,
   placeholder = "",
+  value,
+  onChange,
 }) => {
   const uid = useId();
   return (
     <>
       <label htmlFor={uid}>{label}</label>
-      <input id={uid} type={type} placeholder={placeholder} />
+      <input
+        id={uid}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
     </>
   );
 };
