d66109574c870084ebd842f3e1a432cc93bffcef
bada308 <203190@jnu.ac.kr>
2023-07-04 10:53:09 +0900

    Design : Global Style 적용 - App.css 정리 - GlobalStyle 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import './App.css';
+import GlobalStyle from './styles/GlobalStyle';
 import BreadcrumbPage from './pages/BreadcrumbPage';
 import CarouselPage from './pages/CarouselPage';
 import MainPage from './pages/MainPage';
 import ToastPage from './pages/ToastPage';
 function App() {
-  console.log('App');
-  return (
-    <>
-      <BreadcrumbPage />
-      <MainPage />
-      <CarouselPage />
-      <ToastPage />
-    </>
-  );
+    console.log('App');
+    return (
+        <>
+            <GlobalStyle>
+                <BreadcrumbPage />
+                <MainPage />
+                <CarouselPage />
+                <ToastPage />
+            </GlobalStyle>
+        </>
+    );
 }
 
 export default App;
