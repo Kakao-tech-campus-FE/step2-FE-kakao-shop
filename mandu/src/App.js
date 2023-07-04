81c5ec90c57e1420365afad70743fc39030ff34d
charmeee <mandugohigh@gmail.com>
2023-07-04 15:16:12 +0900

    remove: 1주차 코드 임시 폴더로 이동

--- mandu/src/App.js
+++ mandu/src/App.js
@@ -1,29 +1,11 @@
+import LoginPage from "./pages/Login";
 import './App.css';
-import ToastControlBox from "./components/toastControlBox";
-import BreadcrumbControlBox from "./components/breadcrumbControlBox";
-import CarouselControlBox from "./components/carouselControlBox";
-import CheckboxControlBox from "./components/checkboxControlBox";
-import RadioControlBox from "./components/radioControlBox";
-import ToggleControlBox from "./components/toggleControlBox";
-
 
 function App() {
 
 
     return (
         <div className="App">
-            <ToastControlBox/>
-            <hr/>
-            <BreadcrumbControlBox/>
-            <hr/>
-            <CarouselControlBox/>
-            <hr/>
-            <CheckboxControlBox/>
-            <hr/>
-            <RadioControlBox/>
-            <hr/>
-            <ToggleControlBox/>
-
 
         </div>
     );
