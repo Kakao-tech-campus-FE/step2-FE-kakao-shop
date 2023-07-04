9cbcbd5423de100b2ab58d0a5b0bf0e257bfcb92
lcqff <yeonnu82g@gmail.com>
2023-07-04 16:20:31 +0900

    feat: molecules InputGroup 작성

---
+++ src/components/molecules/InputGroup.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Input from '@components/atoms/Input';
+import Label from '@components/atoms/Label';
+
+interface InputGroupProps {
+  inputName: string;
+  labelName: string;
+}
+
+const InputGroup = ({ inputName, labelName }: InputGroupProps) => {
+  return (
+    <div>
+      <Label htmlFor={inputName}>{labelName}</Label>
+      <Input type="text" id={inputName} />
+    </div>
+  );
+};
+
+export default InputGroup;
