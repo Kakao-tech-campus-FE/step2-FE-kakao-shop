c91748e16b036b7f2085c73fbf38e32385ebb73c
kimjinwoo <rlawlsdn177@naver.com>
2023-07-04 16:01:46 +0900

    week2 과제 수행을 위한 App.js, index.js 병합 및 수정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,6 @@
-import './styles/App.css';
-import Radio from './components/Radio';
-import Checklist from './components/Checklist';
-import Toggle from './components/Toggle';
-import Breadcrumbs from './components/Breadcrumbs';
-import Toast from './components/Toast';
-import Carousel from './components/Carousel';
-//import { createContext, useState, useRef } from 'react';
-
+import RegisterPage from "./pages/RegisterPage";
 function App() {
-  return (
-    <div className="App">
-      <Breadcrumbs/>
-      <Carousel />
-      <Toggle />
-      <Toast />
-      <Radio />
-      <Checklist />
-    </div>
-  );
+  return <RegisterPage />;
 }
 
 export default App;
