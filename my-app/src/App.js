0534afa4c675db3fb3290f6c7cf80b0937b80539
iam454 <realseogy@gmail.com>
2023-07-04 10:25:05 +0900

    remove: 1주차 과제 삭제

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,7 @@
-import ToastExample from "./components/Toast/ToastExample";
-import BreadcrumbExample from "./components/Breadcrumb/BreadcrumbExample";
-import CarouselExample from "./components/Carousel/CarouselExample";
-import RadioExample from "./components/Radio/RadioExample";
-import ToggleExample from "./components/Toggle/ToggleExample";
-import CheckListExample from "./components/CheckList/CheckListExample";
-
 function App() {
   return (
     <>
-      <ToastExample />
-      <BreadcrumbExample />
-      <CarouselExample />
-      <RadioExample />
-      <ToggleExample />
-      <CheckListExample />
+      <div>2주차 과제</div>
     </>
   );
 }
