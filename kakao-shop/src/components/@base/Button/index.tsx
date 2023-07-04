98687b4d964c84cdc458dfa6593b6e0e9a54a6b6
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-07-05 01:26:46 +0900

    feat: 버튼 컴포넌트

---
+++ kakao-shop/src/components/@base/Button/index.tsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+const Button = styled.button`
+  width: 100%;
+  border: none;
+`;
+
+export default Button;
--- kakao-shop/src/components/@base/index.ts
+++ kakao-shop/src/components/@base/index.ts
@@ -1,3 +1,6 @@
 export { default as Toast } from './Toast';
 export { default as Toggle } from './Toggle';
 export { default as CheckBox } from './CheckBox';
+export { default as Label } from './Label';
+export { default as Input } from './Input';
+export { default as Button } from './Button';
