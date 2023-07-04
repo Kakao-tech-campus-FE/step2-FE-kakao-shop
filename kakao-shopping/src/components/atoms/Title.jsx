fafdcf6eeb59e4294b68612c527e348ab2b7bd02
ba0061@naver.com <sweet.waiting0l@icloud.com>
2023-07-04 23:20:41 +0900

    fix: add return
    return 하지 않아서 출력되지 않던 문제 해결

--- kakao-shopping/src/components/atoms/Title.jsx
+++ kakao-shopping/src/components/atoms/Title.jsx
@@ -1,4 +1,4 @@
 const Title = ({children}) => { // 내용 텍스트
-  <h2>{children}</h2>
+  return <h2 className="-mx-3 block bg-slate-200 rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">{children}</h2>
 }
 export default Title;
