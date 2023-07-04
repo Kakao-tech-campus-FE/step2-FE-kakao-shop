b4a04de814acf74f1c07a4617056a30396ea743f
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2023-07-04 11:59:58 +0900

    feat: 토스트의 상태를 useReducer 훅으로 관리

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,16 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import Toast from "../components/Toast/Toast";
 import Button from "../components/Button";
 import { useNavigate } from "react-router-dom";
 
 export default function Home() {
   const navigate = useNavigate();
-  const [toast, setToast] = useState(false);
+  const [toast, setToast] = useReducer((prev) => !prev, false);
 
-  const handleToastClick = () => {
-    setToast((prev) => !prev);
-  };
   return (
     <>
-      <Button text="Test Toast" handleClick={handleToastClick} />
-      <Toast
-        isShow={toast}
-        text="Kakao Tech Campus!"
-        handleToast={handleToastClick}
-      />
+      <Button text="Test Toast" handleClick={setToast} />
+      <Toast isShow={toast} text="Kakao Tech Campus!" handleToast={setToast} />
       <Button
         text="Test Breadcrumb"
         handleClick={() => navigate("/products")}
