3d4522af7f5b5d9764584d86088ba778368e4e62
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-07-05 01:24:14 +0900

    chore: style 폴더 절대경로 설정

--- kakao-shop/craco.config.js
+++ kakao-shop/craco.config.js
@@ -10,6 +10,7 @@ module.exports = {
       '@components': path.resolve(__dirname, 'src/components'),
       '@pages': path.resolve(__dirname, 'src/pages'),
       '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
     },
   },
 };
