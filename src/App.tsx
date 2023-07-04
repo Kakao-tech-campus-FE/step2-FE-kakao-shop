c28cbba908f78cfc6a8a3316b1903751dc0770fe
lcqff <yeonnu82g@gmail.com>
2023-07-04 12:16:39 +0900

    build: meterial tailwind 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import ComponentTest from "./pages/ComponentTest";
 
 function App() {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
 
-module.exports = withMT({
+module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-});
+};
