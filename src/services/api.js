be5b4c19027fdba6700d2822be470186f4ace0ee
kimjinwoo <rlawlsdn177@naver.com>
2023-07-04 16:08:05 +0900

    week2 API 설정을 위한 services/api.js 구현

---
+++ src/services/api.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL:
+    "http://kakao-app-env.eba-kfsgeb74.ap-northeast-2.elasticbeanstalk.com/",
+  //process.env.REACT_APP_API_URL,
+  timeout: 1000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+  return config;
+});
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    //
+  }
+);
+
+export const register = (data) => {
+  const { email, password, username } = data;
+  return instance.post("/join", {
+    email,
+    password,
+    username,
+  });
+};
