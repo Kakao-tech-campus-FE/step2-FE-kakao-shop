1d4b2b748320e8fc1ed15f4ec4e5909e647ba148
publdaze <publdaze@naver.com>
2023-07-05 16:06:12 +0900

    feat: 버튼 클릭 시 기본 동작 방지

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -11,7 +11,10 @@ const Button = ({ type = 'submit', children, onClick }: ButtonProps) => {
     <button
       // eslint-disable-next-line react/button-has-type
       type={type}
-      onClick={onClick}
+      onClick={(e) => {
+        e.preventDefault();
+        onClick(e);
+      }}
       className='flex w-full justify-center rounded-md bg-yellow-300 px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm hover:bg-yellow-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-neutral-600'
     >
       {children}
