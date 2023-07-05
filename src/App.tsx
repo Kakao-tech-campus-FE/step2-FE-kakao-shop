d281bce3b7e12bb09c7580948e7dbcf026cd98d8
Donghyeon Lee <ehdgus001928@gmail.com>
2023-07-05 16:03:52 +0900

    feat: Modify index page

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,26 @@
-import React from 'react';
-import ToastDemo from './components/toastDemo';
-import ToastProvider from './components/toast';
-import RadioButton from './components/radioButton';
-import ToggleButton from './components/toggleButton';
-import Checklist from './components/checklist';
-import CarouselDemo from './components/carouselDemo';
-import './styles/app.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import MainPage from './components/pages/mainPage';
+import RegisterPage from './components/pages/registerPage';
+import LoginPage from './components/pages/loginPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainPage />,
+  },
+  {
+    path: '/register',
+    element: <RegisterPage />,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+]);
 
 function App() {
   return (
-    <div className="demo-page">
-      <ToastProvider>
-        <ToastDemo />
-      </ToastProvider>
-      <hr />
-      <div className="demo-category-title">
-        <a href="/breadcrumb">Breadcrumb</a>
-      </div>
-      <hr />
-      <CarouselDemo />
-      <hr />
-      <RadioButton />
-      <hr />
-      <ToggleButton />
-      <hr />
-      <Checklist />
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
