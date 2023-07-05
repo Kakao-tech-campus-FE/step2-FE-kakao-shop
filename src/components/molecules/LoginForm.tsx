94771aba41c6031b600ae5de3e898d238e669c40
lcqff <yeonnu82g@gmail.com>
2023-07-05 15:56:51 +0900

    feat: login 폼 login api 임시 연동

--- src/components/molecules/LoginForm.tsx
+++ src/components/molecules/LoginForm.tsx
@@ -1,13 +1,24 @@
 import FilledButton from '@components/atoms/FilledButton';
 import Input from '@components/atoms/Input';
 import React from 'react';
+import { AxiosResponse } from 'axios';
 
-const LoginForm = () => {
+interface LoginFromProps {
+  onSubmit: (data: { email: string; password: string }) => Promise<AxiosResponse>;
+}
+
+const LoginForm = ({ onSubmit }: LoginFromProps) => {
   return (
     <div className="flex flex-col space-y-2">
       <Input placeholder="아이디" />
       <Input placeholder="비밀번호" type="password" />
-      <FilledButton>로그인</FilledButton>
+      <FilledButton
+        onClick={() => {
+          onSubmit({ email: 'asdf', password: 'asdfdsfad' });
+        }}
+      >
+        로그인
+      </FilledButton>
     </div>
   );
 };
