0b7d60aa59929bb0b79d931f7bd0d93a2ded3b17
udadai <yujeong470@gmail.com>
2023-07-05 13:56:16 +0900

    fix: .env 파일 경로 수정

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL, // 수정된 부분
   timeout: 1000,
   headers: {
     'Content-Type': 'application/json',
@@ -19,6 +19,6 @@ instance.interceptors.response.use(
 
 export const register = (data) => {
   const { email, password, username } = data;
-  console.log('Request URL:', instance.defaults.baseURL + '/join');
+  // console.log('Request URL:', instance.defaults.baseURL + '/join');
   return instance.post('/join', { email, password, username });
 };
