055acd7f52d1c532cb46e2216ec4739ab181552c
publdaze <publdaze@naver.com>
2023-07-05 14:57:03 +0900

    feat: 회원가입 페이지 구성

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import SubContainer from '../components/atoms/SubContainer';
+import KaKaoLogo from '../components/atoms/KaKaoLogo';
+import RegisterForm from '../components/organisms/RegisterForm';
 
 const RegisterPage = () => {
-  return <div>회원가입 페이지</div>;
+  return (
+    <SubContainer>
+      <KaKaoLogo />
+      <RegisterForm />
+    </SubContainer>
+  );
 };
 
 export default RegisterPage;
