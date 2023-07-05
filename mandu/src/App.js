2ace8b86b9a1df6efe293376945af8e3cdfcc27e
charmeee <mandugohigh@gmail.com>
2023-07-05 15:30:54 +0900

    feat: react router dom을 이용, 경로에 따른 페이지 라우팅

--- mandu/src/App.js
+++ mandu/src/App.js
@@ -1,11 +1,24 @@
-import LoginPage from "./pages/Login";
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
 
 
     return (
         <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route exact path="/" element={<MainPage/>}/>
+                    <Route path="/login" element={<LoginPage/>}/>
+                    <Route path="/signup" element={<SignUpPage/>}/>
+                    <Route path='/*' element={<MainPage/>}/>
+                </Routes>
+            </BrowserRouter>
+
+            {/*<LoginPage  />*/}
 
         </div>
     );
