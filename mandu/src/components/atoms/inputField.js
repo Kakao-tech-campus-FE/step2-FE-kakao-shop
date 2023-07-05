4f4a19c2e78365968de031aca4f9280486603521
charmeee <mandugohigh@gmail.com>
2023-07-05 23:44:05 +0900

    feat: name 파라미터 추가

--- mandu/src/components/atoms/inputField.js
+++ mandu/src/components/atoms/inputField.js
@@ -1,10 +1,18 @@
-const InputField = ({ id, label, type, placeholder, value, onChange,error }) => {
+const InputField = ({id, label, type, placeholder, value, onChange, error}) => {
 
     return (
         <div>
-            {label&&<label className="my-0.5 block" htmlFor={id}>{label}</label>}
-            <input id={id} type={type} placeholder={placeholder} className="w-full border-2 p-2 rounded " value={value} onChange={onChange} />
-            {error&&<p className="text-red-500">{error}</p>}
+            {label && <label className="my-0.5 block" htmlFor={id}>{label}</label>}
+            <input
+                name={name}
+                id={id}
+                type={type}
+                placeholder={placeholder}
+                className="w-full border-2 p-2 rounded "
+                value={value}
+                onChange={onChange}
+            />
+            {error && <p className="text-red-500">{error}</p>}
         </div>
     );
 };
