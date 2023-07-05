ee5b6fa983de7ee4560c868464dccc926c5a95b7
lcqff <yeonnu82g@gmail.com>
2023-07-05 15:56:23 +0900

    feat: login api interceptors 추가

--- src/api/loginApi.ts
+++ src/api/loginApi.ts
@@ -10,9 +10,30 @@ const instance = axios.create({
   },
 });
 
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  const configuration = config;
+  if (token) {
+    configuration.headers.Authorization = `Bearer ${token}`;
+  }
+  console.log(configuration);
+  return configuration;
+});
+
+instance.interceptors.response.use(
+  (response) => {
+    console.log(response);
+    return response;
+  },
+  (error) => {
+    console.error('An error occurred:', error);
+    throw error;
+  },
+);
+
 const login = (data: LoginData) => {
   const { email, password } = data;
-  return instance.post('/login', data);
+  return instance.post('/login', { email, password });
 };
 
 export default login;
