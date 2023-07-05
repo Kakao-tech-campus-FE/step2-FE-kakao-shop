3ec55bfd30762f3516b1244efb3bad75e448f9ca
charmeee <mandugohigh@gmail.com>
2023-07-05 23:44:36 +0900

    feat: useForm hook 추가

---
+++ mandu/src/hook/useForm.js
@@ -0,0 +1,49 @@
+import {useEffect, useState} from "react";
+
+const useForm = (initialValue, onSubmit, validate) => {
+    //initialValue:{key,value}
+    //onSubmit: function //can be null
+    //validate: function(values) => errors //can be null
+
+    const [values, setValues] = useState(initialValue);
+    const [errors, setErrors] = useState({});
+    const [submitting, setSubmitting] = useState(false);
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setValues((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+
+    }
+
+
+    const handleSubmit = (e) => {
+        console.log("handleSubmit")
+        setSubmitting(true);
+        e.preventDefault();
+        setErrors(validate?.(values) ?? {});
+    }
+
+
+    useEffect(() => {
+        if (submitting) {
+            if (Object.keys(errors).length === 0) {
+                onSubmit?.(values);
+            }
+            setSubmitting(false);
+        }
+    }, [errors]);
+
+
+    return {
+        values,
+        errors,
+        submitting,
+        handleChange,
+        handleSubmit,
+    };
+}
+
+export default useForm;
