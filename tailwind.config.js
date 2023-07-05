54d8793c95b9ac2f9fdbcdc6b5f1e019f0a4664c
lcqff <yeonnu82g@gmail.com>
2023-07-05 11:18:13 +0900

    feat: tailwind 커스텀 컬러 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pointPupple: '#A50FFF',
+        subGray: '#F0F0F0',
+      },
+    },
   },
   plugins: [],
 };
