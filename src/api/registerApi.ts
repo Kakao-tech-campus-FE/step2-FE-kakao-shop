3353d4272e715d2ab80e26240ccb9f9725721842
lcqff <yeonnu82g@gmail.com>
2023-07-06 22:59:47 +0900

    fix: 오타 수정

--- src/api/registerApi.ts
+++ src/api/registerApi.ts
@@ -5,7 +5,7 @@ const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   // timeout: 1000,
   headers: {
-    'Content-Type': 'applicatoin/json',
+    'Content-Type': 'application/json',
   },
 });
 
