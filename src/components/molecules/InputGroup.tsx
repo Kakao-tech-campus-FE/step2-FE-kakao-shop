9a72e541fd4ab78a3e088b25d5fb7cd6383a4ee7
lcqff <yeonnu82g@gmail.com>
2023-07-07 03:39:17 +0900

    feat: useInput 커스텀 훅 적용

--- src/components/molecules/InputGroup.tsx
+++ src/components/molecules/InputGroup.tsx
@@ -5,13 +5,15 @@ import Label from '@components/atoms/Label';
 interface InputGroupProps {
   inputName?: string;
   labelName?: string;
+  value?: string;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
 }
 
-const InputGroup = ({ inputName, labelName }: InputGroupProps) => {
+const InputGroup = ({ inputName, labelName, value, onChange }: InputGroupProps) => {
   return (
     <div className="flex flex-col space-y-1">
       <Label htmlFor={inputName}>{labelName}</Label>
-      <Input type="text" id={inputName} />
+      <Input type="text" id={inputName} value={value} onChange={onChange} />
     </div>
   );
 };
--- src/components/organisms/RegisterForm.tsx
+++ src/components/organisms/RegisterForm.tsx
@@ -1,22 +1,38 @@
 import FilledButton from '@components/atoms/FilledButton';
 import InputGroup from '@components/molecules/InputGroup';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AxiosResponse } from 'axios';
+import useInput from '@hooks/useInput';
 
 interface RegisterFromProps {
   onSubmit: (data: { email: string; password: string; username: string }) => Promise<AxiosResponse>;
 }
 
 const RegisterForm = ({ onSubmit }: RegisterFromProps) => {
+  const { value: inputInfo, handleOnChange } = useInput({
+    initialValue: {
+      username: '',
+      email: '',
+      password: '',
+      passwordConfirm: '',
+    },
+  });
+
+  useEffect(() => {
+    console.log(inputInfo);
+  }, [inputInfo]);
+
   return (
     <div className="space-y-3">
-      <InputGroup inputName="email" labelName="email" />
-      <InputGroup inputName="password" labelName="password" />
-      <InputGroup inputName="username" labelName="username" />
+      <InputGroup inputName="username" labelName="username" value={inputInfo.username} onChange={handleOnChange} />
+      <InputGroup inputName="email" labelName="email" value={inputInfo.email} onChange={handleOnChange} />
+      <InputGroup inputName="password" labelName="password" value={inputInfo.password} onChange={handleOnChange} />
       <div className="">
         <FilledButton
           onClick={() => {
-            onSubmit({ email: 'ttt@gmail.com', password: 'tttt', username: 'aaa' }).catch((err) => console.log(err));
+            onSubmit({ email: inputInfo.email, password: inputInfo.password, username: inputInfo.username }).catch(
+              (err) => console.log(err),
+            );
           }}
         >
           제출
