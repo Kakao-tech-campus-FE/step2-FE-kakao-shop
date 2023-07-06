51485b699796e206cc3b5cd1362c91056551b0c8
YANGSEOKWOO <didtjrdn030@naver.com>
2023-07-07 00:59:57 +0900

    feat:Login page and Routing
    Login Page API 연결 및 라우팅

--- assignment/src/App.js
+++ assignment/src/App.js
@@ -1,17 +1,25 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Sub from './subPage';
-// import Main from "./assignmentTest";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import RegisterPage from './pages/RegisterPage';
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/Homepage";
 function App() {
     return (
-    <>
-    {/* 과제 1을 위한 코드 */} 
-        {/* <BrowserRouter><Routes><Route path="/" element={<Main/>}/><Route path="/subPage" element={<Sub/>}/></Routes></BrowserRouter> */} 
-        <RegisterPage />;
-    </>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path ="/login" element = {<LoginPage/>}></Route>
+          <Route path ="/signup" element = {<RegisterPage/>}></Route>
+          <Route path ="/" element = {<HomePage/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
     );
 }
 
 export default App;
+
+// import Sub from './subPage';
+// import Main from "./assignmentTest";
+// {/* 과제 1을 위한 코드 */} 
+//         {/* <BrowserRouter><Routes><Route path="/" element={<Main/>}/><Route path="/subPage" element={<Sub/>}/></Routes></BrowserRouter> */} 
---
+++ assignment/src/pages/Homepage.jsx
@@ -0,0 +1,8 @@
+
+const HomePage = () => {
+    return(
+        <h1> 홈페이지 입니다.</h1>
+    );
+}
+
+export default HomePage;
