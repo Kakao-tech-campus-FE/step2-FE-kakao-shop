2496962b439979630a4d7853f719ecfcc3fd1901
baegyeong <beth0929@naver.com>
2023-07-07 04:03:47 +0900

    feat: 라우터 지정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import routes from "./routes.js";
+import RegisterPage from "./pages/RegisterPage";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path={routes.home} element={<HomePage />} />
+          <Route path={routes.register} element={<RegisterPage />} />
+          <Route path={routes.login} element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
