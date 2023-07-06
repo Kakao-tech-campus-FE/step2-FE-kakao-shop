a030875e240eda8726286d49509db0b3fffeb72d
H-sooyeon <wndqhr__@naver.com>
2023-07-06 19:43:50 +0900

    feat: 로그인 상태 확인

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,21 @@
 import MainShop from "../components/organisms/MainShop";
+import { getLocalStorage } from "../utils/localStorage";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { setEmail } from "../store/slices/userSlice";
 
 const MainPage = () => {
+    const dispatch = useDispatch();
+    const email = useSelector((state) => state.user.email);
+    useEffect(() => {
+        const isLogin = JSON.parse(getLocalStorage("email"));
+        if(isLogin) {
+            dispatch(setEmail({
+                email: isLogin.value,
+            }));
+        };
+    }, [email]);
+
     return(
         <MainShop />
     );
