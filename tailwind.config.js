ff9ff4a9c96028aabe09e69d08a83bb3f9d69915
blackhblee <blackhblee@gmail.com>
2023-07-06 14:48:34 +0900

    chore: Add yellow-kakao
    커스텀 컬러 yellow-kakao를 추가했습니다.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "yellow-kakao": "#ffe342",
+      },
+    },
   },
   plugins: [],
 };
