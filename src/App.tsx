76afa1fa66d8a4fa9a48701f8b0059a41494baa8
loopy-lim <loopy@jnu.ac.kr>
2023-07-06 13:53:41 +0900

    feat: add init jwt validator

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,26 @@ import { router } from "@/routers";
 import ErrorBoundary from "@components/common/ErrorBoundray.component";
 import { Provider } from "react-redux";
 import store from "@/store";
+import { useEffect } from "react";
+import { jwtDecode } from "@/functions/jwt";
+import { localStorage } from "@/functions/localstorage";
 
-const App = () => (
-  <ErrorBoundary>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </ErrorBoundary>
-);
+const App = () => {
+  useEffect(() => {
+    const jwtPayload = jwtDecode(localStorage.get("Authorization") ?? "");
+    if (jwtPayload && jwtPayload.exp < Date.now() / 1000) {
+      localStorage.remove("Authorization");
+      localStorage.remove("isLogin");
+    }
+  }, []);
+
+  return (
+    <ErrorBoundary>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </ErrorBoundary>
+  );
+};
 
 export default App;
