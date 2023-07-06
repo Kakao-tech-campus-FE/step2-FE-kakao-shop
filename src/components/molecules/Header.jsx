29dd5f08cc6cecddf150431581bc96690bbb19f4
H-sooyeon <wndqhr__@naver.com>
2023-07-06 19:41:31 +0900

    feat: 로그인, 로그아웃 상태 변화

--- src/components/molecules/Header.jsx
+++ src/components/molecules/Header.jsx
@@ -4,15 +4,15 @@ import * as Head from '../../styles/molecules/Header';
 import ImgLink from "../atoms/ImgLink";
 import LinkText from "../atoms/LinkText";
 
-const Header = () => {
+const Header = ({email, onClick}) => {
     return(
         <Head.Header>
             <Head.Container>
                 <ImgLink to="/" src={img} alt="톡 쇼핑하기" className="header-logo"/>
-                <Head.MenuUtil>
-                    <ImgLink to="/cart" src={cart} alt="장바구니" className="header-cart"/>
-                    <LinkText text="로그인" to="/login" className="header-login"/>
-                </Head.MenuUtil>
+                    <Head.MenuUtil>
+                        <ImgLink to="/cart" src={cart} alt="장바구니" className="header-cart"/>
+                        <LinkText text={email ? "로그아웃" : "로그인"} to={email ? '/' : "/login"} className="header-login" onClick={onClick}/>
+                    </Head.MenuUtil>
             </Head.Container>
         </Head.Header>
     );
