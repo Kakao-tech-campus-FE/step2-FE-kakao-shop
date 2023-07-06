970c337bab3263256df3f54e14faea763e44cc31
ba0061@naver.com <sweet.waiting0l@icloud.com>
2023-07-06 23:33:15 +0900

    feat: 메인로고 위치 변경
    position absolute에서,  static 으로 변경

--- kakao-shopping/src/pages/RegisterPage.jsx
+++ kakao-shopping/src/pages/RegisterPage.jsx
@@ -5,7 +5,7 @@ import MainLogo from "../components/molecules/MainLogo";
 const RegisterPage = () => {
   return (
     <Container className="h-screen border border-slate-500 border-solid rounded-md flex flex-col items-center justify-center">
-      <MainLogo className="absolute w-40 top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2"/>
+      <MainLogo className="w-40 mb-5"/>
       <Container className="w-fit border border-solid rounded-lg border-gray">
         <RegisterForm />
       </Container>
