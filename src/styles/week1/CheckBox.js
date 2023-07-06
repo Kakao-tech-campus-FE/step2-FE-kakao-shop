51589f2a3f0d30f987b5ae06987a78dd90f9d7ea
H-sooyeon <wndqhr__@naver.com>
2023-07-06 12:32:51 +0900

    chore: 1주차 과제 스타일 경로 변경

---
+++ src/styles/week1/CheckBox.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    border: 1px solid black;
+    width: 400px;
+    height: 200px;
+    margin: auto;
+    text-align: center;
+`
+
+export const ListContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-top: 2.5em;
+`
+
+export const Head = styled.div`
+    font-size: 1.3em;
+    text-align: center;
+    margin: 1em auto;
+`
+
+export const List = styled.span`
+    margin-left: 0.3em;
+`
+
+export const Desc = styled.div`
+    font-size: 1em;
+    text-align: center;
+    margin-top: 2.5em;
+`
