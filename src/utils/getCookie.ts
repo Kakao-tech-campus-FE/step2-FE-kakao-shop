17f4b4a24a7d644ffe76230efee4761e0f7e1d67
dongyeopCha <ckehdduq95@gmail.com>
2023-07-06 20:06:16 +0900

    feat: getCookie 유틸함수 추가

---
+++ src/utils/getCookie.ts
@@ -0,0 +1,4 @@
+export default function getCookie(name: string) {
+  let matches = document.cookie.match(new RegExp('(?:^|; )' + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + '=([^;]*)'));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
