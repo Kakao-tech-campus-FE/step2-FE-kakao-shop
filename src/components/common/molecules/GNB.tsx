6ec31a1330a61520088115e2ac6ac57a391a02d8
dongyeopCha <ckehdduq95@gmail.com>
2023-07-06 20:17:32 +0900

    feat: GNB 컴포넌트 추가

---
+++ src/components/common/molecules/GNB.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { styled } from 'styled-components';
+import colors from '../../../constants/colors';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../../../modules/auth';
+import type { RootState } from '../../..';
+import logoKakao from '../../../assets/logoKakao.png';
+import LinkBtn from '../LinkBtn';
+function GNB() {
+  const dispatch = useDispatch();
+  const { isLogin } = useSelector((state: RootState) => state.authReducer);
+  return (
+    <Wrap>
+      <img src={logoKakao} width={80} height={30} alt="카카오 쇼핑 로고" />
+      {isLogin ? (
+        <LogoutBtn type="button" onClick={() => dispatch(logout())}>
+          로그아웃
+        </LogoutBtn>
+      ) : (
+        <LinkBtn to="/login">로그인</LinkBtn>
+      )}
+    </Wrap>
+  );
+}
+
+export default GNB;
+
+const Wrap = styled.div`
+  position: fixed;
+  display: flex;
+  justify-content: space-between;
+  box-sizing: border-box;
+  padding: 30px;
+  width: 100%;
+  border-bottom: 1px solid ${colors.gray};
+`;
+
+const LogoutBtn = styled.button`
+  background-color: inherit;
+  border: none;
+`;
