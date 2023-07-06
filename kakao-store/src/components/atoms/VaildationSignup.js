a533c9181a5d330743963dbf83b7004ea97f7a50
jihopark <jihodayo@gmail.com>
2023-07-06 13:41:42 +0900

    feat: Add error box

--- kakao-store/src/components/atoms/VaildationSignup.js
+++ kakao-store/src/components/atoms/VaildationSignup.js
@@ -1,58 +1,33 @@
-export const validateForm = (value) => {
-  let error = false;
-
-  if (!testUsername(value)) {
-    error = true;
-  }
-
-  if (!testPassword(value)) {
-    error = true;
-  }
-
-  if (!testPasswordConfirm(value)) {
-    error = true;
-  }
-
-  if (!testEmail(value)) {
-    error = true;
-  }
-
-  return error;
-};
-
-const testUsername = (value) => {
-  if (value.username.length < 2) {
-    console.log("이름은 2글자 이상이어야 합니다.");
-    return false;
-  } else {
-    return true;
-  }
-};
-
 const testPassword = (value) => {
   const passwordRegex =
     /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()])[a-zA-Z\d!@#$%^&*()]+$/;
 
   if (value.password.length < 8) {
-    console.log("비밀번호는 8글자 이상이어야 합니다.");
-    return false;
+    return "비밀번호는 8글자 이상이어야 합니다.";
   } else if (!passwordRegex.test(value.password)) {
-    console.log("영문, 숫자, 특수문자가 포함되어야 합니다. ");
-    return false;
+    return "영문, 숫자, 특수문자가 포함되어야 합니다. ";
   } else if (value.password.includes(" ")) {
-    console.log("비밀번호의 공백이 존재합니다.");
-    return false;
+    return "비밀번호의 공백이 존재합니다.";
   } else {
-    return true;
+    return false;
+  }
+};
+
+const testUsername = (value) => {
+  if (value.username.length < 2) {
+    return "이름은 2글자 이상이어야 합니다.";
+  } else {
+    return false;
   }
 };
 
 const testPasswordConfirm = (value) => {
-  if (value.password !== value.passwordConfirm) {
-    console.log("비밀번호가 일치하지 않습니다.");
-    return false;
+  if (value.passwordConfirm === "") {
+    return "비밀번호 확인을 입력해주세요.";
+  } else if (value.password !== value.passwordConfirm) {
+    return "비밀번호가 일치하지 않습니다.";
   } else {
-    return true;
+    return false;
   }
 };
 
@@ -61,9 +36,36 @@ const testEmail = (value) => {
     /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i; // Regular expression for email validation
 
   if (emailRegex.test(value.email)) {
-    return true;
-  } else {
-    console.log("이메일 형식이 올바르지 않습니다.");
     return false;
+  } else if (value.email === "") {
+    return "이메일을 입력해주세요.";
+  } else {
+    return "이메일 형식이 올바르지 않습니다.";
+  }
+};
+
+export const validateForm = (value) => {
+  const errors = [];
+
+  if (testEmail(value)) {
+    errors.push(testEmail(value));
+  }
+
+  if (testUsername(value)) {
+    errors.push(testUsername(value));
+  }
+
+  if (testPassword(value)) {
+    errors.push(testPassword(value));
+  }
+  
+  if (testPasswordConfirm(value)) {
+    errors.push(testPasswordConfirm(value));
+  }
+
+  if (errors.length === 0) {
+    return false;
+  } else {
+    return errors;
   }
 };
