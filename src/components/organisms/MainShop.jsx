c5a13adf93ed9abc38c357cf27a8b0a05932991d
H-sooyeon <wndqhr__@naver.com>
2023-07-06 19:42:22 +0900

    feat: 로그인, 로그아웃 클릭 이벤트

--- src/components/organisms/MainShop.jsx
+++ src/components/organisms/MainShop.jsx
@@ -1,10 +1,26 @@
 //import Container from "../atoms/Container";
 import Header from "../molecules/Header";
+import { useDispatch, useSelector } from "react-redux";
+import { setEmail } from "../../store/slices/userSlice";
+import { removeLocalStorageItem } from "../../utils/localStorage";
 
 const MainShop = () => {
+    const dispatch = useDispatch();
+    const email = useSelector((state) => state.user.email);
+
+    const onClick = () => {
+        if(email) {
+            dispatch(setEmail({
+                email: null,
+            }));
+            removeLocalStorageItem("email");
+            window.location.reload();
+        };
+    };
+
     return(
         <>
-            <Header />
+            <Header email={email} onClick={onClick}/>
         </>
     );
 };
