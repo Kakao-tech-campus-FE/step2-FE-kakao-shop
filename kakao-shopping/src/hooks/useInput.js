18df5d3561eba41174bbdfb0819f3cb29e3d4147
ba0061@naver.com <sweet.waiting0l@icloud.com>
2023-07-06 16:04:04 +0900

    feat!: let을 사용하지 않도록 변경

--- kakao-shopping/src/hooks/useInput.js
+++ kakao-shopping/src/hooks/useInput.js
@@ -6,14 +6,14 @@ const useInput = (initialValue) => {
   const handleOnChange = (e) => {
     const { id, value } = e.target;
     const idString = id + "IsValid";
-    let validateCheck = false;
-    if(id === 'email') {
-      validateCheck = /^[a-zA-Z0-9]+@[a-zA-Z]+\.com$/.test(value);
-    } else if(id === 'password') {
-      validateCheck = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[~!@##$%^&*])[a-zA-Z0-9~!@##$%^&*]{8,20}$/.test(value);
-    } else if(id === 'username') {
-      validateCheck = /^[a-zA-Z0-9]{4,12}$/.test(value);
-    }
+
+    const validateCheck = 
+    id === 'email' ?  // 이메일이면
+    /^[a-zA-Z0-9]+@[a-zA-Z]+\.com$/.test(value) : // 이메일 체크 
+    id === 'password' ?  // 비밀번호면
+    /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[~!@##$%^&*])[a-zA-Z0-9~!@##$%^&*]{8,20}$/.test(value) : // 비밀번호 체크
+    /^[a-zA-Z0-9]{4,12}$/.test(value); // 둘 다 아니면 이름임. 이름 체크
+    
     setValues((prev) => ({ ...prev, [id]: value, [idString]: validateCheck }));
   };
 
