b42f55002a97a8f873041e1f18a2ef29ec4c3fe6
lcqff <yeonnu82g@gmail.com>
2023-07-07 10:45:44 +0900

    feat: 유효성 검사 - 비밀번호에 숫자,영문,특수문자가 포함돼 있는가 - 이메일이 이메일 형식을 가지고 있는가 - 유저네임이 비어있는 값이 아닌가

--- src/components/organisms/RegisterForm.tsx
+++ src/components/organisms/RegisterForm.tsx
@@ -12,6 +12,9 @@ const RegisterForm = ({ onSubmit }: RegisterFromProps) => {
   const [usernameHT, setUsernameHT] = useState('ㅤ');
   const [emailHT, setEmailHT] = useState('ㅤ');
   const [passwordHT, setPasswordHT] = useState('ㅤ');
+  const passwordPattern = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()])[A-Za-z\d!@#$%^&*()]{8,20}$/;
+  const emailFormPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const passwordLenthPattern = /^[^].{8,25}$/;
   const { value: inputInfo, handleOnChange } = useInput({
     initialValue: {
       username: '',
@@ -20,6 +23,30 @@ const RegisterForm = ({ onSubmit }: RegisterFromProps) => {
       passwordConfirm: '',
     },
   });
+
+  const validationCheck = () => {
+    // 동일한 이메일 확인
+
+    if (inputInfo.username.length <= 0) {
+      setUsernameHT('필수 항목입니다.');
+    } else {
+      setUsernameHT('ㅤ');
+    }
+
+    if (!emailFormPattern.test(inputInfo.email)) {
+      setEmailHT('이메일 형식을 따라야 합니다.');
+    } else {
+      setEmailHT('ㅤ');
+    }
+
+    if (!passwordLenthPattern.test(inputInfo.password)) {
+      setPasswordHT('8에서 20자 이내여야 합니다.');
+    } else if (!passwordPattern.test(inputInfo.password)) {
+      setPasswordHT('영문, 숫자, 특수문자가 포함되어야 합니다.');
+    } else {
+      setPasswordHT('ㅤ');
+    }
+  };
 
   return (
     <div className="space-y-3">
@@ -50,6 +77,7 @@ const RegisterForm = ({ onSubmit }: RegisterFromProps) => {
             onSubmit({ email: inputInfo.email, password: inputInfo.password, username: inputInfo.username }).catch(
               (err) => console.log(err),
             );
+            validationCheck();
           }}
         >
           제출
