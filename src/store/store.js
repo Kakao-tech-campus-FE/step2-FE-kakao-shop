c8e127d48cb5df754893f05809e9549a7b86f79f
unknown <jsh1147@naver.com>
2023-07-07 16:31:16 +0900

    feat: 새로고침 로그인 상태 유지 구현
    페이지 unload가 발생하기 전에 login state를 localstorage에 저장하고,
    페이지 load 시localstorage를 확인하여 state가 있으면 가져오도록 구현함

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
 
+const savedLogInState = localStorage.getItem("logInState");
+const preloadedState = savedLogInState
+  ? { user: JSON.parse(savedLogInState) }
+  : {};
+localStorage.removeItem("logInState");
+
 export const store = configureStore({
   reducer: {
     user: userReducer, // email용
   },
+  preloadedState,
+});
+
+window.addEventListener("beforeunload", () => {
+  const logInState = store.getState().user;
+  localStorage.setItem("logInState", JSON.stringify(logInState));
 });
