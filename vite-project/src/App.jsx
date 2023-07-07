f8c5b1d6dbf1979b6431cb0343502b01029bed0e
Yewon Lee <L2678920@naver.com>
2023-07-07 23:29:59 +0900

    week2_1

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,51 +1,62 @@
-import React, { useState } from "react";
-import Toggle from "./components/Toggle";
-import Toast from "./components/Toast";
-import Radio from "./components/Radio";
-import CheckList from "./components/CheckList";
-import BreadCrumb from "./components/BreadCrumb";
-import Carousel from './components/Carousel';
+// import React, { useState } from "react";
+// import Toggle from "./components_w1/Toggle";
+// import Toast from "./components_w1/Toast";
+// import Radio from "./components_w1/Radio";
+// import CheckList from "./components_w1/CheckList";
+// import BreadCrumb from "./components_w1/BreadCrumb";
+// import Carousel from './components_w1/Carousel';
+
+// function App() {
+//   const [isToggled, setIsToggled] = useState(false);
+//   const handleShowToast = () => {
+//   };
+//   const paths = ['홈', '페이지', '서브페이지'];
+//   const images = [
+//     'carouselItem1.jpeg',
+//     'carouselItem2.jpeg',
+//     'carouselItem3.jpeg',
+//   ];
+  
+//   return (
+//     <>
+//     <div className="App">
+//       <Toggle isToggled={isToggled} onToggle={() => setIsToggled(!isToggled)}/>
+//     </div>
+//     <br />
+
+//       <Toast />
+//     <br />
+
+//       <Radio />
+//     <br />
+
+//       <CheckList />
+//     <br />
+
+//     <div className="App">
+//       <BreadCrumb paths={paths} />
+//     </div>
+//     <br />
+
+//     <div className="App">
+//       <Carousel images={images} />
+//     </div>
+
+//     </>
+//   )
+// }
+
+
+
+// export default App
+
+// week1 내용은 우선 주석 처리해두었습니다.
+
+
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
-  const [isToggled, setIsToggled] = useState(false);
-  const handleShowToast = () => {
-  };
-  const paths = ['홈', '페이지', '서브페이지'];
-  const images = [
-    'carouselItem1.jpeg',
-    'carouselItem2.jpeg',
-    'carouselItem3.jpeg',
-  ];
-  
-  return (
-    <>
-    <div className="App">
-      <Toggle isToggled={isToggled} onToggle={() => setIsToggled(!isToggled)}/>
-    </div>
-    <br />
-
-      <Toast />
-    <br />
-
-      <Radio />
-    <br />
-
-      <Checklist />
-    <br />
-
-    <div className="App">
-      <BreadCrumb paths={paths} />
-    </div>
-    <br />
-
-    <div className="App">
-      <Carousel images={images} />
-    </div>
-
-    </>
-  )
+  return <RegisterPage />;
 }
-
-
-
-export default App
+ 
+export default App;
