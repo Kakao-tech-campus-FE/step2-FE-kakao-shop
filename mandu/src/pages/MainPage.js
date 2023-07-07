6c8c295d2805f929e218c9b979763ef4404bf580
charmeee <mandugohigh@gmail.com>
2023-07-07 19:42:31 +0900

    feat: 처음 렌더링시 로그인 정보 불러와서 저장

--- mandu/src/pages/MainPage.js
+++ mandu/src/pages/MainPage.js
@@ -1,13 +1,37 @@
 import {useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
+import cookie from "react-cookies";
+import {useSelector, useDispatch} from 'react-redux';
+import {setId} from "../redux/userSlice";
 
 const MainPage = () => {
     const navigate = useNavigate();
+    const userId = useSelector((state) => state.user.id);
+    const dispatch = useDispatch();
+    const checkValidUser = () => {
+        const cookieAccessToken = cookie.load('access_token');
+        const cookieUserId = cookie.load('user_id');
+        if (cookieAccessToken && cookieUserId) {
+            dispatch(setId(cookieUserId));
+        }
+    }
+    useEffect(() => {
+        checkValidUser();
+    }, []);
+
 
     return (
         <div>
-            <h1>Main Page</h1>
-            <button className="bg-emerald-400 m-2 p-2 rounded" onClick={()=> navigate('/login')}>Go to Login Page</button>
-            <button className="bg-emerald-400 m-2 p-2 rounded" onClick={()=> navigate('/signup')}>Go to Sign Up Page</button>
+            <h1 className="m-2 p-2 text-lg">Main Page</h1>
+            {userId
+                ? <div></div> : (<>
+                    <button className="bg-emerald-400 m-2 p-2 rounded" onClick={() => navigate('/login')}>Go to Login
+                        Page
+                    </button>
+                    <button className="bg-emerald-400 m-2 p-2 rounded" onClick={() => navigate('/signup')}>Go to Sign Up
+                        Page
+                    </button>
+                </>)}
         </div>
     );
 }
