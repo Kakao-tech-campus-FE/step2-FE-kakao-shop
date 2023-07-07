5b49c5cca152d32d22fb5d299a58de7e68acde65
minjuko <200778@jnu.ac.kr>
2023-07-07 12:14:53 +0900

    Label.jsx 수정

--- src/components/atoms/Label.jsx
+++ src/components/atoms/Label.jsx
@@ -1,3 +1,8 @@
+// src/components/atoms/Label.jsx
+// Label component
+// htmlFor: 라벨과 연결된 입력 필드의 ID
+// children: 라벨에 표시될 내용
+// className: 추가 CSS 클래스 이름
 const Label = ({ htmlFor, children, className}) => {
     return (
         <label htmlFor={htmlFor} className={className} >
