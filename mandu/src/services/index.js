663a1e139ede1f0a71d43f7d2b7b597d227884e6
charmeee <mandugohigh@gmail.com>
2023-07-07 19:11:07 +0900

    feat: 모든 응답값을 (에러포함)을 ResponseType객체로 받음

--- mandu/src/services/index.js
+++ mandu/src/services/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import cookie from 'react-cookies';
+import {ResponseType} from "./type";
 
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
@@ -17,6 +18,42 @@ api.interceptors.request.use(
             config.headers['authorization'] = token;
         }
         return config;
+    }
+);
+api.interceptors.response.use(
+    (response) => {
+        console.log("success", response);
+        return Promise.resolve(new ResponseType(response.data));
+    },
+    (error) => {
+        console.log("error", error)
+        let message = error.response?.data?.error?.message;
+        let status = error.response?.status;
+        if (!message) {
+            if (status === 401) {
+                message = "다시 로그인 해주세요"
+                cookie.remove('access_token', {path: '/'});
+                cookie.remove('user_id', {path: '/'});
+            }
+            if (status === 500) {
+                message = "서버에 문제가 발생했습니다.";
+            }
+            if (status === 501) {
+                message = "잘못된 접근입니다.";
+            }
+        }
+
+        return Promise.resolve(
+            new ResponseType({
+                success: false,
+                response: null,
+                error: {
+                    message,
+                    status,
+                },
+            })
+        );
+
     }
 );
 
