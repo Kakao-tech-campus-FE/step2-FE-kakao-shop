78d54e9df3e0a7ff7873c07d12070ee1d1b1d033
j8won <joowoni99@gmail.com>
2023-07-07 23:41:50 +0900

    feat: organisms 컴포넌트 GlobalNavBar.jsx 생성

---
+++ src/components/organisms/global-navigation-bar/GlobalNavBar.stories.js
@@ -0,0 +1,11 @@
+import GlobalNavBar from "./GlobalNavBar.jsx";
+import { withRouter } from "storybook-addon-react-router-v6";
+
+export default {
+  title: "organisms/Global Navigation Bar",
+  tags: ["autodocs"],
+  component: GlobalNavBar,
+  decorators: [withRouter],
+};
+
+export const Default = {};
