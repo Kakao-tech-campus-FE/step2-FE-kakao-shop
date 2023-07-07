ddd1b41f21c10fbd629d80a199945c0967cac2b0
MINJOO-KIM <gkminju2000@gmail.com>
2023-07-07 23:47:38 +0900

    feat: 과제1 - Organisms Component 구현 - LoginForm - RegisterForm

---
+++ kakao-shopping/src/components/organisms/RegisterForm.jsx
@@ -0,0 +1,78 @@
+import Container from "../atoms/Container";
+import InputGroup from "../atoms/InputGroup";
+import Button from "../atoms/Button";
+import useInput from "../../hooks/useInput";
+
+const RegisterForm = () => {
+  const [value, handleOnchange]= useInput({
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+    onChange={handleOnChange},
+  });
+  
+  const [form, setForm] = useState
+  const { value, handleOnChange } = useInput({
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  });
+
+  useEffect(() => {
+    console.log(value.username);
+  }, [value.username]);
+
+  return (
+    <Container>
+      <InputGroup
+        id="username"
+        type="text"
+        name="username"
+        placeholder="사용자 이름"
+        label="이름"
+        value={value.username}
+        onChange={handleOnChange}
+      />
+      <InputGroup
+        id="email"
+        type="email"
+        name="email"
+        placeholder="이메일(아이디)"
+        label="이메일"
+        value={value.username}
+        onChange={handleOnChange}
+      />
+      <InputGroup
+        id="password"
+        type="password"
+        name= "password"
+        placeholder="비밀번호"
+        label="비밀번호"
+        onChange={handleOnChange}
+      />
+      <InputGroup
+        id="passwordConfirm"
+        type="password"
+        name="passwordConfirm"
+        placeholder="비밀번호 확인"
+        label="비밀번호 확인"
+        onChange={handleOnChange}
+      />
+      <Button
+        onClick={() => {
+          register({
+            email: value.email,
+            password: value.password,
+            username: value.username,
+          });
+        }}
+      >
+        회원가입
+      </Button>
+    </Container>
+  );
+};
+
+export default RegisterForm;
