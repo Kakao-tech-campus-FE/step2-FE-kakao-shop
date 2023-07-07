34f535545022dc3fe304f028a9971c0aa198f874
baegyeong <beth0929@naver.com>
2023-07-07 23:35:13 +0900

    feat: redux 상태관리 모듈 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import routes from "./routes.js";
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import { Provider } from "react-redux";
+import store from "./index.js";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path={routes.home} element={<HomePage />} />
-          <Route path={routes.register} element={<RegisterPage />} />
-          <Route path={routes.login} element={<LoginPage />} />
-        </Routes>
+        <Provider store={store}>
+          <Routes>
+            <Route path={routes.home} element={<HomePage />} />
+            <Route path={routes.register} element={<RegisterPage />} />
+            <Route path={routes.login} element={<LoginPage />} />
+          </Routes>
+        </Provider>
       </BrowserRouter>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { createStore } from "@reduxjs/toolkit";
+
+let loginState = "false";
+
+function reducer(state = loginState, action) {
+  if (action.type === "changeState") {
+    return !state;
+  }
+  return state;
+}
+
+const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -9,3 +21,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+export default store;
