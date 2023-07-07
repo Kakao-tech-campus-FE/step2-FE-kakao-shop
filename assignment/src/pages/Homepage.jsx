1adb01409cf8da1a43f6307760712636dc69e87a
YANGSEOKWOO <didtjrdn030@naver.com>
2023-07-07 15:06:59 +0900

    feat: Router구성
    페이지 연결될 수 있도록 수정

--- assignment/src/pages/Homepage.jsx
+++ assignment/src/pages/Homepage.jsx
@@ -1,8 +1,21 @@
+import { Link } from "react-router-dom";
+import '../styles/pages/Homepage.css';
+
 
 const HomePage = () => {
-    return(
-        <h1> 홈페이지 입니다.</h1>
-    );
-}
+  return (
+    <div className="link-container">
+      <div className="logo-wrapper">
+        <Link to="/">
+          <img src={'logoKaKao.png'} alt="로고" className="logo" />
+        </Link>
+      </div>
+      <div className="link-wrapper">
+        <Link to="/signup">회원가입</Link>
+        <Link to="/login">로그인</Link>
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
--- assignment/src/pages/LoginPage.jsx
+++ assignment/src/pages/LoginPage.jsx
@@ -2,7 +2,9 @@ import LoginForm from "../components/organisms/LoginForm";
 
 const LoginPage = () => {
     return(
-        <LoginForm/>
+        <>
+            <LoginForm/>
+        </>
     );
 }
 
