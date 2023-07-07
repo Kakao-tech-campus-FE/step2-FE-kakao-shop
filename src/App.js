e916433662751c0400a906b502a7dc98ebff734f
kimjinwoo <rlawlsdn177@naver.com>
2023-07-07 17:59:06 +0900

    app.js/index.js 변경

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import Breadcrumb from "./components/Breadcrumbs";
-import Carousel from "./components/Carousel";
-import Checklist from "./components/Checklist";
-import Radio from "./components/Radio";
-import Toast from "./components/Toast";
-import Toggle from "./components/Toggle";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import HomePage from "./pages/HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Breadcrumb />
-      <Carousel />
-      <Checklist />
-      <Radio />
-      <Toast />
-      <Toggle />
+    <div className="App">
+      <BrowserRouter>
+        {/*단독 레이아웃*/}
+        <Routes>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/signup" element={<RegisterPage />}></Route>
+          {/*공통 레이아웃*/}
+          <Route path="/" element={<HomePage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./styles/index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store";
+import { CookiesProvider } from "react-cookie";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <CookiesProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </CookiesProvider>
   </React.StrictMode>
 );
