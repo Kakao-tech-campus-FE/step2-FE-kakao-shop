a5d233caf90663549411f684dedf0f442b6ed486
blackhblee <blackhblee@gmail.com>
2023-07-07 22:37:36 +0900

    feat: Add refresh
    로고 클릭 시 새로고침 기능 추가했습니다.

--- src/components/atoms/Logo.jsx
+++ src/components/atoms/Logo.jsx
@@ -8,7 +8,13 @@ import { Link } from "react-router-dom";
  */
 const Logo = ({ to = "/", className = "" }) => {
   return (
-    <Link to={to} className={`logo ${className}`}>
+    <Link
+      onClick={() => {
+        window.location.replace("/");
+      }}
+      to={to}
+      className={`logo ${className}`}
+    >
       <img
         src="https://st.kakaocdn.net/commerce_ui/front-talkstore/real/20230705/152013/assets/images/pc/pc_logo.png"
         alt="카카오톡 쇼핑하기"
