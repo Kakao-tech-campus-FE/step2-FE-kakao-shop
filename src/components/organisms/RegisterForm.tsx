e4235277d87280cd189493880adf0f92841e49d5
lcqff <yeonnu82g@gmail.com>
2023-07-07 10:58:59 +0900

    feat: password confirm input 추가

--- src/components/organisms/RegisterForm.tsx
+++ src/components/organisms/RegisterForm.tsx
@@ -12,6 +12,7 @@ const RegisterForm = ({ onSubmit }: RegisterFromProps) => {
   const [usernameHT, setUsernameHT] = useState('ㅤ');
   const [emailHT, setEmailHT] = useState('ㅤ');
   const [passwordHT, setPasswordHT] = useState('ㅤ');
+  const [passwordConfirmHT, setPasswordConfirmHT] = useState('ㅤ');
   const passwordPattern = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()])[A-Za-z\d!@#$%^&*()]{8,20}$/;
   const emailFormPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const passwordLenthPattern = /^[^].{8,25}$/;
@@ -26,7 +27,6 @@ const RegisterForm = ({ onSubmit }: RegisterFromProps) => {
 
   const validationCheck = () => {
     // 동일한 이메일 확인
-
     if (inputInfo.username.length <= 0) {
       setUsernameHT('필수 항목입니다.');
     } else {
@@ -39,12 +39,10 @@ const RegisterForm = ({ onSubmit }: RegisterFromProps) => {
       setEmailHT('ㅤ');
     }
 
-    if (!passwordLenthPattern.test(inputInfo.password)) {
-      setPasswordHT('8에서 20자 이내여야 합니다.');
-    } else if (!passwordPattern.test(inputInfo.password)) {
-      setPasswordHT('영문, 숫자, 특수문자가 포함되어야 합니다.');
+    if (inputInfo.password !== inputInfo.passwordConfirm) {
+      setPasswordConfirmHT('비밀번호가 동일하지 않습니다.');
     } else {
-      setPasswordHT('ㅤ');
+      setPasswordConfirmHT('ㅤ');
     }
   };
 
@@ -71,13 +69,17 @@ const RegisterForm = ({ onSubmit }: RegisterFromProps) => {
         helperText={passwordHT}
         onChange={handleOnChange}
       />
+      <InputGroup
+        inputName="passwordConfirm"
+        labelName="비밀번호 확인"
+        value={inputInfo.passwordConfirm}
+        helperText={passwordConfirmHT}
+        onChange={handleOnChange}
+      />
       <div className="">
         <FilledButton
           onClick={() => {
-            onSubmit({ email: inputInfo.email, password: inputInfo.password, username: inputInfo.username }).catch(
-              (err) => console.log(err),
-            );
-            validationCheck();
+            onSubmit({ email: inputInfo.email, password: inputInfo.password, username: inputInfo.username });
           }}
         >
           제출
