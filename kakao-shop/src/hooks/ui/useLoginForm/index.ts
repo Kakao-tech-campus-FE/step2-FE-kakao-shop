bfd2d8733419cfc85669805efac09118c095a6e7
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-07-07 21:34:10 +0900

    feat: 로그인 요청

--- kakao-shop/src/hooks/ui/useLoginForm/index.ts
+++ kakao-shop/src/hooks/ui/useLoginForm/index.ts
@@ -1,8 +1,13 @@
+import { signInRequest } from '@store/Login/reducers';
 import { FormEventHandler } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 import useInput from '@hooks/@common/useInput';
 
 const useLoginForm = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [email, onChangeEmail] = useInput('');
   const [password, onChangePassword] = useInput('');
   const [errorMessage, , setErrorMessage] = useInput('');
@@ -14,6 +19,8 @@ const useLoginForm = () => {
       setErrorMessage('계정을 올바르게 입력해주세요.');
       return;
     }
+
+    dispatch(signInRequest({ email, password, navigate, setErrorMessage }));
   };
 
   return {
