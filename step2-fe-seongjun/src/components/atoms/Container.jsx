be52e3f33c3768a72027e690fed2a208db0af5e4
ketru44@naver.com <ketru44@naver.com>
2023-07-07 19:41:26 +0900

    전남대FE_성준_2주차

--- step2-fe-seongjun/src/components/atoms/Container.jsx
+++ step2-fe-seongjun/src/components/atoms/Container.jsx
@@ -1,5 +1,5 @@
 import '../../styles/atoms/Container.css'
-
+/* children: 감쌀 자식 요소, className: css 클래스 */ 
 const Container = ({children, className=""}) => {
   return <div className= {`container ${className}`}>{children}</div>;
 }
