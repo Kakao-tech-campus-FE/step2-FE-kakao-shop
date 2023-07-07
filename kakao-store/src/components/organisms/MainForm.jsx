8b2bcad5e2483aab49f63d7c98f3f5bc74e36d85
jihopark <jihodayo@gmail.com>
2023-07-07 10:59:25 +0900

    feat: Add Loginbox

--- kakao-store/src/components/organisms/MainForm.jsx
+++ kakao-store/src/components/organisms/MainForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { logout } from "../../store/slices/userSlice";
+import Loginbox from "../atoms/Loginbox";
 
 const MainForm = () => {
   const email = useSelector((state) => state.user.email);
@@ -13,13 +14,7 @@ const MainForm = () => {
     dispatch(logout());
   };
 
-  return (
-    <div>
-      <div>Email: {email}</div>
-      <div>Password: {password}</div>
-      <button onClick={handleLogout}>Logout</button>
-    </div>
-  );
+  return <Loginbox email={email} password={password} onClick={handleLogout} />;
 };
 
 export default MainForm;
