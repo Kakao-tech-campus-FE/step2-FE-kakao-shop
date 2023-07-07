b9ff7845e030436528bf18e0f2a37c2dd2625766
publdaze <publdaze@naver.com>
2023-07-07 20:48:49 +0900

    feat: 새로고침 시에도 상태 유지

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import axios from 'axios';
 
 import './tailwind.css';
 import Router from './Router';
-import store from './store';
+import { store, persistor } from './store';
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 axios.defaults.timeout = 1000;
@@ -14,7 +15,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router />
+      <PersistGate loading={null} persistor={persistor}>
+        <Router />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from './slices/userSlices';
 
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+const reducers = combineReducers({
+  user: userReducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (defaultMiddleware) =>
+    defaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 
-export default store;
+export { store, persistor };
