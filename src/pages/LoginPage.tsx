ff8fc5cf3fdeffdca4f6326e6edbc7ae8279c532
publdaze <publdaze@naver.com>
2023-07-07 23:06:31 +0900

    feat: 회원가입 이동 링크 추가

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import SubContainer from '../components/atoms/SubContainer';
 import KaKaoLogo from '../components/atoms/KaKaoLogo';
 import LoginForm from '../components/organisms/LoginForm';
@@ -8,6 +9,9 @@ const LoginPage = () => {
     <SubContainer>
       <KaKaoLogo />
       <LoginForm />
+      <Link to='/register' className='text-sm text-gray-600'>
+        회원가입
+      </Link>
     </SubContainer>
   );
 };
