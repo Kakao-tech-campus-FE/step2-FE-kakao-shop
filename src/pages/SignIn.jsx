18f8ba3728872ad9e9428eb62b5606f8fa0871bf
j8won <joowoni99@gmail.com>
2023-07-07 21:55:27 +0900

    feat: SignIn.jsx 페이지 생성

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,10 +1,67 @@
-import LabeledInput from "../components/molecules/labled-input/LabeledInput.jsx";
+import { useNavigate } from "react-router-dom";
 
+import Form from "../components/organisms/form/Form.jsx";
+import FORM_INFO from "../constants/FORM_INFO.js";
+import FORM_DEFAULT from "../constants/FORM_DEFAULT.js";
+import AuthTemplate from "../components/templates/auth-template/AuthTemplate.jsx";
+import Button from "../components/atoms/button/Button.jsx";
+import styled from "styled-components";
+import routes from "../constants/routes.js";
+import authAPI from "../api/authAPI.js";
+
+const Styled = {
+  SignUsBtnContainer: styled.div`
+    display: flex;
+    justify-content: right;
+    align-items: center;
+
+    .sign-us-btn {
+      text-align: right;
+      font-size: 0.85rem;
+      color: ${({ theme }) => theme.color.black};
+      cursor: pointer;
+      &:hover {
+        text-decoration: underline;
+        text-underline-position: under;
+      }
+    }
+  `,
+};
 function SignIn() {
+  const navigate = useNavigate();
+
+  const onSignInSubmit = async (data) => {
+    try {
+      const { response } = await authAPI.signIn(data);
+      localStorage.setItem("accessToken", response.token);
+      localStorage.setItem("accessTokenDate", new Date().toString());
+      navigate(routes.home);
+    } catch (e) {
+      alert(e.error.message);
+    }
+  };
   return (
-    <div>
-      <LabeledInput errorMsg="error" />
-    </div>
+    <AuthTemplate title="로그인">
+      <Form
+        style={{ width: "100%", maxWidth: "440px" }}
+        defaultValues={FORM_DEFAULT.SIGN_IN}
+        inputInformations={FORM_INFO.SIGN_IN}
+        onError={(e) => console.log(e)}
+        onSubmit={onSignInSubmit}
+      >
+        <Button
+          type="submit"
+          style={{ width: "100%", margin: "0.5rem 0 1rem" }}
+        >
+          로그인
+        </Button>
+        <Styled.SignUsBtnContainer>
+          <span className="sign-us-btn" onClick={() => navigate(routes.signUp)}>
+            회원가입
+          </span>
+        </Styled.SignUsBtnContainer>
+      </Form>
+    </AuthTemplate>
   );
 }
 
--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -18,6 +18,7 @@ const GlobalStyle = createGlobalStyle`
     max-width: 100%;
     min-height: calc(var(--vh, 1vh) * 100);
     overflow-x: hidden;
+    background-color: white;
   }
   
   button {
