b84b81b18de9f4fcfab5b473e6de943c64651f5f
lcqff <yeonnu82g@gmail.com>
2023-07-07 12:08:51 +0900

    feat: 로그인 유효성 검사 추가

--- src/components/organisms/LoginForm.tsx
+++ src/components/organisms/LoginForm.tsx
@@ -1,7 +1,8 @@
 import FilledButton from '@components/atoms/FilledButton';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { AxiosResponse } from 'axios';
 import useInput from '@hooks/useInput';
+import { checkEmail, checkPassword } from '@utils/validationUtils';
 import InputGroup from '../molecules/InputGroup';
 
 interface LoginFromProps {
@@ -9,6 +10,8 @@ interface LoginFromProps {
 }
 
 const LoginForm = ({ onSubmit }: LoginFromProps) => {
+  const [emailHT, setEmailHT] = useState('');
+  const [passwordHT, setPasswordHT] = useState('');
   const { value: inputInfo, handleOnChange } = useInput({
     initialValue: {
       username: '',
@@ -18,14 +21,30 @@ const LoginForm = ({ onSubmit }: LoginFromProps) => {
     },
   });
 
+  const validationCheck = () => {
+    setEmailHT(checkEmail(inputInfo.email));
+    setPasswordHT(checkPassword(inputInfo.password));
+  };
+
+  useEffect(() => {
+    validationCheck();
+  }, [inputInfo]);
+
   return (
     <div className="flex flex-col space-y-2">
-      <InputGroup inputName="email" labelName="이메일" value={inputInfo.email} onChange={handleOnChange} />
+      <InputGroup
+        inputName="email"
+        labelName="이메일"
+        value={inputInfo.email}
+        helperText={emailHT}
+        onChange={handleOnChange}
+      />
       <InputGroup
         inputName="password"
         labelName="비밀번호"
         inputType="password"
         value={inputInfo.password}
+        helperText={passwordHT}
         onChange={handleOnChange}
       />
       <FilledButton
