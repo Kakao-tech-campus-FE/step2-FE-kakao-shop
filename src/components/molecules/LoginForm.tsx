63b9b6947d28b574684756fe4b5c7783188dc0ce
lcqff <yeonnu82g@gmail.com>
2023-07-07 11:20:02 +0900

    feat: login 페이지 커스텀 훅 적용

--- src/components/molecules/LoginForm.tsx
+++ src/components/molecules/LoginForm.tsx
@@ -2,19 +2,29 @@ import FilledButton from '@components/atoms/FilledButton';
 import Input from '@components/atoms/Input';
 import React from 'react';
 import { AxiosResponse } from 'axios';
+import useInput from '@hooks/useInput';
 
 interface LoginFromProps {
   onSubmit: (data: { email: string; password: string }) => Promise<AxiosResponse>;
 }
 
+const { value: inputInfo, handleOnChange } = useInput({
+  initialValue: {
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  },
+});
+
 const LoginForm = ({ onSubmit }: LoginFromProps) => {
   return (
     <div className="flex flex-col space-y-2">
-      <Input placeholder="아이디" />
-      <Input placeholder="비밀번호" type="password" />
+      <Input placeholder="아이디" value={inputInfo.email} onChange={handleOnChange} />
+      <Input placeholder="비밀번호" type="password" value={inputInfo.password} onChange={handleOnChange} />
       <FilledButton
         onClick={() => {
-          onSubmit({ email: 'ssar@nate.com', password: 'meta1234!' }).catch((err) => console.log(err));
+          onSubmit({ email: inputInfo.email, password: inputInfo.password });
         }}
       >
         로그인
