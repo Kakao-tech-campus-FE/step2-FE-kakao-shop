c940876502c9f74d654813f455fe79b43f0ced8d
lcqff <yeonnu82g@gmail.com>
2023-07-08 03:02:18 +0900

    feat: 메인 페이지 home으로 변경

--- src/router/mainRouter.tsx
+++ src/router/mainRouter.tsx
@@ -1,14 +1,14 @@
 import { useRoutes } from 'react-router-dom';
 import React from 'react';
 import LoginPage from '@pages/LoginPage';
-import ComponentTest from '@pages/ComponentTest';
+import Home from '@pages/Home';
 import RegisterPage from '@pages/RegisterPage';
 
 const mainRouter = () =>
   useRoutes([
     {
       path: '/',
-      element: <ComponentTest />,
+      element: <Home />,
     },
     {
       path: 'login',
