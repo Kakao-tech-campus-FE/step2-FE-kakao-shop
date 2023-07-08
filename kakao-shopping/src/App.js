f36af311944c2b7f6e29f6df333092f81523579b
ba0061@naver.com <sweet.waiting0l@icloud.com>
2023-07-08 17:19:25 +0900

    feat: install react query

--- kakao-shopping/src/App.js
+++ kakao-shopping/src/App.js
@@ -1,21 +1,28 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import {ReactQueryDevtools} from 'react-query/devtools';
 import ComponentTest from './pages/ComponentTest';
 import HomePage from './pages/Main';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import Main from './pages/Main';
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/componentTest" element={<ComponentTest />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/main" element={<Main />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/componentTest" element={<ComponentTest />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/main" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} position='bottom-right'/>
+    </QueryClientProvider>
   );
 };
 
