7c59683af7360205d9ae7a53bcec78ca6c04bba1
ba0061@naver.com <sweet.waiting0l@icloud.com>
2023-07-08 22:45:34 +0900

    feat: 에러 핸들링도 props로 처리

--- kakao-shopping/src/components/templates/MainProductsTemplate.jsx
+++ kakao-shopping/src/components/templates/MainProductsTemplate.jsx
@@ -3,15 +3,27 @@ import Container from "../atoms/Container";
 import MainProductsSkeleton from "../organisms/MainProductsSkeleton";
 import PageSetButton from "../molecules/PageSetButton";
 import MainProducts from "../organisms/MainProducts";
+import { Link } from "react-router-dom";
 
-const MainProductsTemplate = ({isLoading, data}) => {
+const MainProductsTemplate = ({isLoading, data, isError, error}) => {
   const [page, setPage] = useState(0);
 
   const slicedData = data?.data.response.slice(page * 6, page * 6 + 6);
 
-  if(isLoading ) return (
+  if(isLoading ) {
+    return (
     <MainProductsSkeleton />
-  )
+    )
+  }
+  if(isError) {
+    return (
+      <Container className="mt-10 flex flex-col items-center justify-center">
+        <span className="text-2xl font-bold mb-2">에러가 발생했습니다</span>
+        <span className="text-lg mb-2">{error.message}</span>
+        <Link to="/login" className="text-lg">로그인으로 돌아가기</Link>
+      </Container>
+    )
+  }
   return (
     <Container className="flex flex-col items-center justify-center">
       <MainProducts slicedData={slicedData} />
--- kakao-shopping/src/pages/Main.jsx
+++ kakao-shopping/src/pages/Main.jsx
@@ -9,19 +9,10 @@ import { getProducts } from '../apis/api';
 const HomePage = () => {
   const { data, isLoading, isError, error } = useQuery("products", getProducts);
   
-  if(isError) {
-    return (
-      <Container className="flex flex-col items-center justify-center">
-        <span className="text-2xl font-bold mb-2">에러가 발생했습니다</span>
-        <span className="text-lg mb-2">{error.message}</span>
-        <Link to="/main" className="text-lg">메인으로 돌아가기</Link>
-      </Container>
-    )
-  }
   return (
     <Container>
       <GNB />
-      <MainProductsTemplate isLoading={isLoading} data={data}/>
+      <MainProductsTemplate isLoading={isLoading} error={error} isError={isError} data={data}/>
     </Container>
   );
 };
