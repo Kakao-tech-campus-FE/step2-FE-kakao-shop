fdc6a8646794bdcf8328f5bd68d67fae027b1ff5
H-sooyeon <wndqhr__@naver.com>
2023-07-11 17:27:58 +0900

    style: 이미지 스타일 지정

---
+++ src/styles/atoms/Photo.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const ImageContainer = styled.picture`
+    .card {
+        width: 19vw;
+        height: 25vh;
+        object-fit: cover;
+        border-radius: 10px;
+    }
+    img {
+        width: inherit;
+    }
+`
