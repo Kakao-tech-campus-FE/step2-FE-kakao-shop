8f5c75fafe101f6103c5e84bb8ac823116f38d25
H-sooyeon <wndqhr__@naver.com>
2023-07-11 17:08:20 +0900

    chore modified

--- src/components/organisms/RegisterForm.jsx
+++ src/components/organisms/RegisterForm.jsx
@@ -6,7 +6,7 @@ import Footer from "../atoms/Footer";
 import LinkText from "../atoms/LinkText";
 import * as Link from '../../styles/atoms/Link';
 import {useNavigate} from 'react-router-dom';
-import { register } from "../../apis/api";
+import { register } from "../../apis/user";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { setUser } from "../../store/slices/userSlice";
