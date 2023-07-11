4021d62375256db4ac017136b40673e49d8fe196
rlaeksqls@naver.com <rlaeksqls@naver.com>
2023-07-11 11:53:27 +0900

    과제 끝

--- step2-FE-kakao-shop/hw/src/components/orgamisms/MainForm.jsx
+++ step2-FE-kakao-shop/hw/src/components/orgamisms/MainForm.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Title from "../atoms/Title";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { loginRequest, setEmail, logOut } from "../../store/slices/userSlice";
+import { setEmail, logOut } from "../../store/slices/userSlice";
 import Button from "../atoms/Button";
 
 const MainForm = () => {
@@ -10,12 +10,30 @@ const MainForm = () => {
   const dispatch = useDispatch();
   const loggedIn = useSelector((state) => state.user.isLogin);
 
+  // 이부분이 새로고침해도 로그인 유지 하는 부분
   useEffect(() => {
     const email = localStorage.getItem("email");
     if (email) {
       dispatch(setEmail({ email }));
     }
-  }, [dispatch, loggedIn]);
+  }, [dispatch]);
+
+  // 이부분이 일정 시간 지나면 자동으로 로그아웃 되는 부분
+  const logoutTimeSecond = 10; // 10초 뒤 자동 로그아웃
+  const [count, setCount] = useState(logoutTimeSecond);
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCount((count) => count - 1);
+      console.log(count);
+    }, 1000);
+    if (count === 0) {
+      clearInterval(id);
+      if (loggedIn === true) {
+        dispatch(logOut());
+      }
+    }
+    return () => clearInterval(id);
+  }, [count]);
 
   return (
     <div>
