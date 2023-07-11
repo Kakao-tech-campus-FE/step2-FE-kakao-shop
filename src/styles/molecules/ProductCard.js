d3fad09d87eac30ee01212478fb636b9577ab54a
H-sooyeon <wndqhr__@naver.com>
2023-07-11 17:28:54 +0900

    style: 각 상품 정보 스타일 지정

---
+++ src/styles/molecules/ProductCard.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const Delivery = styled.div`
+    font-size: 0.5em;
+    display: inline-block;
+    padding: 0.2em 0.6em;
+    decoration-text: none;
+    margin-bottom: 0.7em;
+    color: #505050;
+    background-color: #E1F6FA;
+`
+
+export const Title = styled.div`
+    decoration-text: none;
+    display: inline-block;
+    font-size: 1em;
+    color: black;
+    @media screen and (max-width: 700px) {
+        font-size: 0.6em;
+    }
+`
+
+export const TalkPriceText = styled.span`
+    color: #148CFF;
+    font-weight: bold;
+`
+
+export const PriceText = styled.span`
+    margin-left: 0.3em;
+    color: black;
+    font-weight: bold;
+`
+
+export const PriceTextContainer = styled.div`
+    text-decoration: none;
+    display: inline-block;
+    font-size: 1.1em;
+    @media screen and (max-width: 700px) {
+        font-size: 0.9em;
+    }
+`
