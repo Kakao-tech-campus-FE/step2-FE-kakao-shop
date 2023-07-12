59825b3e0dd49510f9d71c604547a76e04bab4b3
H-sooyeon <wndqhr__@naver.com>
2023-07-12 20:29:04 +0900

    feat: 무한 스크롤 추가

--- src/components/templates/MainProductTemplate.jsx
+++ src/components/templates/MainProductTemplate.jsx
@@ -1,16 +1,41 @@
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 import Container from "../atoms/Container";
 import ProductGrid from "../organisms/ProductGrid";
 import { fetchProducts } from "../../apis/product";
-import { useQuery } from "react-query";
-import Loader from "../atoms/Loader";
+import { useInfiniteQuery } from "react-query";
+import CardLoader from "../atoms/CardLoader";
+import { useInView } from "react-intersection-observer";
 
 const MainProductTemplate = () => {
-    const {data: products, isLoading} = useQuery('products', () => fetchProducts()); // 구분자, API 요청 함수
+    const {ref, inView} = useInView();
+    const {data: products, 
+        isLoading, 
+        isFetchingNextPage, 
+        fetchNextPage, 
+        hasNextPage
+    } = useInfiniteQuery('products', ({pageParam = 0}) => fetchProducts(pageParam), {
+        getNextPageParam: (lastPage, pages) => {
+            if(lastPage.response && lastPage.response.length === 0) {
+                // 마지막 페이지일 경우 NULL을 반환하여 더 이상 페이지를 불러오지 않음
+                return null;
+            }
+            // 다음 페이지를 요청하기 위해 현재 페이지의 개수를 계산하여 반환
+            return pages.length + 1;
+        }
+    }); // 구분자, API 요청 함수
+
+    useEffect(() => {
+        if(inView && hasNextPage) {
+            fetchNextPage();
+        }
+    }, [inView]);
+    
     return(
         <Container>
-            <Suspense fallback={<Loader />}>
-                {isLoading ? (<Loader />) :  <ProductGrid products={products.response}/>}
+            <Suspense fallback={<CardLoader />}>
+                {isLoading ? (<CardLoader />) :  <ProductGrid products={products.pages.flatMap(page => page.response)}/>}
+                <div ref={ref}></div>
+                {isFetchingNextPage && <CardLoader />}
             </Suspense>
         </Container>
     );
