c1e4545e59217bb28368ba53bcaae4eec348b0c6
dongyeopCha <ckehdduq95@gmail.com>
2023-07-12 16:40:18 +0900

    feat: 라우터 컴포넌트 분리

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './global.css';
-import App from './App';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
+import { RouterProvider } from 'react-router-dom';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import rootReducer from './modules';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  { path: '/login', element: <Login /> },
-  { path: '/signup', element: <Signup /> },
-]);
+import router from './router';
 
 const store = configureStore({
   reducer: rootReducer,
---
+++ src/router.tsx
@@ -0,0 +1,43 @@
+import { lazy, Suspense } from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import App from './App';
+import Spinner from './components/common/atoms/Spinner';
+
+const MainPage = lazy(() => import('./pages/Main'));
+const LoginPage = lazy(() => import('./pages/Login'));
+const SignupPage = lazy(() => import('./pages/Signup'));
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: (
+          <Suspense fallback={<Spinner width={60} height={60} />}>
+            <MainPage />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: (
+      <Suspense fallback={<Spinner width={60} height={60} />}>
+        <LoginPage />
+      </Suspense>
+    ),
+  },
+  {
+    path: '/signup',
+    element: (
+      <Suspense fallback={<Spinner width={60} height={60} />}>
+        <SignupPage />
+      </Suspense>
+    ),
+  },
+]);
+
+export default router;
