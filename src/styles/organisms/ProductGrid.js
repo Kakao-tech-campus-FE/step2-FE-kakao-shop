6e76f9a5bafd7c0712f1adf917b8a309bde02c90
H-sooyeon <wndqhr__@naver.com>
2023-07-12 20:34:01 +0900

    style: 상품들 출력 style

--- src/styles/organisms/ProductGrid.js
+++ src/styles/organisms/ProductGrid.js
@@ -1,16 +1,11 @@
 import styled from 'styled-components';
 
 export const Product = styled.div`
+    min-width: 1200px;
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     grid-gap: 2em;
     margin: 3em 0;
     width: 100%;
-    max-width: inherit;
-    @media screen and (max-width: 700px) {
-        grid-template-columns: repeat(2, 1fr);
-        width: 80vw;
-        margin-left: 10vw;
-        margin-right: 10vw;
-    }
+    padding: 0 5em;
 `
