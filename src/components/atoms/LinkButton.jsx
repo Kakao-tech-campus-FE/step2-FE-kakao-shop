a175392493678f883d8bbb65fad6237088b48d5c
baegyeong <beth0929@naver.com>
2023-07-13 03:07:37 +0900

    feat: LinkButton 컴포넌트 추가 기존의 Button 컴포넌트에서 SubmitButton, LinkButton 컴포넌트로 나눔

---
+++ src/components/atoms/LinkButton.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const Btn = styled.button`
+  all: unset;
+  text-align: center;
+  cursor: pointer;
+
+  width: 5rem;
+  font-weight: ${(props) => props.styles.fontWeight || "400"};
+`;
+
+const LinkButton = ({ type, children, onClick, ...styles }) => {
+  // type: 버튼의 type 지정(submit, click)
+  // children: 버튼 내부 텍스트
+  // onClick: 클릭 이벤트
+  // styles: 버튼의 스타일 지정
+  return (
+    <Btn
+      type={type}
+      {...styles}
+      onClick={(e) => {
+        e.preventDefault();
+        onClick();
+      }}
+    >
+      {children}
+    </Btn>
+  );
+};
+
+export default LinkButton;
