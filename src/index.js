39bedb6aecffafd8c797f885d21f9e5177a047ee
GimGayeon <kky1373@naver.com>
2023-07-12 11:45:25 +0900

    index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
 import { Provider } from 'react-redux';
-import store from './store/index'
+// import store from './store/index'
 
 
 
