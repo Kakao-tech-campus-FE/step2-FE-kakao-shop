cff1dee1090e745ab3b5e45b42d1f7da878f00ce
baegyeong <beth0929@naver.com>
2023-07-13 03:04:42 +0900

    fix: props 형태 변경

--- src/components/atoms/Input.jsx
+++ src/components/atoms/Input.jsx
@@ -9,22 +9,11 @@ const InputBox = styled.input`
   border: 1px solid #979797;
   border-radius: 6px;
 `;
-const Input = ({ id, name, type, value, placeholder, onChange }) => {
-  // id: input 컴포넌트 고유의 id 지정
-  // name: input 요소 이름
-  // type: input 컨트롤 유형
-  // value: input 컴포넌트의 현재 값
-  // placeholder: 아무 내용도 입력되지 않았을 때 보이는 내용
-  // onChange: input 컴포넌트의 변화가 일어났을 때 동작 지정
+const Input = ({ className, children, ...inputProps }) => {
   return (
-    <InputBox
-      id={id}
-      name={name}
-      type={type}
-      value={value}
-      placeholder={placeholder}
-      onChange={onChange}
-    />
+    <InputBox className={`input-${className}`} {...inputProps}>
+      {children}
+    </InputBox>
   );
 };
 
