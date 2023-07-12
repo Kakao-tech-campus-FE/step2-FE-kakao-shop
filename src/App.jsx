f8b1bbdc97e6738eef4b9ac7021ee0c25a42098a
GimGayeon <kky1373@naver.com>
2023-07-12 21:22:46 +0900

    feat:Add globalStyles

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,22 @@ import Home from './pages/Home'
 
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
-
+import { GlobalStyle } from "./css/globalStyles";
 function App() {
   return (
     <>
-      <BrowserRouter><Routes>
-        <Route path={routes.logIn} element={<Login />} />
-        <Route path={routes.signUp} element={<SignUp />} />
-        {/*단독 레이아웃*/}
+      <BrowserRouter>
+        <GlobalStyle></GlobalStyle>
+        <Routes>
+          <Route path={routes.logIn} element={<Login />} />
+          <Route path={routes.signUp} element={<SignUp />} />
+          {/*단독 레이아웃*/}
 
-        <Route element></Route>
-        <Route path={routes.home} element={<Home />} />
+          <Route element></Route>
+          <Route path={routes.home} element={<Home />} />
 
 
-      </Routes></BrowserRouter>
+        </Routes></BrowserRouter>
 
 
 
