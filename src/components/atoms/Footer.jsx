e06fc4479f6531dea93f6471a0c7b1a032c8fd77
Dongjin <ehdwls1638@gmail.com>
2023-07-12 15:32:40 +0900

    feat : add Footer, GNB, MainLayout Dummy

--- src/components/atoms/Footer.jsx
+++ src/components/atoms/Footer.jsx
@@ -1,8 +1,14 @@
+import "../../styles/footer.css"
+
 const Footer = () => {
     return (
-        <footer className="footer">
+        <footer className="footer-dummy">
             <div className="container">
-                <span className="text-muted">Place sticky footer content here.</span>
+                <span className="text-muted">
+                    주소 : 부산광역시 금정구 장전동 123-456 <br/>
+                    전화번호 : 051-123-4567 <br/>
+                    이메일 : tester@pusan.ac.kr <br/>
+                </span>
             </div>
         </footer>
     );
--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import GlobalNavBar from "../organisms/GlobalNavBar";
 import Footer from "../atoms/Footer";
 
+
 const MainLayout = () => {
     return (
         <>
