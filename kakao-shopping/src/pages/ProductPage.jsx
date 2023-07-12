7174c87f91ac7717e3a730ba35b622bd27a52b51
ba0061@naver.com <sweet.waiting0l@icloud.com>
2023-07-12 22:30:05 +0900

    feat: add loader

--- kakao-shopping/src/pages/ProductPage.jsx
+++ kakao-shopping/src/pages/ProductPage.jsx
@@ -1,41 +1,36 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
 import { getIdProduct } from "../apis/api";
 import { useQuery } from "react-query";
 import ProductDetail from "../components/organisms/ProductDetail";
 import ProductOptions from "../components/organisms/ProductOptions";
-import Button from "../components/atoms/Button";
 
 const ProductPage = () => {
   const { id } = useParams();
-  const { data, isLoading, isError, error, refetch } = useQuery("product", ()=>getIdProduct(id));
+  const { data, isLoading, isError, error } = useQuery("product", ()=>getIdProduct(id));
 
   if(isLoading) {
     return (
-      // 로딩중
-      <div>
-        로딩중
+      <div className="flex justify-center items-center h-screen">
+        <div class="loader" />
       </div>
     )
   }
   if(isError) {
     return (
-      // 에러
       <div>
-        에러
+        {error.message}
       </div>
     )
   }
   const product = data.data.response;
   console.log(product)
   return (
-    <div className="flex justify-around">
+    <div className="flex justify-around h-280">
       <ProductDetail product={product} />
-      <div className="h-200 border-l"/>
+      <div className="h-full border-l"/>
       <ProductOptions product={product} />
     </div>
   )
 }
 
 export default ProductPage;
-  
