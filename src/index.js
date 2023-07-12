ecd5444465da2ccf766ea4896d28b862827ed500
GimGayeon <kky1373@naver.com>
2023-07-12 12:47:38 +0900

    conflict

--- src/index.js
+++ src/index.js
@@ -2,17 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
-// import store from './store/index'
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
-    <Provider>
-      <App />
-    </Provider>
-  </React.StrictMode >
-
+    <App />
+  </React.StrictMode>
 );
