2421f9049eb70cf0ddec3bf9dcc79c7eaa1ee0e9
lcqff <yeonnu82g@gmail.com>
2023-07-13 02:49:25 +0900

    feat: 레이아웃 적용한 라우터 설정

--- src/router/mainRouter.tsx
+++ src/router/mainRouter.tsx
@@ -3,20 +3,33 @@ import React from 'react';
 import LoginPage from '@pages/LoginPage';
 import Home from '@pages/Home';
 import RegisterPage from '@pages/RegisterPage';
+import MainLayout from '@components/templates/MainLayout';
+import PageContainer from '@components/atoms/PageContainer';
 
 const mainRouter = () =>
   useRoutes([
     {
       path: '/',
-      element: <Home />,
-    },
-    {
-      path: 'login',
-      element: <LoginPage />,
-    },
-    {
-      path: 'register',
-      element: <RegisterPage />,
+      element: <MainLayout />,
+      children: [
+        {
+          element: <PageContainer />,
+          children: [
+            {
+              index: true,
+              element: <Home />,
+            },
+            {
+              path: 'login',
+              element: <LoginPage />,
+            },
+            {
+              path: 'register',
+              element: <RegisterPage />,
+            },
+          ],
+        },
+      ],
     },
   ]);
 
