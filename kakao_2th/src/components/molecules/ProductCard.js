41c46a8d1e6413676fb179b9e67d56c62bfafe19
Klomachenko <leekyumin0901@naver.com>
2023-07-13 06:02:45 +0900

    feat: atoms-Card, molecules-ProductCard 생성

---
+++ kakao_2th/src/components/molecules/ProductCard.js
@@ -0,0 +1,13 @@
+import Card from "../atoms/Card"
+
+const ProductCard = ({ product }) => {
+    return (
+        <Card to={`/product/${product.id}`}>
+            <img src={product.imageUrl} alt={product.name} />
+            <div className="product-name">{product.name}</div>
+            <div className="product-price">{product.price}원</div>
+        </Card>
+    )
+}
+
+export default ProductCard
