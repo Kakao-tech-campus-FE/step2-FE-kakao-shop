91cbafe5a8d1d4e41e8ffaf315101106f25f3b19
H-sooyeon <wndqhr__@naver.com>
2023-07-12 20:32:11 +0900

    style: 상품 이미지 스타일

--- src/styles/atoms/Photo.js
+++ src/styles/atoms/Photo.js
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 
 export const ImageContainer = styled.picture`
     .card {
-        width: 19vw;
-        height: 25vh;
+        width: 18em;
+        height: 11em;
         object-fit: cover;
         border-radius: 10px;
     }
