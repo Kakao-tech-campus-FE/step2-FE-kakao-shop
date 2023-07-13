fcde31b992d94ee1a7961552cbd04231bf663df3
Junseo <monstazo911@gmail.com>
2023-07-14 02:54:33 +0900

    feat: axios response interceptor

--- src/apis/index.js
+++ src/apis/index.js
@@ -20,9 +20,37 @@ instance.interceptors.request.use((config) => {
 // 응답 인터셉터
 instance.interceptors.response.use(
   (response) => {
-    return response;
+    const statusCode = response.status;
+    if (200 <= statusCode && statusCode < 300) return response;
   },
   (error) => {
+    const statusCode = error.response.status;
+    switch (statusCode) {
+      case 300:
+        console.error("URI가 변경되었습니다.");
+        break;
+      case 302:
+        console.error("URI가 일시적으로 변경되었습니다.");
+        break;
+      case 400:
+        console.error("잘못된 형식의 입력입니다.");
+        break;
+      case 401:
+        console.error("인증되지 않은 사용자입니다.");
+        break;
+      case 403:
+        console.error("접근할 권한이 없습니다.");
+        break;
+      case 404:
+        console.error("잘못된 요청입니다.");
+        break;
+      case 500:
+        console.error("서버에 에러가 있습니다.");
+        break;
+      default:
+        break;
+    }
+
     return Promise.reject(error);
   }
 );
