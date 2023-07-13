345f3ed8cce79f4b257eec0dd0b77c58ff250687
titan <tnptt1122@gmail.com>
2023-07-13 12:11:36 +0900

    feat: week3 work3 api 응답 에러캐칭

--- step2/src/services/index.js
+++ step2/src/services/index.js
@@ -21,11 +21,31 @@ instance.interceptors.response.use(
     return response;
   },
   (error) => {
-    if (error.response.status === 401 ) {
-      localStorage.removeItem("token");
-      window.location.href = "/login";
+    if (error.response) {
+      switch (error.response.status) {
+        case 400:
+          alert("잘못된 요청입니다.");
+          break;
+        case 401:
+          localStorage.removeItem("token");
+          window.location.href = "/login";
+          break;
+        case 403:
+          alert("접근이 거부되었습니다.");
+          break;
+        case 404:
+          alert("해당 리소스를 찾을 수 없습니다.");
+          break;
+        case 500:
+          alert("서버 내부 에러가 발생했습니다.");
+          break;
+        default:
+          alert("알 수 없는 에러가 발생했습니다.");
+      }
+    } else {
+      alert("네트워크 에러가 발생했습니다.");
     }
-    return Promise.reject(error.response);
+    return Promise.reject(error);
   }
 );
 
