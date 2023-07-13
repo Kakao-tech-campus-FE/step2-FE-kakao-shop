377d6c43bada40aeb530962c93911ff985d6c317
Klomachenko <leekyumin0901@naver.com>
2023-07-13 19:36:40 +0900

    feat: detailSlice.js 구현

--- kakao_2th/src/pages/ProductDetailPage.js
+++ kakao_2th/src/pages/ProductDetailPage.js
@@ -1,4 +1,9 @@
+import { useParams } from "react-router-dom"
+
 const ProductDetailPage = () => {
+    const { id } = useParams();
+    const parseId = parseInt(id, 10)
+
     return (
         <div>
             <h1>Product Detail Page</h1>
