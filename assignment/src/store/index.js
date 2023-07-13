d851781bc034f6d35ef75a6c0023e768e6186cfb
YANGSEOKWOO <didtjrdn030@naver.com>
2023-07-14 00:15:29 +0900

    feat: Add isEnd(State)
    isEnd State를 추가하여 Slice단에서 무한스크롤을 할 때 데이터의 개수
    여부에 따라 초반 데이터의 무한로딩을 막았다

--- assignment/src/store/index.js
+++ assignment/src/store/index.js
@@ -1,13 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/userSlice';
-import productsReducer from './slices/productSlice';
+import productReducer from './slices/productSlice';
 
 const store = configureStore({
   // 우리는 이메일 정보를 글로벌로 저장할거임
   reducer: {
     // user 다음에 들어가는 어떤 key값으로 구분된 저장소가 되는거임
     user: userReducer,
-    products: productsReducer,
+    products: productReducer,
   },
 });
 
--- assignment/src/store/slices/productSlice.js
+++ assignment/src/store/slices/productSlice.js
@@ -7,6 +7,7 @@ const initialState = {
   products: [],
   loading: false,
   error: null, // error exit : { message, status }
+  isEnd: false,
 };
 // createAsync Thunk을 하려면 extra Reducer를 쓴다.
 
@@ -21,8 +22,12 @@ const productsSlice = createSlice({
     });
     // Promise.resolve()
     builder.addCase(getProducts.fulfilled, (state, action) => {
+      // 데이터 10개 이하라면 불러오지 않게
+      if (action.payload.response.length < 10) {
+        state.isEnd = true;
+      }
       state.loading = false;
-      // {success, response, error} 셋중 response
+      // {success, response, error} 셋중 response, 최대 10개 요소
       state.products = action.payload.response;
       state.error = action.payload.error;
     });
