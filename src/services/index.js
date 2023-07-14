55c6194904107cd9ecb8667e08c9ac7ccbedaec2
kimchanho97 <nh0903@pusan.ac.kr>
2023-07-14 23:51:54 +0900

    Feat: axios정의 및 인터셉터 구현

---
+++ src/services/index.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+export const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  timeout: 3000,
+  headers: {
+    "Content-Type": "application/json;charset=UTF-8",
+  },
+});
+
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+  return config;
+});
+
+instance.interceptors.response.use(
+  (response) => {
+    console.log("Axios Interceptors Data", response);
+    return response;
+  },
+  (error) => {
+    console.log("Axios Interceptors Error", error);
+    const status = error.response.status;
+    // Client단 에러 처리
+    if (status === 401) {
+      localStorage.clear();
+      window.location.href = "/login";
+    } else if (status === 404) {
+      window.location.href = "/404";
+    }
+    return Promise.reject(error.response);
+  }
+);
+
+// ⭐️ axios.인터셉터
+// - then 또는 catch로 처리되기 전에 요청과 응답을 가로챌수 있습니다.
+
+// ⭐️ 에러
+// - 401: 로그인 실패나 토큰 만료
+// - 404: 페이지 없음
+// - 500: 서버 에러 -> 각 api에서 처리: api 요청이 더 필요한 경우 retry수만큼 요청 시도
