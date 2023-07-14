3e7b120682d7310976260b9a437aff5e3b744a45
SeonghyeonKim <kkgojn@naver.com>
2023-07-14 13:00:43 +0900

    fix: Change Image Url

--- kakao-store/src/components/molecules/ProductCard.jsx
+++ kakao-store/src/components/molecules/ProductCard.jsx
@@ -5,7 +5,7 @@ import Photo from "../atoms/Photo";
 const ProductCard = ({ product }) => {
     return (
         <Card to={`/product/${product.id}`}>
-            <Photo className="card" src={product.image} alt={product.productName} />
+            <Photo className="card" src={`${process.env.REACT_APP_API_URL}${product.image.slice(1)}`} alt={product.productName} />
             <div className="product-name">
                 {product.productName}
             </div>
