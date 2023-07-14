a267d6113d261ed97756a18bc51e806d7b6227d0
kjh302903 <kjh302903@gmail.com>
2023-07-14 19:00:20 +0900

    feat: 메인페이지 구성
    메인페이지에 GNB와 ProductTemplate를 보여주도록함

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,57 +1,11 @@
-import { useSelectedUser } from "@hooks/useSelectedUser";
-import { setSelectedUser } from "@redux/selectedUser/slice";
-import { useLayoutEffect } from "react";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { styled } from "styled-components";
+import ProductTemplate from "@components/templates/ProductTemplate";
 
 function Main() {
-  const selectedUser = useSelectedUser();
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  useLayoutEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      const timeStamp = localStorage.getItem("timeStamp") as string;
-      const currentTime = new Date().getTime();
-      const elapsedTime = currentTime - parseInt(timeStamp, 10);
-
-      if (elapsedTime >= 24 * 60 * 60 * 1000) {
-        logOut();
-      } else {
-        dispatch(setSelectedUser(true));
-      }
-    } else {
-      dispatch(setSelectedUser(false));
-    }
-  }, []);
-
-  const handleState = () => {
-    if (selectedUser) {
-      logOut();
-    } else {
-      navigate("/login");
-    }
-  };
-
-  const logOut = () => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("loginTimestamp");
-    dispatch(setSelectedUser(false));
-  };
-
   return (
     <div>
-      <UserState onClick={handleState}>
-        {selectedUser ? "로그아웃" : "로그인"}
-      </UserState>
+      <ProductTemplate />
     </div>
   );
 }
 
 export default Main;
-
-const UserState = styled.div`
-  cursor: pointer;
-`;
