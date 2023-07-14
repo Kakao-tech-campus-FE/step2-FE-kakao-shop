828a5596fe14e9483405c046c7ec53612a5aea66
YANGSEOKWOO <didtjrdn030@naver.com>
2023-07-14 15:50:15 +0900

    feat: 개별상품조회 주석처리
    useQuery 사용으로 인한 개별상품 조회 주석처리

--- assignment/src/store/index.js
+++ assignment/src/store/index.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/userSlice';
 import productReducer from './slices/productSlice';
+// import detailReducer from './slices/detailSlice';
 
 const store = configureStore({
   // 우리는 이메일 정보를 글로벌로 저장할거임
@@ -8,6 +9,7 @@ const store = configureStore({
     // user 다음에 들어가는 어떤 key값으로 구분된 저장소가 되는거임
     user: userReducer,
     products: productReducer,
+    // detail: detailReducer,
   },
 });
 
