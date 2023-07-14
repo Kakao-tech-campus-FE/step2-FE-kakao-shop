50eed8d5e1eadbe2dc09776d2b7b03dd32b962b8
SeonghyeonKim <kkgojn@naver.com>
2023-07-14 21:43:38 +0900

    fix: Change Loader Comp

--- kakao-store/src/components/atoms/Loader.jsx
+++ kakao-store/src/components/atoms/Loader.jsx
@@ -1,8 +1,8 @@
 const Loader = () => {
     return (
-        <div className="w-20 mx-auto">
+        <div className="flex items-center justify-center w-full mx-auto my-10">
             <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-            width="40px" height="40px" viewBox="0 0 40 40" enable-background="new 0 0 40 40" xmlSpace="preserve">
+            width="40px" height="40px" viewBox="0 0 40 40" enableBackground="new 0 0 40 40" xmlSpace="preserve">
             <path opacity="0.2" fill="#000" d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946
                 s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634
                 c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"/>
