8e562103edde7613926ad3e6f7c7e90dfce86058
blackhblee <blackhblee@gmail.com>
2023-07-14 11:17:40 +0900

    chore: Add react-query
    react-query 사용을 위해 추가했습니다.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./store";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>
 );
