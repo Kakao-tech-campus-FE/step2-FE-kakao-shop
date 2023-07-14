1e78eab5e3a4bb91f360a8725b06d2a0b75e4997
SeonghyeonKim <kkgojn@naver.com>
2023-07-14 12:44:49 +0900

    Add: React-Query

--- kakao-store/src/index.js
+++ kakao-store/src/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from "./store/index.js"
+import { QueryClient, QueryClientProvider, } from "react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>
 );
