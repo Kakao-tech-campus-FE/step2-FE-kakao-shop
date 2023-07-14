92103d74faae54ade1afaead92e79eeca812d5f6
YANGSEOKWOO <didtjrdn030@naver.com>
2023-07-14 15:49:14 +0900

    feat: 상세제품조회 구현
    useQuery를 이용한 비동기처리

--- assignment/src/pages/ProductDetail.jsx
+++ assignment/src/pages/ProductDetail.jsx
@@ -1,19 +1,29 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 import { getDetail } from '../store/slices/detailSlice';
+import Loader from '../components/atoms/Loader';
+import { getProductById } from '../services/product';
 
 const ProductDetailPage = () => {
   const { id } = useParams(); // string
   //   const parsedId = parseInt(id, 10);
-
   const dispatch = useDispatch();
+  const {
+    data: detail,
+    error,
+    isLoading,
+  } = useQuery(`/product/${id}`, () => getProductById(id));
+
   useEffect(() => {
     dispatch(getDetail(id));
   }, [dispatch, id]);
   return (
     <div>
-      <h1> Hi </h1>
+      {isLoading && <Loader />}
+      {error && <div>{error.message}</div>}
+      {detail && <div>{detail.productName}</div>}
     </div>
   );
 };
