d56ba9131df7ae96a4526dd4dd43fe5a87eed077
rlaeksqls@naver.com <rlaeksqls@naver.com>
2023-07-14 09:07:19 +0900

    충돌 수정

--- step2-FE-kakao-shop/hw/src/App.js
+++ step2-FE-kakao-shop/hw/src/App.js
@@ -1,16 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
-import MainPage from "./pages/MainPage";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MainPage />}></Route>
+          {/* 단독 레이아웃 */}
           <Route path="/login" element={<LoginPage />}></Route>
           <Route path="/signup" element={<RegisterPage />}></Route>
+          {/* 공통 레이아웃: GNB, Footer */}
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<HomePage />}></Route>
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
