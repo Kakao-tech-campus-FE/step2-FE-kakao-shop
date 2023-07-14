4a8ad860038ca5c3d28483ff7b56f093f3a1e7c4
MINJOO-KIM <gkminju2000@gmail.com>
2023-07-14 22:53:18 +0900

    feat: 과제1 - redux-thunk을 사용한 비동기 처리 - userSlice - productSlice

---
+++ kakao-shopping/src/store/slices/userSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  email: null,
+  loading: false, // 요청을 보냈을 때는 true, 아닌 경우: 요청이 없었거나, 실패했거나, 성공했을 때 false
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setEmail: (State, action) => {
+      state.email = action.payload.email;
+    },
+    extraReducers: (builder) => {
+      builder.addCase(loginRequest.pending, (state, acti = n) => {
+        state.loading = true;
+      });
+      builder.addCase(loginRequest.fulfilled, (state, action) => {
+        state.loading = false;
+        state.email = action.payload.email;
+      });
+    },
+  },
+});
+
+export const loginReq = createAsyncThunk("user/login", async (data) => {
+  const { email, password } = data;
+  const response = await login({ email, password });
+  return {
+    email: email,
+    token: response.headers.authorization,
+  };
+});
+
+export const { setEmail } = userSlice.actions;
+export default userSlice.reducer;
