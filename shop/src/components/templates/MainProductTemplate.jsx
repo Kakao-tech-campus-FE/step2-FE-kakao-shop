3facc0d57363d5ce0d49366155c4db3c72cd6167
munjihye <m84422583@gmail.com>
2023-07-14 19:56:24 +0900

    feat: 무한 스크롤을 구현하려했지만 실패

--- shop/src/components/templates/MainProductTemplate.jsx
+++ shop/src/components/templates/MainProductTemplate.jsx
@@ -1,64 +1,69 @@
-import { useQuery } from 'react-query';
 import Container from "../atoms/Container"
 import ProductGrid from "../organisms/ProductGrid"
-import { Suspense, useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import Loader from "../atoms/Loader"
 import { fetchProducts } from '../../services/product';
-
+import { useInfiniteQuery } from 'react-query'
+import {useInView} from "react-intersection-observer"
 
 const MainProductTemplate = () => {
-  const [bottom, setBottom] = useState(false);
-  const [page,setPage] = useState(0)
-  const bottomObserver = useRef(null)
-  const { 
-    data: products, 
+  const {ref, inView} = useInView()
+  const {
+    data: products,
     isLoading: loading,
-    error,   
-  } = useQuery(['products'], () =>
-    fetchProducts(page),
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery('products', ({ pageParam = 0 }) => 
+    fetchProducts(pageParam), 
+    {
+      getNextPageParam: (lastPage, pages) => {
+        if(lastPage.response && lastPage.response.length === 0 ){
+          // 마지막 페이지일 경우 null 반환 해서 더이상 페이지 불러오지 않음
+          return null
+        }
+        // 다음 페이지 요청하기 위해 현재 페이지 개수 계산해 반환 
+        return pages.length +1
+      },
+      // Error 처리
+      onError: (error) => {
+        switch(error.status) {
+          case 300: alert(`something went wrong 300:${error.message}`)
+          break;
+          case 400: alert(`something went wrong 400:${error.message}`)
+          break;
+          case 500: alert(`something went wrong 500:${error.message}`)
+          break;
+          default: alert(`something went wrong:${error.message}`)
+        }
+      }
+    }
   );
 
   useEffect(() => {
-    fetchProducts(page);
-  }, [page])
-
-  useEffect(()=>{
-    if(products && products?.data.response.length <10){
-      setBottom(true)
+    if(inView && hasNextPage){
+      fetchNextPage();
     }
-  },[products])
-
-  useEffect(()=>{
-    const io = new IntersectionObserver((entries, observer) =>{
-      entries.forEach((entry)=>{
-        if(entry.isIntersecting && !bottom){
-          // 임계점 퍼센트만큼 보이면 해당 코드 실행 
-          setPage((prevPage) => prevPage + 1);
-          console.log(entry)
-        }
-      })
-    },{
-        threshold: 0.5,
-      }
-    )
-    if(bottom){
-      io.unobserve(bottomObserver.current)
-    } else{
-      io.observe(bottomObserver.current)
-    }
-    
-  }
-  ,[]) // 최초 렌더링때 1번만 선언 
-
+  },[inView])
   
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (loading) {
+        alert("네트워크가 불안정합니다.");
+      }
+    }, 5000);
+    
+    return () => clearTimeout(timer);
+  }, [loading]);
+  
+
   return (
     <Container>
-        {/* <Suspense fallback={<Loader/>}> */}
           {loading ? <Loader/> : (
-            <ProductGrid products={products}/>
+            <ProductGrid products={products?.pages.flatMap((page) => page.data.response)}/>
           )}
-          <div ref={bottomObserver}></div>
-        {/* </Suspense> */}
+          <div ref={ref}></div>
+          {isFetchingNextPage && <Loader/>}
     </Container>
   )
 }
