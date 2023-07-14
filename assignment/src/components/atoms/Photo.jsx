4b511d7655186b1fcafea6c1ace6cf3235f15366
YANGSEOKWOO <didtjrdn030@naver.com>
2023-07-14 17:07:25 +0900

    feat: File 경로 수정
    .env파일의 /로 인해 파일이 제대로 전송되지 않는 문제 수정

--- assignment/src/components/atoms/Photo.jsx
+++ assignment/src/components/atoms/Photo.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import '../../styles/atoms/Photo.css';
 
 const Photo = ({ className, src, alt }) => {
+  console.log(process.env.REACT_APP_API_URL + src);
   return (
     <picture className={className}>
-      {/* <source srcSet={src} /> */}
-      <img src={`/assets/${src}`} alt={alt} />
+      <source srcSet={process.env.REACT_APP_API_URL + src} />
+      <img src={process.env.REACT_APP_API_URL + src} alt={alt} />
     </picture>
   );
 };
