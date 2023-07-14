e02cc70a0fccf9977f8c34232044057b8d1bbf40
YANGSEOKWOO <didtjrdn030@naver.com>
2023-07-14 23:45:04 +0900

    react-query

--- assignment/src/index.js
+++ assignment/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './store/index';
