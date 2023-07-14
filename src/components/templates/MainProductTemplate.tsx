8386fef22ced5419d70cb5644ef96c2d9bf34ad4
publdaze <publdaze@naver.com>
2023-07-14 17:41:06 +0900

    feat: 무한 스크롤 위치에 따라 페이지 수 증가

--- src/components/templates/MainProductTemplate.tsx
+++ src/components/templates/MainProductTemplate.tsx
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import useGetProductsQuery from '../../apis/productApi';
 import ProductGrid from '../organisms/ProductGrid';
 import Container from '../atoms/Container';
 import Loader from '../atoms/Loader';
 
 const MainProductTemplate = () => {
-  const { data: products, isSuccess, isLoading } = useGetProductsQuery({ page: 0 });
+  const MAX_PAGE = 1;
+  const [page, setPage] = useState(0);
+  const bottomObserver = useRef(null);
+  const { data: products, isSuccess, isLoading } = useGetProductsQuery({ page });
 
-  if (isLoading) {
-    return <Loader />;
-  }
+  const io = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && page < MAX_PAGE) {
+          setPage((prev) => prev + 1);
+        }
+      });
+    },
+    {
+      threshold: 1,
+    },
+  );
+
+  useEffect(() => {
+    if (!bottomObserver || !bottomObserver.current || !isSuccess) return;
+    io.observe(bottomObserver.current);
+  }, [isSuccess]);
 
   return (
     <Container className='pb-16 pt-8'>
+      {isLoading && <Loader />}
       <div>{isSuccess && products && <ProductGrid products={products} />}</div>
+      <div ref={bottomObserver} className='h-20' />
     </Container>
   );
 };
