a3a1b0176d00b89a585c2e8a1d7f933942e66eb9
kimchanho97 <nh0903@pusan.ac.kr>
2023-07-14 23:36:23 +0900

    Feat: App에 404페이지, 공통 레이아웃 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
@@ -11,11 +14,19 @@ function App() {
         {/* 단독 레이아웃 */}
         <Route path="/login" element={<LoginPage />}></Route>
         <Route path="/signup" element={<RegisterPage />}></Route>
-        {/* 공통 레이아웃 */}
-        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/404" element={<NotFoundPage />}></Route>
+        {/* 공통 레이아웃(path 선언X): GNB, Footer */}
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/product/:id" element={<ProductDetailPage />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+// ⭐️ 동적매개변수란?
+// - React Router에서 경로에 변수를 포함시키는 방법
+// - useParams 훅을 사용하여 동적 매개변수 값을 추출할 수 있다
