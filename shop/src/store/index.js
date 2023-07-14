8c784fe1bf2de281061dd59c6c1bee3ecc570ffe
munjihye <m84422583@gmail.com>
2023-07-14 19:59:37 +0900

    delete: productSlice 삭제 예정

--- shop/src/store/index.js
+++ shop/src/store/index.js
@@ -6,12 +6,12 @@ import productReducer from "./slices/productSlice"
 
 //데이터 저장소의 형태를 키값으로부터 알 수 있
 const store = configureStore({
-    reducer: {
-        // User reducer : 유저 정보 저장 - email
-        user: userReducer,
-        // Products reducer : products
-        product: productReducer
-    }
+  reducer: {
+      // User reducer : 유저 정보 저장 - email
+      user: userReducer,
+      // Products reducer : products
+      product: productReducer
+  }
 })
 
 export default store;
--- shop/src/store/slices/productSlice.js
+++ shop/src/store/slices/productSlice.js
@@ -20,14 +20,15 @@ const productsSlice = createSlice({
     builder.addCase(getProducts.fulfilled, (state, action)=>{
       // action.payload.response는 최대 10개의 요소가 있을 것
       // 10개보다 작다면 더이상 데이터를 불러오는게 의미 없음
+      state.loading = false
+      
       if (action.payload.response.length <10){
         state.isEnd = true
       }
       
-      state.loading = false
       // 중복 제거 : 무한 스크롤 
       state.products = _.uniqBy([...state.products,...action.payload.response],'id')
-      state.error = action.payload.error
+      state.error = null
     })
     //Promise.reject()
     builder.addCase(getProducts.rejected, (state, action)=>{
@@ -42,8 +43,9 @@ export const getProducts = createAsyncThunk(
   "products/getProducts",
   async (page) => {
     const response = await fetchProducts(page)
-    return response.data //action.payload에 들어감  {success, respose : [], errir}
+    return response.data //action.payload에 들어감  {success, respose : [], error}
   }
 )
+
 
 export default productsSlice.reducer;
