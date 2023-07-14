406735e2b38ec5377584883b7e4dbbd6792a4a86
sihyonn <sihyeon982@naver.com>
2023-07-14 18:13:29 +0900

    import 수정

--- kakao/src/pages/ProductDetailPage.jsx
+++ kakao/src/pages/ProductDetailPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getDetail } from "../store/slices/detailSlice";
 import Loader from "../components/atoms/Loader";
