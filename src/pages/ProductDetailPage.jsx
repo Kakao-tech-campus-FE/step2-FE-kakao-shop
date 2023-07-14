2a2a4b01844c33beafa9f10539aeba2d5d9f5377
rktdnjs <rktdnjs@naver.com>
2023-07-14 15:33:13 +0900

    feat : ProductDetailPage 일부 수정

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,10 +1,11 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { getDetail } from "../store/slices/detailSlice";
 import { getProductById } from "../components/services/product";
 import { useQuery } from "react-query";
 import Loader from "../components/atoms/Loader";
+
+// import { useDispatch } from "react-redux";
+// import { useEffect } from "react";
+// import { getDetail } from "../store/slices/detailSlice";
 // import useSWR from 'swr'
 
 const ProductDetailPage = () => {
@@ -15,13 +16,13 @@ const ProductDetailPage = () => {
     // 그런데 각 쿼리에서 받아온 조건들이 처리되었을 때 렌더링되도록 data1 && data2 ... 비효율적!
     // 따라서 이럴 경우에는 useQueries를 사용하게 된다.
 
+    // const dispatch = useDispatch();
     const { id } = useParams();
-    const dispatch = useDispatch();
     const { data, error, isLoading } = useQuery(`product/${id}`, () => getProductById(id));
 
-    useEffect(() => {
-        dispatch(getDetail(id));
-    }, [dispatch, id])
+    // useEffect(() => {
+    //     dispatch(getDetail(id));
+    // }, [dispatch, id])
 
     // loading & error state에 대한 처리 필요
     return (
@@ -29,7 +30,9 @@ const ProductDetailPage = () => {
             <div className="productDetailPage">
                 {isLoading && <Loader />}
                 {error && <div>{error.message}</div>}
-                {data && <div>{data.productName}</div>}
+                {data && <div>{data.data.response.productName}</div>}
+                {data && <div>{data.data.response.price}원</div>}
+                {data && <div>평점 {data.data.response.starCount}/5 점</div>}
             </div>
         </>
     );
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,12 +1,10 @@
 import RegisterForm from "../components/organisms/RegisterForm";
-import Loader from "../components/atoms/Loader";
 
 const RegisterPage = () => {
     return (
         <>
             <div className="registerPage">
                 <RegisterForm />
-                <Loader/>
             </div>
         </>
     );
