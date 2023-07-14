5947028f8d035ea965e330bce2e50ecc39e3c4c7
blackhblee <blackhblee@gmail.com>
2023-07-14 19:28:07 +0900

    feat: Finish week-3 assignment-1 and assignment-2
    3주차 과제 1과 과제 2 완료했습니다.

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,11 +1,22 @@
+import { useEffect, useState } from "react";
 import LoginForm from "../components/organisms/LoginForm";
+import Loader from "../components/atoms/Loader";
 
 /** 로그인 페이지
  *
  * @return {JSX.Element}
  */
 const LoginPage = () => {
-  return <LoginForm />;
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 600);
+  }, []);
+
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  return <>{loading ? <Loader /> : <LoginForm />}</>;
 };
 
 export default LoginPage;
