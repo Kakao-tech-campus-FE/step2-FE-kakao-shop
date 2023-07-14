39dd7525a79f7301982423c4455582f3f3634823
publdaze <publdaze@naver.com>
2023-07-14 23:47:28 +0900

    feat: 상태 코드별 에러 캐칭

--- src/apis/productApi.ts
+++ src/apis/productApi.ts
@@ -4,7 +4,12 @@ import { useInfiniteQuery } from '@tanstack/react-query';
 const useGetProductsQuery = () => {
   const MAX_PAGE = 1;
   const fetcher = ({ pageParam = 0 }) =>
-    axios.get('/products', { params: { page: pageParam } }).then(({ data }) => data.response);
+    axios
+      .get('/products', { params: { page: pageParam } })
+      .then(({ data }) => data.response)
+      .catch((error) => {
+        throw error;
+      });
 
   return useInfiniteQuery({
     queryKey: ['products'],
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 import './tailwind.css';
 import Router from './Router';
@@ -13,7 +13,26 @@ import { store, persistor } from './store';
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 axios.defaults.timeout = 1000;
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onSuccess: (data) => {
+      console.log(data);
+    },
+    onError: (error) => {
+      if (error instanceof AxiosError) {
+        const statusCode = error.response?.status;
+        if (!statusCode) console.error('알 수 없는 에러가 발생했습니다.');
+        else if (statusCode >= 300 && statusCode < 400) {
+          console.error('리다이렉션 에러가 발생했습니다.');
+        } else if (statusCode >= 400 && statusCode < 500) {
+          console.error('클라이언트 에러가 발생했습니다.');
+        } else if (statusCode >= 500) {
+          console.error('서버 에러가 발생했습니다.');
+        }
+      }
+    },
+  }),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
