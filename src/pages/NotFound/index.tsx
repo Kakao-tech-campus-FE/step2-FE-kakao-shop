4655c4e037c718ab84c05f1088dfd2918d91d996
kjh302903 <kjh302903@gmail.com>
2023-07-14 19:02:00 +0900

    feat: NotFound 페이지 생성
    404 NotFound 페이지 생성

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,11 @@
+import NotFoundTemplate from "@components/templates/NotFoundTemplate";
+
+const NotFound = () => {
+  return (
+    <div>
+      <NotFoundTemplate />
+    </div>
+  );
+};
+
+export default NotFound;
