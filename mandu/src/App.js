d5381d819ba922a38b3d455f8ec5441fec1309fb
charmeee <mandugohigh@gmail.com>
2023-07-14 17:47:05 +0900

    feat: mainlayout router에 적용

--- mandu/src/App.js
+++ mandu/src/App.js
@@ -3,23 +3,27 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
+import MainLayout from "./components/templates/MainLayout";
+import {QueryClient, QueryClientProvider} from "react-query";
+
+const queryClient = new QueryClient()
 
 function App() {
 
-
     return (
-        <div className="App">
-            <BrowserRouter>
-                <Routes>
-                    <Route exact path="/" element={<MainPage/>}/>
-                    <Route path="/login" element={<LoginPage/>}/>
-                    <Route path="/signup" element={<SignUpPage/>}/>
-                    <Route path='/*' element={<MainPage/>}/>
-                </Routes>
-            </BrowserRouter>
-
-
-        </div>
+        <QueryClientProvider client={queryClient}>
+            <div className="App">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/login" element={<LoginPage/>}/>
+                        <Route path="/signup" element={<SignUpPage/>}/>
+                        <Route element={<MainLayout/>}>
+                            <Route path='/' element={<MainPage/>}/>
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        </QueryClientProvider>
     );
 }
 
