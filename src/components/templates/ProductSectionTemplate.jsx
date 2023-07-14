cdf7275828ad9e953da609440ccadc9a0d244ed9
blackhblee <blackhblee@gmail.com>
2023-07-14 23:03:10 +0900

    fix: Fix refresh bug
    새로고침 시 생기는 버그를 수정했습니다.

--- src/components/templates/ProductSectionTemplate.jsx
+++ src/components/templates/ProductSectionTemplate.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef, useState } from "react";
-import { useParams } from "react-router-dom";
 import { useQuery } from "react-query";
 import _ from "lodash";
 import Container from "../atoms/Container";
@@ -12,33 +11,30 @@ import { fetchProducts } from "../../api/product";
  */
 const ProductSectionTemplate = () => {
   const bottomObserver = useRef(null);
-  const { page } = useParams();
-  const [pageNumber, setPageNumber] = useState(
-    // eslint-disable-next-line no-restricted-globals
-    !isNaN(page) ? parseInt(page, 10) : 0
-  );
   const [products, setProducts] = useState([]);
   const [isEnd, setIsEnd] = useState(false);
-  const { data, error, isLoading } = useQuery([pageNumber], () =>
-    fetchProducts(pageNumber)
-  );
+  const [page, setPage] = useState(0);
+
+  const { data, error, loading } = useQuery([page], () => fetchProducts(page));
 
   useEffect(() => {
     if (data) {
       setProducts((prevProducts) =>
         _.uniqBy([...prevProducts, ...data.data.response], "id")
       );
-      setIsEnd(data.data.response.length < 9);
+      setIsEnd(data.data.response.length !== 9);
     }
   }, [data]);
 
   useEffect(() => {
-    const options = { threshold: 1 };
-    const observer = new IntersectionObserver(([entry]) => {
-      if (entry.isIntersecting && !isEnd) {
-        setPageNumber((prev) => prev + 1);
-      }
-    }, options);
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting && !isEnd) {
+          setPage((prev) => prev + 1);
+        }
+      },
+      { threshold: 1 }
+    );
 
     if (bottomObserver.current) {
       observer.observe(bottomObserver.current);
@@ -57,7 +53,7 @@ const ProductSectionTemplate = () => {
 
   return (
     <Container className="product-section flex max-w-none mx-auto w-[1280px] mt-[80px]">
-      <ProductGrid products={products} loading={isLoading} />
+      <ProductGrid products={products} loading={loading} />
       <div ref={bottomObserver} />
     </Container>
   );
