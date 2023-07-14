82f0abf6cc5c22faaa7b23f06c9ef8358a7bbfbd
SeonghyeonKim <kkgojn@naver.com>
2023-07-14 21:44:34 +0900

    feat: Using React-Query, IO

--- kakao-store/src/components/templates/MainProductTemplate.jsx
+++ kakao-store/src/components/templates/MainProductTemplate.jsx
@@ -1,8 +1,10 @@
+/*
 import { useDispatch, useSelector } from "react-redux"
 import Container from "../atoms/Container";
 import { Suspense, useEffect, useRef, useState } from "react";
 import ProductGrid from "../organisms/ProductGrid";
 import { getProducts } from "../../store/slices/productSlice";
+import Loader from "../atoms/Loader";
 
 const MainProductTemplate = () => {
     const [page, setPage] = useState(0);
@@ -23,7 +25,7 @@ const MainProductTemplate = () => {
 
     useEffect(() => {
         io.observe(bottomObserver.current);
-    }, [])
+    }, []);
 
     useEffect(() => {
         dispatch(getProducts(page));
@@ -34,6 +36,67 @@ const MainProductTemplate = () => {
             <Suspense fallback={<Loader />}>
                 <ProductGrid products={products} />
                 <div ref={bottomObserver}></div>
+            </Suspense>
+        </Container>
+    );
+};
+*/
+
+import Container from "../atoms/Container";
+import { Suspense, useEffect } from "react";
+import ProductGrid from "../organisms/ProductGrid";
+import Loader from "../atoms/Loader";
+import { useInfiniteQuery } from "react-query";
+import { fetchProducts } from "../../apis/product";
+import { useInView } from "react-intersection-observer";
+import SkeletonProductGrid from "../organisms/SkeletonProductGrid";
+
+const MainProductTemplate = () => {
+    const {ref, inView} = useInView();
+
+    const {
+        data : products,
+        isLoading, 
+        isFetchingNextPage, 
+        fetchNextPage, 
+        hasNextPage
+      } = useInfiniteQuery('products', ({ pageParam = 0 }) => fetchProducts(pageParam), {
+        getNextPageParam: (lastPage, allPages) => {
+          if (lastPage.response && lastPage.response.length === 0) {
+            return null;
+          }
+          return allPages.length + 1;
+        },
+        onError: (error) => {
+            switch(error.status) {
+                case 300: 
+                    alert(`에러 300: ${error.message}`);
+                    break;
+                case 400: 
+                    alert(`에러 400: ${error.nessage}`);
+                    break;
+                case 500: 
+                    alert(`에러 500: ${error.message}`);
+                    break;
+                default: 
+                    alert(`에러: ${error.message}`);
+                    break;
+            }
+        }
+    });
+    
+    useEffect(() => {
+        if(inView && hasNextPage) {
+            fetchNextPage();
+        }
+    }, [inView]);
+
+    return (
+        <Container>
+            <Suspense fallback={<SkeletonProductGrid />}>
+                {isLoading ? <Loader /> : <ProductGrid products={products.pages.flatMap((page) => page.response)} />}
+                <div ref={ref}></div>
+                {isFetchingNextPage && <SkeletonProductGrid />}
             </Suspense>
         </Container>
     );
