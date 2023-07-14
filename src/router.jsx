91bdf5e08fc4a1aad515984d6be8d70a24c8fdaa
j8won <joowoni99@gmail.com>
2023-07-14 23:01:35 +0900

    feat: router.jsx 개별 상세 페이지 추가

--- src/router.jsx
+++ src/router.jsx
@@ -1,8 +1,10 @@
+import { lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import routes from "@/constants/routes.js";
-import Home from "@/pages/Home.jsx";
-import SignIn from "@/pages/SignIn.jsx";
-import SignUp from "@/pages/SignUp.jsx";
+
+const Home = lazy(() => import("@/pages/Home.jsx"));
+const SignIn = lazy(() => import("@/pages/SignIn.jsx"));
+const SignUp = lazy(() => import("@/pages/SignUp.jsx"));
 
 const router = createBrowserRouter([
   {
@@ -10,12 +12,16 @@ const router = createBrowserRouter([
     element: <Home />,
   },
   {
-    path: "sign-in",
+    path: routes.signIn,
     element: <SignIn />,
   },
   {
     path: routes.signUp,
     element: <SignUp />,
+  },
+  {
+    path: `${routes.product}/:productId`,
+    element: <div>hi</div>,
   },
 ]);
 
