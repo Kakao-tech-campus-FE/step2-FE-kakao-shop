22977530b8cfae5dfecf62f045555f944371f23c
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-07-14 22:26:42 +0900

    refactor: axios timeout 설정

--- kakao-shop/src/apis/client.ts
+++ kakao-shop/src/apis/client.ts
@@ -6,7 +6,7 @@ export const BASE_URL = process.env.REACT_APP_PUBLIC_URL;
 
 export const client = axios.create({
   baseURL: BASE_URL,
-  timeout: 1000,
+  timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
   },
