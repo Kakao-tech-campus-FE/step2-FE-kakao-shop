c030c5cabbb38d5565e7b2dded8c9430a9e538c5
blackhblee <blackhblee@gmail.com>
2023-07-14 21:19:00 +0900

    design: Modify spinner location
    스피너의 위치를 변경했습니다.

--- src/components/atoms/Loader.jsx
+++ src/components/atoms/Loader.jsx
@@ -2,7 +2,7 @@ import Container from "./Container";
 
 const Loader = () => {
   return (
-    <Container className="absolute m-auto ml-[630px] mt-[400px]">
+    <Container className="flex max-w-none justify-center mt-[400px]">
       <div className="w-20 h-20 rounded-full animate-spin border-4 border-solid border-yellow-kakao border-t-transparent shadow-md" />
     </Container>
   );
