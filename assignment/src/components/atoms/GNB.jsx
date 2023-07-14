89390f9969c80281e96410c67c2ca82a6dc74805
YANGSEOKWOO <didtjrdn030@naver.com>
2023-07-14 17:05:53 +0900

    feat : GNB파일 수정
    Email 값이 새로고침이 되면 사라지는 문제 때문에, 로그인, 로그아웃이
    제대로 구현이 되지 않는 문제 발생
    => Token의 값을 읽어오도록 대체

--- assignment/src/components/atoms/GNB.jsx
+++ assignment/src/components/atoms/GNB.jsx
@@ -1,18 +1,44 @@
 import { Link } from 'react-router-dom';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../styles/atoms/GNB.css';
 import { useSelector, useDispatch } from 'react-redux';
 
 function GNB() {
-  const email = useSelector((state) => state.user.id);
+  const getUserToken = () => {
+    const tokenString = localStorage.getItem('user');
+    const tokenObject = JSON.parse(tokenString);
+    return tokenObject?.expiration > Date.now() ? tokenString : null;
+  };
+  const [token, setToken] = useState(getUserToken());
+
+  const email = useSelector((state) => state.user.email);
   const dispatch = useDispatch();
 
   const handleLogout = () => {
-    localStorage.removeItem('token');
-    dispatch(setEmail(null));
-    alert('정상적인 로그아웃');
+    localStorage.removeItem('user');
+    setToken(null);
+    window.location.reload();
   };
+  useEffect(() => {
+    const tokenObject = JSON.parse(localStorage.getItem('user'));
+    const expiration = tokenObject?.expiration;
+    if (expiration && expiration <= Date.now()) {
+      handleLogout();
+    }
+  }, []);
 
+  const renderAuthButton = () => {
+    if (token) {
+      return <button onClick={handleLogout}>로그아웃</button>;
+    }
+    return (
+      <>
+        <Link to="/signup">회원가입</Link>
+        <span> | </span>
+        <Link to="/login">로그인</Link>
+      </>
+    );
+  };
   return (
     <header className="header">
       <div className="contents">
@@ -27,26 +53,7 @@ function GNB() {
               </Link>
             </span>
             <span> | </span>
-            <span>
-              {email ? (
-                <Link
-                  to="/login"
-                  onClick={handleLogout}
-                  style={{ textDecoration: 'none', color: 'black' }}
-                >
-                  {' '}
-                  로그아웃{' '}
-                </Link>
-              ) : (
-                <Link
-                  to="/login"
-                  style={{ textDecoration: 'none', color: 'black' }}
-                >
-                  {' '}
-                  로그인{' '}
-                </Link>
-              )}
-            </span>
+            <span>{renderAuthButton()}</span>
           </div>
         </nav>
       </div>
