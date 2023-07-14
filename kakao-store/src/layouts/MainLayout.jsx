ab2b39e34982417edd84b3277cb63fc9ec7c2ba6
SeonghyeonKim <kkgojn@naver.com>
2023-07-14 12:43:06 +0900

    fix: Change Name

--- kakao-store/src/layouts/MainLayout.jsx
+++ kakao-store/src/layouts/MainLayout.jsx
@@ -1,4 +1,5 @@
 import Gnb from "../components/organisms/Gnb";
+import MainProductTemplate from "../components/templates/MainProductTemplate";
 
 const MainLayout = () => {
     return (
@@ -6,7 +7,7 @@ const MainLayout = () => {
             {/* 로그인 버튼, 장바구니 버튼, 메인 로고 */}
             <Gnb />
             {/* 콘텐츠 영역: 페이지마다 달라지는 영역 */}
-            <Outlet />
+            <MainProductTemplate />
             {/* 푸터 영역 */}
             {/*<Footer />*/}
         </>
