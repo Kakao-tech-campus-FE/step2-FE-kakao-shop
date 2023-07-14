7d29d7b92842b5a7119e2c34f2aaadc41a66ce2a
dabinchiii <dabinchi388@naver.com>
2023-07-14 23:40:53 +0900

    feat: Add global loader

--- src/App.js
+++ src/App.js
@@ -8,24 +8,28 @@ import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
-import { Suspense } from "react";
+import { Suspense, lazy } from "react";
 import Loader from "./components/atoms/Loader";
+
+// const HomePage = lazy(() => import('./pages/HomePage'));
 
 function App() {
     return (
         <div className="App">
-          <BrowserRouter>
-            <Routes>
-              {/* 단독 레이아웃 */}
-              <Route path="/login" element={<LoginPage />}/>
-              <Route path="/register" element={<RegisterPage />}/>
-              <Route path="/notFound" element={<NotFoundPage />} />
-              {/* 공통 레이아웃 */}
-              <Route element={<MainLayout />}>
-                <Route path="/" element={<HomePage />} />
-              </Route>
-            </Routes>
-          </BrowserRouter>
+          <Suspense fallback={<Loader />}>
+            <BrowserRouter>
+              <Routes>
+                {/* 단독 레이아웃 */}
+                <Route path="/login" element={<LoginPage />}/>
+                <Route path="/register" element={<RegisterPage />}/>
+                <Route path="/notFound" element={<NotFoundPage />} />
+                {/* 공통 레이아웃 */}
+                <Route element={<MainLayout />}>
+                  <Route path="/" element={<HomePage />} />
+                </Route>
+              </Routes>
+            </BrowserRouter>
+          </Suspense>
         </div>
     );
 }
