92074b193b655e5c722332ab7c9b3d88dad0561c
SeonghyeonKim <kkgojn@naver.com>
2023-07-14 12:42:31 +0900

    fix: Change Docs

--- kakao-store/src/components/atoms/Button.jsx
+++ kakao-store/src/components/atoms/Button.jsx
@@ -3,9 +3,8 @@
  * @param {function} onClick - 클릭 핸들러 함수
  * @param {React.ReactNode} children
  * @param {string} className 
- * @returns 버튼 컴포넌트 생성
+ * @returns 버튼 컴포넌트 
  */
-
 const Button = ({ onClick, children, className }) => {
 	return (
 		<button className={`whitespace-nowrap ${className}`}
--- kakao-store/src/components/atoms/Container.jsx
+++ kakao-store/src/components/atoms/Container.jsx
@@ -2,9 +2,8 @@
  * 컨테이너 컴포넌트 생성
  * @param {React.ReactNode} children
  * @param {string} className 
- * @returns 컨테이너 컴포넌트 생성
+ * @returns 컨테이너 컴포넌트
  */
-
 const Container = ({ children, className="" }) => {
     return (
         <div className={`container ${className}`}>
