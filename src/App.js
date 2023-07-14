16132a20735966dd50f47af149f6ae3f95e37af1
baegyeong <beth0929@naver.com>
2023-07-14 23:39:16 +0900

    fix: react-query 적용

--- src/App.js
+++ src/App.js
@@ -5,19 +5,24 @@ import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import { Provider } from "react-redux";
 import store from "./store/index.js";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
         <Provider store={store}>
-          <Routes>
-            <Route path={routes.register} element={<RegisterPage />} />
-            <Route path={routes.home} element={<HomePage />} />
-            <Route path={routes.login} element={<LoginPage />} />
-          </Routes>
+          <BrowserRouter>
+            <Routes>
+              <Route path={routes.register} element={<RegisterPage />} />
+              <Route path={routes.home} element={<HomePage />} />
+              <Route path={routes.login} element={<LoginPage />} />
+            </Routes>
+          </BrowserRouter>
         </Provider>
-      </BrowserRouter>
+      </QueryClientProvider>
     </>
   );
 }
