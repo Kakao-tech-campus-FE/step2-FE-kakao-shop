532654ddcbf83d456fe6eada6c5071aa8eda74ae
j8won <joowoni99@gmail.com>
2023-07-14 22:57:24 +0900

    feat: 페이지 전체에 대한 Global Loader 생성 및 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { RouterProvider } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import { ThemeProvider as StyledThemeProvider } from "styled-components";
@@ -5,13 +6,16 @@ import { ThemeProvider as StyledThemeProvider } from "styled-components";
 import GlobalStyle from "@/styles/GlobalStyle.js";
 import theme from "@/styles/theme.js";
 import router from "@/router.jsx";
+import GlobalLoader from "@/pages/GlobalLoader.jsx";
 
 function App() {
   return (
     <HelmetProvider>
       <StyledThemeProvider theme={theme}>
         <GlobalStyle />
-        <RouterProvider router={router} />
+        <Suspense fallback={<GlobalLoader />}>
+          <RouterProvider router={router} />
+        </Suspense>
       </StyledThemeProvider>
     </HelmetProvider>
   );
