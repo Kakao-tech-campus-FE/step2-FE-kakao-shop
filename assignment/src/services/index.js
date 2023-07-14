4c010e4b593baafe0d576dc2e9c2b9c312041be6
YANGSEOKWOO <didtjrdn030@naver.com>
2023-07-14 23:46:21 +0900

    feat : Error 캐칭 구현

--- assignment/src/services/index.js
+++ assignment/src/services/index.js
@@ -15,6 +15,32 @@ instance.interceptors.request.use((config) => {
   }
   return config;
 });
+instance.interceptors.response.use(
+  (response) => {
+    // 성공적인 응답 처리
+    return response;
+  },
+  (error) => {
+    // 에러 응답 처리
+    const { response } = error;
+    if (response) {
+      // 상태 코드에 따른 처리
+      if (response.status >= 300 && response.status < 400) {
+        console.error('Redirection:', response.data);
+      } else if (response.status >= 400 && response.status < 500) {
+        console.error('Client Error:', response.data);
+      } else if (response.status >= 500) {
+        console.error('Server Error:', response.data);
+      } else {
+        console.error('Unhandled Error:', response.data);
+      }
+    } else {
+      // 네트워크 오류 등의 처리
+      console.error('Network Error:', error.message);
+    }
+    return Promise.reject(error);
+  },
+);
 
 export default instance;
 
--- assignment/src/services/product.js
+++ assignment/src/services/product.js
@@ -1,16 +1,15 @@
+import { useQuery } from 'react-query';
 import instance from './index';
 
 // 상품을 가져오는 fetch API
 // Page 형식
-export const fetchProducts = (page = 0) =>
-  instance.get('/products' + '?page=' + page);
-// cursor (LastIndex)
-export const fetchProductFromCursor = (cursor) => {
-  return instance.get('/products' + '?cursor=' + cursor);
+export const fetchProducts = (page = 0) => {
+  return instance.get('/products?page=' + page);
 };
 
 // id 존재여부에 대한 처리 필요함 => 백엔드에 불필요한 것들 방지
 // 관례 : 에러에 대한 캐칭 : 맨위, 정상콜백 : 아래 / 이유 : if문이 지저문해짐
+
 export const getProductById = (id) => {
   if (!id) {
     throw Error('id가 필요합니다.');
