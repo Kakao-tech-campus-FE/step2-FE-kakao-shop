b451b324f38d651eeb130dc1dfa9219b3caa6769
YANGSEOKWOO <didtjrdn030@naver.com>
2023-07-14 23:42:16 +0900

    feat: Product Page 구성
    IntersectinObserver를 통한 threshold 구현

--- assignment/src/components/templates/MainProductTemplate.jsx
+++ assignment/src/components/templates/MainProductTemplate.jsx
@@ -1,32 +1,33 @@
-import React, { useEffect, useReducer, useRef, useState } from 'react';
+import React, { Suspense, useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Container from '../atoms/Container';
-import ProductGrid from '../organisms/ProductGrid';
 import { getProducts } from '../../store/slices/productSlice';
+import ProductGrid from '../organisms/ProductGrid';
 import Loader from '../atoms/Loader';
 
 const MainProductTemplate = () => {
   const [page, setPage] = useState(0);
   const bottomObserver = useRef(null);
   const dispatch = useDispatch();
-  const product = useSelector((state) => state.products.products);
+  const products = useSelector((state) => state.products.products);
   const loading = useSelector((state) => state.products.loading);
-  //   const error = useSelector((state) => state.product.error);
   const isEnd = useSelector((state) => state.products.isEnd);
 
   const io = new IntersectionObserver(
     (entries, observer) => {
       entries.forEach((entry) => {
-        if (!entry.isIntersecting && !isEnd) {
+        if (!entry.isIntersecting) return;
+
+        if (entry.isIntersecting && isEnd) {
           setPage((page) => page + 1);
         }
       });
     },
     {
-      threshold: 1,
+      threshold: 0.5,
     },
   );
-  // useRef에 접근할 때 current
+
   useEffect(() => {
     io.observe(bottomObserver.current);
   }, []);
@@ -37,8 +38,9 @@ const MainProductTemplate = () => {
 
   return (
     <Container>
-      <ProductGrid loading={loading} products={product} />
+      <ProductGrid products={products} loading={loading} />
       <div ref={bottomObserver} />
+      {/* <Loader /> */}
     </Container>
   );
 };
--- assignment/src/layouts/MainLayout.jsx
+++ assignment/src/layouts/MainLayout.jsx
@@ -2,8 +2,6 @@ import { Outlet } from 'react-router-dom';
 import React from 'react';
 import GNB from '../components/atoms/GNB';
 import MainProductTemplate from '../components/templates/MainProductTemplate';
-import MainProductTemplateCursortest from '../components/templates/MainProductTemplateCursortest';
-import MainP from '../components/templates/MainP';
 
 function MainLayout() {
   return (
@@ -11,7 +9,7 @@ function MainLayout() {
       {/* 로그인 버튼, 장바구니 버튼, 메인로고 */}
       <GNB />
       {/* 콘텐츠 영역 : 페이지마다 달라지는 영역 */}
-      <MainP />
+      <MainProductTemplate />
       {/* <Outlet /> */}
       {/* 푸터 영역 */}
     </>
