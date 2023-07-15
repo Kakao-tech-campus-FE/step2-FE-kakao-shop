96d0901019838eb623a9780ead285a01d8e3f205
charmeee <mandugohigh@gmail.com>
2023-07-15 10:41:08 +0900

    feat: NaN type처리

--- mandu/src/services/type.js
+++ mandu/src/services/type.js
@@ -1,6 +1,6 @@
 function ErrorType({message, status}) {
     this.message = message ?? "예상치못한 오류가 발생했습니다.";
-    this.status = Number(status) ?? 520;
+    this.status = !!Number(status) ? Number(status) : 520;
 }
 
 export {ErrorType};
