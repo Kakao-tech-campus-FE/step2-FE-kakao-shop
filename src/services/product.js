770e151117a6e784fbeaac88ead606289700e597
kimchanho97 <nh0903@pusan.ac.kr>
2023-07-17 00:59:51 +0900

    Fix: fetchProducts 에러캐칭 수정

--- src/services/product.js
+++ src/services/product.js
@@ -16,12 +16,9 @@ export const fetchProducts = async (page = 0) => {
     return response.data;
   } catch (error) {
     console.log("FetchProducts Api Error", error);
-    throw error;
+    return error;
   }
 };
-// ❔의문점:
-// - fetchProducts에서 에러를 return 하면 fetchProducts를 요청하는 곳에서
-//   Error객체를 못 받아 에러가 발생하는데 throw를 하면 받을 수 있음 -> useQuery에서 받는건가?
 
 export const getProductById = (id) => {
   if (!id) {
