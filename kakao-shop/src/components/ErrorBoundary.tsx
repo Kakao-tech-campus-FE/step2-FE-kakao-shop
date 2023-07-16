928271027539dd6578ba0bb0bfc42a4bdc3f507e
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-07-16 11:35:01 +0900

    feat: 에러바운더리

---
+++ kakao-shop/src/components/ErrorBoundary.tsx
@@ -0,0 +1,44 @@
+import React, { Fragment, PropsWithChildren } from 'react';
+
+import { Toast } from '@components/atom';
+
+type ErrorBoundaryState = {
+  status: number | null;
+  method: string | null;
+};
+
+export class ErrorBoundary extends React.Component<PropsWithChildren> {
+  state: ErrorBoundaryState = {
+    status: null,
+    method: null,
+  };
+
+  static getDerivedStateFromError(error: any) {
+    // Update state so the next render will show the fallback UI.
+    return { status: error.status, method: error.method };
+  }
+
+  componentDidCatch(error: any, errorInfo: any) {
+    const { status, method } = this.state;
+
+    if (status === 401) {
+      Toast.show(<Fragment>다시 로그인 해주세요.</Fragment>);
+      return;
+    }
+
+    if (status === 400) {
+      Toast.show(<Fragment>잘못된 요청입니다.</Fragment>);
+
+      return;
+    }
+
+    // You can also log the error to an error reporting service
+  }
+
+  render() {
+    const { children } = this.props;
+    const { status, method } = this.state;
+
+    return children;
+  }
+}
--- kakao-shop/src/index.tsx
+++ kakao-shop/src/index.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
+import { ErrorBoundary } from '@components/ErrorBoundary';
+
 import App from './App';
 import { store } from './store';
 import globalStyle from './styles/globalStyle';
@@ -11,8 +13,10 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Global styles={globalStyle} />
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ErrorBoundary>
   </React.StrictMode>,
 );
