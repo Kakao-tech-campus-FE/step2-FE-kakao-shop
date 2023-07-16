e87cf102982189da2df9b178c73b7d4448a63774
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-07-16 11:38:14 +0900

    refactor: 로그인시 이전의 products 데이터 초기화

--- kakao-shop/src/store/Home/reducers.ts
+++ kakao-shop/src/store/Home/reducers.ts
@@ -3,6 +3,7 @@ import { produce, Draft } from 'immer';
 export const FETCH_PRODUCT_DATA = 'home/FETCH_PRODUCT_DATA';
 export const SET_PRODUCT_DATA = 'home/SET_PRODUCT_DATA';
 export const SET_PRODUCT_LOADING_STATE = 'home/SET_PRODUCT_LOADING_STATE';
+export const RESET_HOME_STATE = 'home/RESET_HOME_STATE';
 
 export const productDataRequest = (payload: number): FetchProductDataAction => ({
   type: FETCH_PRODUCT_DATA,
@@ -18,13 +19,17 @@ export const setProductLoadingState = (): SetProductLoadingStateAction => ({
   type: SET_PRODUCT_LOADING_STATE,
 });
 
+export const resetHomeState = (): ResetHomeStateAction => ({
+  type: RESET_HOME_STATE,
+});
+
 export const initialState: HomeState = {
   isLoading: false,
   products: [],
 };
 
 export const homeReducer = produce(
-  (draft: Draft<HomeState>, action: SetProductDataAction | SetProductLoadingStateAction) => {
+  (draft: Draft<HomeState>, action: SetProductDataAction | SetProductLoadingStateAction | ResetHomeStateAction) => {
     switch (action.type) {
       case SET_PRODUCT_LOADING_STATE:
         draft.isLoading = true;
@@ -33,6 +38,11 @@ export const homeReducer = produce(
       case SET_PRODUCT_DATA:
         draft.products.push(...action.payload.response);
         draft.isLoading = false;
+        break;
+
+      case RESET_HOME_STATE:
+        draft.isLoading = false;
+        draft.products = [];
         break;
 
       default:
@@ -54,6 +64,10 @@ export type SetProductDataAction = {
 
 export type SetProductLoadingStateAction = {
   type: typeof SET_PRODUCT_LOADING_STATE;
+};
+
+export type ResetHomeStateAction = {
+  type: typeof RESET_HOME_STATE;
 };
 
 type ProductResponse = {
