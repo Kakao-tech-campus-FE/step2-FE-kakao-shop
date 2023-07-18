028066ee45b0d28946d4a8997b08bafa54e6daad
lcqff <yeonnu82g@gmail.com>
2023-07-18 12:32:23 +0900

    feat: 헤더 css 수정

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { RootState } from 'src/store';
 import { loginStore } from '@store/slices/userSlice';
+import FilledButton from '@components/atoms/button/FilledButton';
 
 const Header = () => {
   const isLoggedIn = useSelector((state: RootState) => state.user.isLoggedIn);
@@ -13,16 +14,18 @@ const Header = () => {
   };
 
   return (
-    <div className="absolute w-screen bg-subGray h-header flex items-center">
-      {!isLoggedIn ? (
-        <Link to="/login" className="absolute right-[10px]">
-          로그인
-        </Link>
-      ) : (
-        <button type="button" onClick={handleLogout} className="absolute right-[10px]">
-          로그아웃
-        </button>
-      )}
+    <div className="absolute w-screen bg-white ">
+      <div className=" h-header flex items-center mx-[100px] float-right">
+        {!isLoggedIn ? (
+          <Link to="/login">
+            <FilledButton>로그인</FilledButton>
+          </Link>
+        ) : (
+          <button type="button" onClick={handleLogout} className="absolute right-[10px]">
+            로그아웃
+          </button>
+        )}
+      </div>
     </div>
   );
 };
