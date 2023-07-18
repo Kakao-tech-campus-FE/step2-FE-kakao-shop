d33ba20cb3a7b6ca4f251a39d8119ca0c0844c9f
charmeee <mandugohigh@gmail.com>
2023-07-18 12:01:31 +0900

    feat: 상품 img hover시 사진 확대

--- mandu/src/components/molecules/ProductCard.js
+++ mandu/src/components/molecules/ProductCard.js
@@ -4,10 +4,11 @@ export const ProductCard = ({id, title, image, link, price}) => {
     return (
         <div id={id} className="w-72 ml-5 mb-7">
             <Link to={link}>
-                <div className="card__image">
+                <div className="w-full h-44 rounded-2xl overflow-hidden">
                     <img
                         loading="lazy"
-                        className="w-full h-40 object-cover rounded-2xl" src={process.env.REACT_APP_API_URL + image}
+                        className="object-fill hover:ease-out hover:scale-105 transition-all duration-500 "
+                        src={process.env.REACT_APP_API_URL + image}
                         alt={title}
                     />
                 </div>
@@ -26,11 +27,11 @@ export const SkeletonProductCard = () => {
     return (
         <div className="w-72 ml-5 mb-7">
             <div className="card__image">
-                <div className="w-full h-40 bg-gray-300 rounded-2xl"></div>
+                <div className="w-full h-40 bg-gray-300 rounded-2xl"/>
             </div>
             <div className="my-2">
-                <h3 className="bg-gray-300 w-1/2 h-4 mb-2"></h3>
-                <h3 className="bg-gray-300 w-1/4 h-4"></h3>
+                <h3 className="bg-gray-300 w-1/2 h-4 mb-2"/>
+                <h3 className="bg-gray-300 w-1/4 h-4"/>
             </div>
         </div>
     );
