77cd90969d84f2d3c424a9f4b757b8a361d864ed
H-sooyeon <wndqhr__@naver.com>
2023-07-19 23:40:55 +0900

    feat: 전체 상품 출력

--- src/components/organisms/ProductGrid.jsx
+++ src/components/organisms/ProductGrid.jsx
@@ -2,7 +2,6 @@ import ProductCard from '../molecules/ProductCard';
 import * as Grid from '../../styles/organisms/ProductGrid';
 
 const ProductGrid = ({ products }) => {
-  console.log('products: ', products);
   return (
     <Grid.Product>
       {products
