103cfae2c3951a218d1d9c6c6d93974602b71506
lcqff <yeonnu82g@gmail.com>
2023-07-19 22:39:41 +0900

    feat: productDetailPage 에러캐칭, 로더 적용

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -1,18 +1,19 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import { getProductById } from '@api/productApi';
 import { getDetail } from '@store/slices/detailSlice';
 import { AppDispatch } from '@store/index';
+import Loader from '@components/atoms/Loader';
 
 const ProductDetailPage = () => {
   const { id } = useParams();
   const parseId = id !== undefined ? parseInt(id, 10) : 1;
   const { data, error, isLoading } = useQuery(`/product/${id}`, () => getProductById(parseId));
   const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
 
-  console.log('Asdf');
   useEffect(() => {
     if (data) {
       if (parseId) dispatch(getDetail(parseId));
@@ -20,11 +21,11 @@ const ProductDetailPage = () => {
   }, [dispatch, data, parseId]);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <Loader />;
   }
 
   if (error) {
-    return <div>Error: {id}</div>;
+    navigate(-1);
   }
 
   return <p>Product ID: {id}</p>;
