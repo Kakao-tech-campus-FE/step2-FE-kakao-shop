07337d00dfc7bd1c89a1b7b1bf557b5a5d09279c
kimjinwoo <rlawlsdn177@naver.com>
2023-07-19 21:23:45 +0900

    Store 파일 추가

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
-
+import productReducer from "./slices/productSlice";
+import detailReducer from "./slices/detailSlice";
 const store = configureStore({
   reducer: {
     //user reducer
     user: userReducer,
+    product: productReducer,
+    detail: detailReducer,
   },
 });
 
