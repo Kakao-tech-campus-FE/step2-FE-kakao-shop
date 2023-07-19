cc0c06c3ef774c8e61fa4eb7265657e9c4524aa0
kjh302903 <kjh302903@gmail.com>
2023-07-19 22:22:46 +0900

    feat: Counter 컴포넌트 추가
    옵션 개수 조절하는 Counter 첨포넌트 추가

---
+++ src/components/atoms/Counter/index.tsx
@@ -0,0 +1,57 @@
+import { styled } from "styled-components";
+import { useState } from "react";
+
+interface Props {
+  onChange: (count: number) => void;
+}
+
+const Counter = ({ onChange }: Props) => {
+  const [count, setCount] = useState(1);
+
+  return (
+    <Wrapper>
+      <button
+        disabled={count === 1 ? true : false}
+        onClick={() => {
+          setCount(count - 1);
+          onChange(count - 1);
+        }}
+      >
+        -
+      </button>
+      <span>{count}</span>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+          onChange(count + 1);
+        }}
+      >
+        +
+      </button>
+    </Wrapper>
+  );
+};
+
+export default Counter;
+
+const Wrapper = styled.div`
+  button {
+    width: 29px;
+    height: 26px;
+    border: 1px solid #d8d8d8;
+    background-color: #fff;
+    cursor: pointer;
+  }
+  span {
+    display: inline-block;
+    width: 50px;
+    height: 24px;
+    border-top: 1px solid #d8d8d8;
+    border-bottom: 1px solid #d8d8d8;
+    background-color: #fff;
+    font-size: 15px;
+    line-height: 26px;
+    vertical-align: top;
+    text-align: center;
+  }
+`;
