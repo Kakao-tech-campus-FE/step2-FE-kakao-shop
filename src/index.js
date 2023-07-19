b901ecaef6f36cb1e6a0760240b500e9a093cf9f
kimjinwoo <rlawlsdn177@naver.com>
2023-07-19 21:24:43 +0900

    index.js 수정

--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@ import "./styles/index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
-import { CookiesProvider } from "react-cookie";
+import { QueryClient, QueryClientProvider } from "react-query";
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <CookiesProvider>
-      <Provider store={store}>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
         <App />
-      </Provider>
-    </CookiesProvider>
+      </QueryClientProvider>
+    </Provider>
   </React.StrictMode>
 );
