64ee0f0db5d00dad3159243bfd1d14713d8b8580
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-07-19 17:25:58 +0900

    feat: Layout

--- kakao-shop/src/App.tsx
+++ kakao-shop/src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import NotFound from '@pages/404';
 import Error from '@pages/500';
@@ -7,12 +8,17 @@ import Home from '@pages/Home';
 import Login from '@pages/Login';
 import SignUp from '@pages/SignUp';
 
+import Footer from '@components/molecules/Footer';
+import Header from '@components/molecules/Header';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={'/'} element={<Home />} />
-        <Route path={'/detail/:id'} element={<Detail />} />
+        <Route element={<Layout />}>
+          <Route path={'/'} element={<Home />} />
+          <Route path={'/detail/:id'} element={<Detail />} />
+        </Route>
         <Route path={'/signup'} element={<SignUp />} />
         <Route path={'/login'} element={<Login />} />
         <Route path={'/500'} element={<Error />} />
@@ -21,5 +27,15 @@ function App() {
     </BrowserRouter>
   );
 }
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
 export default App;
--- kakao-shop/src/pages/Home/index.tsx
+++ kakao-shop/src/pages/Home/index.tsx
@@ -49,7 +49,6 @@ function Home() {
 
   return (
     <Fragment>
-      <Header />
       <S.TopBannerBlock>
         <BannerImageList />
       </S.TopBannerBlock>
