4b06a1faadd94789d756656564c3f68eadd91fba
H-sooyeon <wndqhr__@naver.com>
2023-07-19 23:44:37 +0900

    feat: 개별 상품 정보 출력

---
+++ src/components/molecules/ProductDetailInfo.jsx
@@ -0,0 +1,22 @@
+import { comma } from "../../utils/convert";
+import Photo from "../atoms/Photo";
+import * as Product from "../../styles/molecules/ProductDetailInfo";
+import Star from "../atoms/Star";
+
+const ProductDetailInfo = ({product}) => {
+
+    return(
+        <Product.ProductInfoContainer>
+            <Photo className="productItem" src={product.response.image} alt="개별 상품 이미지"/>
+            <Product.TextContainer>
+                <Star count={product.response.starCount} color="#1478FF"/>
+                <Product.ProductTitle>{product.response.productName}</Product.ProductTitle>
+                <Product.ProductPrice>
+                    톡딜가 {comma(product.response.price)}원 ~
+                </Product.ProductPrice>
+            </Product.TextContainer>
+        </Product.ProductInfoContainer>
+    );
+};
+
+export default ProductDetailInfo;
