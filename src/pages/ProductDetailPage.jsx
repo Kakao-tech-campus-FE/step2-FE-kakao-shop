de83b399643d9633368c397caa68e170c72945e2
H-sooyeon <wndqhr__@naver.com>
2023-07-19 23:42:49 +0900

    feat: 개별 상품 정보 출력

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,8 +1,37 @@
-import Loader from "../components/atoms/Loader"
+import { useQuery } from "react-query";
+import { useNavigate, useParams } from "react-router-dom";
+import { getProductById } from "../apis/product";
+import ProductDetailInfo from "../components/molecules/ProductDetailInfo";
+import Loader from '../components/atoms/Loader';
+import ProductOption from "../components/molecules/ProductOption";
+import * as Product from '../styles/pages/ProductDetailPage';
+import { useEffect } from "react";
 
 const ProductDetailPage = () => {
+    const navigate = useNavigate();
+
+    const { id } = useParams(); // string
+    const parsedId = parseInt(id, 10);
+    const {data: product, isLoading, refetch} = useQuery("product", () => getProductById(parsedId), {
+        onError: (error) => {
+            console.log(`something went wrong ${error.mssage}`);
+            navigate('/error');
+        },
+    });
+
+    useEffect(() => {
+        refetch();
+    }, [refetch, id])
+
     return (
-        <div>{<Loader />}</div>
+        <Product.Container>
+            {isLoading || !product?.data ? <Loader /> : (
+                <>
+                    <ProductDetailInfo product={product.data}/>
+                    <ProductOption product={product.data} />
+                </>
+            )}
+        </Product.Container>
     );
 };
 
