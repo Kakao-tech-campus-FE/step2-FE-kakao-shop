cd679bae3d0bca4515e71745368bd1ff3741ea6f
chaemina <chaemina82@naver.com>
2023-07-20 04:25:48 +0900

    feat : Loader 적용 범위 확장
    전역 변수 Loader를 페이지간의 이동이 발생했을 때 실행되도록 수정함
    Skeleton을 상품 카드만 로드 될 때 적용시키고자 하는데, 오류 발생으로
    우선 로더로 대체

--- src/App.js
+++ src/App.js
@@ -4,11 +4,34 @@ import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
 import MainLayout from "./layouts/MainLayout";
+import Loading from "./components/atoms/Loader";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const handleRouteChangeStart = () => {
+      setIsLoading(true); // 페이지 이동 시작 시 로딩 상태를 설정
+    };
+
+    const handleRouteChangeComplete = () => {
+      setIsLoading(false); // 페이지 이동 완료 시 로딩 상태를 해제
+    };
+
+    // 페이지 이동 이벤트 리스너 등록
+    window.addEventListener("beforeunload", handleRouteChangeStart);
+    window.addEventListener("load", handleRouteChangeComplete);
+
+    return () => {
+      // 페이지 이동 이벤트 리스너 해제
+      window.removeEventListener("beforeunload", handleRouteChangeStart);
+      window.removeEventListener("load", handleRouteChangeComplete);
+    };
+  }, []);
   return (
     <div className="App">
       <BrowserRouter>
+        {isLoading && <Loading />}
         <Routes>
           {/* 단독 레이아웃 */}
           <Route path="/login" element={<LoginPage />}></Route>
--- src/components/atoms/Loader.jsx
+++ src/components/atoms/Loader.jsx
@@ -24,8 +24,8 @@ const LoadingText = styled.div`
 export const Loading = () => {
   return (
     <Background>
-      <LoadingText>잠시만 기다려 주세요</LoadingText>
-      <img src={spinner} alt="loading..." width="30%" />
+      {/* <LoadingText>잠시만 기다려 주세요</LoadingText> */}
+      <img src={spinner} alt="loading..." width="5%" />
     </Background>
   );
 };
