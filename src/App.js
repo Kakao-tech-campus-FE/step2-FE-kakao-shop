89e9a87b8ec615d785f212d00e512880912baff0
baegyeong <beth0929@naver.com>
2023-07-20 22:53:05 +0900

    feat: cart router 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import routes from "./routes.js";
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import CartPage from "./pages/CartPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
 import { Provider } from "react-redux";
 import store from "./store/index.js";
 import { QueryClientProvider, QueryClient } from "react-query";
@@ -19,6 +21,11 @@ function App() {
               <Route path={routes.register} element={<RegisterPage />} />
               <Route path={routes.home} element={<HomePage />} />
               <Route path={routes.login} element={<LoginPage />} />
+              <Route path={routes.cart} element={<CartPage />} />
+              <Route
+                path="/product/:id"
+                element={<ProductDetailPage />}
+              ></Route>
             </Routes>
           </BrowserRouter>
         </Provider>
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const routes = {
   home: "/",
   register: "/register",
   login: "/login",
+  cart: "/carts",
 };
 
 export default routes;
