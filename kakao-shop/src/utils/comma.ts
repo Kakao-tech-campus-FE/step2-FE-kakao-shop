610b3b2e112996283864fe7632bf3395122accd0
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-07-20 22:12:19 +0900

    refactor: comma 파라미터 타입 변경

--- kakao-shop/src/utils/comma.ts
+++ kakao-shop/src/utils/comma.ts
@@ -1,4 +1,4 @@
-export const comma = (num: number) => {
+export const comma = (num?: number) => {
   // 타입스크립트를 사용하더라도 실제 들어오는 값은 선언된 타입의 값이 들어온다는 보장이 없다. 따라서 타입가드로 에러를 대비해줘야한다.
   if (num === null || num === undefined) return 0;
 
