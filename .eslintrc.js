1a6cdfb23522e48d69c8b8582d59c63746b19edc
lcqff <yeonnu82g@gmail.com>
2023-07-20 15:49:17 +0900

    chore: eslint 설정 제거 - 빨간줄만 사라지고 오류는 그대로길래 그냥 설정 뺌..

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,6 @@ module.exports = {
     'import/extensions': 'off',
     'react/require-default-props': 'off',
     'react/jsx-props-no-spreading': 'off',
-    'react/react-in-jsx-scope': 'off',
     'react/function-component-definition': [
       'error',
       {
