e0f0544e02a309389d1ce427f953f7bc3d99680d
Heo_Donghyuk <missko30@pusan.ac.kr>
2023-07-21 02:51:07 +0900

    feat: add Link to MainPage on Login, Register Page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -3,17 +3,21 @@
 //추후에 RegisterPage에 다른 컴포넌트들이 추가될 여지가 있기 때문에 분리
 //페이지에서 이용되는 API 관리 코드가 추가될 수 있기 때문
 
+import { Link } from "react-router-dom";
 import Container from "../components/atoms/Container";
 import RegisterForm from "../components/organisms/RegisterForm";
 
 const RegisterPage = () => {
   return (
     <Container className="mx-auto pt-12">
-      <img
-        src="/logoKakaoText.png"
-        alt="logoKakaoText"
-        className="w-30 h-11 mx-auto"
-      />
+      <Link to={"/"}>
+        <img
+          src="/logoKakaoText.png"
+          alt="logoKakaoText"
+          className="w-30 h-11 mx-auto"
+        />
+      </Link>
+
       <RegisterForm />
     </Container>
   );
