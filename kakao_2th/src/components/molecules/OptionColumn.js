4166ad0f7ceaaeb653579dbbb7ef09a4eef834f2
Klomachenko <leekyumin0901@naver.com>
2023-07-21 06:22:26 +0900

    feat: optioncolumn 수정

--- kakao_2th/src/components/molecules/OptionColumn.js
+++ kakao_2th/src/components/molecules/OptionColumn.js
@@ -1,23 +1,20 @@
-import OptionList from "../atoms/OptionList"
 import "../../styles/Molecules/OptionColumn.css"
-import { useState } from "react"
-import Counter from "../atoms/Counter"
-import { comma } from "../../utils/convert"
-import { useMutation } from "react-query"
-import Button from "../atoms/Button"
-import { addCart } from "../services/cart"; // Import the addCart function from cart.js
+import React, { useState } from "react";
+import OptionList from "../atoms/OptionList";
+import Counter from "../atoms/Counter";
+import { comma } from "../../utils/convert";
+import Button from "../atoms/Button";
+import { useMutation } from "react-query";
+import { addCart } from "../services/cart";
 
 const OptionColumn = ({ product }) => {
-    const [selectedOptions, setSelectedOptions] = useState([])
+    const [selectedOptions, setSelectedOptions] = useState([]);
 
     const handleOnClickOption = (option) => {
-
-        const isOptionSelected = selectedOptions.find(
-            (el) => el.optionId === option.id
-        )
+        const isOptionSelected = selectedOptions.find((el) => el.optionId === option.id);
 
         if (isOptionSelected) {
-            return
+            return;
         }
 
         setSelectedOptions((prev) => [
@@ -26,45 +23,43 @@ const OptionColumn = ({ product }) => {
                 optionId: option.id,
                 quantity: 1,
                 price: option.price,
-                name: option.optionName
-            }
-        ])
-    }
+                name: option.optionName,
+            },
+        ]);
+    };
 
     const handleOnChange = (count, optionId) => {
-        setSelectedOptions((prev) => {
-            return prev.map((el) => {
-                if (el.optionId === optionId) {
-                    return {
+        setSelectedOptions((prev) =>
+            prev.map((el) =>
+                el.optionId === optionId
+                    ? {
                         ...el,
                         quantity: count,
                     }
-                }
-                return el;
-            })
-        })
-    }
+                    : el
+            )
+        );
+    };
 
-    const { mutate } = useMutation({
-        mutationFn: addCart,
-    })
+    const { mutate, isLoading, isError } = useMutation(addCart, {
+        onSuccess: () => {
+            alert("장바구니에 담겼습니다.");
+        },
+        onError: () => {
+            alert("장바구니 담기에 실패했습니다.");
+        },
+    });
 
     return (
         <div className="option-column">
             <h3>옵션 선택</h3>
-            {/* 옵션 담기 영역 */}
-            <OptionList
-                options={product.options}
-
-                onClick={handleOnClickOption}
-            />
-            {/* 담긴 옵션이 표기 */}
+            <OptionList options={product.options} onClick={handleOnClickOption} />
             {selectedOptions.map((option) => (
-                <ol key={option.id} className="selected-option-list">
+                <ol key={option.optionId} className="selected-option-list">
                     <li className="selected-option">
                         <Counter
-                            onIncrease={(count) => handleOnChange(count, option.id)}
-                            onDecrease={(count) => handleOnChange(count, option.id)}
+                            onIncrease={(count) => handleOnChange(count, option.optionId)}
+                            onDecrease={(count) => handleOnChange(count, option.optionId)}
                         />
                         <span className="name">{option.name}</span>
                         <span className="price">{comma(option.price)}원</span>
@@ -74,44 +69,33 @@ const OptionColumn = ({ product }) => {
             <hr />
             <div className="total-price">
                 <span>
-                    총 수량:{" "}
-                    {comma(selectedOptions.reduce((acc, cur) => {
-                        return acc + cur.quantity
-                    }, 0))}
-                    개
+                    총 수량: {comma(selectedOptions.reduce((acc, cur) => acc + cur.quantity, 0))}개
                 </span>
-                <span>총 상품금액
-                    {comma(selectedOptions.reduce((acc, cur) => {
-                        return acc + cur.quantity * cur.price
-                    }, 0))}
-                    개
+                <span>
+                    총 상품금액:{" "}
+                    {comma(
+                        selectedOptions.reduce((acc, cur) => acc + cur.quantity * cur.price, 0)
+                    )}원
                 </span>
             </div>
             <div className="button-group">
                 <Button
                     onClick={() => {
                         mutate(
-                            selectedOptions.map((el) => {
-                                return {
-                                    optionId: el.id,
-                                    quantity: el.quantity
-                                }
-                            }), {
-                            onSuccess: () => {
-                                alert("장바구니에 담겼습니다.")
-                            },
-                            onError: () => {
-                                alert("장바구니 담기에 실패했습니다.")
-                            }
-                        }
-
-                        )
-                    }}>
-
+                            selectedOptions.map((el) => ({
+                                optionId: el.optionId,
+                                quantity: el.quantity,
+                            }))
+                        );
+                    }}
+                    disabled={isLoading}
+                >
+                    {isLoading ? "처리 중..." : "장바구니 담기"}
                 </Button>
+                {isError && <div>에러가 발생했습니다.</div>}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default OptionColumn
+export default OptionColumn;
