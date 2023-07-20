b7a6ef193f0c348dd0e6d87860ee5e9fae2efb0c
Klomachenko <leekyumin0901@naver.com>
2023-07-21 06:25:20 +0900

    feat: CartPage 수정

--- kakao_2th/src/pages/CartPage.js
+++ kakao_2th/src/pages/CartPage.js
@@ -1,16 +1,17 @@
-import { Suspense } from "react";
-import { useQuery } from "react-query"
+import React from "react";
+import { useQuery } from "react-query";
 import Loader from "../components/atoms/Loader";
 import CartList from "../components/molecules/CartList";
-
+import { getCart } from "../services/cart"; // Import the API function to get cart data
 
 const CartPage = () => {
-    const { data } = useQuery("cart", getCart);
-    return (
-        <Suspense fallback={<Loader />}>
-            <CartList data={data} />
-        </Suspense>
-    )
-}
+    const { data, isLoading } = useQuery("cart", getCart);
 
-export default CartPage
+    if (isLoading) {
+        return <Loader />;
+    }
+
+    return <CartList data={data} />;
+};
+
+export default CartPage;
