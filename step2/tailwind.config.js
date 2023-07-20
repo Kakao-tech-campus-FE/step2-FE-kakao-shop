b7d488f2e64a8a7fb84415e21b970518a2030ad5
JH <tnptt1122@gmail.com>
2023-07-21 00:33:28 +0900

    refactor: tailwind 단위 수정

--- step2/tailwind.config.js
+++ step2/tailwind.config.js
@@ -9,17 +9,27 @@ module.exports = {
         "primary-active": "#f2d00f",
       },
       minWidth: {
-        "400": "400px",
+        400: "400px",
+        800: "800px",
       },
       maxWidth: {
-        "750": "750px",
+        750: "750px",
+      },
+      minHeight: {
+        50: "50px",
       },
       width: {
-        "75": "75%",
-        "90": "90px",
+        30: "30px",
+        50: "50px",
+        60: "60px",
+        90: "90px",
+        "75%": "75%",
+      },
+      height: {
+        60: "60px",
+        80: "80px",
       },
     },
   },
   plugins: [],
-}
-
+};
