de5189371beb6b19b2fa3d12588c8a16dd570396
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-07-20 22:18:13 +0900

    feat: 선택된 옵션사항 목록 컴포넌트

---
+++ kakao-shop/src/components/page/Detail/SelectOptionItemList/index.tsx
@@ -0,0 +1,30 @@
+import { Fragment, MouseEventHandler } from 'react';
+
+import SelectOptionItem from '@components/page/Detail/SelectOptionItem';
+
+import { UserSelectOption } from '@hooks/page/Detail/useOptionForm';
+
+type Props = {
+  options?: UserSelectOption[];
+  increaseQuantity: (id: number) => MouseEventHandler<HTMLButtonElement>;
+  decreaseQuantity: (id: number) => MouseEventHandler<HTMLButtonElement>;
+};
+const SelectOptionItemList = ({ options, increaseQuantity, decreaseQuantity }: Props) => {
+  return (
+    <Fragment>
+      {options?.map(option => {
+        if (!option.isSelected) return null;
+        return (
+          <SelectOptionItem
+            key={option.id}
+            option={option}
+            increaseQuantity={increaseQuantity}
+            decreaseQuantity={decreaseQuantity}
+          />
+        );
+      })}
+    </Fragment>
+  );
+};
+
+export default SelectOptionItemList;
