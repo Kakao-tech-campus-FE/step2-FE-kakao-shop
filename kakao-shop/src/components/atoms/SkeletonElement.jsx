affb29dde91aab057f3b903225d4c1ff3dbbc359
Song Jihye <qwer15948@pusan.ac.kr>
2023-07-20 16:03:08 +0900

    week4/Refactor: Refactoring SkeletonElement component
    - 코드 리뷰에서 제안해주신 내용 적용

--- kakao-shop/src/components/atoms/SkeletonElement.jsx
+++ kakao-shop/src/components/atoms/SkeletonElement.jsx
@@ -1,16 +1,14 @@
 const SkeletonElement = ({ type, className }) => {
-  const typeObj = {
-    title: "skeleton-title h-5",
-    text: "skeleton-text h-4",
-    thumbnail: "skeleton-thumbnail w-inherit h-60 border-radius-10",
-    price: "skeleton-price h-6 w-1/2",
-    icon: "skeleton-icon h-6 w-6",
+  const typeToClassMap = {
+    title: 'skeleton-title h-5',
+    text: 'skeleton-text h-4',
+    thumbnail: 'skeleton-thumbnail w-inherit h-60 border-radius-10',
+    price: 'skeleton-price h-6 w-1/2',
+    icon: 'skeleton-icon h-6 w-6',
   };
 
   return (
-    <div
-      className={`skeleton animate-pulse bg-slate-200 rounded ${typeObj[type]} ${className}`}
-    ></div>
+    <div className={`skeleton animate-pulse bg-slate-200 rounded ${typeToClassMap[type] ?? ''} ${className}`}></div>
   );
 };
 
