73d501e1f077790a5d470ca32739dc4706740483
lcqff <yeonnu82g@gmail.com>
2023-07-20 11:04:53 +0900

    chore: atomic design pattern에 맞는 폴더구조 정리 - pages 폴더를 components 안으로 넣음 - 1주차 과제는 밖으로 뺌

--- src/router/mainRouter.tsx
+++ src/router/mainRouter.tsx
@@ -1,12 +1,12 @@
 import { useRoutes } from 'react-router-dom';
 import React from 'react';
-import LoginPage from '@pages/LoginPage';
-import Home from '@pages/Home';
-import RegisterPage from '@pages/RegisterPage';
-import MainLayout from '@components/templates/MainLayout';
+import LoginPage from '@components/pages/LoginPage';
+import Home from '@components/pages/Home';
+import RegisterPage from '@components/pages/RegisterPage';
+import MainLayout from '@components/layout/MainLayout';
 import PageContainer from '@components/atoms/PageContainer';
-import ProductListPage from '@pages/ProductListPage';
-import ProductDetailPage from '@pages/ProductDetailPage';
+import ProductListPage from '@components/pages/ProductListPage';
+import ProductDetailPage from '@components/pages/ProductDetailPage';
 
 const mainRouter = () =>
   useRoutes([
