0998596e6476b8e3db283b0c410533831809802c
Heo_Donghyuk <missko30@pusan.ac.kr>
2023-07-20 12:44:58 +0900

    feat: implement appi request method getCart, updateCart at cart.js

--- src/services/api/cart.js
+++ src/services/api/cart.js
@@ -4,7 +4,7 @@ import { instance } from "./index" // axios instance import
  * 장바구니 담기
  * @param {object} payload
  * */
-const addCart = (payload) => {
+export const addCart = (payload) => {
   // payload에 담기는 내용의 형태
   /* 
   {
@@ -13,4 +13,19 @@ const addCart = (payload) => {
   } */
   return instance.post("/carts/add", payload)
 }
-export default addCart;
+
+/**
+ * 장바구니 조희
+ * @param {object} payload
+ * */
+export const getCart = () => {
+  return instance.get("/carts")
+}
+
+/**
+ * 장바구니 수정
+ * @param {object} payload
+ * */
+export const updateCart = (payload) => {
+  return instance.post("/carts/update", payload)
+}
