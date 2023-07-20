b15facfe185fb95c2b5c8b36db38cda88c8cdf68
lcqff <yeonnu82g@gmail.com>
2023-07-20 16:09:12 +0900

    chore: 필요없는 코드 삭제

--- src/components/pages/ProductDetailPage.tsx
+++ src/components/pages/ProductDetailPage.tsx
@@ -1,10 +1,7 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import { getProductById } from '@api/productApi';
-import { getDetail } from '@store/slices/detailSlice';
-import { AppDispatch } from '@store/index';
 import Loader from '@components/atoms/Loader';
 import ProductDetailTemplate from '@components/templates/ProductDetailTemplate';
 
@@ -12,15 +9,8 @@ const ProductDetailPage = () => {
   const { id } = useParams();
   const parseId = id !== undefined ? parseInt(id, 10) : 1;
   const { data, error, isLoading } = useQuery(`/product/${id}`, () => getProductById(parseId));
-  // const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
   const product = data?.data.response;
-
-  // useEffect(() => {
-  //   if (data) {
-  //     if (parseId) dispatch(getDetail(parseId));
-  //   }
-  // }, [dispatch, data, parseId]);
 
   return (
     <>
