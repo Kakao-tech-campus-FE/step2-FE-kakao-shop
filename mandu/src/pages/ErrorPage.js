412c6f965890f4b3671a131bcb4d4fce48924553
charmeee <mandugohigh@gmail.com>
2023-07-21 18:46:38 +0900

    feat: 에러 페이지 추가

---
+++ mandu/src/pages/ErrorPage.js
@@ -0,0 +1,16 @@
+import {Link} from "react-router-dom";
+
+const ErrorPage = ({error}) => {
+    const {status, message} = error
+    return (
+        <div className="mx-auto my-40">
+            <h1 className="text-6xl font-bold text-center">{status}</h1>
+            <h2 className="text-3xl text-center">{message}</h2>
+            <div className="text-center m-3">
+                <Link to={"/"} className="text-2xl bg-gray-300 p-3 inline-block rounded">메인으로 돌아가기</Link>
+            </div>
+        </div>
+    )
+}
+
+export default ErrorPage;
