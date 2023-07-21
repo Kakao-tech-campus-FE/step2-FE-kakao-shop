04c719e6ca2c8671947ec3b88b37a04a3d98cecd
Heo_Donghyuk <missko30@pusan.ac.kr>
2023-07-22 07:35:46 +0900

    feat: add, implement item number badge at GNBMenuUtil - cart

--- src/components/molecules/GNBMenuUtil.jsx
+++ src/components/molecules/GNBMenuUtil.jsx
@@ -1,13 +1,36 @@
 import { Link } from "react-router-dom";
 import Box from "../atoms/Box";
+import { useQuery } from "react-query";
+import { getCart } from "../../services/api/cart";
+import { useState } from "react";
+import { useEffect } from "react";
 
 // 검색, 배송정보, 장바구니
 // 장바구니 이미지로만 임시 구현
 const GNBMenuUtil = ({ className = "" }) => {
+  const { data } = useQuery("cart", getCart);
+  const [itemNum, setItemNum] = useState(data?.data?.response?.products.length);
+
+  useEffect(() => {
+    setItemNum(
+      data?.data?.response?.products.reduce((acc, cur) => {
+        return (
+          acc +
+          !cur.carts.every((option) => {
+            return option.quantity === 0;
+          })
+        );
+      }, 0)
+    );
+  });
+
   return (
     <Box className={className}>
       <Link to="/cart">
         <img alt="shopping cart" src="/cart.png" className="w-9 h-9" />
+        <span className="absolute top-[24px] left-[24px] min-w-[17px] px-[5px] rounded-[17px] text-center text-[11px] text-white font-semibold bg-red-500">
+          {itemNum && itemNum === 0 ? "" : itemNum}
+        </span>
       </Link>
     </Box>
   );
