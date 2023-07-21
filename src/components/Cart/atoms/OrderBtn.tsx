8959280b9d54201b004e8fe049a6310e935ad307
dongyeopCha <ckehdduq95@gmail.com>
2023-07-21 16:51:23 +0900

    feat: 주문하기 버튼 컴포넌트 구현

---
+++ src/components/Cart/atoms/OrderBtn.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function OrderBtn() {
+  return (
+    <button type="button" className="bg-yellow-300 p-3 font-bold w-full">
+      주문하기
+    </button>
+  );
+}
+
+export default OrderBtn;
