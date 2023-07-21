77c266c4c842d3f93565b6b3d4ecd6d40f4c7230
charmeee <mandugohigh@gmail.com>
2023-07-21 18:46:53 +0900

    feat: 로딩 페이지 추가

---
+++ mandu/src/pages/LoadingPage.js
@@ -0,0 +1,11 @@
+import Loader from "../components/atoms/Loader";
+
+const LoadingPage = () => {
+    return (
+        <div className="mx-auto pb-40 min-h-screen flex justify-center items-center">
+            <Loader/>
+        </div>
+    )
+}
+
+export default LoadingPage;
