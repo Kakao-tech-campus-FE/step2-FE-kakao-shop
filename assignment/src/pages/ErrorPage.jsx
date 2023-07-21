c2c08a0e1cad09bb399baeeed980283e96fd1d32
YANGSEOKWOO <didtjrdn030@naver.com>
2023-07-21 23:25:12 +0900

    feat: Error페이지 구현

--- assignment/src/pages/ErrorPage.jsx
+++ assignment/src/pages/ErrorPage.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
-export const ErrorPage = () => {
-  return <h1> 404 NOT FOUND</h1>;
+export const ErrorPage = ({ message }) => {
+  return (
+    <>
+      <h1> 404 Not Found</h1>
+      <h1> {message}</h1>;
+    </>
+  );
 };
