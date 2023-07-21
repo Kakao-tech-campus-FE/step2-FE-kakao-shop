a990fe14299d07c96ffdcf99c4f0b8d20b65ae7f
baegyeong <beth0929@naver.com>
2023-07-21 23:20:14 +0900

    fix: provider 추가

--- src/App.js
+++ src/App.js
@@ -4,32 +4,40 @@ import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import CartPage from "./pages/CartPage";
+import OrdersPage from "./pages/OrdersPage";
 import ProductDetailPage from "./pages/ProductDetailPage";
 import { Provider } from "react-redux";
 import store from "./store/index.js";
-import { QueryClientProvider, QueryClient } from "react-query";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+export let persistor = persistStore(store);
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <Provider store={store}>
-        <QueryClientProvider client={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          {/* <PersistGate persistor={persistor}> */}
           <BrowserRouter>
             <Routes>
               <Route path={routes.register} element={<RegisterPage />} />
               <Route path={routes.home} element={<HomePage />} />
               <Route path={routes.login} element={<LoginPage />} />
               <Route path={routes.cart} element={<CartPage />} />
+              <Route path={routes.orders} element={<OrdersPage />} />
               <Route
                 path="/product/:id"
                 element={<ProductDetailPage />}
               ></Route>
             </Routes>
           </BrowserRouter>
-        </QueryClientProvider>
-      </Provider>
+          {/* </PersistGate> */}
+        </Provider>
+      </QueryClientProvider>
     </>
   );
 }
