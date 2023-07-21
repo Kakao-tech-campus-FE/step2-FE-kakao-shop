1dbe52b7f6933a03bd317bfe211d845e2a9ce8f3
Heo_Donghyuk <missko30@pusan.ac.kr>
2023-07-21 12:38:44 +0900

    feat: Modify to update request immediately On Cart Counter

--- src/components/molecules/CartList.jsx
+++ src/components/molecules/CartList.jsx
@@ -81,57 +81,15 @@ const CartList = ({ data }) => {
         },
       ];
     }
-
-    setTotalPrice((prev) => {
-      return prev + price;
-    });
-
-    setCartItems((prev) => {
-      return prev.map((item) => {
-        return {
-          ...item,
-          carts: item.carts.map((cart) => {
-            if (cart.id === optionId) {
-              return { ...cart, quantity };
-            }
-            return cart;
-          }),
-        };
-      });
-    });
-  };
-
-  return (
-    <Container className="cart-list">
-      <Box>
-        <h1>장바구니</h1>
-      </Box>
-      {/* 상품별 장바구니 */}
-      {Array.isArray(cartItems) &&
-        cartItems.map((item) => {
-          return (
-            <CartItem
-              key={item.id}
-              item={item}
-              onChange={handleOnChangeCount} // 개수 변경
-            />
-          );
-        })}
-      <div className="row">
-        <span className="expect">주문 예상금액</span>
-        <div className="sum-price">{comma(totalPrice)}원</div>
-      </div>
-      <Button
-        className="order-btn"
-        onClick={() => {
-          // update cart
-          // 장바구니 정보를 수정하는 api 호출(개수 변경이 있는 경우에)
-          // post method
-          // 방법 1. 전체 장바구니 목록의 개수를 적절히 파실하여 페이로드로 보내주기
-          // 방법 2. 변경된 개수만 파싱하여 페이로드로 보내주기
-          // 방법 2가 더 적은 데이터를 보내므로 더 좋은 방법이지만 개발이 어렵다면 방법 1을 이용할 수도 있다.
-          // 우리는 방법 2를 이용할 예정
-          /* 어떻게 변경된 부분만을 보낼 수 있을까?
+    // 장바구니 update 요청
+    // update cart
+    // 장바구니 정보를 수정하는 api 호출(개수 변경이 있는 경우에)
+    // post method
+    // 방법 1. 전체 장바구니 목록의 개수를 적절히 파실하여 페이로드로 보내주기
+    // 방법 2. 변경된 개수만 파싱하여 페이로드로 보내주기
+    // 방법 2가 더 적은 데이터를 보내므로 더 좋은 방법이지만 개발이 어렵다면 방법 1을 이용할 수도 있다.
+    // 우리는 방법 2를 이용할 예정
+    /* 어떻게 변경된 부분만을 보낼 수 있을까?
           handleOnChangeCount가 일어난 값들은 수량의 변화가 있었다는 의미
           => payload로 전달될 값들을 저장하는 updatePayload를 useState를 이용하여 관리하고
           => handleOnChangeCount내에서 변경이 일어나는 값들을 updatePayload에 추가
@@ -140,23 +98,66 @@ const CartList = ({ data }) => {
           이를 해결하려면 장바구니 초기 상태를 저장하는 initPayload를 useRef로 지정하여 관리하며
           요청을 보내기 전에 initPayload와 중복되는 값을 제거한 후 요청을 보내는 방식으로 해결할 수 있다.
            */
-          console.log(cartItems);
-          console.log(updatePayload.current);
-          mutate(updatePayload.current, {
-            onSuccess: (data) => {
-              // navigate to order page
-              // 주문 페이지로 이동
-              route("/order");
-            },
-            onError: (error) => {
-              alert(error.message);
-            },
+    mutate(updatePayload.current, {
+      // update 요청 성공시 결과를 state에 반영
+      onSuccess: (data) => {
+        setTotalPrice((prev) => {
+          return prev + price;
+        });
+
+        setCartItems((prev) => {
+          return prev.map((item) => {
+            return {
+              ...item,
+              carts: item.carts.map((cart) => {
+                if (cart.id === optionId) {
+                  return { ...cart, quantity };
+                }
+                return cart;
+              }),
+            };
           });
-        }}
-      >
-        <span>총 {getTotalCartCountIncludeOptions()}건 주문하기</span>
-      </Button>
-    </Container>
+        });
+      },
+      onError: (error) => {
+        alert(error.message);
+      },
+    });
+  };
+
+  return (
+    <main className="pt-1 bg-[#F4F4F4] bg-opacity-70">
+      <Container className="w-[870px] mx-auto">
+        <Box className="h-12 border rounded bg-white text-center">
+          <h1 className="font-semibold text-[15px] mt-3">장바구니</h1>
+        </Box>
+        {/* 상품별 장바구니 */}
+        {Array.isArray(cartItems) &&
+          cartItems.map((item) => {
+            return (
+              <CartItem
+                key={item.id}
+                item={item}
+                onChange={handleOnChangeCount} // 개수 변경
+              />
+            );
+          })}
+        <div className="row">
+          <span className="expect">주문 예상금액</span>
+          <div className="sum-price">{comma(totalPrice)}원</div>
+        </div>
+        <Button
+          className="order-btn"
+          onClick={() => {
+            // navigate to order page
+            // 주문 페이지로 이동
+            route("/order");
+          }}
+        >
+          <span>총 {getTotalCartCountIncludeOptions()}건 주문하기</span>
+        </Button>
+      </Container>
+    </main>
   );
 };
 export default CartList;
