623105a3bef4db642a8afc68f343ef08202190f0
Chaeeun Lee <cyleecodms@naver.com>
2023-07-21 23:02:34 +0900

    feat : 재푸쉬입니다

--- OneDrive - Chonnam National University/바탕 화면/step2-FE-kakao-shop-main/my-app/craco.config.js
+++ OneDrive - Chonnam National University/바탕 화면/step2-FE-kakao-shop-main/my-app/craco.config.js
@@ -1,5 +1,6 @@
 // CommonJS 방식을 따른 첫번째 코드는 Ruby처럼 require 키워드를 사용하여 여타 다른 변수를 할당하듯이 모듈을 불러오는 반면에, 
 // ES6 방식을 따른 두번째 코드는 Java나 Python처럼 import 키워드를 사용하여 좀 더 명시적으로 모듈을 불러옵니다.
+
 const path = require('path'); // require 사용
 
 module.exports = {
