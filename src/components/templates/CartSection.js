83813e6ef43f3fc3a511538cd343128ec2bdd986
unknown <jsh1147@naver.com>
2023-07-21 23:54:50 +0900

    feat: 장바구니 CSS 구현(미완성)
    장바구니 CSS 일부 구현

--- src/components/templates/CartSection.js
+++ src/components/templates/CartSection.js
@@ -26,7 +26,7 @@ export default function CartSection() {
     <Container className="">
       {/* 장바구니 */}
       <div>
-        <h1>장바구니</h1>
+        <h1 className="font-bold">장바구니</h1>
       </div>
       {isLoading && <Loader />}
       {error && <div>{error}</div>}
@@ -37,18 +37,28 @@ export default function CartSection() {
           <>
             {/* 전체삭제 */}
             <div>
-              <Button onClick={handleDeleteAllClick}>전체삭제</Button>
+              <Button className="border" onClick={handleDeleteAllClick}>
+                전체삭제
+              </Button>
             </div>
             {/* 카트 리스트 */}
+            <div className="space-y-2 border">
             <CartList
               products={data.data.response.products}
               refetch={refetch}
             />
+            </div>
             {/* 주문 예상금액 및 주문하기 */}
             <div>
-              <span>주문 예상금액</span>
-              <span>{convertToPrice(data.data.response.totalPrice)}</span>
-              <Link to="/order">{`${data.data.response.products.length}건 주문하기`}</Link>
+              <span className="text-xl bord">주문 예상금액</span>
+              <span className="text-xl bord text-blue-700">
+                {convertToPrice(data.data.response.totalPrice)}
+              </span>
+              <br />
+              <Link
+                className="inline-block px-32 py-2 bg-yellow-300 font-bold"
+                to="/order"
+              >{`${data.data.response.products.length}건 주문하기`}</Link>
             </div>
           </>
         ))}
