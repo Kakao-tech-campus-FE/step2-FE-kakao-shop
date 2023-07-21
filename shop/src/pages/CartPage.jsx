e34e6fb9996586a233816cdb01d3597eb37b2206
munjihye <m84422583@gmail.com>
2023-07-21 11:28:32 +0900

    fix: 장바구니에 담은 데이터가 넘어오지만 data가 undefined인 상황 발생
    useEffect를 사용해 data가 랜더링 될때마다 refetch를 통해 useQuery를
    업데이트 해줬더니 받아와졌다 !!!

--- shop/src/pages/CartPage.jsx
+++ shop/src/pages/CartPage.jsx
@@ -1,11 +1,28 @@
-import React, { Suspense } from 'react'
+import React, { Suspense,useEffect } from 'react'
 import { useQuery } from 'react-query'
 import Loader from "../components/atoms/Loader"
 import CartList from '../components/molecules/CartList'
 import { getCart } from '../services/cart'
 
 const CartPage = () => {
-  const {data} = useQuery("cart", getCart)
+  const {data, refetch} = useQuery("carts", 
+    getCart,
+    {
+      onError: (error) => {
+        console.error("Error fetching cart data:", error);
+      },
+    }
+  )
+
+  useEffect(()=>{  
+    if(data===undefined){
+      refetch()
+    }
+    console.log('data')
+    console.log(data)
+  },[data])
+
+
   return (
     <Suspense fallback={<Loader/>}>
       <CartList data={data}/>
