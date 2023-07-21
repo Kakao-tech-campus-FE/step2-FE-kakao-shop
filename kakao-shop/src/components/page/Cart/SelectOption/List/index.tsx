6a2e27bccf194bba4aa03ddeec2a7333806fbbad
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-07-21 12:46:22 +0900

    feat: 장바구니 SelectOptionItemList

---
+++ kakao-shop/src/components/page/Cart/SelectOption/List/index.tsx
@@ -0,0 +1,15 @@
+import { Fragment } from 'react';
+
+import SelectOptionItem from '@components/page/Cart/SelectOption/Item';
+
+const SelectOptionItemList = () => {
+  return (
+    <Fragment>
+      <SelectOptionItem />
+      <SelectOptionItem />
+      <SelectOptionItem />
+    </Fragment>
+  );
+};
+
+export default SelectOptionItemList;
