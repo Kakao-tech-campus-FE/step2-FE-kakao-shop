f9e2932f668edce32fe9cbb5337de1f453420393
charmeee <mandugohigh@gmail.com>
2023-07-21 18:50:19 +0900

    feat: 카트 페이지 추가

---
+++ mandu/src/pages/CartPage.js
@@ -0,0 +1,46 @@
+import {useMutation, useQuery} from "react-query";
+import {getCart} from "../services/apis";
+import ErrorPage from "./ErrorPage";
+import LoadingPage from "./LoadingPage";
+import CartProductCard from "../components/organisms/CartProductCard";
+import {ErrorType} from "../services/type";
+import ElevatedButton from "../components/atoms/Buttons";
+
+const CartPage = () => {
+    const {isLoading, isError, data, error} = useQuery(['cart'],
+        getCart, {
+            retry: 1,
+        });
+
+    //updataCart
+    const cartMutation = useMutation(() => {
+    },);
+
+
+    if (isLoading) return <LoadingPage/>
+    if (isError) return <ErrorPage error={error}/>
+
+    return (
+        <div className="bg-gray-100 py-8 min-h-screen">
+            <div className="max-w-screen-md w-full  mx-auto ">
+                <h1 className="text-xl font-bold bg-white text-gray-800 text-center py-2 mb-4">장바구니</h1>
+                <div className="divide-y-8 divide-gray-100 bg-white">
+                    {
+                        data.products.map((item) => {
+                            let filteredProduct = item.carts.filter((cart) => cart.quantity > 0);
+                            if (filteredProduct.length === 0) return (<></>);
+                            return (
+                                <CartProductCard key={"cartItem" + item.id} id={item.id} productName={item.productName}
+                                                 carts={item.carts}/>);
+                        })}
+                </div>
+
+                <ElevatedButton className="bg-amber-300" onClick={() => {
+                }}>주문하기</ElevatedButton>
+            </div>
+
+        </div>
+    )
+}
+
+export default CartPage;
