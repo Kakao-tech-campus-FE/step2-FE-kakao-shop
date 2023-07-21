a846eb944ff7e1922e0e3ecc1a2e3ece9f9411d7
localgaji <eldorado829@naver.com>
2023-07-22 05:21:10 +0900

    토스트 슬라이스 등록

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,18 @@
 import { combineReducers } from "redux";
 import { loginSlice } from "./loginSlice";
 import { persistReducer } from "redux-persist";
-import storageSession from "redux-persist/lib/storage/session";
+import storage from "redux-persist/lib/storage";
+import { toastSlice } from "./toastSlice";
 
 const persistConfig = {
   key: "root",
-  storage: storageSession,
+  storage,
   whitelist: ["login"],
 };
 
 const rootReducer = combineReducers({
   login: loginSlice.reducer,
+  toast: toastSlice.reducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
