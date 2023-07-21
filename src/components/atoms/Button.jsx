6d9230b34973ebbe9e1c4aa903098ddf56f7a4ea
baegyeong <beth0929@naver.com>
2023-07-21 23:29:03 +0900

    feat: Button 컴포넌트 구현

---
+++ src/components/atoms/Button.jsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+const Btn = styled.button`
+  height: 3rem;
+
+  border: 0;
+  border-radius: 8px;
+
+  cursor: pointer;
+
+  width: ${(props) => props.width};
+  color: ${(props) => colors[props.color]};
+  background-color: ${(props) => colors[props.backgroundColor]};
+`;
+
+const colors = {
+  yellow: "#ffe600",
+  black: "#000000",
+  white: "#ffffff",
+};
+
+const Button = ({ children, onClick, width, color, backgroundColor }) => {
+  return (
+    <Btn
+      onClick={onClick}
+      width={width}
+      color={color}
+      backgroundColor={backgroundColor}
+    >
+      {children}
+    </Btn>
+  );
+};
+
+export default Button;
