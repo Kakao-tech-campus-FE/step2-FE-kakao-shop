e166a4d31fc2e2a80dd9121006720dae333fe73c
JeonDoGyun <wjsehrbs2632@naver.com>
2023-07-21 22:38:46 +0900

    Fix: Login Error 해결 및 token 추가
    Login에서 username을 가져오는 것으로 API가 변경되어서 해당 부분을 수정했다.
    Authorization 인증이 필요해서 회원가입 과정에서 JWT Token을 가져와 저장할 수 있도록 수정했다.

--- my-app/src/services/user.jsx
+++ my-app/src/services/user.jsx
@@ -5,6 +5,7 @@ export const register = (data) => {
   return instance.post("/join", {
     email,
     password,
+    username,
   });
 };
 
@@ -14,17 +15,9 @@ export const checkEmail = (data) => {
 };
 
 export const login = (data) => {
-  const { email, password, username } = data;
+  const { email, password } = data;
   return instance.post("/login", {
     email,
     password,
   });
 };
-
-// token 전달
-// response : jwt 토큰 안에 사용자 정보
-// jwt expired_at 검증(유효기간)
-// expired return 401 status code
-// export const profile = () => {
-//   return instance.get("/profile");
-// };
