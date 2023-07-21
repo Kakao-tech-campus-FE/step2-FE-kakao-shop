a499e15a487452c45f8fe3ba643fbe9de568fdb6
localgaji <eldorado829@naver.com>
2023-07-22 05:30:05 +0900

    App.js - 토스트 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import GNB from "./components/organisms/GNB";
-import MainPage from "./pages/MainPage";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import CartPage from "./pages/CartPage";
+import MainPage from "pages/MainPage";
+import LoginPage from "pages/LoginPage";
+import RegisterPage from "pages/RegisterPage";
+import CartPage from "pages/CartPage";
 import DetailPage from "pages/DetailPage";
-import OrderResultPage from "./pages/OrderResultPage";
-import PageContainer from "./components/atoms/PageContainer";
-import Loader from "./components/molecules/Loader";
+import OrderResultPage from "pages/OrderResultPage";
+
+import PageContainer from "components/atoms/PageContainer";
+import Loader from "components/molecules/Loader";
+import Toast from "components/molecules/Toast";
+import GNB from "components/organisms/GNB";
 
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
-import ErrorFallback from "./components/organisms/ErrorFallback";
+import ErrorFallback from "components/organisms/ErrorFallback";
 
 function App() {
   return (
@@ -32,6 +34,7 @@ function App() {
             </Routes>
           </Suspense>
         </ErrorBoundary>
+        <Toast />
       </PageContainer>
     </BrowserRouter>
   );
