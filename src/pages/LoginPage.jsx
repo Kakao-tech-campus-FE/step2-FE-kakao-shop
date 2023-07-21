f112c7b70e401aadd99538503402ceb564d357fa
blackhblee <blackhblee@gmail.com>
2023-07-21 17:30:11 +0900

    refactor: Remove loader component test
    로더 테스트를 위한 코드를 제거했습니다.

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,22 +1,11 @@
-import { useEffect, useState } from "react";
 import LoginForm from "../components/organisms/LoginForm";
-import Loader from "../components/atoms/Loader";
 
 /** 로그인 페이지
  *
  * @return {JSX.Element}
  */
 const LoginPage = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 600);
-  }, []);
-
-  // eslint-disable-next-line react/jsx-no-useless-fragment
-  return <>{loading ? <Loader /> : <LoginForm />}</>;
+  return <LoginForm />;
 };
 
 export default LoginPage;
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,22 +1,11 @@
-import { useEffect, useState } from "react";
 import RegisterForm from "../components/organisms/RegisterForm";
-import Loader from "../components/atoms/Loader";
 
 /** 회원가입 페이지
  *
  * @return {JSX.Element}
  */
 const RegisterPage = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 600);
-  }, []);
-
-  // eslint-disable-next-line react/jsx-no-useless-fragment
-  return <>{loading ? <Loader /> : <RegisterForm />}</>;
+  return <RegisterForm />;
 };
 
 export default RegisterPage;
