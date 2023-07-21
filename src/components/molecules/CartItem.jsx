0ee2c94199d84dce3b9437a4dc03e346a118841c
Dongjin <ehdwls1638@gmail.com>
2023-07-21 12:25:35 +0900

    fix : cart deletion button size

--- src/components/molecules/CartItem.jsx
+++ src/components/molecules/CartItem.jsx
@@ -17,35 +17,37 @@ const CartItem = ({item, onChange}) => {
         <div className={"cart-item m-5 p-5 text-left bg-light-gray-900"}>
             <h5 className={"text-lg"}>{item.productName}</h5>
             {item.carts.map((cart) =>
-                 cart.quantity !== 0 && (
-                    <div
-                        key={cart.id}
-                        className={"cart-item pb-5 px-5 m-3 border-s-4 border-light-gray-700"}>
-                        <button className={"del-button flex  w-full justify-end items-end"}
-                                onClick={ () => onChange(cart.id, 0, -cart.quantity*cart.option.price)}
+                    cart.quantity !== 0 && (
+                        <div
+                            key={cart.id}
+                            className={"cart-item pb-5 px-5 m-3 border-s-4 border-light-gray-700 flex flex-col items-end"}>
+                            <button className={"del-button flex justify-end items-end"}
+                                    onClick={() => onChange(cart.id, 0, -cart.quantity * cart.option.price)}
                             >
-                            <RxCross2 className={"text-right"}/>
-                        </button>
-                        <div className="option-name flex flex-row justify-between my-2">
-                            <span>{cart.option.optionName}</span>
-                            <span>{comma(cart.option.price)}원</span>
-                        </div>
+                                <RxCross2 className={"text-right"}/>
+                            </button>
+                            <div className={"flex flex-col w-full"}>
+                                <div className="option-name flex flex-row justify-between my-2">
+                                    <span>{cart.option.optionName}</span>
+                                    <span>{comma(cart.option.price)}원</span>
+                                </div>
 
-                        <div className={"row flex flex-row"}>
-                            <div className="option-count w-32">
-                                <Counter
-                                    value={cart.quantity}
-                                    handleOnChange={(value) => {
-                                        return onChange(cart.id, value, (value - cart.quantity) * cart.option.price)
-                                    }}
-                                />
-                            </div>
-                            <div className="option-price w-full text-right">
-                                <span>{comma(cart.option.price * cart.quantity)}원</span>
+                                <div className={"row flex flex-row"}>
+                                    <div className="option-count w-32">
+                                        <Counter
+                                            value={cart.quantity}
+                                            handleOnChange={(value) => {
+                                                return onChange(cart.id, value, (value - cart.quantity) * cart.option.price)
+                                            }}
+                                        />
+                                    </div>
+                                    <div className="option-price w-full text-right">
+                                        <span>{comma(cart.option.price * cart.quantity)}원</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                )
+                    )
             )}
             <div className={"cart-price text-right"}>
                 <div className="row">
