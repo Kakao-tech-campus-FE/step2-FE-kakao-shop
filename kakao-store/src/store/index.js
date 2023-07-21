013f26f4b690210e00cc7a06bee2554cd8c39e3a
SeonghyeonKim <kkgojn@naver.com>
2023-07-21 17:30:41 +0900

    feat: Add Detail Reducer

--- kakao-store/src/store/index.js
+++ kakao-store/src/store/index.js
@@ -1,13 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice"
 import productReducer from "./slices/productSlice"
+import detailReducer from "./slices/detailSlice"
 
 const store = configureStore({
 	reducer: {
 		// User reducer: email
 		user: userReducer,
 		// Products reducer: products
-		product: productReducer
+		product: productReducer,
+		// Detail reducer: 
+		detail: detailReducer,
 	},
 });
 
