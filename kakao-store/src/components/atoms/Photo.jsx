e8847da69e6a1d384c2060ec902c020604cd9353
jihopark <jihodayo@gmail.com>
2023-07-21 14:44:34 +0900

    fix: Modify Photo Size

--- kakao-store/src/components/atoms/Photo.jsx
+++ kakao-store/src/components/atoms/Photo.jsx
@@ -6,15 +6,14 @@
  * @returns
  */
 
-const Photo = ({ src, alt }) => {
+const Photo = ({ src, alt, className }) => {
   return (
     <picture>
-      <source
-        srcSet={`http://kakao-app-env.eba-kfsgeb74.ap-northeast-2.elasticbeanstalk.com${src}`}
-      />
+      <source srcSet={`http://kakao-app-env.eba-kfsgeb74.ap-northeast-2.elasticbeanstalk.com${src}`} />
       <img
         src={`http://kakao-app-env.eba-kfsgeb74.ap-northeast-2.elasticbeanstalk.com${src}`}
         alt={alt}
+        className={className}
       />
     </picture>
   );
