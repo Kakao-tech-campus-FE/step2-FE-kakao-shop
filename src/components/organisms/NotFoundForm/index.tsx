7a97338dd70b685e6c5d17f5b2f1dffa129892cc
kjh302903 <kjh302903@gmail.com>
2023-07-21 21:20:35 +0900

    refactor: 불필요한 <br/>태그 제거
    불필요한 <br/>태그 제거하고 margin으로 조절함

--- src/components/organisms/NotFoundForm/index.tsx
+++ src/components/organisms/NotFoundForm/index.tsx
@@ -12,22 +12,17 @@ const NotFoundForm = () => {
           <br />
           요청하신 페이지를 찾을 수 없습니다.
         </h4>
-        <br />
         <p>
           방문하시려는 페이지의 주소가 잘못 입력되었거나,
           <br />
           페이지의 주소가 변경 혹은 삭제되어 요청하신 페이지를 찾을 수 없습니다.
-          <br />
-          <br />
-          입력하신 주소가 정확한지 다시 한번 확인해 주시기 바랍니다.
-          <br />
-          <br />
+        </p>
+        <p>입력하신 주소가 정확한지 다시 한번 확인해 주시기 바랍니다.</p>
+        <p>
           관련 문의사항은 <strong>카카오 고객센터</strong>에 알려주시면 친절하게
           안내해 드리겠습니다.
-          <br />
-          <br />
-          감사합니다.
         </p>
+        <p>감사합니다.</p>
         <ButtonWrapper>
           <Button
             width={"100px"}
@@ -63,6 +58,7 @@ const Wrapper = styled.div`
   }
 
   p {
+    margin-top: 24px;
     font-size: 14px;
   }
   strong {
