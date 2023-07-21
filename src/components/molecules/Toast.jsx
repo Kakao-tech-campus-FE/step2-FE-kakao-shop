29a64d55f880e7d179a7dd160d34b3636fa0e219
localgaji <eldorado829@naver.com>
2023-07-22 05:25:24 +0900

    토스트 (전역 관리) 컴포넌트 생성

---
+++ src/components/molecules/Toast.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react' 
+import { ToastBox, ToastBtn, ToastContent } from 'components/atoms/toast';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { toastOffReducer, toastOnReducer } from 'reducers/toastSlice';
+
+export const useToast = () => {
+  const dispatch = useDispatch();
+  const setToast = (message, moveTo="") => {
+    dispatch(toastOnReducer( {
+      isOn: true,
+      message: message,
+      moveTo: moveTo,
+    }))
+  }
+  return setToast
+}
+
+const Toast = () => {
+  const dispatch = useDispatch()
+  const toastState = useSelector((state) => state.toast)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      dispatch(toastOffReducer())
+    }, 3000)
+
+    return () => { clearTimeout(timer) }
+
+  }, [toastState.isOn])
+  
+  const btnHandler = () => {
+    if (toastState.moveTo !== "") {
+      navigate(toastState.moveTo)
+    }
+    dispatch(toastOffReducer())
+  }
+
+  return (
+    toastState.isOn && 
+    <ToastBox>
+      <ToastContent>
+        {toastState.message}
+      </ToastContent>
+      <ToastBtn onClick={btnHandler}>
+        {toastState.moveTo !== "" ? "바로가기" : "닫기"}
+      </ToastBtn>
+    </ToastBox>
+  )
+}
+
+export default Toast
