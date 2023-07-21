642361753479e69b6da8b593d9103b4326e34d0f
dongyeopCha <ckehdduq95@gmail.com>
2023-07-21 16:35:05 +0900

    feat: 카트 로고 추가/ 링크 연결

--- src/components/common/molecules/GNB.tsx
+++ src/components/common/molecules/GNB.tsx
@@ -5,20 +5,29 @@ import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../../../modules/auth';
 import type { RootState } from '../../..';
 import logoKakao from '../../../assets/logoKakao.png';
+import cart from '../../../assets/cart.png';
 import LinkBtn from '../LinkBtn';
+
 function GNB() {
   const dispatch = useDispatch();
   const { isLogin } = useSelector((state: RootState) => state.authReducer);
   return (
     <Wrap>
-      <img src={logoKakao} width={80} height={30} alt="카카오 쇼핑 로고" />
-      {isLogin ? (
-        <LogoutBtn type="button" onClick={() => dispatch(logout())}>
-          로그아웃
-        </LogoutBtn>
-      ) : (
-        <LinkBtn to="/login">로그인</LinkBtn>
-      )}
+      <LinkBtn to="/">
+        <img src={logoKakao} width={80} height={40} alt="카카오 쇼핑 로고" />
+      </LinkBtn>
+      <NavList>
+        <LinkBtn to="/cart">
+          <img src={cart} width={40} height={40} alt="장바구니 로고" className="mr-3" />
+        </LinkBtn>
+        {isLogin ? (
+          <LogoutBtn type="button" onClick={() => dispatch(logout())}>
+            로그아웃
+          </LogoutBtn>
+        ) : (
+          <LinkBtn to="/login">로그인</LinkBtn>
+        )}
+      </NavList>
     </Wrap>
   );
 }
@@ -36,6 +45,9 @@ const Wrap = styled.div`
   border-bottom: 1px solid ${colors.gray};
 `;
 
+const NavList = styled.div`
+  display: flex;
+`;
 const LogoutBtn = styled.button`
   background-color: inherit;
   border: none;
