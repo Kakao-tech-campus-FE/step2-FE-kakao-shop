86b09bf97cc9c21cb337b96cdc2ae3db328ad055
charmeee <mandugohigh@gmail.com>
2023-07-21 18:48:31 +0900

    feat: 선택된 옵션 리스트 컴포넌트

---
+++ mandu/src/components/molecules/SelectedOptionList.js
@@ -0,0 +1,30 @@
+const SelectedOptionList = ({selectedOptions, rawOptions, onIncrease, onRemove, onDecrease}) => {
+    return (
+        <div className="">
+            {selectedOptions.map((selectedOption) => {
+                const {optionId, quantity} = selectedOption;
+                const option = rawOptions.find((option) => option.id === optionId);
+                const {optionName, price} = option;
+                return (
+                    <div className="bg-gray-200 mb-4 p-2">
+                        <div className="flex justify-between">
+                            <h3>{optionName}</h3>
+                            <button onClick={() => onRemove(optionId)}>X</button>
+                        </div>
+                        <div className="flex justify-between items-center">
+                            <div className="flex items-center">
+                                <button className="px-2 m-2" onClick={() => onDecrease(optionId)}>-</button>
+                                <p className="px-2">{quantity}</p>
+                                <button className="px-2 m-2" onClick={() => onIncrease(optionId)}>+</button>
+                            </div>
+                            <p>{(price * quantity).toLocaleString() + '원'}</p>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+
+    );
+}
+
+export default SelectedOptionList;
