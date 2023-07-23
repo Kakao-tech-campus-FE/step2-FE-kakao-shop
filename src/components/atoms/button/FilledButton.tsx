3fe0479d7aacf3fe1c65c15fe91517511885072d
lcqff <yeonnu82g@gmail.com>
2023-07-23 22:05:57 +0900

    feat: filledButton css 수정 - px 증가 - shadow convex 적용

--- src/components/atoms/button/FilledButton.tsx
+++ src/components/atoms/button/FilledButton.tsx
@@ -3,7 +3,11 @@ import { ButtonProps } from './Button.interface';
 
 const FilledButton = ({ onClick, children }: ButtonProps) => {
   return (
-    <button type="button" onClick={onClick} className="bg-pointPupple h-[40px] rounded text-white font-bold p-[10px]">
+    <button
+      type="button"
+      onClick={onClick}
+      className="flex items-center bg-pointPupple h-[40px] rounded text-white font-bold py-[10px] px-[20px] shadow-convex"
+    >
       {children}
     </button>
   );
