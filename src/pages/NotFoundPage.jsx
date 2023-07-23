a924762c8d188238c69bdafac7c386543e829f38
baegyeong <beth0929@naver.com>
2023-07-24 02:53:02 +0900

    fix: not found page style 추가

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,8 +1,22 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 100vw;
+  height: 100vh;
+`;
+
 const NotFoundPage = () => {
   return (
     <>
-      <h1>404</h1>
-      <p>상품이 존재하지 않습니다.</p>
+      <Container>
+        <h1>404</h1>
+        <p>상품을 찾을 수 없습니다.</p>
+      </Container>
     </>
   );
 };
