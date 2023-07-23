83a8c1796267e0b837a64cdcca87ac93c4a93f84
munjihye <m84422583@gmail.com>
2023-07-23 19:37:00 +0900

    fix: data 렌더링 에러 해결
    refetch를 통해 다시 한번 렌더링을 해줌으로써 data가 undefined 되는
    에러를 해결했다고 믿었지만 새로 고침을 했을 경우 같은 오류가 발생했다.
    
    suspense 안에서 data를 넘겨주기 때문에 suspense=true라는 옵션을 줘서
    loader가 실행되는 동안 data를 받아오지 않도록 막아줘야 했다. 이제
    완벽히오류 해결 !!

--- shop/src/pages/CartPage.jsx
+++ shop/src/pages/CartPage.jsx
@@ -1,27 +1,19 @@
-import React, { Suspense,useEffect } from 'react'
+import React, { Suspense} from 'react'
 import { useQuery } from 'react-query'
 import Loader from "../components/atoms/Loader"
 import CartList from '../components/molecules/CartList'
 import { getCart } from '../services/cart'
 
 const CartPage = () => {
-  const {data, refetch} = useQuery("carts", 
+  const {data} = useQuery("carts", 
     getCart,
     {
       onError: (error) => {
         console.error("Error fetching cart data:", error);
       },
+      suspense: true
     }
   )
-
-  useEffect(()=>{  
-    if(data===undefined){
-      refetch()
-    }
-    console.log('data')
-    console.log(data)
-  },[data])
-
 
   return (
     <Suspense fallback={<Loader/>}>
