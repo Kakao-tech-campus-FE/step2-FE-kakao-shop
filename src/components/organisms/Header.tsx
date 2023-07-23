d32c0f2a07b5c7e752beb5a05d4cc3cc4e6f0a04
lcqff <yeonnu82g@gmail.com>
2023-07-23 23:27:37 +0900

    feat: 장바구니 버튼 추가

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { RootState } from 'src/store';
 import { loginStore } from '@store/slices/userSlice';
 import FilledButton from '@components/atoms/button/FilledButton';
+import { BsCart } from 'react-icons/bs';
+import TextButton from '@components/atoms/button/TextButton';
 
 const Header = () => {
   const isLoggedIn = useSelector((state: RootState) => state.user.isLoggedIn);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleLogout = () => {
     // localStorage.removeItem('token');
     dispatch(loginStore({ isLoggedIn: false, email: null }));
@@ -15,15 +18,18 @@ const Header = () => {
 
   return (
     <div className="absolute w-screen bg-white ">
-      <div className=" h-header flex items-center mx-[100px] float-right">
+      <div className="flex justify-end items-center space-x-5 h-header mx-[100px]">
+        <BsCart
+          onClick={() => {
+            navigate('/cart');
+          }}
+        />
         {!isLoggedIn ? (
           <Link to="/login">
             <FilledButton>로그인</FilledButton>
           </Link>
         ) : (
-          <button type="button" onClick={handleLogout} className="absolute right-[10px]">
-            로그아웃
-          </button>
+          <TextButton onClick={handleLogout}>로그아웃</TextButton>
         )}
       </div>
     </div>
