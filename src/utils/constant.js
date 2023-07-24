18d74eca90df2ab837e544589fe0ca2bf32ac5d8
kimchanho97 <nh0903@pusan.ac.kr>
2023-07-25 02:27:18 +0900

    Chore: 유틸변수 추가

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,10 @@
 export const PRODUCT_NUM_PER_PAGE = 9;
+
+export const ERROR_MSG = {
+  none: "",
+  required: "필수 정보입니다.",
+  invalidEmail:
+    "유효하지 않은 이메일 주소입니다. 올바른 형식으로 입력해주세요.",
+  invalidPw:
+    "비밀번호는 영문, 숫자, 특수문자를 포함하여 8~20자로 입력해주세요.",
+};
