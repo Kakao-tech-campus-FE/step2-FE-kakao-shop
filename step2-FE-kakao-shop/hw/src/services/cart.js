1c4e43ec0b61a92412ded84edc99c02f542b6565
rlaeksqls@naver.com <rlaeksqls@naver.com>
2023-07-24 19:57:02 +0900

    수정2

--- step2-FE-kakao-shop/hw/src/services/cart.js
+++ step2-FE-kakao-shop/hw/src/services/cart.js
@@ -4,10 +4,18 @@ import { instance } from "./index"; // axios instance
  * 장바구니 담기
  * @param {object} payload
  */
-export const addCart = (payload) => {
-  // payload: {
+export const addCart = (items) => {
+  // items: {
   //   optionId: 1
   //   quantity: 1
   // }
-  return instance.post("/carts/add", payload);
+  return instance.post("/carts/add", items);
+};
+
+export const getCart = () => {
+  return instance.get("/carts");
+};
+
+export const updateCart = (items) => {
+  return instance.post("/carts/update", it - tems);
 };
