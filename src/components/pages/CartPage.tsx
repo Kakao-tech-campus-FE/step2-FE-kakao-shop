d66f3f6d7fe5907585072b79e413a8e79d9040cc
lcqff <yeonnu82g@gmail.com>
2023-07-24 21:13:35 +0900

    feat: 장바구니 스켈레톤 적용

--- src/components/pages/CartPage.tsx
+++ src/components/pages/CartPage.tsx
@@ -1,18 +1,14 @@
 import { queryCart } from '@api/cartApi';
-import Loader from '@components/atoms/Loader';
-import CartList from '@components/organisms/CartList';
-import React, { Suspense } from 'react';
+import CartList from '@components/organisms/CartList/CartList';
+import CartListSkeleton from '@components/organisms/CartList/CartListSkeleton';
+import React from 'react';
 import { useQuery } from 'react-query';
 
 const CartPage = () => {
-  const { data } = useQuery('cart', queryCart);
+  const { data, isLoading } = useQuery('cart', queryCart);
   const response = data?.data;
 
-  return (
-    <Suspense fallback={<Loader />}>
-      <CartList data={response} />
-    </Suspense>
-  );
+  return isLoading ? <CartListSkeleton /> : <CartList data={response} />;
 };
 
 export default CartPage;
