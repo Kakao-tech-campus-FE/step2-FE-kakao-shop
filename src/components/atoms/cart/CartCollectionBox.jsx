f1a0a6e450e34b7c2e9422b003def13416d2d016
localgaji <eldorado829@naver.com>
2023-07-24 14:18:38 +0900

    cart 컴포넌트 구조 변경 (체크박스 삭제)

--- src/components/atoms/cart/CartCollectionBox.jsx
+++ src/components/atoms/cart/CartCollectionBox.jsx
@@ -3,12 +3,14 @@ import styled from 'styled-components';
 
 const Box = styled.div`
   display: flex;
+  flex-direction: column;
+  width : 100%;
   box-shadow: 0px 0px 5px rgb(199, 199, 199);
   border-radius: 10px;
   padding: 10px;
   margin: 5px 0;
 `
-const CartItemBox = (props) => {
+const CartCollectionBox = (props) => {
     return (
       <Box style={props.style}>
         {props.children}
@@ -16,4 +18,4 @@ const CartItemBox = (props) => {
     );
 };
 
-export default CartItemBox
+export default CartCollectionBox
