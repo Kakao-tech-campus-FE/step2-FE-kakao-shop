5671dded9904027913166717711f81454cfdb0cf
localgaji <eldorado829@naver.com>
2023-07-24 14:21:00 +0900

    DetailOption - 주석 추가

--- src/components/organisms/DetailOption.jsx
+++ src/components/organisms/DetailOption.jsx
@@ -12,6 +12,12 @@ import addToCart from 'api/addToCart'
 import SelectedItemBox from 'components/atoms/option/SelectedItemBox'
 import { useToast } from 'components/molecules/Toast'
 
+/**
+ * 상품 상세 페이지의 옵션 선택 부분
+ * @param {object} props 
+ * @param {array} props.options - react query로 받아온 상품 데이터 중, 옵션 정보 배열
+ * @returns 
+ */
 const DetailOption = (props) => {
   const initialList = props.options.map((item) => { 
     return {id:item.id, quantity: 0, optionName: item.optionName, price: item.price} 
@@ -25,6 +31,11 @@ const DetailOption = (props) => {
   
   const setToast = useToast()
 
+  /**
+   * 옵션 목록(initialList) 에서 옵션id에 해당하는 항목을 찾아서, 선택 목록 (selected)에 추가
+   * @param {string} id - 옵션id 
+   * @returns 
+   */
   const selectOption = (id) => {
     if (!loginState.islogin) {
       setToast("로그인 해주세요.", "/login")
@@ -32,13 +43,14 @@ const DetailOption = (props) => {
     }
     setOpen(prev => false)
 
+    // 이미 선택된 옵션인지 먼저 확인
     for (const item of selected) {
       if (item.id === id && item.quantity > 0) {
         setToast("이미 선택된 옵션입니다.")
         return
       }
     }
-    // 추가일때 : initialList 에서 정보 찾아와서 넣기
+    // 선택안된 옵션일때 : initialList 에서 정보 찾아와서 추가
     for (const item of initialList) {
       if (item.id === id) {
         setSelected(prev => [...prev, { ...item, quantity: 1 }])
@@ -47,13 +59,20 @@ const DetailOption = (props) => {
     }
   }
 
+  /**
+   * 수량 변경 : 선택 목록 (selected) 에서 id로 항목을 찾아 수량을 변경
+   * @param {string} id 
+   * @param {number} newQuantity 
+   * @param {boolean} clear 
+   * @returns 
+   */
   const changeQuantity = (id, newQuantity, clear=false) => {
  
     if ((newQuantity === 0 && !clear) || Number.isNaN(newQuantity)) {return}
 
     const newSelected = [...selected]
     
-    // 변경일때 : selected 에서 찾아서 수량 바꾸기
+    // selected 에서 id로 항목 찾아서 수량 바꾸기
     for (let i=0; i < selected.length; i++) {
       if (selected[i].id === id) {
 
@@ -70,6 +89,7 @@ const DetailOption = (props) => {
     }
   }
 
+  // 선택, 삭제, 변경 시 총금액, 총수량 변경
   useEffect( () => {
     let p = 0;
     let q = 0;
