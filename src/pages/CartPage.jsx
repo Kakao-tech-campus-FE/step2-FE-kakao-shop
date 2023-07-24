ecce4e7a81c75bf2bf511c2bb208323c97b522f3
baegyeong <beth0929@naver.com>
2023-07-24 20:27:12 +0900

    fix: 데이터 요청 발생시 로더 추가 로딩 상태 표시

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,20 +1,18 @@
-import { useNavigate } from "react-router-dom";
 import CartList from "../components/molecules/CartList";
 import Gnb from "../components/organisms/Gnb";
-import { Suspense, useEffect, useState } from "react";
+import Loader from "../components/atoms/Loader";
+import NotFoundPage from "./NotFoundPage";
 import { getCart } from "../services/cart";
 import { useQuery } from "react-query";
-import Loader from "../components/atoms/Loader";
 
 const CartPage = () => {
-  const { data } = useQuery("carts", getCart);
+  const { data, isLoading, isError } = useQuery("carts", getCart);
 
   return (
     <>
       <Gnb />
-      {/* <Suspense fallback={<Loader />}> */}
-      <CartList data={data} />
-      {/* </Suspense> */}
+      {isLoading ? <Loader /> : <CartList data={data} />}
+      {isError && <NotFoundPage />}
     </>
   );
 };
