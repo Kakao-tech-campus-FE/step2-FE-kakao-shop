cdd965cd2cd793b12785f42008acb8afa527afab
ba0061@naver.com <sweet.waiting0l@icloud.com>
2023-07-24 17:44:30 +0900

    feat: change loader position

--- kakao-shopping/src/pages/OrderPage.jsx
+++ kakao-shopping/src/pages/OrderPage.jsx
@@ -5,8 +5,8 @@ const OrderPage = () => {
     return (
       <div>
           <GNB />
-        <div className="flex flex-col items-center w-full">
-          <Suspense fallback={<div className="loader"></div>}>
+        <div className="flex flex-col justify-center items-center w-full">
+          <Suspense fallback={<div className="loader absolute top-1/3"></div>}>
             <OrderTemplate />
           </Suspense>
         </div>
