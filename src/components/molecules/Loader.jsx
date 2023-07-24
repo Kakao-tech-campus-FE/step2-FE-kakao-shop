3115f7b7c439d9e0cd78355688f61bc8e453f84e
localgaji <eldorado829@naver.com>
2023-07-24 14:20:34 +0900

    Loader - size를 props로 지정

--- src/components/molecules/Loader.jsx
+++ src/components/molecules/Loader.jsx
@@ -9,14 +9,16 @@ const LoaderBox = styled.div`
 
 const StyledLoader = styled.div`
   margin: auto;
+  width: 40px;
+  height: 40px;
 `
 
-const Loader = () => {
+const Loader = (props) => {
   return (
     <LoaderBox id="loader-box">
-      <StyledLoader>
+      <StyledLoader style={ { width: props.width, height: props.width } }>
         <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-        width="40px" height="40px" viewBox="0 0 40 40" enableBackground="new 0 0 40 40" xmlSpace="preserve">
+        width="100%" height="100%" viewBox="0 0 40 40" enableBackground="new 0 0 40 40" xmlSpace="preserve">
         <path opacity="0.2" fill="#000" d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946
           s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634
           c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"/>
