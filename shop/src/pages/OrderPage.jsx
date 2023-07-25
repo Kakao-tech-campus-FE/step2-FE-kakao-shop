f142671704100d05d0a1ca071a6ea9a7e16f85f8
munjihye <m84422583@gmail.com>
2023-07-26 00:45:22 +0900

    feat: orderTemplate으로 data 넘겨주기

--- shop/src/pages/OrderPage.jsx
+++ shop/src/pages/OrderPage.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import OrderTemplate from '../components/templates/OrderTemplate'
+import { Suspense } from 'react'
+import { useQuery } from 'react-query'
+import { getCart } from '../services/cart'
+import Loader from '../components/atoms/Loader'
 
 const OrderPage = () => {
+  const {data} = useQuery("carts", 
+    getCart,
+    {
+      onError: (error) => {
+        console.error("Error fetching cart data:", error);
+      },
+      suspense: true
+    }
+  )
+
   return (
-    <div>OrderPage</div>
+    <Suspense fallback={<Loader/>}>
+      <OrderTemplate data={data}/>
+    </Suspense>
   )
 }
+
 
 export default OrderPage
