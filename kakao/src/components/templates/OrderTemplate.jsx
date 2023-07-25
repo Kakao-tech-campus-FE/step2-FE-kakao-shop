4a3ae560fa06501b719defa34b1bc3ef6add4cb8
sihyonn <sihyeon982@naver.com>
2023-07-25 22:46:56 +0900

    OrderTemplate css 완성, Footer 완성

--- kakao/src/components/templates/OrderTemplate.jsx
+++ kakao/src/components/templates/OrderTemplate.jsx
@@ -5,6 +5,9 @@ import { useNavigate } from "react-router-dom";
 import "../../styles/template/OrderTemplate.css";
 
 const OrderTemplate = ({ data }) => {
+  console.log(data);
+  console.log("넘어옴");
+
   const { products, totalPrice } = data?.data?.response;
   const navigate = useNavigate();
   const { mutate } = useMutation({
@@ -27,16 +30,18 @@ const OrderTemplate = ({ data }) => {
         item.carts.map((cart) => {
           return (
             <div key={cart.id} className="order-option">
-              <div className="product-name">
-                <span>{`${item.productName}`}</span>
-                <br />
-                <span>{`[옵션]${cart.option.optionName}`}</span>
+              <div className="namegroup">
+                <span class="material-symbols-outlined">storefront</span>
+                <span className="prodcut-name">{`${item.productName}`}</span>
+              </div>
+              <div className="optionNamegroup">
+                <span className="option-name">{`[옵션]${cart.option.optionName}`}</span>
                 <span className="product-quantity">
                   {comma(cart.quantity)}개
                 </span>
               </div>
 
-              <div className="product-price">
+              <div className="option-price">
                 <span>{comma(cart.price * cart.quantity)}원</span>
               </div>
             </div>
@@ -50,83 +55,93 @@ const OrderTemplate = ({ data }) => {
 
   return (
     <div className="order-container">
-      <div className="order-top">
-        <strong>주문하기</strong>
-      </div>
-
-      <div className="order-delivery">
-        <strong>배송지 정보(kakao계정정보)</strong>
-        <div className="delivery-info">
-          <p className="customer-name">
-            춘식이<span>기본배송지</span>
-          </p>
-          <p className="customer-number">010-1234-5678</p>
-          <p className="customer-address">
-            <p>(12345)광주 카카오로 라이언길 1</p> (라이언동, 춘식팰리스)
-            111-111
-          </p>
+      <div className="order-innerwrap">
+        <div className="order-top">
+          <span>주문하기</span>
         </div>
-      </div>
 
-      <div className="order-product-info">
-        <strong>주문상품정보</strong>
-        {/* 각 주문 정보 */}
-        <OrderItems />
+        <div className="order-delivery-info">
+          <span className="info-title">배송지 정보</span>
+          <span>(kakao계정정보)</span>
 
-        <div className="order-total-price">
-          <strong>총 주문 금액</strong>
-          <span>{comma(totalPrice)}원</span>
+          <div className="delivery-customer-info">
+            <span className="name">춘식이</span>
+            <span className="destination">기본배송지</span>
+            <p className="number">010-1234-5678</p>
+            <p className="address">
+              (12345)판교 카카오로 가는길 1 <br /> (쿠키즈동, 춘식팰리스)
+              101-111
+            </p>
+          </div>
         </div>
-      </div>
 
-      <div className="order-agree">
-        <div className="agree-title">
-          <input type="checkbox" id="agree-all" className="check" />
-          <label htmlFor="agree-all">
-            <span>전체 동의</span>
-          </label>
+        <div className="order-info">
+          <div className="order-product-info">
+            <span className="info-title">주문상품정보</span>
+            {/* 각 주문 정보 */}
+            <OrderItems />
+            <div className="order-total-price">
+              <span>총 주문 금액</span>
+              <span className="won">{comma(totalPrice)}원</span>
+            </div>
+          </div>
         </div>
-        <div className="agree-group">
-          <input type="checkbox" id="agree-1" className="check" />
-          <label htmlFor="agree-1">
-            <span>구매조건 확인 및 결제 진행 동의</span>
-          </label>
 
-          <input type="checkbox" id="agree-2" className="check" />
-          <label htmlFor="agree-2">
-            <span>개인정보 제3자 제공 동의</span>
-          </label>
-        </div>
-      </div>
+        <div className="order-info">
+          <div className="order-agree">
+            <div className="agree-title">
+              <input type="checkbox" id="agree-all" className="check" />
+              <label htmlFor="agree-all">
+                <span>전체 동의</span>
+              </label>
+            </div>
 
-      <div className="order-bottom">
-        <div className="legal-notice">
-          <span>법적고지</span>
-          <br />
-          <span>
-            (주)카카오에서 판매하는 상품 중에는 개별 판매자가 판매하는 상품이
-            포함되어 있습니다. 개별 판매자가 판매하는 상품에 대해 (주)카카오는
-            통신중개 판매업자로서 통신판매의 당사자가 아니며 상품의 주문, 배송
-            및 환불 등과 관련한 의무와 책임은 각 판매자에게 있습니다.
-          </span>
+            <div className="agree-group">
+              <div className="check">
+                <input type="checkbox" id="agree-1" />
+                <label htmlFor="agree-1">
+                  <span> 구매조건 확인 및 결제 진행 동의</span>
+                </label>
+              </div>
+              <div className="check">
+                <input type="checkbox" id="agree-2" />
+                <label htmlFor="agree-2">
+                  <span> 개인정보 제3자 제공 동의</span>
+                </label>
+              </div>
+            </div>
+          </div>
         </div>
-        <button
-          className="checkout-btn"
-          onClick={() => {
-            mutate(null, {
-              onError: () => {
-                alert("주문에 실패했습니다🥲");
-              },
-              onSuccess: (res) => {
-                const id = res.response.id;
-                alert("주문이 완료되었습니다!😉");
-                navigate(`/orders/complete/${id}`);
-              },
-            });
-          }}
-        >
-          결제하기
-        </button>
+
+        <div className="order-bottom">
+          <div className="legal-notice">
+            <span>법적고지</span>
+            <br />
+            <span>
+              (주)카카오에서 판매하는 상품 중에는 개별 판매자가 판매하는 상품이
+              포함되어 있습니다. 개별 판매자가 판매하는 상품에 대해 (주)카카오는
+              통신중개 판매업자로서 통신판매의 당사자가 아니며 상품의 주문, 배송
+              및 환불 등과 관련한 의무와 책임은 각 판매자에게 있습니다.
+            </span>
+          </div>
+          <button
+            className="checkout-btn"
+            onClick={() => {
+              mutate(null, {
+                onError: () => {
+                  alert("주문에 실패했습니다🥲");
+                },
+                onSuccess: (res) => {
+                  const id = res.response.id;
+                  alert("주문이 완료되었습니다!😉");
+                  navigate(`/orders/complete/${id}`);
+                },
+              });
+            }}
+          >
+            결제하기
+          </button>
+        </div>
       </div>
     </div>
   );
