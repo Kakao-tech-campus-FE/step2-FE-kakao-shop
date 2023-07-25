486b06c515726cabc8975b1e2b07be42a16617e9
Dongjin <ehdwls1638@gmail.com>
2023-07-25 17:11:58 +0900

    style : option list bound color

--- src/components/atoms/OptionList.jsx
+++ src/components/atoms/OptionList.jsx
@@ -6,7 +6,7 @@ const OptionList = ({options, onClick}) => {
             {options.map((option) => {
                     return (
                         <li key={option.id} onClick={() => onClick(option)}
-                            className={"w-full p-1 flex flex-col items-start box-border border-solid border border-light-gray box-s cursor-pointer" }>
+                            className={"w-full p-1 flex flex-col items-start box-border border-solid border border-gray-500 box-s cursor-pointer" }>
                             <span className={"p-0.5 w-full text-ellipsis text-justify whitespace-nowrap overflow-hidden"}>{option.optionName}</span>
                             <span className={"p-0.5 block text-sm"}>{comma(option.price)}원</span>
                         </li>
