13ac9c9e84e4b081377b16d898205c0f79cc25cb
unknown <jsh1147@naver.com>
2023-07-25 21:10:10 +0900

    fix: 계정 필요 페이지 접근 제어 수정
    기존의 <Navigate>를 통해 렌더링 도중에 결정하던 코드에서,
    useNaviage를 사용하여 useEffect 안에서 결정하도록 수정함
    이를 통해 useNavigate(-1)로 이전 페이지 이동이 가능해짐

--- src/components/templates/CheckAuth.js
+++ src/components/templates/CheckAuth.js
@@ -1,13 +1,16 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 
 export default function CheckAuth() {
-  return !window.localStorage.getItem("token") ? (
-    window.confirm("로그인이 필요한 메뉴입니다.\n로그인 하시겠습니까?") ? (
-      <Navigate to="/login" />
-    ) : (
-      <Navigate to="/" />
-    )
-  ) : (
-    <Outlet />
-  );
+  const navigate = useNavigate();
+  const auth = window.localStorage.getItem("token");
+
+  useEffect(() => {
+    if (!auth)
+      if (window.confirm("로그인이 필요한 메뉴입니다.\n로그인 하시겠습니까?"))
+        navigate("/login");
+      else navigate(-1);
+  }, [auth, navigate]);
+
+  return auth && <Outlet />;
 }
