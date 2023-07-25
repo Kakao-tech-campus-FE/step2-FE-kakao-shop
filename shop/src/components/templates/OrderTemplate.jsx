4a85f546f54246262b18616f9c474866740351e8
munjihye <m84422583@gmail.com>
2023-07-26 01:42:10 +0900

    feat: 결제 버튼 누르면 결제 완료 페이지로 이동 구현
    error 발생: checkbox를 클릭했을때, checked값이 제대로 변하지 않음

--- shop/src/components/templates/OrderTemplate.jsx
+++ shop/src/components/templates/OrderTemplate.jsx
@@ -1,15 +1,44 @@
-import {useState} from 'react'
 import { comma } from '../../utils/convert'
 import Button from "../atoms/Button"
 import {useMutation} from "react-query"
 import { orderCart } from '../../services/order'
 import { useNavigate } from 'react-router-dom'
+import { useRef, useState } from 'react'
 
 const OrderTemplate = ({data}) => {
   const {products, totalPrice} = data?.data?.response
   const navigate = useNavigate()
+  const [allAgree, setAllAgree] = useState(false)
+  const [agreePayment, setAgreePayment] = useState(false)
+  const [agreePollcy, setAgreePollcy] = useState(false)
+  const allAgreeRef = useRef(null)
+  const agreePaymentRef = useRef(null)
+  const agreePollcyRef = useRef(null)
+
+  const handleAllAgree = (e)=>{
+    const value = e.target.checked
+    console.log(value)
+    setAgreePayment(value)
+    setAgreePollcy(value)
+
+  }
+
+  const handleAgree = (e)=>{
+    const{name, checked} = e.target
+    console.log(name, checked)
+    if(name === 'payment-agree'){
+      setAgreePayment(checked)
+    } else if(name === 'pollcy-agree'){
+      setAgreePollcy(checked)
+    }
+
+    if(!checked){
+      setAllAgree(false)
+    }
+  }
 
   const {mutate} = useMutation({
+    mutationKey: "orders",
     mutationFn: orderCart
   })
 
@@ -67,33 +96,66 @@ const OrderTemplate = ({data}) => {
         {/* 동의 사항 */}
         <div className="flex flex-col p-4 gap-4">
           <div className="flex gap-2">
-            <input type="checkbox" id='all-agree'/>
+            <input 
+              type="checkbox" 
+              id='all-agree'
+              ref={allAgreeRef}
+              checked={allAgree}
+              onChange={handleAllAgree}
+            />
             <label htmlFor="all-agree" className='text-xl font-bold'>전체 동의</label>
           </div>
           <div className="flex gap-2">
-            <input type="checkbox" id='agree'/>
+            <input 
+              type="checkbox" 
+              id='agree'
+              naem='payment-agree'
+              ref={agreePaymentRef}
+              checked={agreePayment}
+              onChange={handleAgree}
+            />
             <label htmlFor="agree" className='text-xl'>
               구매조건 확인 및 결제 진행 동의
             </label>
           </div>
           <div className="flex gap-2">
-            <input type="checkbox" id='policy'/>
+            <input 
+              type="checkbox" 
+              id='policy'
+              naem='pollcy-agree'
+              ref={agreePollcyRef}
+              checked={agreePollcy}
+              onChange={handleAgree}
+            />
             <label htmlFor="policy" className='text-xl'>
               개인정보 제 3자 제공동의 
             </label>
           </div>
           {/* 결제하기 버튼 */}
           <Button 
-            className="bg-yellow-500 w-full p-4 font-medium"
+            className={`
+              w-full p-4 font-medium
+              ${agreePayment && agreePollcy 
+                ?"bg-yellow-500 text-black"
+                :"bg-gray-300 text-gray-500"}`
+            }
+            
             onClick={()=>{
+              if(agreePayment === false || agreePollcy === false){
+                alert("모든 항목에 동의가 필요합니다.")
+                return
+              }
+
               mutate(null, {
-                onError: ()=>{
+                onError: (err)=>{
+                  console.log(err)
                   alert("주문에 실패했습니다.")
                 },
                 onSuccess: (res)=>{
-                  const id = res.response.id
+                  console.log(res)
+                  const id = res.data.response.id
                   alert("주문 완료 :)")
-                  navigate(`/orders/complete/${id}`)
+                  navigate(`/orders/${id}`)
                 }
               })
             }}
