17dcd3d05956cca1518f99008838ec97a685aa53
JiHye.Song <48706964+qwer15948@users.noreply.github.com>
2023-07-26 12:31:42 +0900

    week5/Feat: Handle required login
    - 로그인 필요 페이지 따로 관리
    - 라우터 전체를 Suspense로 묶어서 관리하고 각 페이지에서는 제외

--- kakao-shop/src/pages/CartPage.jsx
+++ kakao-shop/src/pages/CartPage.jsx
@@ -1,5 +1,4 @@
 import CartList from "../components/molecules/CartList";
-import { Suspense } from "react";
 import { getCart } from "../apis/cart";
 import { useQuery } from "@tanstack/react-query";
 import Loader from "../components/atoms/Loader";
@@ -27,13 +26,7 @@ const CartPage = () => {
     return <ErrorPage message="장바구니를 불러오는 중 에러가 발생했습니다." />;
   }
 
-  return (
-    <div className="bg-gray-50 pb-6 h-[80vh]">
-      <Suspense fallback={<Loader />}>
-        <CartList data={data} />
-      </Suspense>
-    </div>
-  );
+  return <CartList data={data} />;
 };
 
 export default CartPage;
