41f7e3e8fb728b8c45f605e47a5a902b8d7cc958
charmeee <mandugohigh@gmail.com>
2023-07-26 18:28:04 +0900

    fix: 에러 속성 변경

--- mandu/src/components/organisms/ProductInfo.js
+++ mandu/src/components/organisms/ProductInfo.js
@@ -24,7 +24,7 @@ const ProductInfo = ({product, showDialog}) => {
             setSelectedOption([]);
         },
         onError: (error) => {
-            alert(error.response.data.message);
+            alert(error.message);
         },
     });
 
