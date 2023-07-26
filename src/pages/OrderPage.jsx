f36567793eedde4bdfb682a6e7728ef30e0e6ef4
H-sooyeon <wndqhr__@naver.com>
2023-07-26 19:04:53 +0900

    feat: 주문하기 페이지 추가

--- src/pages/OrderPage.jsx
+++ src/pages/OrderPage.jsx
@@ -1,6 +1,16 @@
+import { Suspense } from "react";
+import OrderTemplate from "../components/templates/OrderTemplate";
+import Loader from "../components/atoms/Loader";
+import { useQuery } from "react-query";
+import { getCart } from "../apis/cart";
+
 const OrderPage = () => {
+    const {data: cart, isLoading} = useQuery("cart", getCart);
+
     return(
-        <div>OrderPage</div>
+        <Suspense fallback={<Loader />}>
+            {isLoading ? <Loader /> : <OrderTemplate data={cart}/>}
+        </Suspense>
     );
 };
 
