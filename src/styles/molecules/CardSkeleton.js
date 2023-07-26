81f5e61d1f91c876680b7e1e31cde8e36f4cb201
H-sooyeon <wndqhr__@naver.com>
2023-07-26 19:08:35 +0900

    style: 카드 이미지 width 더 크게 변경

--- src/styles/molecules/CardSkeleton.js
+++ src/styles/molecules/CardSkeleton.js
@@ -3,15 +3,16 @@ import styled from 'styled-components';
 export const Container = styled.div`
     display: grid;
     grid-template-columns: repeat(4, 1fr);
-    grid-gap: 2em;
+    grid-gap: 4.5em;
     margin: 3em 0;
     width: 100%;
+    padding: 0 5em;
     max-width: 1200px;
 `
 
 export const ImageLoader = styled.div`
-    width: 18em;
-    height: 11em;
+    width: 20em;
+    height: 13em;
     background-color: #f2f2f2;
     border-radius: 10px;
     animation: skeleton-gradient 1.5s infinite ease-in-out;
@@ -25,11 +26,12 @@ export const ImageLoader = styled.div`
         100% {
           background-color: rgba(165, 165, 165, 0.1);
         }
+    }
 `
 
 export const DeliveryLoader = styled.div`
     margin: 0.3em 0;
-    width: 3em;
+    width: 4em;
     height: 1.1em;
     border-radius: 3px;
     background-color: #f2f2f2;
@@ -49,7 +51,7 @@ export const DeliveryLoader = styled.div`
 
 export const TitleLoader = styled.div`
     display: inline-block;
-    width: 18em;
+    width: 20em;
     height: 3em;
     border-radius: 5px;
     background-color: #f2f2f2;
@@ -68,7 +70,7 @@ export const TitleLoader = styled.div`
 `
 
 export const PriceTextContainerLoader = styled.div`
-    width: 18em;
+    width: 20em;
     height: 2em;
     border-radius: 5px;
     background-color: #f2f2f2;
