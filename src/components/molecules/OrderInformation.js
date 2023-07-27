8dd90b32f9c262aad3953217b6afc7f483baf5ce
unknown <jsh1147@naver.com>
2023-07-28 04:44:21 +0900

    feat: 주문 결제 페이지 로직 구현
    주문 결제 페이지에 필요한 로직을 구현함
    (select & textarea, checkbox, button)

--- src/components/molecules/OrderInformation.js
+++ src/components/molecules/OrderInformation.js
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { convertToPrice } from "utils/convert";
 
 export default function OrderInformation({ data }) {
   const email = useSelector((state) => state.user.email);
+  const [request, setRequest] = useState("");
+
+  const handleRequestChange = (event) => {
+    setRequest(() => event.target.value);
+  };
 
   return (
     <div className="space-y-2 text-left">
@@ -20,18 +26,26 @@ export default function OrderInformation({ data }) {
           <p>010-1234-5678</p>
           <p>(12345) 광주광역시 북구 전남대로 6번길 7-8</p>
         </div>
-        <select className="w-full mb-2 px-1 py-2 rounded border text-sm">
-          <option disabled={true} selected={true}>
+        <select
+          className="w-full mb-2 px-1 py-2 rounded border text-sm"
+          onChange={handleRequestChange}
+          defaultValue=""
+        >
+          <option value="" disabled={true}>
             배송 요청사항을 선택해주세요
           </option>
-          <option>배송 전 연락바랍니다.</option>
-          <option>부재 시 경비실에 맡겨주세요.</option>
-          <option>부재 시 연락주세요.</option>
-          <option>직접입력</option>
+          <option value="배송 전 연락바랍니다.">배송 전 연락바랍니다.</option>
+          <option value="부재 시 경비실에 맡겨주세요.">
+            부재 시 경비실에 맡겨주세요.
+          </option>
+          <option value="부재 시 연락주세요.">부재 시 연락주세요.</option>
+          <option value="">직접입력</option>
         </select>
         <textarea
           className="w-full h-16 resize-none p-2 rounded border text-sm"
           placeholder="배송 시 요청사항을 선택해주세요 (최대 50자)"
+          onChange={handleRequestChange}
+          value={request}
         />
       </div>
       <div className="bg-white border">
@@ -52,7 +66,9 @@ export default function OrderInformation({ data }) {
         <div className="p-3 border-b text-lg font-bold">결제정보</div>
         <div className="flex justify-between p-3 ">
           <span className="">최종 결제금액</span>
-          <span className="text-lg font-bold">{convertToPrice(data.totalPrice)}</span>
+          <span className="text-lg font-bold">
+            {convertToPrice(data.totalPrice)}
+          </span>
         </div>
       </div>
     </div>
--- src/components/templates/OrderSection.js
+++ src/components/templates/OrderSection.js
@@ -1,40 +1,106 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { getCartReq } from "apis/cart";
+import { orderReq } from "apis/order";
 
 import Loader from "components/atoms/Loader";
 
 import OrderInformation from "components/molecules/OrderInformation";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { convertToPrice } from "utils/convert";
 
 export default function OrderSection() {
+  const navigate = useNavigate();
+  const [agreement, setAgreement] = useState({
+    agreeBuy: false,
+    agreeAccount: false,
+  });
   const { isLoading, error, data } = useQuery({
     queryKey: ["carts"],
     queryFn: getCartReq,
+    onSuccess: (res) => {
+      if (res.data.response.totalPrice !== 0) return;
+      alert("장바구니가 비어있습니다. 메인 페이지로 돌아갑니다.");
+      navigate("/");
+    },
   });
+  const { mutate } = useMutation({ mutationFn: orderReq });
+
+  const handleCheckClick = (e) => {
+    const { id } = e.target;
+    switch (id) {
+      case "agreeAll":
+        setAgreement((prevAgree) => ({
+          agreeBuy: !(prevAgree.agreeBuy && prevAgree.agreeAccount),
+          agreeAccount: !(prevAgree.agreeBuy && prevAgree.agreeAccount),
+        }));
+        break;
+      case "agreeBuy":
+      case "agreeAccount":
+        setAgreement((prevAgree) => ({ ...prevAgree, [id]: !prevAgree[id] }));
+        break;
+      default:
+        console.log("체크박스 오류 발생");
+    }
+  };
+
+  const handleButtonClick = () => {
+    if (!agreement.agreeBuy)
+      alert("결제진행을 위한 구매조건 확인 동의를 체크해주세요.");
+    else if (!agreement.agreeAccount)
+      alert("상품배송을 위한 개인정보 제3자 제공 동의를 체크해주세요.");
+    else
+      mutate({
+        onSuccess: (res) => {
+          navigate(`/result/${res.response.id}`);
+        },
+        onError: (err) => {
+          console.dir(err);
+        },
+      });
+  };
+
   return (
     <>
-      {isLoading && <Loader />}
+      {isLoading && <div><Loader /></div>}
       {error && <span>Error</span>}
       {data && (
         <div className="flex-grow bg-gray-100">
           <div className="inline-block w-[800px] mx-auto mt-4 mb-16">
-            <h1 className="block py-2 bg-white border text-sm font-bold">주문하기</h1>
+            <h1 className="block py-2 bg-white border text-sm font-bold">
+              주문하기
+            </h1>
             <OrderInformation data={data.data.response} />
             <div className="bg-white mt-2 border text-left">
               <div className="p-3 border-b space-x-2">
-                <input id="agreeAll" type="checkbox" />
+                <input
+                  id="agreeAll"
+                  type="checkbox"
+                  checked={agreement.agreeBuy && agreement.agreeAccount}
+                  onClick={handleCheckClick}
+                />
                 <label htmlFor="agreeAll" className="text-lg font-bold">
                   전체 동의하기
                 </label>
               </div>
               <div className="p-3 border-b">
-                <input id="agreeBuy" type="checkbox" />
+                <input
+                  id="agreeBuy"
+                  type="checkbox"
+                  checked={agreement.agreeBuy}
+                  onClick={handleCheckClick}
+                />
                 <label htmlFor="agreeBuy">
                   {" "}
                   구매조건 확인 및 결제 진행 동의
                 </label>
                 <br />
-                <input id="agreeAccount" type="checkbox" />
+                <input
+                  id="agreeAccount"
+                  type="checkbox"
+                  checked={agreement.agreeAccount}
+                  onClick={handleCheckClick}
+                />
                 <label htmlFor="agreeAccount"> 개인정보 제3자 제공 동의</label>
               </div>
               <div className="p-3 bg-gray-50 border-b space-y-1 text-xs">
@@ -46,7 +112,10 @@ export default function OrderSection() {
                   배송 및 환불 등과 관련한 의무와 책임은 각 판매자에게 있습니다.
                 </p>
               </div>
-              <button className="block w-full p-3 bg-yellow-300 text-xl font-bold">
+              <button
+                className="block w-full p-3 bg-yellow-300 text-xl font-bold"
+                onClick={handleButtonClick}
+              >
                 {convertToPrice(data.data.response.totalPrice)} 결제하기
               </button>
             </div>
