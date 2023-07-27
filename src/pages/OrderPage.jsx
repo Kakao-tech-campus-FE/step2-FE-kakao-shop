0ab76f9e168b5fff95d343a0d823315e1d6114a7
Heo_Donghyuk <missko30@pusan.ac.kr>
2023-07-27 10:48:32 +0900

    feat: applied Loader on OrderPage

--- src/pages/OrderPage.jsx
+++ src/pages/OrderPage.jsx
@@ -1,15 +1,16 @@
 import { useQuery } from "react-query";
-import GNB from "../components/organisms/GNB";
 import OrderTemplate from "../components/templates/OrderTemplate";
 import { getCart } from "../services/api/cart";
+import { Suspense } from "react";
+import Loader from "../components/atoms/Loader";
 
 const OrderPage = () => {
   const { data, error, isLoading } = useQuery("cart", getCart);
 
   return (
-    <div>
+    <Suspense fallback={<Loader />}>
       <OrderTemplate />
-    </div>
+    </Suspense>
   );
 };
 export default OrderPage;
