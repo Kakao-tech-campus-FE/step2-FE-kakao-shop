0d7c97d0d71da777d543036b2f2e3f4db4f5a40b
Donghyeon Lee <ehdgus001928@gmail.com>
2023-07-27 16:31:10 +0900

    feat: Add product image in cartPage and orderPage

--- src/components/organisms/orderItemCard.tsx
+++ src/components/organisms/orderItemCard.tsx
@@ -1,5 +1,6 @@
 import { CartItem } from '../../types/product';
 import { comma } from '../../utils/comma';
+import Photo from '../atoms/photo';
 
 export default function OrderItemCard({
   id,
@@ -8,9 +9,14 @@ export default function OrderItemCard({
 }: CartItem) {
   return (
     <div>
-      <h2 className="my-2 font-bold">
-        {productName}
-      </h2>
+      <section className="flex flex-row items-center gap-2">
+        <div className="w-16 rounded-sm">
+          <Photo src={`${new URL(`/images/${id}.jpg`, process.env.REACT_APP_KAKAO_API_URL).toString()}`} alt={productName} />
+        </div>
+        <h2 className="my-2 font-bold">
+          {productName}
+        </h2>
+      </section>
       <ul>
         {carts.map((cart) => (
           cart.quantity > 0 ? (
