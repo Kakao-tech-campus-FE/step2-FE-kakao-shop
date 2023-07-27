6ad72aee836f5f1cc37bbe9fb24b1166c152457e
localgaji <eldorado829@naver.com>
2023-07-27 18:32:36 +0900

    add page title component

---
+++ src/components/atoms/PageTitleBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageTitleBox = ( { title } ) => {
+  return (
+    <div className='mx-auto mb-4'>
+        <h1 className='text-lg'>{title}</h1>
+    </div>
+  )
+}
+
+export default PageTitleBox
