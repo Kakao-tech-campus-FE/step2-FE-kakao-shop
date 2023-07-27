cca7c69c0b90051f26b86d35b0d5ee43688f1d30
localgaji <eldorado829@naver.com>
2023-07-27 22:31:54 +0900

    modified section container style

--- src/components/atoms/Section.jsx
+++ src/components/atoms/Section.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Section = (props) => {
   return (
     <section className='
-        flex flex-col items-stretch w-[92%] max-w-[900px] mx-auto
+        flex flex-col items-stretch w-[95%] max-w-[900px] mx-auto
     '>
         {props.children}
     </section>
