67a0281e0c8b979bf6b8aa733d3ec5354fb04be4
kjh302903 <kjh302903@gmail.com>
2023-07-27 21:51:20 +0900

    refactor: 장바구니, 결제 페이지 배경 컴포넌트 분리
    장바구니, 결제 페이지에서 배경을 Background 컴포넌트로 분리

---
+++ src/components/atoms/Background/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { styled } from "styled-components";
+
+interface Props {
+  children?: React.ReactNode;
+}
+
+const Background = ({ children }: Props) => {
+  return <Wrapper>{children}</Wrapper>;
+};
+
+export default Background;
+
+const Wrapper = styled.div`
+  height: auto;
+  min-height: 100vh;
+  min-width: 1280px;
+  background-color: #f4f4f4;
+  overflow: auto;
+`;
