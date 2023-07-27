0aab2365cc07f979babce1fdc0ad49db759a0708
localgaji <eldorado829@naver.com>
2023-07-28 03:12:57 +0900

    add custom hook for submission login

---
+++ src/hooks/useLogin.jsx
@@ -0,0 +1,54 @@
+import { useState, useCallback, useEffect } from 'react'
+import postLogin from 'api/login'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { setUserReducer } from 'reducers/loginSlice'
+
+const useLogin = (user) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [wrong, setWrong] = useState(false)
+  
+  useEffect(()=>{
+    setWrong(prev => false)
+  }, [user])
+
+  // 로그인 실패 횟수 : 로컬스토리지에 저장하고 최초 로딩시 초기값으로 불러오기
+  const [failCnt, setFailCnt] = useState(() => {
+    if (localStorage.getItem("failCnt") === undefined) {
+      return  0
+    } 
+    return Number(localStorage.getItem("failCnt"))
+  })
+
+  const handler = useCallback( () => {
+    postLogin(user)
+      .then((response) => {
+        // Redux store에 상태 저장
+        dispatch(setUserReducer( {
+          email: user.email,
+          token: response,
+          loginTime: Date.now(),
+          islogin: true,
+        } ))
+
+        localStorage.removeItem("failCnt")  // 로그인실패횟수 초기화
+        navigate("/")
+      })
+      .catch((error) => {
+        // 로그인 실패 시 실패횟수 +1, 실패 상태 true
+        if (error.response && error.response.status === 401) {
+          setFailCnt(prev => {
+            localStorage.setItem('failCnt', prev + 1)
+            setWrong(prev => true)
+            return (prev + 1)
+          })
+        }
+      }) 
+  }) 
+
+  return [handler, wrong, failCnt]
+}
+
+export default useLogin
