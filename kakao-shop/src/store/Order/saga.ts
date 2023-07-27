49f3b506e573fed34e77435355649ce649e5fdbe
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-07-27 21:37:44 +0900

    feat: 주문하기 서버 요청

---
+++ kakao-shop/src/store/Order/saga.ts
@@ -0,0 +1,54 @@
+import type { OrderResponse, PaymentResponse } from '@apis/Order';
+import { approveAPI, orderAPI, paymentAPI } from '@apis/Order';
+import {
+  APPROVE_REQUEST,
+  ApproveRequestAction,
+  ORDER_PRODUCT_REQUEST,
+  PAYMENT_REQUEST,
+  PaymentRequestAction,
+  approveSuccessAction,
+  orderProductFailureAction,
+  orderProductRequestAction,
+  orderProductSuccessAction,
+  paymenSuccessAction,
+} from '@store/Order/reducers';
+import type { AxiosError, AxiosResponse } from 'axios';
+import { call, put, takeLatest } from 'redux-saga/effects';
+
+export function* watchOrder() {
+  try {
+    const response: AxiosResponse<OrderResponse> = yield call(orderAPI);
+    yield put(orderProductSuccessAction(response));
+  } catch (err: unknown) {
+    const error = err as AxiosError<OrderResponse>;
+    yield put(orderProductFailureAction(error));
+  }
+}
+
+export function* watchPayment({ payload }: PaymentRequestAction) {
+  try {
+    const response: AxiosResponse<PaymentResponse> = yield call(paymentAPI, payload);
+    yield put(paymenSuccessAction(response));
+  } catch (err: unknown) {
+    // const error = err as AxiosError<OrderResponse>;
+  }
+}
+
+export function* watchApprove({ payload }: ApproveRequestAction) {
+  try {
+    yield call(approveAPI, payload);
+    yield put(approveSuccessAction());
+    yield put(orderProductRequestAction());
+    const response: AxiosResponse<OrderResponse> = yield call(orderAPI);
+    yield put(orderProductSuccessAction(response));
+    window.close();
+  } catch (err: unknown) {
+    // const error = err as AxiosError<OrderResponse>;
+  }
+}
+
+export function* OrderSaga() {
+  yield takeLatest(ORDER_PRODUCT_REQUEST, watchOrder);
+  yield takeLatest(PAYMENT_REQUEST, watchPayment);
+  yield takeLatest(APPROVE_REQUEST, watchApprove);
+}
--- kakao-shop/src/store/rootReducer.ts
+++ kakao-shop/src/store/rootReducer.ts
@@ -2,6 +2,7 @@ import { CartReducer } from '@store/Cart/reducers';
 import { DetailReducer } from '@store/Detail/reducers';
 import { homeReducer } from '@store/Home/reducers';
 import { signInReducer } from '@store/Login/reducers';
+import { OrderReducer } from '@store/Order/reducers';
 import { combineReducers } from 'redux';
 
 import { signUpReducer } from './SignUp/reducers';
@@ -12,4 +13,5 @@ export const rootReducer = combineReducers({
   home: homeReducer,
   detail: DetailReducer,
   cart: CartReducer,
+  order: OrderReducer,
 });
