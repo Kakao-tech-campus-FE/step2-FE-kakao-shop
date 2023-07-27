9dde6c5adb4ecf09bcea5da007792c44d3d18412
Klomachenko <leekyumin0901@naver.com>
2023-07-27 21:50:51 +0900

    feat: tailwind 적용, 주문 관련 버튼, 동의 체크 구현

--- kakao_2th/src/components/templates/OrderTemplate.js
+++ kakao_2th/src/components/templates/OrderTemplate.js
@@ -1,4 +1,15 @@
+import { useMutation } from "react-query"
+import { comma } from "../../utils/convert"
+import { order } from "../../services/order"
+import { useNavigate } from "react-router-dom"
+
 const OrderTemplate ({ data }) => {
+    const carts = []
+    const navigate = useNavigate()
+
+    const { mutate } = useMutation({
+        queryFn: () => order
+    })
 
     return (
         <div className="py-20">
@@ -22,6 +33,65 @@ const OrderTemplate ({ data }) => {
                 </div>
                 <div className="border py-4">
                     <span>광주광역시 북구 용봉로 77</span>
+                </div>
+                <div className="border py-4">
+                    <h2>주문상품 정보</h2>
+                </div>
+                {carts.map((item) => {
+                    return (
+                        <div key={item.id}>
+                            <div className="product-name">
+                                <span>{item.name}</span>
+                                <span>{item.carts[0].optionName}</span>
+                            </div>
+                            <div className="quantity">
+                                <span>{comma(item.quantity)}개</span>
+                            </div>
+                            <div className="price">
+                                <span>{comma(item.price)}원</span>
+                            </div>
+                        </div>
+                    )
+                })}
+                <div className="border p-4 flex items-center justyfy-between">
+                    <h3>총 주문 금액</h3>
+                    <span className="price">{comma(data.totalPrice)}원</span>
+                </div>
+                <div className="border p-4">
+                    <div className="flex">
+                        <input type="checkbox" id="all-agree" />
+                        <label htmlFor="all-agree" className="text-xl font-bold">
+                            전체 동의
+                        </label>
+                    </div>
+                    <div className="flex">
+                        <input type="checkbox" id="agree" />
+                        <label htmlFor="agree" className="text-sm">
+                            구매조건 확인 및 결제 진행 동의
+                        </label>
+                    </div>
+                    <div className="flex">
+                        <input type="checkbox" id="policy" />
+                        <label htmlFor="policy" className="text-sm">
+                            개인정보 제 3자 제공동의
+                        </label>
+                    </div>
+                    <button
+                        onClick={() => {
+                            mutate(null, {
+                                onError: () => {
+                                    alert("주문에 실패했습니다.")
+                                },
+                                onSuccess: (res) => {
+                                    const id = res.response.id
+                                    alert("주문이 완료되었습니다.")
+                                    navigate(`/orders/complete/${id}`)
+                                }
+                            })
+                        }}
+                        className="bg-yellow-500 w-full p-4 font-medium">
+                        결제하기
+                    </button>
                 </div>
             </div>
         </div>
