e0b724aeac64424cc1007507bcd3a3ef015b6249
kjh302903 <kjh302903@gmail.com>
2023-07-27 21:54:24 +0900

    feat: Order 페이지 추가
    Order 페이지 추가

---
+++ src/components/atoms/OrderContainer/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { styled } from "styled-components";
+
+interface Props {
+  children?: React.ReactNode;
+}
+
+const OrderContainer = ({ children }: Props) => {
+  return (
+    <Container>
+      <Header>
+        <p>주문하기</p>
+      </Header>
+      {children}
+    </Container>
+  );
+};
+
+export default OrderContainer;
+
+const Container = styled.div`
+  margin: 100px auto 50px;
+  width: 870px;
+`;
+
+const Header = styled.div`
+  height: 44px;
+  border-top: 1px solid #ebebeb;
+  background-color: #fff;
+  text-align: center;
+
+  & > p {
+    margin-top: 11px;
+    font-weight: 700;
+    font-size: 15px;
+    line-height: 21px;
+  }
+`;
--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,5 +1,12 @@
+import Background from "@components/atoms/Background";
+import OrderTemplate from "@components/templates/OrderTemplate";
+
 const Order = () => {
-  return <div></div>;
+  return (
+    <Background>
+      <OrderTemplate />
+    </Background>
+  );
 };
 
 export default Order;
