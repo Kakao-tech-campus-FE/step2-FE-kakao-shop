54f0caa7fdf86506d679b0427e532f6c739b47c2
Klomachenko <leekyumin0901@naver.com>
2023-07-27 22:07:12 +0900

    feat: OrderTemplate 스타일링

--- kakao_2th/src/components/templates/OrderTemplate.js
+++ kakao_2th/src/components/templates/OrderTemplate.js
@@ -18,14 +18,14 @@ const OrderTemplate ({ data }) => {
         products.forEach((item) => {
             renderComponent.push(item.carts.map((cart) => {
                 return (
-                    <div key={cart.id}>
-                        <div className="product-name">
+                    <div key={cart.id} className="p-4 border-t">
+                        <div className="product-name font-bold">
                             <span>{`${item.productName} - ${cart.option.optionName}`}</span>
                         </div>
                         <div className="quantity">
                             <span>{comma(cart.quantity)}개</span>
                         </div>
-                        <div className="price">
+                        <div className="price font-bold">
                             <span>{comma(cart.price) * cart.quantity}원</span>
                         </div>
                     </div>
@@ -65,23 +65,23 @@ const OrderTemplate ({ data }) => {
                 <OrderItems />
 
                 <div className="border p-4 flex items-center justyfy-between">
-                    <h3>총 주문 금액</h3>
-                    <span className="price">{comma(totalPrice)}원</span>
+                    <h3 className="font-bold text-xl">총 주문 금액</h3>
+                    <span className="price text-xl text-indigo-700">{comma(totalPrice)}원</span>
                 </div>
-                <div className="border p-4">
-                    <div className="flex">
+                <div className="border flex flex-col p-4 gap-4">
+                    <div className="flex gap-2">
                         <input type="checkbox" id="all-agree" />
                         <label htmlFor="all-agree" className="text-xl font-bold">
                             전체 동의
                         </label>
                     </div>
-                    <div className="flex">
+                    <div className="flex gap-2">
                         <input type="checkbox" id="agree" />
                         <label htmlFor="agree" className="text-sm">
                             구매조건 확인 및 결제 진행 동의
                         </label>
                     </div>
-                    <div className="flex">
+                    <div className="flex gap-2">
                         <input type="checkbox" id="policy" />
                         <label htmlFor="policy" className="text-sm">
                             개인정보 제 3자 제공동의
