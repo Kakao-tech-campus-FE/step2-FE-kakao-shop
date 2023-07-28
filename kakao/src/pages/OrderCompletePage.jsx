70eeed1b18c7ec22a2aa7a3c353737765b0922e9
sihyonn <sihyeon982@naver.com>
2023-07-28 15:40:51 +0900

    테스팅 준비

--- kakao/src/pages/OrderCompletePage.jsx
+++ kakao/src/pages/OrderCompletePage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import OrderCompleteTemplate from "../components/templates/OrderCompleteTemplate";
 
 function OrderCompletePage() {
-  return <div>OrderCompletePage</div>;
+  return (
+    <div className="orderCompletepage">
+      <OrderCompleteTemplate />
+    </div>
+  );
 }
 
 export default OrderCompletePage;
