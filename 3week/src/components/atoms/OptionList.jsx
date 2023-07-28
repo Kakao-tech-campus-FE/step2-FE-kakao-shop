c3a92a9ed042d41dc2630ce05cf1b695751e3ae8
Sang Min Park <125769061+s010419@users.noreply.github.com>
2023-07-28 21:22:50 +0900

    Update OptionList.jsx

--- 3week/src/components/atoms/OptionList.jsx
+++ 3week/src/components/atoms/OptionList.jsx
@@ -2,6 +2,8 @@ import { comma } from "../../utils/convert"
 import "../../styles/atoms/OptionList.css"
 
 const OptionList = ({options, onClick}) => {
+  // options 옵션들이 담긴 배열
+  // onClick 각 옵션 클릭 시 호출되는 함수
   return (
     <ol className="option-list">
       {options.map((option, index) => (
