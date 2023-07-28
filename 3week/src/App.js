c1a6a9b4c49911e9ed6aa0cca251caa5fdf1a7c6
Sang Min Park <125769061+s010419@users.noreply.github.com>
2023-07-28 21:29:17 +0900

    Update App.js

--- 3week/src/App.js
+++ 3week/src/App.js
@@ -2,8 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import RegisterPage from "./pages/RegisterPage"
 import LoginPage from "./pages/LoginPage"
 import HomePage from "./pages/HomePage"
+import CartPage from "./pages/CartPage"
+import OrderPage from "./pages/OrderPage"
 import MainLayout from "./layouts/MainLayout"
 import ProductDetailPage from "./pages/ProductDetailPage"
+import RequiredAuthLayout from "./layouts/RequiredAuthLayout"
+import "./App.css"
 
 function App() {
   return (
@@ -17,6 +21,11 @@ function App() {
           <Route element={<MainLayout />}>
             <Route path="/" element={<HomePage />} />
             <Route path="/product/:id" element={<ProductDetailPage />} />
+          </Route>
+          {/* 사용자가 로그인됐을 때만 접근 가능한 레이아웃 */}
+          <Route element={<RequiredAuthLayout />}>
+            <Route path="/cart" element={CartPage}></Route>
+            <Route path="/order" element={OrderPage}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
