b291eb313a9fc2bbd1792723f81bc5657e0d7e55
blackhblee <blackhblee@gmail.com>
2023-07-28 16:48:44 +0900

    fix: Fix refresh bug
    새로고침 시 생기는 버그를 수정했습니다.

--- src/components/templates/ProductSectionTemplate.jsx
+++ src/components/templates/ProductSectionTemplate.jsx
@@ -11,41 +11,49 @@ import { fetchProducts } from "../../api/product";
  */
 const ProductSectionTemplate = () => {
   const bottomObserver = useRef(null);
+  const isFirstLoad = useRef(true);
   const [products, setProducts] = useState([]);
   const [isEnd, setIsEnd] = useState(false);
   const [page, setPage] = useState(0);
 
-  const { data, error, loading } = useQuery([page], () => fetchProducts(page));
-
-  useEffect(() => {
-    if (data) {
-      setProducts((prevProducts) =>
-        _.uniqBy([...prevProducts, ...data.data.response], "id")
-      );
-      setIsEnd(data.data.response.length !== 9);
-    }
-  }, [data]);
+  const { data, error, isLoading } = useQuery([page], () =>
+    fetchProducts(page)
+  );
 
   useEffect(() => {
     const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting && !isEnd) {
-          setPage((prev) => prev + 1);
-        }
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting && !isEnd) {
+            // eslint-disable-next-line no-shadow
+            setPage((page) => page + 1);
+          }
+        });
       },
-      { threshold: 1 }
+      { root: null, threshold: 1 }
     );
-
-    if (bottomObserver.current) {
+    const currentObserver = bottomObserver.current;
+    if (currentObserver && !isFirstLoad.current && !isLoading) {
       observer.observe(bottomObserver.current);
     }
 
     return () => {
-      if (bottomObserver.current) {
-        observer.unobserve(bottomObserver.current);
-      }
+      // eslint-disable-next-line no-shadow
+      const currentObserver = bottomObserver.current;
+      if (currentObserver) observer.unobserve(currentObserver);
     };
-  }, [bottomObserver, isEnd]);
+  }, [isLoading, isEnd]);
+
+  useEffect(() => {
+    if (!isLoading && data) {
+      setProducts(_.uniqBy([...products, ...data.data.response], "id"));
+      if (data?.data?.response?.length < 9) setIsEnd(() => true);
+    }
+  }, [data, isLoading]);
+
+  useEffect(() => {
+    isFirstLoad.current = false;
+  }, []);
 
   if (error) {
     return <div>{error.message}</div>;
@@ -53,7 +61,7 @@ const ProductSectionTemplate = () => {
 
   return (
     <Container className="product-section flex max-w-none mx-auto w-[1280px] mt-[80px]">
-      <ProductGrid products={products} loading={loading} />
+      <ProductGrid products={products} loading={isLoading} />
       <div ref={bottomObserver} />
     </Container>
   );
