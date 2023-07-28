0406f9e0d627dc9f6d41e05c1eab278bae2eb219
SeonghyeonKim <kkgojn@naver.com>
2023-07-28 17:40:19 +0900

    fix: Main Layout
    make more universally

--- kakao-store/src/layouts/MainLayout.jsx
+++ kakao-store/src/layouts/MainLayout.jsx
@@ -1,5 +1,7 @@
+import { Outlet } from "react-router-dom";
 import Gnb from "../components/organisms/Gnb";
 import MainProductTemplate from "../components/templates/MainProductTemplate";
+import Footer from "../components/organisms/Footer";
 
 const MainLayout = () => {
     return (
@@ -7,9 +9,9 @@ const MainLayout = () => {
             {/* 로그인 버튼, 장바구니 버튼, 메인 로고 */}
             <Gnb />
             {/* 콘텐츠 영역: 페이지마다 달라지는 영역 */}
-            <MainProductTemplate />
+            <Outlet />
             {/* 푸터 영역 */}
-            {/*<Footer />*/}
+            <Footer />
         </>
     );
 }
