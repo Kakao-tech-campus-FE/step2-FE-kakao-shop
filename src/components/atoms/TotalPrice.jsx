59bab140ef9e3eb174ec3ccd5ece78c97de756e3
baegyeong <beth0929@naver.com>
2023-07-28 23:42:10 +0900

    feat: 총 주문 금액 출력

---
+++ src/components/atoms/TotalPrice.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import { useState } from "react";
+import { comma } from "../../utils/convert";
+
+const Price = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 32rem;
+  padding: 0.8rem;
+  font-weight: bold;
+`;
+
+const TotalPrice = ({ item }) => {
+  const orderItem = [];
+
+  item.map((eachItem) => {
+    orderItem.push(eachItem.carts);
+  });
+
+  const totalPrice = orderItem
+    .flatMap((x) => x.map((x) => x.price))
+    .reduce((acc, cur) => acc + cur);
+
+  return (
+    <>
+      <Price className="row">
+        <div>총 주문 금액</div>
+        <div>{comma(totalPrice)}원</div>
+      </Price>
+    </>
+  );
+};
+
+export default TotalPrice;
