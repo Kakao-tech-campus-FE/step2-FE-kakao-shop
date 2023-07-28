df85e17c2c74422ec813694829d26093e14556b6
localgaji <eldorado829@naver.com>
2023-07-28 10:23:17 +0900

    add submit handler : order > result

--- src/components/organisms/Order.jsx
+++ src/components/organisms/Order.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 
 import Section from 'components/atoms/Section'
 import SubmitButton from 'components/atoms/SubmitButton'
+import PageTitleBox from 'components/atoms/PageTitleBox'
+
 import AccordionBox from 'components/molecules/AccordionBox'
 import CheckItem from 'components/molecules/CheckItem'
 import OrderProducts from 'components/molecules/Order/OrderProducts'
@@ -10,10 +12,11 @@ import RadioGroup from 'components/molecules/RadioGroup'
 
 import useCheckbox from 'hooks/useCheckbox'
 import strPrice from 'utils/price'
-import PageTitleBox from 'components/atoms/PageTitleBox'
+import { saveOrder } from 'api/order'
+import { useNavigate } from 'react-router-dom'
 
 
-const Order = ( { query, userAddress, agreeList, paymentList } ) => {
+const Order = ( { data, userAddress, agreeList, paymentList } ) => {
 
   const [value, setValue] = useState(null)
   const [checkedSet, setCheckedSet, allChecked] = useCheckbox( agreeList )
@@ -22,17 +25,29 @@ const Order = ( { query, userAddress, agreeList, paymentList } ) => {
   const radioOnChange = (event) => {
     setPayment(prev => event.target.id)
   }
+  
+  const navigate = useNavigate()
+  /** 제출버튼 클릭 시 주문 요청 */
+  const submitHandler = () => {
+    saveOrder()
+    .then((res) => {
+      navigate(`/orders/${res.id}`)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
 
   return (
     <Section>
       <PageTitleBox title="주문하기"/>
-      <AccordionBox title='배송 정보' initialOpen={false}>
+      <AccordionBox title='배송 정보' initialOpen>
         <OrderAddress info={userAddress} value={value} 
           onChange={(e) => setValue(prev=> e.target.value)}/>
       </AccordionBox>
 
-      <AccordionBox title='주문상품 정보' initialOpen={false}>
-        <OrderProducts products={query.data.products} />
+      <AccordionBox title='주문상품 정보' initialOpen>
+        <OrderProducts products={data.products} />
       </AccordionBox>
 
       <KakaoBox>
@@ -53,18 +68,21 @@ const Order = ( { query, userAddress, agreeList, paymentList } ) => {
             onChange={setCheckedSet} label='전체 동의'
           />
         } 
-        initialOpen={true} fixed={true} >
+        initialOpen fixed >
 
-          {agreeList.map(item => (
-            <CheckItem 
-              id={item.id} checked={checkedSet.has(item.id)} 
-              onChange={setCheckedSet} label={item.title}
-            />
-          ))}
+        {agreeList.map(item => (
+          <CheckItem 
+            id={item.id} checked={checkedSet.has(item.id)} 
+            onChange={setCheckedSet} label={item.title}
+          />
+        ))}
       </AccordionBox>
       
-      <SubmitButton disabled={!allChecked || payment === null}>
-        {strPrice(query.data.totalPrice)} 결제하기
+      <SubmitButton onClick={submitHandler} 
+        disabled={!allChecked || payment === null} 
+        disabledColor='yellow'
+      >
+        {strPrice(data.totalPrice)} 결제하기
       </SubmitButton>
     </Section>
   )
