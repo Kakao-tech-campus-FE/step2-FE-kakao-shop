ccf8e2f81dfe05cbf86e4ff9662862d0dc859324
dabinchiii <dabinchi388@naver.com>
2023-07-28 12:12:50 +0900

    style: Apply tailwind css

--- src/components/atoms/Photo.jsx
+++ src/components/atoms/Photo.jsx
@@ -1,10 +1,15 @@
-import styles from "./Photo.module.css";
-
 const Photo = ({ className, src, alt }) => {
     return (
-        <picture className={`${styles.photo} ${className}`}>
-            <source srcSet={src} />
-            <img src={src} alt={alt} />
+        <picture className={`photo ${className} w-[100%]`}>
+            <source 
+                className={className} 
+                srcSet={src} 
+            />
+            <img 
+                className={`${className} w-inherit border border-[0.1px] border-[#f1f1f1]`}
+                src={src}
+                alt={alt} 
+            />
         </picture>
     )
 }
