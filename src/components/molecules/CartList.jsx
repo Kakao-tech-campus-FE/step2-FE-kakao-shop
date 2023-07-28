1fbaed7fc64b006365d40bca9e2b3d280c8ace96
minjuko <200778@jnu.ac.kr>
2023-07-28 21:44:55 +0900

    장바구니 페이지 에러 처리

--- src/components/molecules/CartList.jsx
+++ src/components/molecules/CartList.jsx
@@ -23,6 +23,20 @@ const CartList = () => {
 
   const { mutate } = useMutation({
     mutationFn: updateCart,
+    onError: (error) => {
+      if (error.response && error.response.status === 401) {
+        // 로그인 정보가 없어 헤더에 authorization이 없는 경우 401 에러를 처리하여 로그인 페이지로 이동한다.
+        alert("로그인 정보가 없습니다. 로그인 페이지로 이동합니다.");
+        navigate("/login");
+      } else if (error.response && error.response.status === 404) {
+        // 페이지를 찾을 수 없는 경우 404 페이지로 이동한다.
+        alert("페이지를 찾을 수 없습니다. 404 페이지로 이동합니다.");
+        navigate("/*");
+      } else {
+        // 서버 에러의 경우 alert창을 띄운다.
+        alert("주문에 실패했습니다. 다시 시도해주세요.");
+      }
+    },
   });
 
   useEffect(() => {
