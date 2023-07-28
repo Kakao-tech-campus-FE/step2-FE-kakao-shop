0a1c2a1bf76e0efcc4c9dd499a62e15c6f97c81a
Sang Min Park <125769061+s010419@users.noreply.github.com>
2023-07-28 23:13:01 +0900

    Update CartPage.jsx

--- 3week/src/pages/CartPage.jsx
+++ 3week/src/pages/CartPage.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom"
 import CartList from "../components/molecules/CartList"
 import { Suspense, useEffect, useState } from "react"
-import { getCart } from "../apis/cart"
+import { getCart } from "../services/cart"
 import React from "react"
 import { useQuery } from "react-query"
 import Loader from "../components/atoms/Loader"
