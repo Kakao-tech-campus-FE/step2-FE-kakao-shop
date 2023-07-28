68da04445f88b203696896e62d344f5871a1d4a2
SeonghyeonKim <kkgojn@naver.com>
2023-07-28 17:26:40 +0900

    feat: Add Auth Layout

---
+++ kakao-store/src/Link.test.js
@@ -0,0 +1,26 @@
+import renderer from 'react-test-renderer';
+import Link from '../Link';
+
+it('changes the class when hovered', () => {
+  const component = renderer.create(
+    <Link page="http://www.facebook.com">Facebook</Link>,
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  // manually trigger the callback
+  renderer.act(() => {
+    tree.props.onMouseEnter();
+  });
+  // re-rendering
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  // manually trigger the callback
+  renderer.act(() => {
+    tree.props.onMouseLeave();
+  });
+  // re-rendering
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
