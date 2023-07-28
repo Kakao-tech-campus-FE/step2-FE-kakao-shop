749c279cddc122e4f882c7c1fbb4d613231a8858
Klomachenko <leekyumin0901@naver.com>
2023-07-28 15:09:30 +0900

    feat: ordertemplate 에 orderitems 컴포넌트 추가 및 주문 상품 정보 반영

--- kakao_2th/src/components/templates/OrderTemplate.js
+++ kakao_2th/src/components/templates/OrderTemplate.js
@@ -4,15 +4,11 @@ import { order } from "../../services/order"
 import { useNavigate } from "react-router-dom"
 import { useRef, useState } from "react"
 
-const OrderTemplate ({ data }) => {
-    const product = []
-    const totalPrice = 0
-    // const { products, totalPrice } = data?.data?.response
-    const navigate = useNavigate()
-    // const [allAgree, setAllagree] = useState(false)
+const OrderTemplate = ({ data }) => {
     const [agreePayment, setAgreePayment] = useState(false)
     const [agreePolicy, setAgreePolicy] = useState(false)
 
+    const navigate = useNavigate()
     const allAgreeRef = useRef(null)
     const agreePaymentRef = useRef(null)
     const agreePolicyRef = useRef(null)
@@ -38,30 +34,25 @@ const OrderTemplate ({ data }) => {
         queryFn: () => order
     })
 
-    // const OrderItems = () => {
-    //     let renderComponent = []
-    //     if (Array.isArray(products) === false) return
+    const OrderItems = () => {
+        if (!Array.isArray(data?.products)) return null;
 
-    //     products.forEach((item) => {
-    //         renderComponent.push(item.carts.map((cart) => {
-    //             return (
-    //                 <div key={cart.id} className="p-4 border-t">
-    //                     <div className="product-name font-bold">
-    //                         <span>{`${item.productName} - ${cart.option.optionName}`}</span>
-    //                     </div>
-    //                     <div className="quantity">
-    //                         <span>{comma(cart.quantity)}개</span>
-    //                     </div>
-    //                     <div className="price font-bold">
-    //                         <span>{comma(cart.price) * cart.quantity}원</span>
-    //                     </div>
-    //                 </div>
-    //             )
-    //         }))
-    //     })
-
-    //     return renderComponent
-    // }
+        return data.products.map((item) =>
+            item.items.map((cart) => (
+                <div key={cart.id} className="p-4 border-t">
+                    <div className="product-name font-bold">
+                        <span>{`${item.productName} - ${cart.optionName}`}</span>
+                    </div>
+                    <div className="quantity">
+                        <span>{comma(cart.quantity)}개</span>
+                    </div>
+                    <div className="price font-bold">
+                        <span>{comma(cart.price * cart.quantity)}원</span>
+                    </div>
+                </div>
+            ))
+        );
+    };
 
     return (
         <div className="py-20">
@@ -72,9 +63,10 @@ const OrderTemplate ({ data }) => {
                 <div className="border py-4">
                     <h2 className="text-sm font-bold">배송지 정보</h2>
                 </div>
+                {/* 배송지 정보 렌더링 */}
                 <div className="border py-4">
                     <div className="flex items-center gap-2">
-                        <spna>홍길동</spna>
+                        <span>홍길동</span>
                         <span className="text-blue-400 bg-blue-100 rounded-md text-xs p-1">
                             기본배송지
                         </span>
@@ -89,15 +81,18 @@ const OrderTemplate ({ data }) => {
                 <div className="border py-4">
                     <h2>주문상품 정보</h2>
                 </div>
+                {/* 주문 상품 목록 렌더링 */}
                 <OrderItems />
 
-                <div className="border p-4 flex items-center justyfy-between">
+                <div className="border p-4 flex items-center justify-between">
                     <h3 className="font-bold text-xl">총 주문 금액</h3>
-                    <span className="price text-xl text-indigo-700">{comma(totalPrice)}원</span>
+                    <span className="price text-xl text-indigo-700">{comma(data?.totalPrice)}원</span>
                 </div>
                 <div className="border flex flex-col p-4 gap-4">
                     <div className="flex gap-2">
-                        <input type="checkbox" id="all-agree"
+                        <input
+                            type="checkbox"
+                            id="all-agree"
                             ref={allAgreeRef}
                             checked={agreePayment && agreePolicy}
                             onChange={handleAllAgree}
@@ -107,54 +102,57 @@ const OrderTemplate ({ data }) => {
                         </label>
                     </div>
                     <div className="flex gap-2">
-                        <input type="checkbox" id="agree"
+                        <input
+                            type="checkbox"
+                            id="agree"
                             name="payment-agree"
                             ref={agreePaymentRef}
                             checked={agreePayment}
-                            onChange={handleAgreement} />
+                            onChange={handleAgreement}
+                        />
                         <label htmlFor="agree" className="text-sm">
                             구매조건 확인 및 결제 진행 동의
                         </label>
                     </div>
                     <div className="flex gap-2">
-                        <input type="checkbox" id="policy"
+                        <input
+                            type="checkbox"
+                            id="policy"
                             name="policy-agree"
                             ref={agreePolicyRef}
                             checked={agreePolicy}
-                            onChange={handleAgreement} />
+                            onChange={handleAgreement}
+                        />
                         <label htmlFor="policy" className="text-sm">
                             개인정보 제 3자 제공동의
                         </label>
                     </div>
                     <button
                         onClick={() => {
-
                             if (agreePayment === false || agreePolicy === false) {
-                                alert("모든 항목에 동의가 필요합니다")
-                                return
+                                alert("모든 항목에 동의가 필요합니다");
+                                return;
                             }
                             mutate(null, {
                                 onError: () => {
-                                    alert("주문에 실패했습니다.")
+                                    alert("주문에 실패했습니다.");
                                 },
                                 onSuccess: (res) => {
-                                    const id = res.response.id
-                                    alert("주문이 완료되었습니다.")
-                                    navigate(`/orders/complete/${id}`)
+                                    const id = res.response.id;
+                                    alert("주문이 완료되었습니다.");
+                                    navigate(`/orders/complete/${id}`);
                                 }
-                            })
+                            });
                         }}
-                        className={`
-                        w-full p-4 font-medium
-                        ${agreePayment && agreePolicy ? "bg-yellow-500 text-black" : "bg-gray-300 text-grey-500"
-                            }`}
+                        className={`w-full p-4 font-medium
+                        ${agreePayment && agreePolicy ? "bg-yellow-500 text-black" : "bg-gray-300 text-grey-500"}`}
                     >
                         결제하기
                     </button>
                 </div>
             </div>
-        </div >
-    )
-}
+        </div>
+    );
+};
 
-export default OrderTemplate
+export default OrderTemplate;
