20ce7c36c26ca51a952820894068ecfa81d0277f
minjuko <200778@jnu.ac.kr>
2023-07-28 20:49:34 +0900

    페이지 경로 추가

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import CartPage from "./pages/CartPage";
 import MainLayout from "./components/layouts/MainLayout";
 import ProductDetailPage from "./pages/ProductDetailPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import OrderSuccessPage from "./pages/OrderSuccessPage.jsx";
+import OrderPage from "./pages/OrderPage";
 
 const App = () => {
   return(
@@ -19,6 +21,8 @@ const App = () => {
             <Route path="/products/:id" element={<ProductDetailPage />} />
             <Route path="/cart" element={<CartPage />} />
             <Route path="*" element={<NotFoundPage />} />
+            <Route path="/order" element={<OrderPage />} />
+            <Route path="/orders/complete/:id" element={<OrderSuccessPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
