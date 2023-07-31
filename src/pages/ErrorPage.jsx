9d86bf381ee7ec6491bff25078921d293fe9509a
H-sooyeon <wndqhr__@naver.com>
2023-07-31 21:49:50 +0900

    feat: 오류 status 출력

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,10 +1,10 @@
-const ErrorPage = () => {
+const ErrorPage = ({error}) => {
     return(
         <div class="page-wrap d-flex flex-row align-items-center" style={{height: '100vh'}}>
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-md-12 text-center">
-                        <span class="display-1 d-block">404</span>
+                        <span class="display-1 d-block">{error}</span>
                         <div class="mb-4 lead">The page you are looking for was not found.</div>
                         <a href="/" class="btn btn-link">Back to Home</a>
                     </div>
