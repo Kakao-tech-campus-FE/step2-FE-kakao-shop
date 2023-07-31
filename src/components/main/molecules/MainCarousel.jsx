f6076c6f65b1abdfd55b48cca4d38c94a6483f1d
kimchanho97 <nh0903@pusan.ac.kr>
2023-07-31 20:42:02 +0900

    Feat: 무한 캐러셀 구현

---
+++ src/components/main/molecules/MainCarousel.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import Container from "../../common/atoms/Container";
+import Button from "../../common/atoms/Button";
+import "../../../styles/molecules/Carousel.css";
+
+export default function Carousel({ images }) {
+  const [curImgIdx, setCurImgIdx] = useState(1);
+  // 기존 이미지 배열을 복제하여 사용할 새로운 배열을 만듭니다.
+  const [extendedImages, setExtendedImages] = useState([...images]);
+  const [carouselTransition, setCarouselTransition] = useState(
+    "transform 500ms ease-in-out"
+  );
+
+  const moveToNthSlide = (index) => {
+    // transition을 없애고, 이미지를 이동
+    // transition의 설정시간보다 setTimeout의 시간이 길어야함
+    setTimeout(() => {
+      setCarouselTransition("none");
+      setCurImgIdx(index);
+    }, 500);
+  };
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const handleNextImg = () => {
+    const nextImgIdx = curImgIdx + 1;
+    console.log(nextImgIdx);
+    if (nextImgIdx === images.length + 1) {
+      moveToNthSlide(1);
+    }
+    setCurImgIdx((prev) => {
+      setCarouselTransition("transform 500ms ease-in-out");
+      return prev + 1;
+    });
+  };
+
+  const handlePrevImg = () => {
+    const nextImgIdx = curImgIdx - 1;
+    if (nextImgIdx === 0) {
+      moveToNthSlide(images.length);
+    }
+    setCurImgIdx((prev) => {
+      setCarouselTransition("transform 500ms ease-in-out");
+      return prev - 1;
+    });
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => handleNextImg(), 3000);
+    return () => clearInterval(interval);
+  }, [handleNextImg]);
+
+  useEffect(() => {
+    setExtendedImages([images.at(-1), ...images, images[0]]);
+  }, [images]);
+
+  return (
+    <Container className="carousel-container">
+      {extendedImages.map((image, idx) => (
+        <img
+          src={image}
+          alt={`carouselImage-${idx}`}
+          key={idx}
+          style={{
+            transform: `translateX(-${curImgIdx * 100}%)`,
+            transition: carouselTransition,
+          }}
+        />
+      ))}
+      <Button onClick={handlePrevImg} className="carousel-left-button">
+        {"<"}
+      </Button>
+      <Button onClick={handleNextImg} className="carousel-right-button">
+        {">"}
+      </Button>
+      <div className="carousel-dot-container">
+        {images.map((_, idx) => (
+          <span
+            key={idx}
+            className={`carousel-dot ${
+              curImgIdx % images.length === idx ? "carousel-dot-selected" : ""
+            }`}
+          ></span>
+        ))}
+      </div>
+    </Container>
+  );
+}
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,16 @@
+import Carousel from "../components/main/molecules/MainCarousel";
 import MainProductTemplate from "../components/main/templates/MainProductTemplate";
+import carouselImage1 from "../assets/carouselItems/carouselItem1.jpeg";
+import carouselImage2 from "../assets/carouselItems/carouselItem2.jpeg";
+import carouselImage3 from "../assets/carouselItems/carouselItem3.jpeg";
+
+const CAROUSEL_IMAGES = [carouselImage1, carouselImage2, carouselImage3];
 
 export default function HomePage() {
-  return <MainProductTemplate />;
+  return (
+    <div>
+      <Carousel images={CAROUSEL_IMAGES} />
+      <MainProductTemplate />
+    </div>
+  );
 }
