b127175ac856bce4164d97621d4b0c6b30d61867
H-sooyeon <wndqhr__@naver.com>
2023-07-31 21:49:24 +0900

    feat: 오류 발생 시 에러페이지 출력

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,22 +1,26 @@
 import { useQuery } from "react-query";
 import { getCart } from "../apis/cart";
-import { Suspense } from "react";
 import Loader from "../components/atoms/Loader";
 import CartList from "../components/molecules/CartList";
 import { useNavigate } from "react-router-dom";
+import ErrorPage from "./ErrorPage";
 
 // 장바구니 페이지
 const CartPage = () => {
-    const {data, isError, isLoading} = useQuery("cart", getCart);
+    const {data, isError, isLoading, error} = useQuery("cart", getCart);
     
     const navigate = useNavigate();
     if(isError) {
-        alert('장바구니가 비어있습니다.');
+        alert(`${error.status} error`);
         navigate('/');
     };
     
     return(
-        isLoading ? <Loader /> : <CartList data={data}/>
+        <>
+            {isError ? <ErrorPage error={error.status}/> : null}
+            {isLoading ? <Loader /> : <CartList data={data}/>}
+        </>
+
     );
 };
 
