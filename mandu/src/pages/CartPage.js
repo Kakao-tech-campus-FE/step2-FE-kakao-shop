bf809867f56ccbcc19f77aa41181b9ce681f5278
charmeee <mandugohigh@gmail.com>
2023-07-31 18:27:37 +0900

    refactor: 연산들어가는컴포넌트 분리

--- mandu/src/pages/CartPage.js
+++ mandu/src/pages/CartPage.js
@@ -26,25 +26,38 @@ const CartPage = () => {
     return (
         <MdLayOut>
             <h1 className="text-xl font-bold bg-white text-gray-800 text-center py-2 mb-4">장바구니</h1>
+            <ProductWithOptionList products={data.products}/>
             {data.products.length !== 0 &&
-                <>
-                    <div className="divide-y-8 divide-gray-100 bg-white">
-                        {
-                            data.products.map((item) => {
-                                let filteredProduct = item.carts.filter((cart) => cart.quantity > 0);
-                                if (filteredProduct.length !== 0) {
-                                    return (
-                                        <ProductWithOptionCard key={"cartItem_" + item.id} id={item.id}
-                                                               productName={item.productName}
-                                                               carts={item.carts}/>);
-                                }
-                            })}
-                    </div>
-                    <ElevatedButton className="bg-amber-300" onClick={onPurchase}>주문하기</ElevatedButton>
-                </>
+                <ElevatedButton className="bg-amber-300" onClick={onPurchase}>주문하기</ElevatedButton>
             }
         </MdLayOut>
     )
+}
+
+const ProductWithOptionList = ({products}) => {
+    if (products.length === 0) {
+        return (
+            <div className="flex flex-col items-center justify-center h-64">
+                <p className="text-2xl font-bold text-gray-800">장바구니가 비었습니다.</p>
+                <p className="text-lg text-gray-600">상품을 추가해주세요.</p>
+            </div>
+        );
+    }
+    return (
+        <div className="divide-y-8 divide-gray-100 bg-white">
+            {
+                products.map((item) => {
+                    let filteredProduct = item.carts.filter((cart) => cart.quantity > 0);
+                    if (filteredProduct.length !== 0) {
+                        return (
+                            <ProductWithOptionCard key={"cartItem_" + item.id} id={item.id}
+                                                   productName={item.productName}
+                                                   carts={item.carts}/>);
+                    }
+                })}
+        </div>
+    );
+
 }
 
 export default CartPage;
