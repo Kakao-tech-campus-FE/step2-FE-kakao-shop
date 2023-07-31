383c5c57177312793a25b857d4a87e1c02b0736d
H-sooyeon <wndqhr__@naver.com>
2023-07-31 17:19:29 +0900

    feat: suspense 제거

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -3,19 +3,20 @@ import { getCart } from "../apis/cart";
 import { Suspense } from "react";
 import Loader from "../components/atoms/Loader";
 import CartList from "../components/molecules/CartList";
+import { useNavigate } from "react-router-dom";
 
 // 장바구니 페이지
 const CartPage = () => {
-    const {data, isError, error, isLoading} = useQuery("cart", getCart);
+    const {data, isError, isLoading} = useQuery("cart", getCart);
     
+    const navigate = useNavigate();
     if(isError) {
-        alert('에러!');
+        alert('장바구니가 비어있습니다.');
+        navigate('/');
     };
     
     return(
-        <Suspense fallback={<Loader />}>
-            {isLoading ? <Loader /> : <CartList data={data}/>}
-        </Suspense>
+        isLoading ? <Loader /> : <CartList data={data}/>
     );
 };
 
