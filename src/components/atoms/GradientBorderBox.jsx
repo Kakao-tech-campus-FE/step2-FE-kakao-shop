061234744012e6282a01e19341e51ede79b29b3c
localgaji <eldorado829@naver.com>
2023-07-31 19:25:18 +0900

    remove useless props

--- src/components/atoms/GradientBorderBox.jsx
+++ src/components/atoms/GradientBorderBox.jsx
@@ -3,10 +3,10 @@ import styled from 'styled-components';
 
 const Container = styled.div`
     box-shadow: 0px 0px 5px rgb(199, 199, 199);
-    border-radius: 5px;
+    border-radius: 8px;
 `
 
-const GradientBorderBox = ( { width, className, children } ) => {
+const GradientBorderBox = ( { className, children } ) => {
   return (
       <Container className={className}>
         {children}
