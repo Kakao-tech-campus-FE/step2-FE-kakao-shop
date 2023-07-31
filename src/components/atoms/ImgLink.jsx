72bace4977b484859b035c27e69cff2022449d01
H-sooyeon <wndqhr__@naver.com>
2023-07-31 17:04:00 +0900

    feat: 로그인이 아닌 상태에서 장바구니 진입 캐치

--- src/components/atoms/ImgLink.jsx
+++ src/components/atoms/ImgLink.jsx
@@ -2,9 +2,14 @@ import { Link } from "react-router-dom";
 import * as Img from '../../styles/atoms/ImgLink';
 
 const ImgLink = ({to, src, alt, className}) => {
+    const handleOnCartClick = () => {
+        if(className === 'header-cart' && to === '/login') {
+            alert('로그인이 필요합니다.');
+        };
+    };
     return(
         <Img.img>
-        <Link to={to}>
+        <Link to={to} onClick={handleOnCartClick}>
             <img src={src} alt={alt} className={className}/>
         </Link>
         </Img.img>
