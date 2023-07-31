85bb8187460a4d57fc9b14e46c717f75bc9c01a8
H-sooyeon <wndqhr__@naver.com>
2023-07-31 21:48:50 +0900

    feat: 에러발생 시 에러 페이지 출력

--- src/components/templates/MainProductTemplate.jsx
+++ src/components/templates/MainProductTemplate.jsx
@@ -1,18 +1,21 @@
-import { Suspense, useEffect } from "react";
+import { useEffect } from "react";
 import Container from "../atoms/Container";
 import ProductGrid from "../organisms/ProductGrid";
 import { fetchProducts } from "../../apis/product";
 import { useInfiniteQuery } from "react-query";
 import MainPageSkeleton from "../molecules/MainPageSkeleton";
 import { useInView } from "react-intersection-observer";
+import ErrorPage from "../../pages/ErrorPage";
 
 const MainProductTemplate = () => {
     const {ref, inView} = useInView();
     const {data: products, 
         isFetchingNextPage, 
         fetchNextPage, 
-        hasNextPage
-    } = useInfiniteQuery('products', ({pageParam = 0}) => fetchProducts(pageParam), {
+        hasNextPage,
+        isError,
+        error,
+    } = useInfiniteQuery(['products'], ({pageParam = 0}) => fetchProducts(pageParam), {
         getNextPageParam: (lastPage, pages) => {
             if(lastPage.response && lastPage.response.length === 0) {
                 // 마지막 페이지일 경우 NULL을 반환하여 더 이상 페이지를 불러오지 않음
@@ -47,6 +50,7 @@ const MainProductTemplate = () => {
     
     return(
         <Container>
+            {isError ? <ErrorPage error={error.status}/> : null}
             {products ? 
                 <ProductGrid products={products.pages.flatMap(page => page.response)} />
                 : <MainPageSkeleton />}
