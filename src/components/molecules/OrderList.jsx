3eefae24192428a5735ec3247a48998439e00247
H-sooyeon <wndqhr__@naver.com>
2023-07-31 17:17:31 +0900

    feat: quantity가 0인 상품 화면에서 제거

--- src/components/molecules/OrderList.jsx
+++ src/components/molecules/OrderList.jsx
@@ -2,13 +2,16 @@ import { comma } from "../../utils/convert";
 import * as Order from '../../styles/molecules/OrderList';
 
 const OrderList = ({products}) => {
+    
     return(
         <Order.Container>
             {products.map((item) => (
+                item.carts.some(cart => cart.quantity) ?
                 <Order.OneProductContainer key={item.id}>
                     <Order.ProductName>{item.productName}</Order.ProductName>
                     <Order.OptionContainer>
                         {item.carts.map((cart) => (
+                            cart.quantity ?
                             <Order.Option key={cart.id}>
                                 <span className="product-option-name">
                                     {cart.option.optionName}
@@ -18,9 +21,11 @@ const OrderList = ({products}) => {
                                     <span>{comma(cart.price)}원</span>
                                 </Order.OptionQuantityPrice>
                              </Order.Option>
+                             : null
                         ))}
                     </Order.OptionContainer>
                 </Order.OneProductContainer>
+                : null
             ))}
         </Order.Container> 
     );
