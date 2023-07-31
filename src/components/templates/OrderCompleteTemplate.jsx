c188dcf0294d30f23e87c3ff8ca2cf0884ec2b0f
Heo_Donghyuk <missko30@pusan.ac.kr>
2023-07-31 16:47:39 +0900

    fix: Modified not to show products with zero quantity

--- src/components/templates/OrderCompleteTemplate.jsx
+++ src/components/templates/OrderCompleteTemplate.jsx
@@ -6,36 +6,40 @@ const OrderCompleteTemplate = ({ data }) => {
   const products = data?.data?.response?.products;
   const totalPrice = data?.data?.response?.totalPrice;
   const OrderItems = ({ products }) => {
+    console.log(products);
     return products?.map((item) => {
-      return (
-        <div
-          key={item.productName}
-          className="border rounded bg-white p-4 my-2"
-        >
-          {item.items.map((option) => {
-            if (option.quantity !== 0) {
-              return (
-                <div className="flex items-center gap-3 py-2" key={option.id}>
-                  <div className="product-info">
-                    <div className="product-name-option-quantity">
-                      <p className="font-semibold">{item.productName}</p>
-                      <span className="text-sm">
-                        [옵션] {option.optionName},{" "}
-                      </span>
-                      <span>{option.quantity}개</span>
-                    </div>
-                    <div className="price">
-                      <p className="text-[17px] font-bold">
-                        {comma(option.price)}원
-                      </p>
+      console.log(item);
+      // 해당 상품의 모든 옵션의 상품 수가 0이지 않을 경우에만 표현
+      if (!item?.items.every((option) => option.quantity === 0))
+        return (
+          <div
+            key={item.productName}
+            className="border rounded bg-white p-4 my-2"
+          >
+            {item.items.map((option) => {
+              if (option.quantity !== 0) {
+                return (
+                  <div className="flex items-center gap-3 py-2" key={option.id}>
+                    <div className="product-info">
+                      <div className="product-name-option-quantity">
+                        <p className="font-semibold">{item.productName}</p>
+                        <span className="text-sm">
+                          [옵션] {option.optionName},{" "}
+                        </span>
+                        <span>{option.quantity}개</span>
+                      </div>
+                      <div className="price">
+                        <p className="text-[17px] font-bold">
+                          {comma(option.price)}원
+                        </p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              );
-            }
-          })}
-        </div>
-      );
+                );
+              }
+            })}
+          </div>
+        );
     });
   };
   return (
--- src/components/templates/OrderTemplate.jsx
+++ src/components/templates/OrderTemplate.jsx
@@ -39,44 +39,47 @@ const OrderTemplate = ({ data }) => {
 
   const OrderItems = () => {
     return products?.map((item) => {
-      return (
-        <div key={item.id} className="border rounded bg-white p-4 my-2">
-          {item.carts.map((option) => {
-            // 수량이 0인 옵션은 표현하지 않는다
-            if (option.quantity !== 0) {
-              return (
-                <div className="flex items-center gap-3 py-2" key={option.id}>
-                  <img
-                    className="w-[80px] h-[80px] border rounded-lg"
-                    src={`${process.env.REACT_APP_API_URL}/images/${item.id}.jpg`}
-                    alt=""
-                  />
-                  <div className="product-info">
-                    {/* 상품 이름, CSS와 항상 바인딩이 될 필요는 없다. 직관적 이해에 도움을 주도록 사용할 수 있다 */}
-                    <div className="product-name-option-quantity">
-                      <p className="font-semibold">{item.productName}</p>
-                      <span className="text-sm">
-                        [옵션] {option.option.optionName},{" "}
-                      </span>
-                      <span>{option.quantity}개</span>
-                    </div>
-                    <div className="price">
-                      <p className="text-[17px] font-bold">
-                        {comma(option.price)}원
-                      </p>
+      console.log(item);
+      // 해당 상품의 모든 옵션의 상품 수량이 0이라면 표현하지 않는다
+      if (!item?.carts.every((option) => option.quantity === 0))
+        return (
+          <div key={item.id} className="border rounded bg-white p-4 my-2">
+            {item.carts.map((option) => {
+              // 수량이 0인 옵션은 표현하지 않는다
+              if (option.quantity !== 0) {
+                return (
+                  <div className="flex items-center gap-3 py-2" key={option.id}>
+                    <img
+                      className="w-[80px] h-[80px] border rounded-lg"
+                      src={`${process.env.REACT_APP_API_URL}/images/${item.id}.jpg`}
+                      alt=""
+                    />
+                    <div className="product-info">
+                      {/* 상품 이름, CSS와 항상 바인딩이 될 필요는 없다. 직관적 이해에 도움을 주도록 사용할 수 있다 */}
+                      <div className="product-name-option-quantity">
+                        <p className="font-semibold">{item.productName}</p>
+                        <span className="text-sm">
+                          [옵션] {option.option.optionName},{" "}
+                        </span>
+                        <span>{option.quantity}개</span>
+                      </div>
+                      <div className="price">
+                        <p className="text-[17px] font-bold">
+                          {comma(option.price)}원
+                        </p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              );
-            }
-          })}
-          <hr />
-          <div className="pt-4 text-center text-blue-500">
-            <BsTruck className="inline text-2xl" />
-            <span className="px-1 font-bold">무료배송</span>
+                );
+              }
+            })}
+            <hr />
+            <div className="pt-4 text-center text-blue-500">
+              <BsTruck className="inline text-2xl" />
+              <span className="px-1 font-bold">무료배송</span>
+            </div>
           </div>
-        </div>
-      );
+        );
     });
   };
   return !!!products ? (
