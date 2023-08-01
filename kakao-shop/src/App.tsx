d3a353fe6ca10715e268f2671fb1d06bf50ecf86
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-08-02 02:00:24 +0900

    refactor: LazyLoading

--- kakao-shop/src/App.tsx
+++ kakao-shop/src/App.tsx
@@ -1,38 +1,43 @@
+import { lazy, Suspense } from 'react';
 import { Route, Routes, BrowserRouter, Outlet } from 'react-router-dom';
 
-import NotFound from '@pages/404';
-import Error from '@pages/500';
-import Cart from '@pages/Cart';
-import Home from '@pages/Home';
-import Login from '@pages/Login';
-import Order from '@pages/Order';
 import Approve from '@pages/Pay/Approve';
 import Cancel from '@pages/Pay/Cancel';
-import PayResult from '@pages/PayResult';
-import ProductDetail from '@pages/ProductDetail';
-import SignUp from '@pages/SignUp';
 
 import Footer from '@components/molecules/Footer';
 import Header from '@components/molecules/Header';
+import PageLoader from '@components/molecules/PageLoader';
+
+const Home = lazy(() => import('@pages/Home'));
+const ProductDetail = lazy(() => import('@pages/ProductDetail'));
+const Cart = lazy(() => import('@pages/Cart'));
+const Order = lazy(() => import('@pages/Order'));
+const PayResult = lazy(() => import('@pages/PayResult'));
+const SignUp = lazy(() => import('@pages/SignUp'));
+const Login = lazy(() => import('@pages/Login'));
+const Error = lazy(() => import('@pages/500'));
+const NotFound = lazy(() => import('@pages/404'));
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path={'/'} element={<Home />} />
-          <Route path={'/detail/:id'} element={<ProductDetail />} />
-          <Route path={'/cart'} element={<Cart />} />
-          <Route path={'/order'} element={<Order />} />
-          <Route path={'/payResult'} element={<PayResult />} />
-        </Route>
-        <Route path={'/signup'} element={<SignUp />} />
-        <Route path={'/login'} element={<Login />} />
-        <Route path={'/500'} element={<Error />} />
-        <Route path={'/*'} element={<NotFound />} />
-        <Route path={'/pay/cancel'} element={<Cancel />} />
-        <Route path={'/pay/Approve'} element={<Approve />} />
-      </Routes>
+      <Suspense fallback={<PageLoader />}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path={'/'} element={<Home />} />
+            <Route path={'/detail/:id'} element={<ProductDetail />} />
+            <Route path={'/cart'} element={<Cart />} />
+            <Route path={'/order'} element={<Order />} />
+            <Route path={'/payResult'} element={<PayResult />} />
+          </Route>
+          <Route path={'/signup'} element={<SignUp />} />
+          <Route path={'/login'} element={<Login />} />
+          <Route path={'/500'} element={<Error />} />
+          <Route path={'/*'} element={<NotFound />} />
+          <Route path={'/pay/cancel'} element={<Cancel />} />
+          <Route path={'/pay/Approve'} element={<Approve />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
