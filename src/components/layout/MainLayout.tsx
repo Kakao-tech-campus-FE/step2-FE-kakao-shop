eead0b36003096c39b9460bf2f64ce5ebdbc1430
lcqff <yeonnu82g@gmail.com>
2023-08-01 21:44:01 +0900

    feat: 헤더 홈 아이콘 추가 - 아이콘 추가 - 헤더 파일 layout폴더로 이동 - 로그아웃시 토큰 삭제하는 코드 주석해제

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,6 +1,6 @@
-import Header from '@components/organisms/Header';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from './Header';
 
 const MainLayout = () => {
   return (
