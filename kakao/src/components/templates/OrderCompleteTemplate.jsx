abaea7d6b0c117080b0e45a7f6e80579ee725e3a
sihyonn <sihyeon982@naver.com>
2023-08-02 03:45:47 +0900

    주문완료페이지 완성(css 작업완료)

--- kakao/src/components/templates/OrderCompleteTemplate.jsx
+++ kakao/src/components/templates/OrderCompleteTemplate.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { comma } from "../../utils/convert";
 import { useNavigate } from "react-router-dom";
+import "../../styles/template/OrderCompleteTemplate.css";
 
 const OrderCompleteTemplate = ({ data }) => {
   const navigate = useNavigate();
@@ -21,17 +22,19 @@ const OrderCompleteTemplate = ({ data }) => {
     return Object.keys(groupedProducts).map((productName) => (
       <div key={productName} className="ordered-product">
         <div className="namegroup">
-          <span className="material-symbols-outlined">storefront</span>
-          <span className="prodcut-name">{`${productName}`}</span>
+          <span className="material-symbols-outlined" id="shop-icon">
+            storefront
+          </span>
+          <span className="product-name">{`${productName}`}</span>
         </div>
 
         {groupedProducts[productName].map((product) =>
           product.items.map((item) => (
-            <div key={item.id} className="ordered-option">
+            <div key={item.id} className="ordered-option-item">
               <div className="optionNamegroup">
                 <span className="option-name">{`[옵션] ${item.optionName}`}</span>
-                <span className="product-quantity">
-                  {comma(item.quantity)}개
+                <span className="option-quantity">
+                  주문수량: {comma(item.quantity)}개
                 </span>
               </div>
 
@@ -50,37 +53,65 @@ const OrderCompleteTemplate = ({ data }) => {
       <div className="complete-innerwrap">
         <div className="complete-messagebox">
           <div className="complete-messages">
-            <span className="complete-num">주문번호 : 12345678abc</span>
+            <img src="/춘식이.png" alt="주문완료!춘식이" width={65} />
             <span className="message1">구매완료!</span>
             <span className="message2">구매가 정상적으로 완료되었습니다.</span>
           </div>
           <div className="complete-next-buttons">
-            <button className="next-button1">전체 주문 내역</button>
-            <button className="next-button2" onClick={() => navigate("/")}>
+            <button className="button1">전체 주문 내역</button>
+            <button className="button2" onClick={() => navigate("/")}>
               쇼핑 계속하기
             </button>
           </div>
+          <div className="add-kakao">
+            <span>
+              카카오톡 쇼핑하기 <br />
+              <strong>지금 친구 추가하면 500포인트 적립!</strong>
+            </span>
+            <img
+              src="/add-kakao.png"
+              alt="카카오 친구추가"
+              style={{ width: "45px", borderRadius: "5px" }}
+            />
+          </div>
         </div>
 
-        <div className="complete-info" id="complete-deliveryinfo">
+        <div className="complete-info" id="complete-delivery-info">
           <div className="complete-info-tit">
             <span>배송지 정보</span>
           </div>
-          <div className="complete-info-content"></div>
+          <div className="complete-info-content">
+            <div className="complete-delivery-consumer">
+              <strong className="name">춘식이</strong>
+              <span className="number">010-1234-5678</span>
+              <p className="address">
+                (12345)판교 카카오로 가는길 1(쿠키즈동, 춘식팰리스) 101-111
+              </p>
+            </div>
+            <div className="delivery-update-btn">
+              <button>요청사항 수정</button>
+              <button>배송지 수정</button>
+            </div>
+          </div>
         </div>
 
-        <div className="complete-info" id="complete-productinfo">
+        <div className="complete-info" id="complete-product-info">
           <div className="complete-info-tit">
             <span>주문상품 정보</span>
           </div>
           <div className="complete-info-content">
             <div className="complete-product-list">
               <OrderedItems />
-              <div className="ordered-price">
-                <div className="label">총 결제금액</div>
-                <span>{comma(totalPrice)}원(카카오페이머니)</span>
-              </div>
             </div>
+          </div>
+        </div>
+
+        <div className="complete-info" id="complete-delivery-info">
+          <div className="ordered-price-info">
+            <span className="ordered-price-tit">총 결제금액</span>
+            <span className="ordered-total-price">
+              {comma(totalPrice)}원(카카오페이머니)
+            </span>
           </div>
         </div>
       </div>
