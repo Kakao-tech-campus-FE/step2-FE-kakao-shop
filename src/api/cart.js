deba068c92262afa422f72e81e1567624ede3995
localgaji <eldorado829@naver.com>
2023-08-02 03:56:58 +0900

    remove unused function

--- src/api/cart.js
+++ src/api/cart.js
@@ -8,10 +8,6 @@ export const getCarts = () => {
   return instance.get(`/carts`);
 };
 
-export const postOrder = () => {
-  return instance.post(`/orders/save`);
-};
-
 export const updateCart = (updateList) => {
   return instance.post("/carts/update", updateList);
 };
