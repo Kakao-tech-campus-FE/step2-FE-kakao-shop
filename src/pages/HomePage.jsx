3cb4bfba82cf8229c8cb3a8f06c7a4e762a0461e
Heo_Donghyuk <missko30@pusan.ac.kr>
2023-08-01 17:27:28 +0900

    feat: Implement MainCarousel and applied

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
+import MainCarousel from "../components/atoms/MainCarousel";
 import MainProductTemplate from "../components/templates/MainProductTemplate";
 
 const HomePage = () => {
   return (
     <div>
+      <MainCarousel />
       <MainProductTemplate />
     </div>
   );
