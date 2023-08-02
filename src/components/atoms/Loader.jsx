17cece2fe0990422e0f625c661e6767a2500e021
Heo_Donghyuk <missko30@pusan.ac.kr>
2023-08-02 17:07:43 +0900

    design: modified Loader CSS position

--- src/components/atoms/Loader.jsx
+++ src/components/atoms/Loader.jsx
@@ -3,20 +3,18 @@ import "../../styles/atoms/Loader.css";
 const Loader = () => {
   return (
     <div id="wrapper">
-      <div className="profile-main-loader">
-        <div className="loader">
-          <svg className="circular-loader" viewBox="25 25 50 50">
-            <circle
-              className="loader-path"
-              cx="50"
-              cy="50"
-              r="20"
-              fill="none"
-              stroke="#a3a3a3"
-              strokeWidth="2"
-            />
-          </svg>
-        </div>
+      <div className="loader">
+        <svg className="circular-loader" viewBox="25 25 50 50">
+          <circle
+            className="loader-path"
+            cx="50"
+            cy="50"
+            r="20"
+            fill="none"
+            stroke="#a3a3a3"
+            strokeWidth="2"
+          />
+        </svg>
       </div>
     </div>
   );
