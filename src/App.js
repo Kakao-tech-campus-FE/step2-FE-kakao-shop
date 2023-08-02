4422bb96e18f8d74f6282b206800d1136ca8c319
baegyeong <beth0929@naver.com>
2023-08-02 15:50:53 +0900

    fix: persistGate, Provider 함수 index.js로 옮김

--- src/App.js
+++ src/App.js
@@ -8,13 +8,7 @@ import OrderPage from "./pages/OrderPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import ProductDetailPage from "./pages/ProductDetailPage";
 import CompleteOrderPage from "./pages/CompleteOrderPage";
-import { Provider } from "react-redux";
-import store from "./store/index.js";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistStore } from "redux-persist";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-export let persistor = persistStore(store);
 
 const queryClient = new QueryClient();
 
@@ -22,25 +16,18 @@ function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <Provider store={store}>
-          {/* <PersistGate persistor={persistor}> */}
-          <BrowserRouter>
-            <Routes>
-              <Route path={routes.register} element={<RegisterPage />} />
-              <Route path={routes.home} element={<HomePage />} />
-              <Route path={routes.login} element={<LoginPage />} />
-              <Route path={routes.cart} element={<CartPage />} />
-              <Route path={routes.orders} element={<OrderPage />} />
-              <Route path="/*" element={<NotFoundPage />} />
-              <Route
-                path="/product/:id"
-                element={<ProductDetailPage />}
-              ></Route>
-              <Route path="/orders/:id" element={<CompleteOrderPage />}></Route>
-            </Routes>
-          </BrowserRouter>
-          {/* </PersistGate> */}
-        </Provider>
+        <BrowserRouter>
+          <Routes>
+            <Route path={routes.register} element={<RegisterPage />} />
+            <Route path={routes.home} element={<HomePage />} />
+            <Route path={routes.login} element={<LoginPage />} />
+            <Route path={routes.cart} element={<CartPage />} />
+            <Route path={routes.orders} element={<OrderPage />} />
+            <Route path="/*" element={<NotFoundPage />} />
+            <Route path="/product/:id" element={<ProductDetailPage />}></Route>
+            <Route path="/orders/:id" element={<CompleteOrderPage />}></Route>
+          </Routes>
+        </BrowserRouter>
       </QueryClientProvider>
     </>
   );
