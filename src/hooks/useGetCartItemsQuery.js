3b7025ec5b43734f148e44f7ddef93b00bbf86eb
j8won <joowoni99@gmail.com>
2023-08-03 07:11:36 +0900

    feat: useGetCartItemsQuery.js propTypes 추가

--- src/hooks/useGetCartItemsQuery.js
+++ src/hooks/useGetCartItemsQuery.js
@@ -1,10 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import API from "@/constants/API.js";
 import cartAPI from "@/api/cartAPI.js";
+import PropTypes from "prop-types";
 
-function useGetCartItemsQuery() {
+function useGetCartItemsQuery({ id }) {
   const { data } = useQuery(
-    [API.KEYS.GET_CART_ITEMS],
+    [API.KEYS.GET_CART_ITEMS, id],
     async () => {
       const { data } = await cartAPI.getCartItems();
       return data.response;
@@ -19,5 +20,9 @@ function useGetCartItemsQuery() {
 
   return { data };
 }
+
+useGetCartItemsQuery.propTypes = {
+  id: PropTypes.string || PropTypes.number,
+};
 
 export default useGetCartItemsQuery;
