84cef05eb9300aad63b0ce54284b706a909212fe
H-sooyeon <wndqhr__@naver.com>
2023-08-02 16:21:17 +0900

    feat: suspense 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
-import { Routes, Route } from "react-router-dom";
-import RegisterPage from "./pages/RegisterPage";
-import LoginPage from "./pages/LoginPage";
-import MainPage from "./pages/MainPage";
-import CartPage from "./pages/CartPage";
-import MainLayout from "./components/layouts/MainLayout";
-import ProductDetailPage from "./pages/ProductDetailPage";
-import ErrorPage from "./pages/ErrorPage";
-import OrderPage from "./pages/OrderPage";
-import OrderCompletePage from "./pages/OrderCompletePage";
+import { Routes, Route } from 'react-router-dom';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
+import MainPage from './pages/MainPage';
+import CartPage from './pages/CartPage';
+import MainLayout from './components/layouts/MainLayout';
+import ProductDetailPage from './pages/ProductDetailPage';
+import ErrorPage from './pages/ErrorPage';
+import OrderPage from './pages/OrderPage';
+import OrderCompletePage from './pages/OrderCompletePage';
+import { Suspense } from 'react';
+import Loader from './components/atoms/Loader';
+import MainPageSkeleton from './components/molecules/MainPageSkeleton';
 
 const App = () => {
-  return(
+  return (
     <>
+      <Suspense fallback={<Loader className="productDetail" />}>
         <Routes>
           <Route path="/signup" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route element={<MainLayout />}>
-            <Route path="/" element={<MainPage />} />
+            <Route
+              path="/"
+              element={
+                <Suspense fallback={<MainPageSkeleton />}>
+                  <MainPage />
+                </Suspense>
+              }
+            />
             <Route path="/product/:id" element={<ProductDetailPage />} />
             <Route path="/cart" element={<CartPage />} />
             <Route path="/order" element={<OrderPage />} />
@@ -24,6 +35,7 @@ const App = () => {
           </Route>
           <Route path="/error" element={<ErrorPage />} />
         </Routes>
+      </Suspense>
     </>
   );
 };
