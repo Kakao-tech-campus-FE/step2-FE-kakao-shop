65fc2a33b0609e791813ff85962113d7402e5edd
sihyonn <sihyeon982@naver.com>
2023-08-03 01:18:18 +0900

    CartPage css 완성

--- kakao/src/components/atoms/CartItem.jsx
+++ kakao/src/components/atoms/CartItem.jsx
@@ -1,22 +1,22 @@
-import Box from "./Box";
 import Counter from "./Counter";
 import { comma } from "../../utils/convert";
 import Card from "../atoms/Card";
+import "../../styles/atoms/CartItem.css";
 
 // 각 상품별 장바구니 항목
 // 이 항목안에는 여러 옵션 저장 가능
 const CartItem = ({ item, onChange }) => {
   return (
-    <Box className="cart-item-box">
-      <h5>{item.productName}</h5>
+    <div className="cart-item-box">
+      <span className="cart-item-name">{item.productName}</span>
 
       {item.carts.map((cart) => (
         // cart.id = 옵션아이디
-        <Card key={cart.id} className="cart-item-list">
-          <div className="cart-item-list-up">
-            <span>{cart.option.optionName}</span>
+        <div key={cart.id} className="cart-item-option">
+          <div className="option-top">
+            <span className="option-name">{cart.option.optionName}</span>
           </div>
-          <div className="cart-item-list-">
+          <div className="option-bottom">
             <Counter
               quantity={cart.quantity}
               onIncrease={(count) => {
@@ -27,28 +27,26 @@ const CartItem = ({ item, onChange }) => {
                 onChange(cart.id, count, -cart.option.price);
               }}
             />
-            <div className="price">
+            <div className="option-price">
               <span>{comma(cart.option.price * cart.quantity)}원</span>
             </div>
           </div>
-        </Card>
+        </div>
       ))}
 
-      <Card className="cart-total-price">
-        <div className="row">
-          <h5>주문금액</h5>
-          <div className="cart-price">
-            {comma(
-              // item.carts = 옵션들이 저장된 배열의미.
-              item.carts.reduce((acc, cur) => {
-                return acc + cur.option.price * cur.quantity;
-              }, 0)
-            )}
-            원
-          </div>
-        </div>
-      </Card>
-    </Box>
+      <div className="cart-option-total">
+        <span>주문금액</span>
+        <span>
+          {comma(
+            // item.carts = 옵션들이 저장된 배열의미.
+            item.carts.reduce((acc, cur) => {
+              return acc + cur.option.price * cur.quantity;
+            }, 0)
+          )}
+          원
+        </span>
+      </div>
+    </div>
   );
 };
 
