29712405bb2c1c8a4e481115c79ba98c95bf9849
j8won <joowoni99@gmail.com>
2023-08-03 04:21:46 +0900

    feat: main.jsx QueryClientProvider, jotai Provider App.jsx로 이동

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,42 @@ import GlobalStyle from "@/styles/GlobalStyle.js";
 import theme from "@/styles/theme.js";
 import router from "@/router.jsx";
 import GlobalLoader from "@/pages/GlobalLoader.jsx";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Provider } from "jotai";
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: 0,
+      },
+    },
+    queryCache: new QueryCache({
+      onError: (error, query) => {
+        console.log(error, query);
+      },
+    }),
+  });
+
   return (
-    <HelmetProvider>
-      <StyledThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Suspense fallback={<GlobalLoader />}>
-          <RouterProvider router={router} />
-        </Suspense>
-      </StyledThemeProvider>
-    </HelmetProvider>
+    <Provider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={true} />
+        <HelmetProvider>
+          <StyledThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Suspense fallback={<GlobalLoader />}>
+              <RouterProvider router={router} />
+            </Suspense>
+          </StyledThemeProvider>
+        </HelmetProvider>
+      </QueryClientProvider>
+    </Provider>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,4 @@
 import ReactDOM from "react-dom/client";
 import App from "@/App.jsx";
-import { Provider } from "jotai";
-import {
-  QueryClient,
-  QueryClientProvider,
-  QueryCache,
-} from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 0,
-    },
-  },
-  queryCache: new QueryCache({
-    onError: (error, query) => {
-      console.log(error, query);
-      if (query.state.data !== undefined) {
-        console.log(`에러: ${error.message}`);
-      }
-    },
-  }),
-});
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider>
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={true} />
-      <App />
-    </QueryClientProvider>
-  </Provider>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
