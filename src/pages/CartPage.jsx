94e63a114bdccc6ffe5d32ade80436c3255527e5
H-sooyeon <wndqhr__@naver.com>
2023-08-02 10:16:11 +0900

    refactor: prettier 적용

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,27 +1,26 @@
-import { useQuery } from "react-query";
-import { getCart } from "../apis/cart";
-import Loader from "../components/atoms/Loader";
-import CartList from "../components/molecules/CartList";
-import { useNavigate } from "react-router-dom";
-import ErrorPage from "./ErrorPage";
+import { useQuery } from 'react-query';
+import { getCart } from '../apis/cart';
+import Loader from '../components/atoms/Loader';
+import CartList from '../components/molecules/CartList';
+import { useNavigate } from 'react-router-dom';
+import ErrorPage from './ErrorPage';
 
 // 장바구니 페이지
 const CartPage = () => {
-    const {data, isError, isLoading, error} = useQuery("cart", getCart);
-    
-    const navigate = useNavigate();
-    if(isError) {
-        alert(`${error.status} error`);
-        navigate('/');
-    };
-    
-    return(
-        <>
-            {isError ? <ErrorPage error={error.status}/> : null}
-            {isLoading ? <Loader /> : <CartList data={data}/>}
-        </>
+  const { data, isError, isLoading, error } = useQuery('cart', getCart);
 
-    );
+  const navigate = useNavigate();
+  if (isError) {
+    alert(`${error.status} error`);
+    navigate('/');
+  }
+
+  return (
+    <>
+      {isError ? <ErrorPage error={error.status} /> : null}
+      {isLoading ? <Loader /> : <CartList data={data} />}
+    </>
+  );
 };
 
 export default CartPage;
