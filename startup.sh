23237826c02bdd3518f9f0700ff1cc4c286e71f3
loopy-lim <loopy@jnu.ac.kr>
2023-08-02 16:23:13 +0900

    fix: remove temp files

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 perl -i -pe's@/assets/@'"$PUBLIC_URL"'/assets/@g' /usr/share/nginx/html/index.html
-perl -i -pe's@%BASE_URL%@'"$PUBLIC_URL"'@g' /usr/share/nginx/html/**/*.js
+perl -i -pe's@%BASE_URL%@'"$PUBLIC_URL"'@g' /usr/share/nginx/html/assets/**/*.js
--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# perl -i -pe's@/assets/@'"$PUBLIC_URL"'/assets/@g' /usr/share/nginx/html/index.html
-perl -i -pe's@%BASE_URL%@'"$PUBLIC_URL"'@g' index.js
