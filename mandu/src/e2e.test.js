bffb1602251a7522074b0b88b40ebf5f5cd500cc
charmeee <mandugohigh@gmail.com>
2023-08-02 13:25:20 +0900

    fix: 테스트코드 에러 해결

--- mandu/src/e2e.test.js
+++ mandu/src/e2e.test.js
@@ -1,5 +1,4 @@
 import puppeteer from "puppeteer";
-import cookie from "react-cookies";
 
 
 describe('login-test', () => {
@@ -7,15 +6,16 @@ describe('login-test', () => {
     let page;
 
     const email = 'hi@naver.com';
+    const asciiEmail = 'hi%40naver.com';
     const password = '1q2w3e4r5t!';
 
     beforeAll(async () => {
-        await cookie.remove('access_token', {path: '/'});
-        await cookie.remove('user_id', {path: '/'});
+
         browser = await puppeteer.launch();
         page = await browser.newPage();
+        const cookies = await page.cookies()
+        console.log(cookies);
         await page.goto('http://localhost:3000');
-        await page.waitForNavigation();
         page.on('dialog', async dialog => {
             console.log(dialog.message());
             await dialog.accept();
@@ -23,26 +23,23 @@ describe('login-test', () => {
     });
 
     it('go to login page', async () => {
-        const loginButton = await page.$('#header-login-btn');
+        const loginButton = await page.waitForSelector('#header-login-btn');
         await loginButton.click();
-        await page.waitForNavigation();
         expect(page.url()).toBe('http://localhost:3000/login');
     });
 
     it('login', async () => {
-        const emailInput = await page.$('#login-id');
+        const emailInput = await page.waitForSelector('#login-id');
         await emailInput.type(email);
-        const passwordInput = await page.$('#login-password');
+        const passwordInput = await page.waitForSelector('#login-password');
         await passwordInput.type(password);
-        const loginButton = await page.$('#login-btn');
+        const loginButton = await page.waitForSelector('#login-btn');
         await loginButton.click();
         await page.waitForNavigation();
-        const cookieAccessToken = await cookie.load('access_token');
-        const cookieUserId = await cookie.load('user_id');
-        console.log(cookieAccessToken);
-        console.log(cookieUserId);
-        expect(cookieAccessToken).not.toBe(undefined || null || "");
-        expect(cookieUserId).toBe(email);
+        const cookies = await page.cookies()
+        console.log(cookies);
+        expect(cookies.find(e => e.name === 'access_token')["value"]).not.toBe(undefined || null || "");
+        expect(cookies.find(e => e.name === 'user_id')["value"]).toBe(asciiEmail);
     });
 
     it('check header user id', async () => {
