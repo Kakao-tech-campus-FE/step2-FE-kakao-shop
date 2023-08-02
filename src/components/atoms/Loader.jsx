d7efa986b53132fd34a1f9e406e2cd3072b4a9d8
H-sooyeon <wndqhr__@naver.com>
2023-08-02 16:22:29 +0900

    feat: className 추가

--- src/components/atoms/Loader.jsx
+++ src/components/atoms/Loader.jsx
@@ -1,58 +1,59 @@
 import * as LoadComponent from '../../styles/atoms/Loader';
 
-const Loader = () => {
-    return(
-        <LoadComponent.LoaderContainer>
-            <div className="loader loader--style5" title="4">
-                <svg 
-                version="1.1" 
-                id="Layer_1" 
-                xmlns="http://www.w3.org/2000/svg" 
-                xmlnsXlink="http://www.w3.org/1999/xlink" 
-                x="0px" 
-                y="0px"
-                width="24px" 
-                height="30px" 
-                viewBox="0 0 24 30" 
-                style={{enableBackground: "new 0 0 50 50"}} 
-                xmlSpace="preserve"
-                >
-                    <rect x="0" y="0" width="4" height="10" fill="#333">
-                        <animateTransform 
-                            attributeType="xml"
-                            attributeName="transform" 
-                            type="translate"
-                            values="0 0; 0 20; 0 0"
-                            begin="0" 
-                            dur="0.6s" 
-                            repeatCount="indefinite" 
-                        />
-                    </rect>
-                    <rect x="10" y="0" width="4" height="10" fill="#333">
-                        <animateTransform 
-                            attributeType="xml"
-                            attributeName="transform" 
-                            type="translate"
-                            values="0 0; 0 20; 0 0"
-                            begin="0.2s" 
-                            dur="0.6s" 
-                            repeatCount="indefinite" 
-                        />
-                    </rect>
-                    <rect x="20" y="0" width="4" height="10" fill="#333">
-                        <animateTransform 
-                            attributeType="xml"
-                            attributeName="transform" 
-                            type="translate"
-                            values="0 0; 0 20; 0 0"
-                            begin="0.4s" 
-                            dur="0.6s" 
-                            repeatCount="indefinite" />
-                    </rect>
-                </svg>
-            </div>
-        </LoadComponent.LoaderContainer>
-    );
+const Loader = ({ className }) => {
+  return (
+    <LoadComponent.LoaderContainer>
+      <div className={`loader loader--style5 ${className}`} title="4">
+        <svg
+          version="1.1"
+          id="Layer_1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          x="0px"
+          y="0px"
+          width="24px"
+          height="30px"
+          viewBox="0 0 24 30"
+          style={{ enableBackground: 'new 0 0 50 50' }}
+          xmlSpace="preserve"
+        >
+          <rect x="0" y="0" width="4" height="10" fill="#333">
+            <animateTransform
+              attributeType="xml"
+              attributeName="transform"
+              type="translate"
+              values="0 0; 0 20; 0 0"
+              begin="0"
+              dur="0.6s"
+              repeatCount="indefinite"
+            />
+          </rect>
+          <rect x="10" y="0" width="4" height="10" fill="#333">
+            <animateTransform
+              attributeType="xml"
+              attributeName="transform"
+              type="translate"
+              values="0 0; 0 20; 0 0"
+              begin="0.2s"
+              dur="0.6s"
+              repeatCount="indefinite"
+            />
+          </rect>
+          <rect x="20" y="0" width="4" height="10" fill="#333">
+            <animateTransform
+              attributeType="xml"
+              attributeName="transform"
+              type="translate"
+              values="0 0; 0 20; 0 0"
+              begin="0.4s"
+              dur="0.6s"
+              repeatCount="indefinite"
+            />
+          </rect>
+        </svg>
+      </div>
+    </LoadComponent.LoaderContainer>
+  );
 };
 
 export default Loader;
