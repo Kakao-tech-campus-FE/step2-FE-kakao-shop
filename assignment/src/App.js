825a6f69af71ae159c43e221ee86c7095356d0a1
YANGSEOKWOO <didtjrdn030@naver.com>
2023-08-02 17:32:08 +0900

    feat: Error Fix
    Suspense Error 수정

--- assignment/src/App.js
+++ assignment/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import React from 'react';
+import React, { Suspense } from 'react';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 // import HomePage from './pages/Homepage'
@@ -17,28 +17,30 @@ import OrderCompletePage from './pages/OrderCompletePage';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<RegisterPage />} />
-          <Route path="/*" element={<ErrorPage />} />
-          <Route path="/error" element={<ErrorPage />} />
+      <Suspense fallback={<Loader />}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<RegisterPage />} />
+            <Route path="/*" element={<ErrorPage />} />
+            <Route path="/error" element={<ErrorPage />} />
 
-          <Route element={<MainLayout />}>
-            <Route path="/" element={<NewHomePage />} />
-            <Route path="/product/:id" element={<ProductDetailPage />} />
-            <Route
-              path="/orders/complete/:id"
-              element={<OrderCompletePage />}
-            />
-          </Route>
+            <Route element={<MainLayout />}>
+              <Route path="/" element={<NewHomePage />} />
+              <Route path="/product/:id" element={<ProductDetailPage />} />
+              <Route
+                path="/orders/complete/:id"
+                element={<OrderCompletePage />}
+              />
+            </Route>
 
-          <Route element={<RequiredAuthLayout />}>
-            <Route path="/order" element={<OrderPage />} />
-            <Route path="/cart" element={<CartPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+            <Route element={<RequiredAuthLayout />}>
+              <Route path="/order" element={<OrderPage />} />
+              <Route path="/cart" element={<CartPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
     </div>
   );
 }
