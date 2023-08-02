4e615fb4910bf449466c7025ebf294fc7f970511
baegyeong <beth0929@naver.com>
2023-08-03 03:50:36 +0900

    feat: OrderCompleteTemplate 컴포넌트 생성

---
+++ src/components/templates/OrderCompleteTemplate.jsx
@@ -0,0 +1,30 @@
+import SubmitButton from "../atoms/SubmitButton";
+import OrderList from "../molecules/OrderList";
+import { comma } from "../../utils/convert";
+
+const OrderCompleteTemplate = ({ data }) => {
+  const orderData = data?.data.response;
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h2 className="text-xl">구매 완료!</h2>
+      <h5 className="text-sm mb-12">구매가 정상적으로 완료되었습니다.</h5>
+
+      <div className="border w-[50rem] text-center">
+        <div className="text-sm font-bold mb-9">주문상품 정보</div>
+      </div>
+      {orderData?.products.map((items) => {
+        return <OrderList key={items.id} items={items} />;
+      })}
+      <div className="flex mt-5">
+        <div className="w-[28rem] font-bold">일반 결제 금액</div>
+        <div className="text-sky-400 font-bold">
+          {comma(orderData?.totalPrice)}원
+        </div>
+      </div>
+      <SubmitButton type="click">쇼핑 계속하기</SubmitButton>
+    </div>
+  );
+};
+
+export default OrderCompleteTemplate;
