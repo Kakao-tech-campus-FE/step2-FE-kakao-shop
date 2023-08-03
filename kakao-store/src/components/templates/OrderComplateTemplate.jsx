7035daa87c28d5f11dd6d13886d3708ba5d469d1
SeonghyeonKim <kkgojn@naver.com>
2023-08-03 12:06:36 +0900

    fix: Modify Style

--- kakao-store/src/components/templates/OrderComplateTemplate.jsx
+++ kakao-store/src/components/templates/OrderComplateTemplate.jsx
@@ -1,11 +1,11 @@
 import { useNavigate } from "react-router-dom";
 import OptionItem from "../atoms/OptionItem";
+import { comma } from "../../utils/convert";
 
 const ProductItems = (products) => products.map((product) => {
     return (
-        <div className="product" key={product.id}>
-            <div className="label">상품명</div>
-            <p>{product.productName}</p>
+        <div className="p-2 product" key={product.id}>
+            <p className="font-bold">{product.productName}</p>
             <OptionItem key={product.id} items={product.items} />
         </div>
     );
@@ -20,24 +20,26 @@ const OrderComplateTemplate = ({data}) => {
     const navigate = useNavigate();
 
     return (
-        <section className="py-10 my-10 mx-auto max-w-[500px] w-full bg-gray-100">
+        <section className="my-5 mx-auto max-w-[700px] w-full">
             <div className="complete-message">
-                <h1>구매완료!</h1>
-                <h2>구매가 정상적으로 완료됐습니다.</h2>
+                <h1 className="m-2 text-xl font-bold text-center">구매완료!</h1>
+                <h2 className="m-2 text-center text-bs">구매가 정상적으로 완료됐습니다.</h2>
             </div>
-            <div className="detail-box">
-                <div className="header">
+            <div className="m-5 border detail-box">
+                <div className="p-4 font-bold text-center border header">
                     주문상품 정보
                 </div>
                 <div className="product-list">
-                    {ProductItems(data.data.response.products)}  
-                    <div className="label">총 금액</div>
-                    <p>{data.data.totalPrice}</p>
+                    {ProductItems(data.data.response.products)} 
                 </div>
             </div>
-            <div className="payment-box">
-                <div className="header">일반 결제 금액</div>
-                <button
+            <div className="m-5 payment-box">
+                <div className="flex justify-between p-4 border">
+                    <div className="font-bold header">일반 결제 금액</div>
+                    <p className="font-bold text-blue-600">{comma(data.data.response.totalPrice)}원</p>
+                </div>
+                <button 
+                    className="w-full p-4 font-bold bg-yellow-300"
                     onClick={() => {                        
                         // 주문 완료 후 메인 페이지로 이동
                         navigate("/")
