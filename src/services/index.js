449ce298cbe8700144d8a5a9693d01ccdf9e9d2e
dabinchiii <dabinchi388@naver.com>
2023-08-03 16:16:04 +0900

    feat: Modify api url for deployment

--- src/services/index.js
+++ src/services/index.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 import cookies from "react-cookies";
 
+const staticServerUri = process.env.REACT_APP_PATH || "";
+
 export const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: staticServerUri + process.env.REACT_APP_API_URL,
     timeout: 1000,
     headers: {
         "Content-Type" : "application/json",
@@ -42,7 +44,7 @@ instance.interceptors.response.use(
             alert("요청이 만료되었습니다.");
         }
         else if(status === 404){
-            window.location.href="/notFound";
+            window.location.href = staticServerUri + "/notFound";
         }
         else if(status >= 500){
             alert("오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
