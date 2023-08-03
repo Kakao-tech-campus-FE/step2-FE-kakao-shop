38ba2fc51a806cd7b5c6f68aa72dd6f19b785623
LimSumi <thddkwl5330@naver.com>
2023-08-03 15:01:19 +0900

    feat: 과제 전체 수정

--- src/component/organisms/ProductGrid.jsx
+++ src/component/organisms/ProductGrid.jsx
@@ -2,7 +2,7 @@ import ProductCard from "../molecules/ProductCard"
 import axios from "axios";
 import React, { useState, useEffect} from "react";
 import Photo from "../atoms/Photo";
-
+import Card from "../atoms/Card";
 
     const ProductGrid = ({ product }) => {
         const [products, setData] = useState(null);
@@ -26,7 +26,7 @@ import Photo from "../atoms/Photo";
 
 //presentation components: 데이어를 단순히 표기만 하는 용도
     return(
-        <div className="product-grid">
+      <div className="product-grid">
             {products && products.map((product)=>(
                 <ProductCard key={product.id} product={product} />
             ))}
