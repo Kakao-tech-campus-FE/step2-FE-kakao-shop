a546ea27520c32df3dc69df3bc51f9a44dbc640c
loopy-lim <loopy@jnu.ac.kr>
2023-08-03 15:28:52 +0900

    feat: remove image settings

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@
 perl -i -pe's@/assets/@'"$PUBLIC_URL"'/assets/@g' /usr/share/nginx/html/index.html
 perl -i -pe's@%BASE_URL%@'"$PUBLIC_URL"'@g' /usr/share/nginx/html/**/*.js
 perl -i -pe's@%SERVER_URL%@'"$PUBLIC_URL"/api'@g' /usr/share/nginx/html/**/*.js
-perl -i -pe's@%IMAGE_URL%@'"$PUBLIC_URL"'@g' /usr/share/nginx/html/**/*.js
+perl -i -pe's@%IMAGE_URL%@'"$PUBLIC_URL"/api'@g' /usr/share/nginx/html/**/*.js
