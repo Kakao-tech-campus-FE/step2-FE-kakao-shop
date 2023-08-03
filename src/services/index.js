0a3f3388fa1a8d120813fb1ebfe09b49981eedff
rhalswn3521 <rhalswn3521@naver.com>
2023-08-03 16:33:08 +0900

    api static

--- src/services/index.js
+++ src/services/index.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router";
 const staticServerUri = process.env.REACT_APP_PATH || "";
 
 export const instance = axios.create({
-  baseURL: `${staticServerUri}/api/`,
+  baseURL: staticServerUri + "/api",
   timeout: 1000,
   headers: {
     "Content-Type": "application/json",
