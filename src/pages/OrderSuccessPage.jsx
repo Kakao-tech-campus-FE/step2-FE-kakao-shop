4605d1deec79bf9010c8a321ef3e6cebfb6ca01e
dabinchiii <dabinchi388@naver.com>
2023-08-03 16:13:46 +0900

    feat: Add order result page

--- src/pages/OrderSuccessPage.jsx
+++ src/pages/OrderSuccessPage.jsx
@@ -1,62 +1,54 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
-import Box from "../components/atoms/Box";
-import { Suspense } from "react";
+import { Suspense, useMemo } from "react";
+import { useLocation } from "react-router";
+import { useQuery } from "react-query";
+import { getOrderResult} from "../services/order";
+import OrderResultTemplate from "../components/templates/OrderResultTemplate";
 import Loader from "../components/atoms/Loader";
-import Button from "../components/atoms/Button";
-import { comma } from "../utils/convert";
 
 export default function OrderSuccessPage() {
-  const [searchParams] = useSearchParams();
-  const navigate = useNavigate();
+    const location = useLocation();
+    const orderId = location.state?.orderId;
+    const { data } = useQuery(
+        ["orderResult", orderId],
+        getOrderResult,
+        {
+            onError: (err) => {
+                console.log(err);
+            }
+        }
+    );
+    const validData = useMemo(() => {
+        
+        console.log(data?.data?.response);
+        const filteredData = {
+            id: data?.data?.response.id,
+            products: [],
+            totalPrice: data?.data?.response.totalPrice
+        }
 
-  return (
-    <div className="bg-bg_gray">
-      <Suspense fallback={<Loader />}>
-        <div className="w-[870px] mx-auto my-0">
-          <div className="py-8">
-            <span className="block text-[36px] text-center">
-              구매완료!
-            </span>
-            <span>{`${searchParams.get("orderId")}님!`}</span>
-            <br />
-            <span>
-              구매가 정상적으로 완료되었습니다.
-            </span>
-          </div>
-          <Box className="title_wrap text-center leading-[44px] bg-white border border-border_gray border-b-0">
-              <h1 className="font-semibold">주문상품 정보</h1>
-          </Box>
-          <Box className="bg-white border border-border_gray">
-            <h2 className="inline-block">상품명</h2>
-            <span></span>
-            <br />
-            <h2 className="inline-block">주문번호</h2>
-            <span></span>
-            <br />
-            <h2 className="inline-block">옵션</h2>
-            <span></span>
-            <br />
-          </Box>
+        data?.data?.response.products.map(productItem => {
+            const newItems = [];
+            productItem.items.map(item => {
+                if(item.quantity > 0)
+                    newItems.push(item);
+            });
 
-          <Box className="bg-white border border-border_gray mt-3">
-            <div>
-              <span className="float-left text-[18px] font-semibold leading-[21px] p-5 pr-[24px] pl-[16px]">
-                일반 결제 금액
-              </span>
-              <span className="block text-right text-[18px] font-semibold leading-[19px] text-blue p-5 pl-0">
-                {`${Number(
-                  searchParams.get("amount")
-                ).toLocaleString()}원`}
-              </span>
-            </div>
-            <Button onClick={() => {
-              navigate("/");
-            }}>
-              <strong>쇼핑 계속하기</strong>
-            </Button>
-          </Box>
+            if(newItems.length > 0){
+                filteredData.products.push({
+                    ...productItem,
+                    items: newItems,
+                });
+            };
+        });
+
+        return filteredData;
+    }, [data]);
+  
+    return (
+        <div className="bg-bg_gray">
+            <Suspense fallback={<Loader />}>
+                <OrderResultTemplate data={validData}/>
+            </Suspense>
         </div>
-      </Suspense>
-    </div>
-  );
+    );
 }
