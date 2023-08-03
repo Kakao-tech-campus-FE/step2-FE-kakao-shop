971a2f40a19e36abba31daf3d8c6f90a772fde48
udadai <yujeong470@gmail.com>
2023-08-03 18:41:03 +0900

    Fix: Header 영역 경로 수정

--- src/components/atoms/Header.jsx
+++ src/components/atoms/Header.jsx
@@ -2,17 +2,19 @@ import { Link } from "react-router-dom";
 import logoKakao from "../../assets/logoKakao.png";
 import cart from "../../assets/cart.png";
 
+const staticServerUri = process.env.REACT_APP_PATH || "";
+
 function Header() {
   return (
     <div className="flex border-b justify-between">
-      <Link to="/">
+      <Link to={staticServerUri + "/"}>
         <img
           src={logoKakao}
           alt="카카오 쇼핑하기 로고"
           className="h-10 mx-4 my-2"
         />
       </Link>
-      <Link to="/cart">
+      <Link to={staticServerUri + "/cart"}>
         <img src={cart} alt="장바구니 버튼" className="h-10 mx-4" />
       </Link>
     </div>
