9cc6dca20747803f89b22a5232c49489c69e54be
baegyeong <beth0929@naver.com>
2023-08-03 14:31:58 +0900

    fix: css 수정

--- src/components/templates/OrderCompleteTemplate.jsx
+++ src/components/templates/OrderCompleteTemplate.jsx
@@ -14,24 +14,28 @@ const OrderCompleteTemplate = ({ data }) => {
 
   return (
     <div className="flex flex-col justify-center items-center">
-      <h2 className="text-xl">구매 완료!</h2>
-      <h5 className="text-sm mb-12">구매가 정상적으로 완료되었습니다.</h5>
+      <div className="flex flex-col w-[50rem]">
+        <h2 className="text-xl text-center">구매 완료!</h2>
+        <h5 className="text-sm mb-12 text-center">
+          구매가 정상적으로 완료되었습니다.
+        </h5>
 
-      <div className="border w-[50rem] text-center">
-        <div className="text-sm font-bold mb-9">주문상품 정보</div>
-      </div>
-      {orderData?.products.map((items) => {
-        return <OrderList key={items.id} items={items} />;
-      })}
-      <div className="flex mt-5">
-        <div className="w-[28rem] font-bold">일반 결제 금액</div>
-        <div className="text-sky-400 font-bold">
-          {comma(orderData?.totalPrice)}원
+        <div className="border">
+          <div className="text-sm font-bold m-6 text-center">주문상품 정보</div>
         </div>
+        {orderData?.products.map((items) => {
+          return <OrderList key={items.id} items={items} />;
+        })}
+        <div className="flex mt-5 justify-between">
+          <div className="font-bold text-lg">일반 결제 금액</div>
+          <div className="text-[#0044ff] font-bold text-lg">
+            {comma(orderData?.totalPrice)}원
+          </div>
+        </div>
+        <SubmitButton type="click" onClick={continueShopping}>
+          쇼핑 계속하기
+        </SubmitButton>
       </div>
-      <SubmitButton type="click" onClick={continueShopping}>
-        쇼핑 계속하기
-      </SubmitButton>
     </div>
   );
 };
