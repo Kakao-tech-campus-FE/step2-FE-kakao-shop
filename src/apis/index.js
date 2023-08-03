cc9b932364fc3ad42c80930159bbfcce8f5accd8
Junseo <monstazo911@gmail.com>
2023-08-03 16:15:09 +0900

    fix: axios api prefix

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_PATH,
   timeout: 1000,
   headers: {
     "Content-Type": "application/json",
