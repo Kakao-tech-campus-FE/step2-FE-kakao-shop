1a395cc6785ce37b82b1058e65a99b58ab078e10
Sang Min Park <125769061+s010419@users.noreply.github.com>
2023-08-03 16:19:08 +0900

    Delete 2week directory

--- 2week/src/components/atoms/Container.jsx
+++
@@ -1,7 +0,0 @@
-import '../../styles/atoms/Container.css'
-
-const Container = ({ children, className = "" }) => {
-  return <div className={`container ${className}`}>{children}</div>
-}
-
-export default Container
