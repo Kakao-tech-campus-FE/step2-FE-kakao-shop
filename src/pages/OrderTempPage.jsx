84d61054fc7f17f2aa2e934b536d702b83548ac3
dabinchiii <dabinchi388@naver.com>
2023-08-03 16:22:22 +0900

    feat: Add order temp page

--- src/pages/OrderTempPage.jsx
+++ src/pages/OrderTempPage.jsx
@@ -1,21 +1,35 @@
 import { useMutation } from "react-query";
 import { useEffect } from "react";
 import { orderSave } from "../services/order";
-import { useNavigate } from "react-router";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const OrderTempPage = () => {
+    const [searchParams] = useSearchParams();
     const navigate = useNavigate();
+
+    console.log(searchParams);
 
     const orderSaveMutation = useMutation({
         mutationFn: orderSave,
         onSuccess: (res) => {
             console.log('order save res', res);
-            navigate("/order/success");
+            // alert(res.data.response.id);
+            navigate("/order/success", {
+                state: {
+                    orderId: res.data.response.id,
+                }
+            });
+            console.log(res.data?.response);
         },
         onError: (err) => {
             console.log('order save err', err)
             alert("주문 진행 중 에러가 발생했습니다.");
-            navigate("/order/fail");
+            navigate("/order/fail", {
+                replace: true,
+                state: {
+                    ...searchParams
+                }
+            });
         }
     });
 
