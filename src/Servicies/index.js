34ad144078c096d8d158be4611d60b2b1f49b6dd
harin9 <83578728+harin9@users.noreply.github.com>
2023-08-03 17:06:44 +0900

    Axios baseurl path 변경

--- src/Servicies/index.js
+++ src/Servicies/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+const staticServerUri = process.env.REACT_APP_PATH || "";
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: staticServerUri + "/api",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
