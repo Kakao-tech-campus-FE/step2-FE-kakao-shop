af3b4e062ec53c0aa43ba1f4599ac17c63e0ef07
jsh1147 <jsh1147@naver.com>
2023-08-03 22:49:34 +0900

    fix: 상품 목록 페이지 이미지 경로 수정
    더이상 BE 서버에서 이미지를 제공하지 않으므로 public 공간에서 이미지를 받아 제공함

--- src/components/molecules/ProductCard.js
+++ src/components/molecules/ProductCard.js
@@ -9,11 +9,11 @@ export default function ProductCard({ product }) {
   return (
     <Card
       className="w-[200px] text-left space-y-1"
-      to={`/product/${product.id}`}
+      to={staticServerUri + `/product/${product.id}`}
     >
       <Image
         className="w-full rounded border"
-        src={staticServerUri + product.image}
+        src={`%PUBLIC_URL%${product.image}`}
         alt={product.productName}
       />
       <h3 className="text-sm">{product.productName}</h3>
