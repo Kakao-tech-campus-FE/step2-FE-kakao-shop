18fc4e08ac5b3ebfaf1e01f50c80a9edb3b774e2
gomdarachi <=>
2023-08-03 20:57:20 +0900

    config 9

--- src/components/molecules/ProductCard.jsx
+++ src/components/molecules/ProductCard.jsx
@@ -3,10 +3,11 @@ import Card from '../atoms/Card'
 import Photo from '../atoms/Photo'
 import { comma } from '../../utils/convert'
 
+const staticServerUri = process.env.REACT_APP_PATH || "";
 
 function ProductCard({ product }) {
   return (
-    <Card to={`/product/${product.id}`}>
+    <Card to={staticServerUri + `product/${product.id}`}>
       <Photo src={product.image} alt={product.productName} size="284px"/>
       <div style={{
         width: "200px",
