e7bf2dd747f02417b883e06ea38e77edf9b1669f
baegyeong <beth0929@naver.com>
2023-08-03 16:12:46 +0900

    fix: baseURL 변경

--- src/services/index.js
+++ src/services/index.js
@@ -12,7 +12,6 @@ const instance = axios.create({
 
 instance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
