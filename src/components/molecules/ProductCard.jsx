d3d858ef562ed62b1dac072c7b2a9ccfc4420354
sihyonn <sihyeon982@naver.com>
2023-08-04 02:59:56 +0900

    img 수정

--- src/components/molecules/ProductCard.jsx
+++ src/components/molecules/ProductCard.jsx
@@ -3,16 +3,10 @@ import Card from "../atoms/Card";
 import Photo from "../atoms/Photo";
 import "../../styles/molecules/ProductCard.css";
 
-const staticServerUri = process.env.REACT_APP_PATH || "";
-
 const ProductCard = ({ product }) => {
   return (
     <Card to={`/product/${product.id}`}>
-      {/* <Photo src={product.image} alt={product.productName} />   */}
-      <Photo
-        src={`${staticServerUri}${product.image}`}
-        alt={product.productName}
-      />
+      <Photo src={product.image} alt={product.productName} />
       <div className="product-name">{product.productName}</div>
       <div className="product-price">{comma(product.price)}원</div>
     </Card>
