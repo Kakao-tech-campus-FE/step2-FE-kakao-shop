da764c48f4c97ed5c9c1f3d9a45f3c701c2158b3
kimjinwoo <rlawlsdn177@naver.com>
2023-08-03 11:11:07 +0900

    메인 페이지 스타일 수정

--- src/components/molecules/ProductCard.jsx
+++ src/components/molecules/ProductCard.jsx
@@ -9,8 +9,13 @@ const ProductCard = ({ product, className }) => {
         src={`http://kakao-app-env.eba-kfsgeb74.ap-northeast-2.elasticbeanstalk.com${product.image}`}
         alt={product.productName}
       />
-      <h3>{product.productName}</h3>
-      <p>{comma(product.price)}원</p>
+      <h3 className="h-[24px] w-[249.25px] text-ellipsis whitespace-nowrap overflow-hidden">
+        {product.productName}
+      </h3>
+      <span className="badge text-gray-600 bg-slate-200 mx-10 block bt-2 rounded-md text-xs p-1">
+        무료 배송
+      </span>
+      <p className="text-xl font-bold">{comma(product.price)}원 ~</p>
     </Card>
   );
 };
