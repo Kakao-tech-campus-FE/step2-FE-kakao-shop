1a1e0ad51c737587699f5408b47e390b32842c72
SeonghyeonKim <kkgojn@naver.com>
2023-08-03 12:04:34 +0900

    feat: Modify Style

--- kakao-store/src/components/atoms/OptionItem.jsx
+++ kakao-store/src/components/atoms/OptionItem.jsx
@@ -1,18 +1,24 @@
-import Container from "./Container";
+import { comma } from "../../utils/convert";
 
 const OptionItem = ({ items }) => {
     return (
         <>
             {items.map((item) => {
                 return (
-                    <Container>
-                        <div className="label">옵션 {item.id + 1}.</div>
-                        <p>{item.optionName}</p>
-                        <div className="label">구매수량</div>
-                        <p>{item.quantity}</p>
-                        <div className="label">금액</div>
-                        <p>{item.price * item.quantity}</p>
-                    </Container>
+                    <div className="p-2 m-2 border">
+                        <div className="flex py-1 space-x-4 text-sm">
+                            <span className="w-20">옵션</span>
+                            <span className="w-full">{item.optionName}</span>
+                        </div>
+                        <div className="flex py-1 space-x-4 text-sm">
+                            <span className="w-20">구매수량</span>
+                            <span className="w-full">{item.quantity}</span>
+                        </div>
+                        <div className="flex py-1 space-x-4 text-sm">
+                            <span className="w-20">금액</span>
+                            <span className="w-full">{comma(item.price)}원</span>
+                        </div>
+                    </div>
                 );
             })}
         </>
