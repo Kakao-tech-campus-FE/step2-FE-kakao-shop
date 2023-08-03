da2db6c520cb4fc8afc6c3fd28768268a10953d7
dabinchiii <dabinchi388@naver.com>
2023-08-03 16:21:56 +0900

    feat: Filter cart data for valid items

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,6 +1,6 @@
 import { useQuery } from "react-query";
 import { getCart } from "../services/cart";
-import { Suspense } from "react";
+import { Suspense, useMemo } from "react";
 import CartList from "../components/molecules/CartList";
 import Loader from "../components/atoms/Loader";
 
@@ -14,13 +14,38 @@ const CartPage = () => {
                 alert('장바구니 로딩 에러');
             }
         }
-    )
-    // console.log('cart data', data?.data?.response);
+    );
+    const validData = useMemo(() => {
+        console.log('data filtering ...');
+        console.log('before filtering', data?.data?.response);
+
+        const filteredData = {
+            products: [],
+            totalPrice: data?.data?.response.totalPrice
+        };
+
+        data?.data?.response.products.map(productItem => {
+            const newCarts = [];
+            productItem.carts.map(optionItem => {
+                if(optionItem.quantity > 0)
+                    newCarts.push(optionItem);
+            });
+
+            if(newCarts.length > 0){
+                filteredData.products.push({
+                    ...productItem,
+                    carts: newCarts,
+                });
+            };
+        });
+
+        return filteredData;
+    }, [data]);
 
     return (
         <div className="bg-bg_gray">
             <Suspense fallback={<Loader />}>
-                <CartList data={data?.data?.response} />
+                <CartList data={validData} /> 
             </Suspense>
         </div>
     );
