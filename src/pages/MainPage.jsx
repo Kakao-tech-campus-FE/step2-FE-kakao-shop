c7e9fae3a905d15bcfae57bef2223979348fb448
kimchanho97 <nh0903@pusan.ac.kr>
2023-08-04 00:38:55 +0900

    Fix: GNB, Carousel 스타일 수정

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,8 +1,13 @@
 import MainCarousel from "../components/main/molecules/MainCarousel";
 import MainProductTemplate from "../components/main/templates/MainProductTemplate";
 import { CAROUSEL_IMAGES } from "../utils/constant";
+import { useEffect } from "react";
 
 export default function HomePage() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div>
       <MainCarousel images={CAROUSEL_IMAGES} />
