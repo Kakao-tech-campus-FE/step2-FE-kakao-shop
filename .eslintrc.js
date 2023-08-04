1a4c869fc09c6fa979477a5e3498fd3e71a3ad97
wjsehrbs2632 <wjsehrbs2632@naver.com>
2023-08-04 18:52:57 +0900

    배포 앱 빌드 오류 해결

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+		"no-unused-vars": "off",
+		"react/prop-types": "off"
+    }
+}
--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM krmp-d2hub-idock.9rum.cc/goorm/node:16
 WORKDIR /usr/src/app
 COPY package*.json ./
 
-RUN npm ci
+RUN npm i
 
 COPY . .
 
