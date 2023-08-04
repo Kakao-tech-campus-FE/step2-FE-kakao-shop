d0fd18af467bbade22b66a4d9f3ce196e0472c5e
wjsehrbs2632 <wjsehrbs2632@naver.com>
2023-08-04 21:00:16 +0900

    이미지 에러로 인한 source 태그 수정

--- src/components/atoms/Photo.jsx
+++ src/components/atoms/Photo.jsx
@@ -3,9 +3,10 @@ import "../../styles/atoms/Photo.css";
 const staticServerUri = process.env.REACT_APP_PATH || "";
 
 const Photo = ({ className, src, alt }) => {
+	const imageUri = staticServerUri + src
   return (
     <picture className={className}>
-      <source className="border-0" srcSet={src} />
+      <source className="border-0" srcSet={staticServerUri + src} />
       <img className="border-1 rounded-lg w-96" src={staticServerUri + src} alt={alt} />
     </picture>
   );
