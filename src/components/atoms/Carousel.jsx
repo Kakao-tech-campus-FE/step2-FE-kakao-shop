819b69eefa4ffc41e184cd39340d3b7f483d1a8b
baegyeong <beth0929@naver.com>
2023-08-04 20:07:23 +0900

    fix: carousel 컴포넌트 수정

--- src/components/atoms/Carousel.jsx
+++ src/components/atoms/Carousel.jsx
@@ -1,42 +1,35 @@
 import styled from "styled-components";
 import { useState, useEffect, useRef } from "react";
-import images01 from "../assets/images_01.png";
-import images02 from "../assets/images_02.png";
-import images03 from "../assets/images_03.png";
 
 const Window = styled.div`
   overflow: hidden;
   position: relative;
-  height: 15rem;
-  width: 26.6rem;
+  width: 100vw;
+  margin-bottom: 5rem;
 `;
 
 const Slide = styled.div`
   display: flex;
-  transition: all 0.5s ease-in-out;
 `;
 
 const SlideItem = styled.div`
-  height: 15rem;
   display: flex;
 `;
 
-const Button = styled.button`
-  .prevbtn {
+const ButtonGroup = styled.div`
+  button {
     all: unset;
     position: absolute;
     color: white;
     top: 45%;
+    font-size: 2rem;
+    cursor: pointer;
+  }
+  .prevbtn {
     left: 5%;
-    font-size: 1.5rem;
   }
   .nextbtn {
-    all: unset;
-    position: absolute;
-    color: white;
-    top: 45%;
     right: 5%;
-    font-size: 1.5rem;
   }
 `;
 
@@ -53,31 +46,36 @@ const Carousel = () => {
   };
 
   useEffect(() => {
-    slideRef.current.style.transform = `translateX(-${currentSlide}00%)`; // 백틱을 사용하여 슬라이드로 이동하는 에니메이션을 만듭니다.
+    slideRef.current.style.transition = `all 0.5s ease-in-out`;
+    slideRef.current.style.transform = `translateX(-${currentSlide}00vw)`;
   }, [currentSlide]);
 
+  const staticServerUri = process.env.REACT_APP_PATH || "";
+
+  const carousel = [
+    `${staticServerUri}/carouselItem1.jpeg`,
+    `${staticServerUri}/carouselItem2.jpeg`,
+    `${staticServerUri}/carouselItem3.jpeg`,
+  ];
+
   return (
-    <>
-      <Window>
-        <Slide ref={slideRef}>
-          <SlideItem>
-            <img src={images01} alt="0" />
-          </SlideItem>
-          <SlideItem>
-            <img src={images02} alt="1" />
-          </SlideItem>
-          <SlideItem>
-            <img src={images03} alt="2" />
-          </SlideItem>
-        </Slide>
-        <Button className="prevbtn" onClick={handlePrevSlide}>
+    <Window>
+      <Slide ref={slideRef}>
+        <SlideItem>
+          {carousel.map((item, idx) => {
+            return <img src={item} key={idx} alt={`${currentSlide + 1}`} />;
+          })}
+        </SlideItem>
+      </Slide>
+      <ButtonGroup>
+        <button className="prevbtn" onClick={handlePrevSlide}>
           {"<"}
-        </Button>
-        <Button className="nextbtn" onClick={handleNextSlide}>
+        </button>
+        <button className="nextbtn" onClick={handleNextSlide}>
           >
-        </Button>
-      </Window>
-    </>
+        </button>
+      </ButtonGroup>
+    </Window>
   );
 };
 
