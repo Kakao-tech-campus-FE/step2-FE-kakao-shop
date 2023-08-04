76bd7f48ffcd52c27fa1f0a8dd8c2d0607ea9e48
wjsehrbs2632 <wjsehrbs2632@naver.com>
2023-08-04 20:15:01 +0900

    이미지 불러오는 경로 수정

--- src/components/atoms/Photo.jsx
+++ src/components/atoms/Photo.jsx
@@ -1,10 +1,12 @@
 import "../../styles/atoms/Photo.css";
 
+const staticServerUri = process.env.REACT_APP_PATH || "";
+
 const Photo = ({ className, src, alt }) => {
   return (
     <picture className={className}>
       <source className="border-0" srcSet={src} />
-      <img className="border-1 rounded-lg w-96" src={src} alt={alt} />
+      <img className="border-1 rounded-lg w-96" src={staticServerUri + src} alt={alt} />
     </picture>
   );
 };
