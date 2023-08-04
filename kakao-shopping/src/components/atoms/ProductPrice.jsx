50be38ca2a56049764e6b04f45843cf6540cf005
ba0061@naver.com <sweet.waiting0l@icloud.com>
2023-08-04 14:28:35 +0900

    refactor: 스크립트 상의 서버 실행 방법에 따라 폴더 내용 전부 바깥으로 뺌

--- kakao-shopping/src/components/atoms/ProductPrice.jsx
+++
@@ -1,9 +0,0 @@
-const ProductPrice = ({price, className}) => {
-  const priceString = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  
-  return (
-    <span className={className}>{priceString}</span>
-  )
-}
-
-export default ProductPrice;
