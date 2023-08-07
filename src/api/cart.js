8295d87f57fa29188464ec7abd3624a961fb3e39
localgaji <eldorado829@naver.com>
2023-08-07 09:26:27 +0900

    add cart data processing

--- src/api/cart.js
+++ src/api/cart.js
@@ -4,8 +4,28 @@ export const addToCart = (postData) => {
   return instance.post(`/carts/add`, postData);
 };
 
-export const getCarts = () => {
-  return instance.get(`/carts`);
+export const getCarts = async () => {
+  const response = await instance.get(`/carts`);
+
+  const validCollections = new Set();
+  let totalQuantity = 0;
+
+  for (const product of response.products) {
+    let collectionQuantity = 0;
+    for (const option of product.carts) {
+      collectionQuantity += option.quantity;
+    }
+    if (collectionQuantity > 0) {
+      validCollections.add(product.id);
+    }
+    totalQuantity += collectionQuantity;
+  }
+  return [
+    response.products,
+    response.totalPrice,
+    totalQuantity,
+    validCollections,
+  ];
 };
 
 export const updateCart = (updateList) => {
