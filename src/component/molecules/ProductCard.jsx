6989685a9c6448a118139881be5dd6614b420b36
thddkwl5330 <thddkwl5330@naver.com>
2023-08-07 14:04:59 +0000

    배포

--- src/component/molecules/ProductCard.jsx
+++ src/component/molecules/ProductCard.jsx
@@ -1,18 +1,13 @@
 import { comma } from "../../utils/convert";
 import Card from "../atoms/Card";
 import Photo from "../atoms/Photo";
-import Image from "../atoms/Image";
 
 const staticServerUrl = process.env.REACT_APP_PATH || "";
 
 const ProductCard=({product}) => {
     return(
-        <Card to={staticServerUrl + `/product/${product.id}`}>
-            <Image
-        className="w-full rounded border"
-        src={staticServerUrl + product.image}
-        alt={product.productName}
-      />
+        < Card to={`${staticServerUrl}/product/${product.id}`}>
+           <img src={`${staticServerUrl}${product.image}`} alt={product.name} width='200px'></img>
             <div className="description">{product.productName}</div>
             <div className="product-price">{comma(product.price)}원</div>
             {/*<h3>{product.productName}</h3>
--- src/component/molecules/ProductInformationColumn.jsx
+++ src/component/molecules/ProductInformationColumn.jsx
@@ -1,17 +1,12 @@
 import {comma} from '../../utils/convert';
 import Photo from '../atoms/Photo';
-import Image from '../atoms/Image'
 
 	const staticServerUrl = process.env.REACT_APP_PATH || "";
 const ProductInformationColumn = ({product}) => {
   const {productName, price, image} = product;
   return (
        <div className="flex py-8 pr-8 w-[400px]">
-                <Image
-        className="w-[25rem] h-[25rem] border"
-        src={staticServerUrl + product.image}
-        alt={product.productName}
-      />
+                <Photo src={staticServerUrl+ product.image} alt={product.productName} />
             
     </div>
   );
