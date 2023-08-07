d1064a7845cd16bcca45362ddf423290307cbdd2
thddkwl5330 <thddkwl5330@naver.com>
2023-08-07 15:28:04 +0000

    정리

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,35 +1,30 @@
-import { Suspense } from "react";
-import { useQuery } from "react-query"
+import { useNavigate } from "react-router-dom";
+import { Suspense,useEffect,useState } from "react";
+import { getCart } from "../services/addCart"; 
+import React from "react";
+import { useQuery } from "react-query";
 import Loader from "../component/atoms/Loader";
 import CartList from "../component/molecules/CartList";
-import {getCart} from "../services/addCart"
-import GNB from "../component/atoms/GNB";
-import { useState } from "react";
-import { useEffect } from "react";
+import { QueryClient, QueryClientProvider } from 'react-query';
+const queryClient = new QueryClient();
+const Cart = () => {
+    const { data, isLoading } = useQuery(["cart"] ,()=>getCart());
+    // console.log(data)
+    // data.data.response={
+    //     products,
+    //     totalPrice,
+    // }
+    //data 전부 파싱하는 이유는 데이터 없을때 fallback 되게 하기 위해서.
+    return (
+     <QueryClientProvider client={queryClient}>
+        
+        {isLoading ? <Loader /> : <CartList data={data} />}
+       
+        </QueryClientProvider>
+    
+    
+       
+    );
+};
 
-const CartPage=()=>{
-  const { data, isLoading } = useQuery(['cart'], getCart);
-    // const [carts, setCarts] = useState([]);
-
-
-    // useEffect(() => {
-    //     getCart()
-    //       .then((res) => {
-    //         console.log(res);
-    //         setCarts(res);
-    //       })
-    //       .catch((error) => {
-    //         console.log(error);
-    //       });
-    //   }, []);
-
-      return (
-        <div className="cartpage">
-          <Suspense fallback={<Loader />}>
-            {/* <CartList data={carts} /> */}
-            <CartList data={data} isLoading={isLoading}/>
-          </Suspense>
-        </div>
-      );
-    };
-export default CartPage;
+export default Cart;
