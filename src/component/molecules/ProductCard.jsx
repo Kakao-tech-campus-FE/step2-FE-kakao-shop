f38bca875744279854760185b078fb5805f6ec80
thddkwl5330 <thddkwl5330@naver.com>
2023-08-07 13:10:35 +0000

    사진

--- src/component/molecules/ProductCard.jsx
+++ src/component/molecules/ProductCard.jsx
@@ -1,13 +1,14 @@
 import { comma } from "../../utils/convert";
 import Card from "../atoms/Card";
 import Photo from "../atoms/Photo";
+import 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 from "/workspace/step2-FE-kakao-shop/src/img/1.jpg"
 
 	const staticServerUrl = process.env.REACT_APP_PATH || "";
 
 const ProductCard=({product}) => {
     return(
         <Card to={staticServerUrl + `/product/${product.id}`}>
-            <Photo src={`staticServerUrl/${product.image}`} alt={product.productName}/>
+            <Photo src={product.id} alt={product.productName}/>
             <div className="description">{product.productName}</div>
             <div className="product-price">{comma(product.price)}원</div>
             {/*<h3>{product.productName}</h3>
