49dc63eece4a66b12a6f94171c6b02e2167a9569
thddkwl5330 <thddkwl5330@naver.com>
2023-08-07 14:14:37 +0000

    배포

--- src/component/molecules/CartList.jsx
+++ src/component/molecules/CartList.jsx
@@ -1,137 +1,160 @@
-import { useCallback, useEffect, useRef, useState } from "react"
-import { useMutation } from "react-query";
-import { useNavigation } from "react-router-dom";
-import CartItem from "../atoms/CartItem";
-import Card from "../atoms/Card";
-import Button from "../atoms/Button";
-import Box from "../atoms/Box";
-import { comma } from "../../utils/convert";
-import {updateCart} from "../../services/addCart";
 import Container from "../atoms/Container";
+import Button from "../atoms/Button";
+import { useCallback, useEffect, useState,useRef } from "react";
+import Box from "../atoms/Box";
+import Card from "../atoms/Card";
+import CartItems from "../atoms/CartItems";
+// import { comma } from "../../utils/convert";
+import { useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import { updateCart } from "../../services/cart";
+const staticServerUri = process.env.REACT_APP_PATH || "";
+const CartList = ({data}) => {
+   
+    const route=useNavigate();
+    const [cartItems,setCartItems]=useState([]);
+    const [totalPrice,setTotalPrice]=useState(0);
+    const [updatePayload,setUpdatePayload]=useState([]);
+    const initPayload = useRef([]);
+      //useRef 는 상태를 담아둘때도 사용함.
 
+    const {mutate} = useMutation({
+        mutationFn:updateCart,
+    })
 
-const CartList=({data})=>{
-const route=useNavigation();
-const [cartItems, setCartItems]=useState([]);
-const [totalPrice, setTotalPrice]=useState(0);
-const [updatePayload, setUpdatePayload]=useState([]);//렌더링에 관여x
+    useEffect(()=>{
+        // console.log(data?.data?.response?.products)
+        setCartItems(data?.data?.response?.products);
+        let item=data?.data?.response?.products;
+        console.log(item)
+        // let items=item.carts.reduce((hap)=>{
+        //     hap=
+        // })
+        // setTotalPrice(items);
+        //안좋은 패턴
+    },[data])
 
-//const updatePayload=useRef([]);
-const initPayload=useRef([]);//렌더링에 관여하지 않는다.
-	
-const staticServerUrl = process.env.REACT_APP_PATH || "";
-
-const {mutate}=useMutation({
-    mutationFn: updateCart,
-});
-/**
- * 옵션의 수량 변경과 가격 변경을 관리
- * @param {number} optionId 
- */
-
-useEffect(()=>{
-    //validate 또는 구조분해할당하는게 좋음
-    setCartItems(data?.data?.response?.products);
-    initPayload(data?.data?.response?.products);
-    setTotalPrice(data?.data?.response?.totalPrice);
-}, [data]);
-
-useEffect(() => {
-    mutate(updatePayload);
-  }, [updatePayload, mutate]);
-
-
-
-const getTotalCartCountIncludeOptions=useCallback(()=>{
-    let count=0;
-    cartItems.forEach((item) => {
-        item.carts.forEach((cart)=>{
-            count+=cart.quantity;
-        });
-    });
-    return comma(count);
-}, [cartItems]);
-
-
-const handleOnChangeCount=(optionId, quantity, price, )=>{
-    setUpdatePayload((prev)=>{
-        const isExist=prev.find((item)=>item.cartId===optionId);
-        
-        if (isExist){
-            return[
-                ...prev.filter((item)=> item.cartId !== optionId),{
-                    cartId:optionId,
-                    quantity,
-                }
-            ]
-        }
-
-        return[
-            ...prev,
-            {
-                cartId:optionId,
-                quantity,
-            },
-        ];
-    });
-
-    setTotalPrice((prev)=>prev+price);
-    setCartItems((prev)=> {
-        return prev.map((item)=>{
-            return{
-                ...item,
-                carts:item.carts.map((cart)=>{
-                    if (cart.id === optionId){
-                        return{...cart, quantity};
-                    }
-                    return cart;
-                })
-            };
-        });
-    });
-};
-
-return(
-    <Container className="cart-List">
-        <Box>
-            <h1>장바구니</h1>
-            </Box>
-            <Card className="cart-item">
-                {/* 상품별 장바구니 */}
-                {Array.isArray(cartItems) && cartItems.map((item) =>{
-            return(
-                <CartItem
-                key={item.id}
-                item={item}
-                onChange={handleOnChangeCount}
-                mutate={mutate}
-                />
-            );
+    /**
+     * 옵션의 수량 변경과 가격 변경 관리
+     * @param {number} optionId  :옵션 아이디
+     * @param {number} quantity  :옵션 수량
+     * @param {number} price :옵션 가격
+     */
+    const handleOnChangeCount =(optionId,quantity,price)=>{
+        setCartItems((prev) =>
+        prev.map((item) => {
+          return {
+            ...item,
+            carts: item.carts.map((cart) => {
+              if (cart.id === optionId) {
+                return { ...cart, quantity };
+              }
+              return cart;
+            }),
+          };
         })
-        }
-    </Card>
-    <Card>
-        <div className="row">
-            <span className="expect">주문 예상금액</span>
-            <div className="sum-price">{comma(totalPrice)}원</div>
+      );
+      setTotalPrice((prev) => prev + price);}
+        // setUpdatePayload((prev)=>{
+        //     const isExist = prev.find((item)=>item.cartId === optionId);
+        //     if(isExist){
+        //         return [
+        //             ...prev.filter((item)=>item.cartId !== optionId),{
+        //                 cartId:optionId,
+        //                 quantity,
+        //             }
+        //         ]
+        //     }
+        //     return [
+        //         ...prev,
+        //         {
+        //             cartId:optionId,
+        //             quantity,
+        //         },
+        //     ]
+        // })
+        // setTotalPrice((prev)=>{
+        //     console.log(prev)
+        //     prev=prev+price
+        // })
+    //     setCartItems((prev)=>{
+    //         return prev.map((item)=>{
+    //             return{
+    //                 ...item,
+    //                 carts: item.carts.map((cart)=>{
+    //                     if(cart.id===optionId){
+    //                         return {...cart,quantity}
+    //                     }
+    //                     return cart;
+    //                 })
+    //             }
+    //         })
+    //     })
+    // }
+    const getPayload = useCallback(() => {
+        const payload = cartItems.flatMap((item) => {
+          return item.carts.map((cart) => {
+            return { cartId: cart.id, quantity: cart.quantity };
+          });
+        });
+        return payload;
+      }, [cartItems]);
+
+    const getTotalCartCountIncludeOptions= useCallback(()=>{
+        let count =0;
+        cartItems.forEach((item)=>{
+            // console.log(item)
+            item.carts.forEach((cart)=>{
+                count+=cart.quantity;
+            });      
+        });
+        return count;
+    },[cartItems])
+    
+    return (
+        <Container>
+            <Box>
+                <p>장바구니</p>
+            </Box>
+            <Card>
+                {/* 상품 별 장바구니 */}
+                {Array.isArray(cartItems)&&
+                 cartItems.map((item)=>{
+                    return(
+                        <CartItems
+                        key={item.id}
+                        item={item}
+                        onChange={handleOnChangeCount} //개수 변경 관리
+                        />
+                    )
+                 })
+                }
+
+            </Card>
+            <Card>
+                <div>
+                    <span>주문 예상금액</span>
+                    <div>{totalPrice}원</div>
                 </div>
             </Card>
-            <button className="order-btn"
-                onClick={() => {//update cart api
-                    //장바구니 정보를 수정하는 api호출(개수 변경이 있다면)
-                    mutate(updatePayload,{
-                        onSuccess:(data)=>{
-                            route.push(staticServerUrl + "/order")
-                        },
-                        onError:(error)=>{
-            
-                        },
-                    })
-                }}
-                >
-                    <span className='font-bold text-xl'>총 {getTotalCartCountIncludeOptions}건 주문하기</span>
-                </button>  
-    </Container>
-    )
-            }
+            <Button
+            onClick={()=>{
+                //update cart
+                //navigate to order page
+                console.log(getPayload())
+                mutate(getPayload(),{
+                    onSuccess:(data)=>{
+                        route(`${staticServerUri}/order`)
+                    },
+                    onError:(error)=>{
+
+                    },
+                })
+                
+            }}>주문하기</Button>
+            <span>총 {getTotalCartCountIncludeOptions()}건 주문하기</span>
+        </Container>
+    );
+};
+
 export default CartList;
